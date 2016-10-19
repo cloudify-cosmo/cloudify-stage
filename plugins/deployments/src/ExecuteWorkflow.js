e502a2c96492eb34db70e139f585d3ae5c2bf9cc
Kinneret Zin <kinnza@gmail.com>
2016-10-19 11:39:23 +0300

    deployment delete

---
+++ plugins/deployments/src/ExecuteWorkflow.js
@@ -0,0 +1,18 @@
+/**
+ * Created by kinneretzin on 19/10/2016.
+ */
+
+export default class extends React.Component {
+    constructor(props,context) {
+        super(props,context);
+
+        this.state = {
+        }
+    }
+
+    render () {
+        return (
+            <i className="road icon link bordered" title="Execute workflow"></i>
+        );
+    }
+}
