3dd689979020df2fa95215ce75e323871f31f8a7
addihorowitz <addihorowitz@gmail.com>
2016-11-08 15:54:56 +0200

    display manager's status icon

--- app/components/Managers.js
+++ app/components/Managers.js
@@ -4,6 +4,7 @@
 
 import React, { Component, PropTypes } from 'react';
 import { browserHistory } from 'react-router';
+import config from '../config.json';
 
 export default class Managers extends Component {
     static propTypes = {
@@ -11,6 +12,14 @@ export default class Managers extends Component {
         onManagerConfig: PropTypes.func.isRequired,
         onManagerChange: PropTypes.func.isRequired
     };
+
+    constructor(props,context){
+        super(props, context);
+
+        this.state = {
+            selectedManagerStatus: ''
+        };
+    }
 
     handleClick(event) {
         if (event.target.id === "configureManagerIcon") {
@@ -20,11 +29,35 @@ export default class Managers extends Component {
         }
     }
 
+
+    _getManagerUrl(selectedManager) {
+        return `http://${config.proxyIp}:8000/?su=http://${selectedManager.ip}`;
+    }
+
+    _getManagerStatus(selectedManager)
+    {
+        var thi$ = this;
+        $.ajax({
+            url: thi$._getManagerUrl(selectedManager) + '/api/v2.1/events?_include=status',
+            dataType: 'json',
+            method: 'get',
+            success: function(response) {this.setState({selectedManagerStatus: response.status});}
+        })
+    }
+
     render() {
         var selectedManager = _.find(this.props.managers.items,{id:this.props.managers.selected});
+        this._getManagerStatus(selectedManager);
         return (
             <div className="ui inline dropdown item managersMenu" ref={select=>$(select).dropdown({action: 'hide'})}>
-                <div className="dropDownText text">{selectedManager.name}</div>
+                <div className="dropDownText text">
+                                {selectedManager.name}
+                                {
+                                    this.state.selectedManagerStatus ?
+                                        <i className="circle icon small green"/>
+                                    :
+                                        <i className="circle icon small red"/>
+                                }</div>
                 <i className="inverted dropdown icon"></i>
                 <div className="menu">
                     {
