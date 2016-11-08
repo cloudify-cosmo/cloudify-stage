d5e3090b3ca89aae2d22179c261731c09e493752
addihorowitz <addihorowitz@gmail.com>
2016-11-08 10:48:59 +0200

    display remove page icon only on hover

--- app/components/PagesList.js
+++ app/components/PagesList.js
@@ -19,11 +19,11 @@ export default class PagesList extends Component {
             <div className="pages">
                 {
                     _.filter(this.props.pages, (p)=>{return !p.isDrillDown}).map(function(page){
-                        return <div key={page.id} className={'pageRemoveButton item ' + (this.props.selected === page.id ? 'active' : '')} onClick={()=>{this.props.onPageSelected(page);} }>
+                        return <div key={page.id} className={'item ' + (this.props.selected === page.id ? 'active' : '') + ' pageMenuItem'} onClick={()=>{this.props.onPageSelected(page);} }>
                         {page.name}
                         {
                             this.props.isEditMode && page.id != "0" ?
-                                <i className="remove link icon small" onClick={()=>this.props.onPageRemoved(page)}/>
+                                <i className="remove link icon small pageRemoveButton" onClick={()=>this.props.onPageRemoved(page)}/>
                             :
                             ''
                         }
