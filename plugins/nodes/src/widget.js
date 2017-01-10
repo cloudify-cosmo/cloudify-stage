9dd0754fbe2380028fd4fe56a371e4a0d2e5c300
Jakub Niezgoda <jakub.niezgoda@gigaspaces.com>
2017-01-10 12:29:05 +0100

    Added node instances table to nodes widget with modal window showing node instance details (STAGE-103)

--- plugins/nodes/src/widget.js
+++ plugins/nodes/src/widget.js
@@ -13,7 +13,10 @@ Stage.addPlugin({
     color : 'blue',
     isReact: true,
     initialConfiguration: [],
-    fetchUrl: '[manager]/nodes?_include=id,deployment_id,blueprint_id,type,number_of_instances,host_id,relationships[params]',
+    fetchUrl: {
+        nodes: '[manager]/nodes?_include=id,deployment_id,blueprint_id,type,number_of_instances,host_id,relationships[params]',
+        nodeInstances: '[manager]/node-instances?_include=id,node_id,deployment_id,state,relationships,runtime_properties'
+    },
     pageSize: 5,
 
     fetchParams: function(widget, toolbox) {
@@ -29,21 +32,31 @@ Stage.addPlugin({
             return <Stage.Basic.Loading/>;
         }
 
+        const CONNECTED_TO_RELATIONSHIP = 'cloudify.relationships.connected_to';
+        const SELECTED_NODE_ID = toolbox.getContext().getValue('nodeId');
+
         let params = this.fetchParams(widget, toolbox);
-        let formattedData = data;
-        formattedData = Object.assign({}, formattedData, {
-            items : _.map (formattedData.items, (item) => {
-                return Object.assign({}, item, {
-                    deploymentId : item.deployment_id,
-                    blueprintId : item.blueprint_id,
-                    containedIn : item.host_id,
-                    connectedTo : item.relationships.filter((r) => r.type === 'cloudify.relationships.connected_to')
-                                                    .map((r) => r.target_id)
-                                                    .join(','),
-                    numberOfInstances : item.number_of_instances
+
+        let nodes = data.nodes.items;
+        let instances = data.nodeInstances.items;
+
+        let formattedData = Object.assign({}, data.nodes, {
+            items: _.map (nodes, (node) => {
+                return Object.assign({}, node, {
+                    deploymentId: node.deployment_id,
+                    blueprintId: node.blueprint_id,
+                    containedIn: node.host_id,
+                    connectedTo: node.relationships.filter((r) => r.type === CONNECTED_TO_RELATIONSHIP)
+                                                   .map((r) => r.target_id)
+                                                   .join(','),
+                    numberOfInstances: node.number_of_instances,
+                    instances: instances.filter((instance) =>
+                                                instance.node_id === node.id &&
+                                                instance.deployment_id === node.deployment_id),
+                    isSelected: (node.id + node.deployment_id) === SELECTED_NODE_ID
                 })
             }),
-            total : _.get(data, 'metadata.pagination.total', 0),
+            total : _.get(data.nodes, 'metadata.pagination.total', 0),
             blueprintId : params.blueprint_id,
             deploymentId : params.deployment_id
         });
