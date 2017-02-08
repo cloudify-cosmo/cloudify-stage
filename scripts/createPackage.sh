5e58f278eaa20760ca4b0807bd1d8f9ef8aaebe1
Kinneret Zin <kinnza@gmail.com>
2017-02-08 10:34:42 +0200

    trying to fix package build

--- scripts/createPackage.sh
+++ scripts/createPackage.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 set -x
-tar -cvzf stage.tar.gz dist/** backend/** conf/**
+
+mkdir .tmp
+# make sure its empty (could exist before)
+rm -R .tmp/**
+
+mkdir .tmp/dist
+mkdir .tmp/backend
+mkdir .tmp/conf
+
+cp dist/** .tmp/dist
+cp backend/** .tmp/backend
+cp conf/** .tmp/conf
+cp scripts/package-template.json .tmp
+cd .tmp
+mv package-template.json package.json
+tar -cvzf stage.tar.gz **
+#tar -cvzf stage.tar.gz dist/** backend/** conf/** package.json
+cd ..
+cp .tmp/stage.tar.gz .
+rm -R .tmp/**
