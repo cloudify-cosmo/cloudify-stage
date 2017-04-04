612aa45cc58594b11004102d8bc184ff859fe09b
Pawel Posel <pposel@cisco.com>
2017-04-04 17:23:57 +0200

    Remove ECMAScript 6 notations

--- backend/routes/Style.js
+++ backend/routes/Style.js
@@ -2,21 +2,21 @@
  * Created by jakubniezgoda on 03/04/2017.
  */
 
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let ejs = require('ejs');
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var ejs = require('ejs');
 
-let router = express.Router();
+var router = express.Router();
 
-let configuration = require('../../conf/app.json');
-let styleTemplateFile = path.resolve(__dirname, '../templates', 'style.ejs');
+var configuration = require('../../conf/app.json');
+var styleTemplateFile = path.resolve(__dirname, '../templates', 'style.ejs');
 
 router.get('/', function(req, res, next) {
-    let whiteLabel = configuration.whiteLabel;
-    let stylesheetTemplate = fs.readFileSync(styleTemplateFile, 'utf8');
+    var whiteLabel = configuration.whiteLabel;
+    var stylesheetTemplate = fs.readFileSync(styleTemplateFile, 'utf8');
 
-    let stylesheet = ejs.render(stylesheetTemplate, {
+    var stylesheet = ejs.render(stylesheetTemplate, {
         logoUrl: whiteLabel.enabled && whiteLabel.logoUrl || '/app/images/Cloudify-logo.png',
         mainColor: whiteLabel.enabled && whiteLabel.mainColor || '#39b9d4',
         headerTextColor: whiteLabel.enabled && whiteLabel.headerTextColor || '#ffffff'
