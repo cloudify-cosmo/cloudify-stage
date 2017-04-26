d3f880bb63ca2d378995b2d5bf3f509edd61e386
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2017-04-26 09:39:46 +0200

    Cluster credentials changes (#204)
    * CFY-6682 Update the "join cluster" flow after API changes
    
    The new join procedure is as follows:
      - first, run a request to the cluster master, PUT /cluster/nodes/name,
        so that it can run validations and generate credentials, which it
        will return
      - pass the received credentials to the new node in the join API call
        (the PUT /cluster)
    
    The credentials are a opaque object - the client only needs to pass
    them to the new node, doesn't need to care what's inside.
    
    * CFY-6744 Cluster start: let the server autogenerate encryption_key
    
    The client doesn't need to send the encryption key when starting a
    cluster, it can simply allow the server to autogenerate it.

--- widgets/HAManagement/src/actions.js
+++ widgets/HAManagement/src/actions.js
@@ -6,18 +6,6 @@ const DEFAULT_CLUSTER_NODE_NAME_PREFIX = 'Cluster_Node_';
 const WAIT_FOR_CLUSTER_TIMEOUT = 120000;
 const WAIT_FOR_CLUSTER_STATUS_PING_INTERVAL = 3000;
 
-function randomIntInc (low, high) {
-    return Math.floor(Math.random() * (high - low + 1) + low);
-}
-
-function genString(length) {
-    var bytes = '';
-    for (var i=0;i<length;++i) {
-        bytes += randomIntInc(0, 9);
-    }
-
-    return bytes;
-}
 
 function waitPromise (timeout) {
     return new Promise((resolve)=>{
@@ -36,58 +24,52 @@ export default class {
 
         return this.toolbox.getManager().doPut(`/cluster`,null,{
             host_ip: this.toolbox.getManager().getIp(),
-            node_name: name || (DEFAULT_CLUSTER_NODE_NAME_PREFIX + this.toolbox.getManager().getIp()),
-            encryption_key: btoa(genString(16))
+            node_name: name || (DEFAULT_CLUSTER_NODE_NAME_PREFIX + this.toolbox.getManager().getIp())
         });
     }
 
     doJoinCluster(clusterManagerIp,clusterManagerUser,clusterManagerPass,name) {
+        name = name || (DEFAULT_CLUSTER_NODE_NAME_PREFIX + this.toolbox.getManager().getIp());
 
         var auth = btoa(`${clusterManagerUser}:${clusterManagerPass}`);
         var external = this.toolbox.getExternal({basicAuth : auth});
 
-        var cluster = {};
-        var tenant = null;
         var clusterManager = this.toolbox.getNewManager(clusterManagerIp);
 
         // Currently this is only available in v3 of the API anyways
-        return external.doGet(clusterManager.getManagerUrl('/tenants'))
-            .then(tenants=>{
-                if (tenants.items.length === 0) {
-                    return Promise.reject('The remote manager doesnt have any tenants');
-                }
-
-                tenant = tenants.items[0].name;
-                return external.doGet(clusterManager.getManagerUrl('/cluster'),null,true,{tenant});
-            })
+        return external.doGet(clusterManager.getManagerUrl('/cluster'),null,true)
             .then(clusterStatus=>{
                 if (!clusterStatus.initialized) {
                     return Promise.reject({message: 'The remote manager doesnt belong to any cluster'});
                 }
 
-                // Save the cluster info
-                cluster = clusterStatus;
+                return Promise.all([
+                    // get the current nodes in the cluster, so that we can pass all of
+                    // them when joining, which results in faster join, and increases
+                    // reliability in case the master dies while we're joining
+                    external.doGet(clusterManager.getManagerUrl('/cluster/nodes'), null, true),
 
-                // Get the list of nodes
-                return external.doGet(clusterManager.getManagerUrl('/cluster/nodes'),null,true,{tenant});
+                    // also tell the master we want to join to the cluster, let it create new credentials
+                    // (ssl keys) for us
+                    external.doPut(clusterManager.getManagerUrl(`/cluster/nodes/${name}`), null, {
+                        node_name: name,
+                        host_ip: this.toolbox.getManager().getIp()
+                    }, true)
+                ])
             })
-            .then(clusterNodes=>{
-                if (!_.isEmpty(name) && _.find(clusterNodes.items,{name:name}))  {
-                    return Promise.reject(`A node named ${name} already exists in the cluster`);
-                }
-
-                var joinAddrs = _.map(clusterNodes.items,'host_ip');
-
-                return this._doJoinCluster(name,cluster.encryption_key,joinAddrs);
-            });
+            .then(results=>{
+                var [nodes, newNode] = results;
+                var joinAddrs = _.map(nodes.items,'host_ip');
+                return this._doJoinCluster(name, newNode.credentials, joinAddrs);
+            })
     }
 
 
-    _doJoinCluster(name,encryptionKey,joinAddrs) {
+    _doJoinCluster(name,credentials,joinAddrs) {
         return this.toolbox.getManager().doPut(`/cluster`,null,{
             host_ip: this.toolbox.getManager().getIp(),
-            node_name: name || (DEFAULT_CLUSTER_NODE_NAME_PREFIX + this.toolbox.getManager().getIp()),
-            encryption_key: encryptionKey,
+            node_name: name,
+            credentials: credentials,
             join_addrs: joinAddrs
         });
     }
