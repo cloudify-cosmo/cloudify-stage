dcf01c35c7e4854452b9673d852c286d928acd92
Kinneret Zin <kinnza@gmail.com>
2017-05-22 15:40:35 +0300

    added framework to add a page with a specfic widget. Also thickened edit mode tests

--- e2e/utils/WidgetsHandler.js
+++ e2e/utils/WidgetsHandler.js
@@ -14,12 +14,54 @@ module.exports =  {
             .click('@editModeItem');
     },
 
+    moveOutOfEditMode: function(client) {
+        var page = client.page.page();
+
+        return page.section.userMenu
+            .click('@userName')
+            .waitForElementVisible('@userDropdownMenu', 2000)
+            .click('@editModeItem');
+
+    },
+
     addPage: function(client) {
         var page = client.page.page();
 
-        return page.section.sidebar
-            .click('@addPageButton')
-            .pause(2000); // waiting to move to the new page
+        page.section.sidebar
+            .click('@addPageButton');
+
+        return client.pause(2000); // waiting to move to the new page
+    },
+
+    addWidget: function(client,widgetId) {
+        var page = client.page.page();
+
+        page.section.page
+            .click('@addWidgetButton');
+
+        client.pause(1000); // Wait for modal to open
+
+        client
+            .click('.addWidgetModal .widgetsList .item[data-id="'+widgetId+'"] .extra .button');
+
+        return client.pause(2000); // waiting to move to the new page
+    },
+
+    removeLastPage : function(client) {
+        var page = client.page.page();
+
+        page.section.sidebar
+            .click('@lastPageRemoveButton');
+
+        return client.pause(2000); // waiting to move to the new page
+
+    },
+
+    prepareTestWidget: function(client,widgetId) {
+        this.moveToEditMode(client);
+        this.addPage(client);
+        this.addWidget(client,widgetId);
+        this.moveOutOfEditMode(client);
     }
 
 };
