d96675168726f42e100dd2ff5489b380a441007b
Pawel Posel <pposel@cisco.com>
2017-05-24 16:50:48 +0200

    STAGE-315 Creating an "Info" widget for blueprints

---
+++ widgets/blueprintInfo/src/BlueprintInfo.js
@@ -0,0 +1,63 @@
+/**
+ * Created by pposel on 06/02/2017.
+ */
+
+
+export default class BlueprintInfo extends React.Component {
+
+    constructor(props,context) {
+        super(props,context);
+
+        this.state = {
+            error: ""
+        }
+    }
+
+    render() {
+        var {ErrorMessage, Grid, Image, PrivateMarker, Message} = Stage.Basic;
+
+        var blueprint = this.props.data;
+
+        if (!blueprint.id) {
+            return (
+                <div>
+                    <Message info>No blueprint selected</Message>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <ErrorMessage error={this.state.error}/>
+
+                <Grid>
+                    <Grid.Row className="bottomDivider">
+                        <Grid.Column width="4"><Image src={`/ba/image/${blueprint.id}`} centered={true}/></Grid.Column>
+                        <Grid.Column width="12">
+                            <h3 className="ui icon header verticalCenter">
+                                <a className="underline" href="javascript:void(0)">{blueprint.id}</a>
+                            </h3>
+                            <PrivateMarker show={blueprint.private_resource} title="Private resource" className="rightFloated"/>
+                        </Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row className="noPadded">
+                        <Grid.Column width="7"><h5 className="ui icon header">Created</h5></Grid.Column>
+                        <Grid.Column width="9">{blueprint.created_at}</Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row className="noPadded">
+                        <Grid.Column width="7"><h5 className="ui icon header">Updated</h5></Grid.Column>
+                        <Grid.Column width="9">{blueprint.updated_at}</Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row className="noPadded">
+                        <Grid.Column width="7"><h5 className="ui icon header">Creator</h5></Grid.Column>
+                        <Grid.Column width="9">{blueprint.created_by}</Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+
+        );
+    }
+};
