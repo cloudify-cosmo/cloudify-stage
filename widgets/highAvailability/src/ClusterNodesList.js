a233c7b4191220f78af6f656fd3662779f7a1b61
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2017-06-21 13:52:10 +0200

    STAGE-345 - HA - remove create/join/delete capabilities (#258)
    * Removed create/join/delete capabilities from HA management widget (STAGE-345)
    
    * Fixed minor issues after code review
    
    * Updated documentation link

--- widgets/highAvailability/src/ClusterNodesList.js
+++ widgets/highAvailability/src/ClusterNodesList.js
@@ -2,14 +2,11 @@
  * Created by kinneretzin on 28/03/2017.
  */
 
-import Actions from './actions';
-
 export default class ClusterNodesList extends React.Component {
     constructor(props,context) {
         super(props,context);
 
         this.state = {
-            confirmDelete: false,
             error: null
         }
     }
@@ -18,37 +15,8 @@ export default class ClusterNodesList extends React.Component {
         return this.props.toolbox.refresh(fetchParams);
     }
 
-    _deleteNodeConfirm (item,event) {
-        event.stopPropagation();
-
-        this.setState({
-            confirmDelete: true,
-            item: item
-        });
-    }
-
-    _removeFromCluster() {
-        this.props.toolbox.loading(true);
-
-        this.setState({confirmDelete: false});
-
-        var actions = new Actions(this.props.toolbox);
-        actions.doDeleteClusterNode(this.state.item)
-            .then(()=> {
-                this.setState({error: null});
-                this.props.toolbox.loading(false);
-                this.props.toolbox.refresh();
-                this.props.toolbox.getEventBus().trigger('cluster:refresh');
-            })
-            .catch((err)=>{
-                this.setState({error: err.message});
-                this.props.toolbox.loading(false);
-            });
-    }
-
-
     render () {
-        let {DataTable,Checkmark,ErrorMessage,Icon,Confirm} = Stage.Basic;
+        let {DataTable,Checkmark,ErrorMessage} = Stage.Basic;
         return (
             <div>
                 <ErrorMessage error={this.state.error}/>
@@ -65,7 +33,6 @@ export default class ClusterNodesList extends React.Component {
                     <DataTable.Column label="Initialized" name="initialized" width="10%"/>
                     <DataTable.Column label="Is master" name="master" width="10%"/>
                     <DataTable.Column label="Is online" name="online" width="10%"/>
-                    <DataTable.Column width="15%"/>
 
                     {
                         this.props.nodes.items.map((item)=>{
@@ -78,20 +45,11 @@ export default class ClusterNodesList extends React.Component {
                                     <DataTable.Data><Checkmark value={item.initialized}/></DataTable.Data>
                                     <DataTable.Data><Checkmark value={item.master}/></DataTable.Data>
                                     <DataTable.Data><Checkmark value={item.online}/></DataTable.Data>
-                                    <DataTable.Data className="center aligned rowActions">
-                                        <Icon name='trash' link bordered title="Delete" onClick={this._deleteNodeConfirm.bind(this,item)} />
-                                    </DataTable.Data>
                                 </DataTable.Row>
                             );
                         })
                     }
                 </DataTable>
-
-
-                <Confirm content='This action is irreversible. Separating from the cluster will leave the Cloudify Manager instance unusable.Are you sure?'
-                         open={this.state.confirmDelete}
-                         onConfirm={this._removeFromCluster.bind(this)}
-                         onCancel={()=>this.setState({confirmDelete : false})} />
 
             </div>
         )
