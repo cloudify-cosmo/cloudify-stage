4aa3ef886a01d87bd478e64305491cc97e907c77
Kinneret Zin <kinnza@gmail.com>
2017-06-21 12:18:53 +0300

    add ability to get the manager url for e2e tests from an env variable

--- e2e/nightwatch.conf.js
+++ e2e/nightwatch.conf.js
@@ -3,5 +3,12 @@ module.exports = (function(settings) {
     if (process.platform === 'win32') {
         settings.selenium.cli_args['webdriver.chrome.driver']  = "./e2e/bin/chromedriver.exe";
     }
+
+    var managerUrl = process.env.MANAGER_URL;
+    if (managerUrl) {
+        managerUrl = managerUrl.trim();
+        console.log('Connecting to manager: '+managerUrl);
+        settings.test_settings.default.launch_url = 'http://'+managerUrl;
+    }
     return settings;
 })(require('./nightwatch.json'));
