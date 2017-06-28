7bfc93563a0b63bdfaf78f966af8f9e224972619
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2017-06-28 09:23:37 +0200

    Created documentation for status components (STAGE-358) (#264)
    Refactored Loading and ErrorMessage components to use semantic-ui-react components instead of raw semantic-ui constructions

--- app/components/basic/ErrorMessage.js
+++ app/components/basic/ErrorMessage.js
@@ -3,9 +3,28 @@
  */
  
 import React, { Component, PropTypes } from 'react';
+import { Message } from 'semantic-ui-react';
 
+/**
+ * ErrorMessage is a component which uses [Message](https://react.semantic-ui.com/elements/message) component from Semantic-UI-React
+ * to display error message.
+ *
+ * ## Usage
+ * ![ErrorMessage](manual/asset/ErrorMessage_0.png)
+ * ```
+ * <ErrorMessage header="Fatal error" error="File cannot be opened" />
+ * ```
+ *
+ */
 export default class ErrorMessage extends Component {
 
+    /**
+     * propTypes
+     * @property {string} [header='Error Occured'] header of error text message
+     * @property {object} [error] string or object containing error text message/messages
+     * @property {boolean} [show=true] specifies if error message shall be shown
+     * @property {string} [className=''] additional CSS classes to [Message](https://react.semantic-ui.com/elements/message) component
+     */
     static propTypes = {
         header: PropTypes.string,
         error: PropTypes.any,
@@ -34,10 +53,12 @@ export default class ErrorMessage extends Component {
             }
         }
 
-        return <div className={`ui error message ${this.props.className}`} style={{"display":(this.props.show?"block":"none")}}>
-                    <div className="header">{header}</div>
-                    <p>{error}</p>
-                </div>
+        return (
+            <Message error className={this.props.className} visible={this.props.show}>
+                <Message.Header>{header}</Message.Header>
+                <p>{error}</p>
+            </Message>
+        )
     }
 }
  
--- test/components/basic/ErrorMessageTest.js
+++ test/components/basic/ErrorMessageTest.js
@@ -6,13 +6,14 @@ import React from 'react'
 import {shallow , mount} from 'enzyme'
 import {expect} from 'chai';
 import ErrorMessage from '../../../app/components/basic/ErrorMessage';
+import { Message } from 'semantic-ui-react';
 
 describe('(Component) ErrorMessage', () => {
 
     it("doesn't render if error empty",()=>{
         var wrapper = shallow(<ErrorMessage/>);
 
-        expect(wrapper.find('div.error.message')).to.not.exist;
+        expect(wrapper.find('<Message/>')).to.not.exist;
     });
 
     var wrapper = shallow(<ErrorMessage error="test" className="testClassName" header="test header" show={false}/>);
@@ -21,15 +22,15 @@ describe('(Component) ErrorMessage', () => {
     });
 
     it('renders classname',()=>{
-        expect(wrapper.find('div.error.message')).to.have.className('testClassName');
+        expect(wrapper.find(Message).get(0).props.className).to.equal('testClassName');
     });
 
     it('renders header name',()=>{
-        expect(wrapper.find('div.header')).to.have.text('test header');
+        expect(wrapper.find(Message.Header).get(0).props.children).to.equal('test header');
     });
 
     it('checks if hidden',()=>{
-        expect(wrapper.find('div.error.message')).to.have.style("display", "none");
+        expect(wrapper.find(Message).get(0).props.visible).to.equal(false);
     });
 
 });
