e570ec84c199f595bda3540e9a2594e8c869ea66
Alex Kachura <alex.kachura@gmail.com>
2017-06-29 11:05:32 +0200

    link to composer (#270)
    STAGE-360

---
+++ widgets/composerLink/src/widget.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Alex on 29/06/2017.
+ */
+const { Button } = Stage.Basic;
+
+Stage.defineWidget({
+    id: "composerLink",
+    name: "Composer link",
+    description: 'Opens Cloudify Composer in a different tab',
+    initialWidth: 2,
+    initialHeight: 4,
+    showHeader: false,
+    showBorder: false,
+    isReact: true,
+
+    render: function(widget,data,error,toolbox) {
+        const composerUrl = `${location.protocol}//${location.hostname}:3000`;
+
+        return (
+            <Button className="labeled icon" color="blue" fluid icon="external"
+                    onClick={()=>{window.open(composerUrl, '_blank')}}
+                    content="Cloudify Composer"/>
+        );
+
+    }
+
+});
