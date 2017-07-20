bd3fb7e516a33754b5b6604d4a0f488d878b5b27
pawelposel <pawel.posel@gigaspaces.com>
2017-07-20 08:52:36 +0200

    STAGE-395 Dev mode create a better way to override config default (#293)

---
+++ e2e/config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by pposel on 25/01/2017.
+ */
+
+var config = require('./config.json');
+
+var me = null;
+try {
+    me = require('../conf/me.json');
+} catch(err) {
+    if (err.code !== "MODULE_NOT_FOUND") {
+        throw err;
+    }
+}
+var _ = require('lodash');
+
+_.merge(config, me.e2e);
+
+module.exports = config;
