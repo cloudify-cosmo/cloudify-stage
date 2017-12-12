620c6f2d7e513632a7d8bd97376760ced2017e20
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2017-12-12 09:58:56 +0100

    Optimized and improved backend's /file routes

--- backend/routes/File.js
+++ backend/routes/File.js
@@ -10,33 +10,34 @@ var upload = multer({limits: {fileSize: 50000}});
 var logger = require('log4js').getLogger('File');
 var YAML = require('yamljs');
 
-router.post('/text', passport.authenticate('token', {session: false}), upload.single('file'), function (req, res, next) {
-    if (req.file) {
-        logger.debug('Text file uploaded, name:',req.file.originalname,', size:',req.file.size);
-        var data = req.file.buffer.toString();
-        res.contentType('application/text').send(data);
-    } else {
+function checkIfFileUploaded(req, res, next) {
+    if (!req.file) {
         var errorMessage = 'No file uploaded.';
         logger.error(errorMessage);
         res.status(400).send({message: errorMessage})
+    } else {
+        next();
     }
+}
+
+router.post('/text', passport.authenticate('token', {session: false}), upload.single('file'), checkIfFileUploaded, function (req, res, next) {
+    logger.debug(`Text file uploaded, name: ${req.file.originalname}, size: ${req.file.size}`);
+    var data = req.file.buffer.toString();
+    res.contentType('application/text').send(data);
 })
 
-router.post('/yaml', passport.authenticate('token', {session: false}), upload.single('file'), function (req, res, next) {
-    if (req.file) {
-        logger.debug('YAML file uploaded, name:',req.file.originalname,', size:',req.file.size);
-        var yamlString = req.file.buffer.toString();
-        try {
-            var json = YAML.parse(yamlString);
-        } catch (error) {
-            res.status(400).send({message: `Cannot parse YAML file. Error: ${error}`})
-        }
-        res.contentType('application/json').send(json);
-    } else {
-        var errorMessage = 'No file uploaded.';
+router.post('/yaml', passport.authenticate('token', {session: false}), upload.single('file'), checkIfFileUploaded, function (req, res, next) {
+    logger.debug(`YAML file uploaded, name: ${req.file.originalname}, size: ${req.file.size}`);
+    var yamlString = req.file.buffer.toString();
+    try {
+        var json = YAML.parse(yamlString);
+    } catch (error) {
+        var errorMessage = `Cannot parse YAML file. Error: ${error}`;
         logger.error(errorMessage);
-        res.status(400).send({message: errorMessage})
+        res.status(400).send({message: errorMessage});
+        return;
     }
+    res.contentType('application/json').send(json);
 })
 
 module.exports = router;
