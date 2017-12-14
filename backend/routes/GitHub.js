430151e5358554f49f009ef5f2a569fc47eef77b
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2017-12-14 16:01:29 +0100

    Support private github repos as source for the blueprint catalog (STAGE-614)

--- backend/routes/GitHub.js
+++ backend/routes/GitHub.js
@@ -44,38 +44,36 @@ function _getAuthorizationHeader(user, tenant) {
     return _.get(authList, `${user}.${tenant}`, '');
 }
 
-function setAuthorizationHeader(fetchCredentials) {
-    return function(req, res, next) {
-        var user = _.get(req, 'user.username', '');
-        var tenant = req.header('tenant');
-        var fetchCredentials = fetchCredentials || _.isEmpty(_getAuthorizationHeader(user, tenant));
+function _setAuthorizationHeader(req, res, next, fetchCredentials) {
+    var user = _.get(req, 'user.username', '');
+    var tenant = req.header('tenant');
+    var fetchCredentials = fetchCredentials || _.isEmpty(_getAuthorizationHeader(user, tenant));
 
-        if (fetchCredentials) {
-            var userSecret = _getSecretName(params.username);
-            var passSecret = _getSecretName(params.password);
-            Promise
-                .all([
-                    ManagerHandler.jsonRequest('GET', `/secrets/${userSecret}`, req.headers),
-                    ManagerHandler.jsonRequest('GET', `/secrets/${passSecret}`, req.headers)
-                ])
-                .then((data) => {
-                    var username = data[0];
-                    var password = data[1];
-                    var authorization = 'Basic ' + new Buffer(username.value + ':' + password.value).toString('base64');
-                    req.headers['authorization'] = authorization;
-                    _.set(authList, `${user}.${tenant}`, authorization);
-                    logger.debug('Setting authorization header from fetched credentials. GitHub user:', username.value);
-                    next();
-                })
-                .catch((error) => {
-                    logger.debug('Cannot set authorization header for GitHub user. GitHub\'s username and password not set properly in secrets. Error:', error);
-                    next();
-                });
-        } else {
-            logger.debug('Setting authorization header from cached data.');
-            req.headers['authorization'] = _getAuthorizationHeader(user, tenant);
-            next();
-        }
+    if (fetchCredentials) {
+        var userSecret = _getSecretName(params.username);
+        var passSecret = _getSecretName(params.password);
+        Promise
+            .all([
+                ManagerHandler.jsonRequest('GET', `/secrets/${userSecret}`, req.headers),
+                ManagerHandler.jsonRequest('GET', `/secrets/${passSecret}`, req.headers)
+            ])
+            .then((data) => {
+                var username = data[0];
+                var password = data[1];
+                var authorization = 'Basic ' + new Buffer(username.value + ':' + password.value).toString('base64');
+                req.headers['authorization'] = authorization;
+                _.set(authList, `${user}.${tenant}`, authorization);
+                logger.debug('Setting authorization header from fetched credentials. GitHub user:', username.value);
+                next();
+            })
+            .catch((error) => {
+                logger.debug('Cannot set authorization header for GitHub user. GitHub\'s username and password not set properly in secrets. Error:', error);
+                next();
+            });
+    } else {
+        logger.debug('Setting authorization header from cached data.');
+        req.headers['authorization'] = _getAuthorizationHeader(user, tenant);
+        next();
     }
 }
 
@@ -88,15 +86,20 @@ function addIsAuthToResponseBody(req, res, next) {
 
 router.get('/search/repositories',
            passport.authenticate('token', {session: false}),
-           setAuthorizationHeader(true),
+           function (req, res, next) {
+               _setAuthorizationHeader(req, res, next, true);
+           },
            function (req, res, next) {
                _pipeRequest(req, res, next, 'https://api.github.com/search/repositories', true);
            },
-           addIsAuthToResponseBody);
+           addIsAuthToResponseBody
+           );
 
 router.get('/repos/:user/:repo/git/trees/master',
            passport.authenticate('token', {session: false}),
-           setAuthorizationHeader(false),
+           function (req, res, next) {
+               _setAuthorizationHeader(req, res, next, false);
+           },
            function (req, res, next) {
                _pipeRequest(req, res, next, 'https://api.github.com/repos/' + req.params.user + '/' + req.params.repo + '/git/trees/master');
            });
