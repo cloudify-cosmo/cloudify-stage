b6a9d3b08a862bd7317cd1f5ab4299134de99743
idob-cloudify <idob@cloudify.co>
2017-12-25 16:26:55 +0200

    CFY-5793 Ability to exclude logs and events from snapshots

--- widgets/snapshots/src/actions.js
+++ widgets/snapshots/src/actions.js
@@ -39,11 +39,13 @@ export default class {
         return this.toolbox.getManager().doDownload(snapshotDownloadUrl, snapshotFileName);
     }
 
-    doCreate(snapshotId, includeMetrics=false, includeCredentials=false){
+    doCreate(snapshotId, includeMetrics=false, includeCredentials=false, excludeLogs=false, excludeEvents=false){
         snapshotId = encodeURIComponent(snapshotId);
         return this.toolbox.getManager().doPut(`/snapshots/${snapshotId}`, null, {
             include_metrics: includeMetrics,
-            include_credentials: includeCredentials
+            include_credentials: includeCredentials,
+            include_logs: !excludeLogs,
+            include_events: !excludeEvents
         });
     }
 }
