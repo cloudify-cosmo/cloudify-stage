e837aa182d653e12a0cc98ae2e9212588eaceee8
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-03-08 12:05:12 +0100

    Changed to segment-based popup message instead of popup for RolesPresenter

--- widgets/common/src/RolesPresenter.js
+++ widgets/common/src/RolesPresenter.js
@@ -17,52 +17,38 @@ export default class RolesPresenter extends React.Component {
     };
 
 
-    render() {
-        let {Popup} = Stage.Basic;
-        const HELP_MESSAGE = 'When the roles are inherited from a user group, ' +
-            'the name of the user group is also shown, ' +
-            'for example: viewer (Viewers)';
-
+    render(){
         let restOfGroupRoles = '';
         _.forEach(_.omit(this.props.groupRoles, this.props.directRole), (groups, role) => {
-            restOfGroupRoles += role + ' (' + groups.join(', ') + '), ';
+            restOfGroupRoles += role+' ('+groups.join(', ')+'), ';
         });
         restOfGroupRoles = restOfGroupRoles.slice(0, -2);
-
-        let RolesList = function(directRole, groupRoles) {
-            return (
-                _.isEmpty(groupRoles)
-                ?
-                //Use case 1: user
-                directRole && <span>{directRole}</span>
-                :
-                _.has(groupRoles, directRole)
-                    ?
-                    _.isEmpty(restOfGroupRoles)
-                        ?
-                        //Use case 2: user (gp1)
-                        <span>{directRole} ({groupRoles[directRole].join(', ')})</span>
-                        :
-                        //Use case 3: user (gp1), viewer (gp2)
-                        <span>{directRole} ({groupRoles[directRole].join(', ')}), {restOfGroupRoles}</span>
-                    :
-                    directRole
-                        ?
-                        //Use case 4: user, viewer (gp2)
-                        <span>{directRole}, {restOfGroupRoles}</span>
-                        :
-                        //Use case 5: viewer (gp2)
-                        <span>{restOfGroupRoles}</span>
-            );
-        };
 
         return (
             _.isEmpty(this.props.groupRoles)
                 ?
-                RolesList(this.props.directRole, this.props.groupRoles)
+                //Use case 1: user
+                this.props.directRole && <span>{this.props.directRole}</span>
                 :
-                <Popup trigger={RolesList(this.props.directRole, this.props.groupRoles)} content={HELP_MESSAGE} />
-        );
+                _.has(this.props.groupRoles, this.props.directRole)
+                    ?
+                    _.isEmpty(restOfGroupRoles)
+                        ?
+                        //Use case 2: user (gp1)
+                        <span>{this.props.directRole} ({this.props.groupRoles[this.props.directRole].join(', ')})</span>
+                        :
+                        //Use case 3: user (gp1), viewer (gp2)
+                        <span>{this.props.directRole} ({this.props.groupRoles[this.props.directRole].join(', ')}), {restOfGroupRoles}</span>
+                    :
+                    this.props.directRole
+                        ?
+                        //Use case 4: user, viewer (gp2)
+                        <span>{this.props.directRole}, {restOfGroupRoles}</span>
+                        :
+                        //Use case 5: viewer (gp2)
+                        <span>{restOfGroupRoles}</span>
+            )
+
     }
 }
 
