110bacb44e7c8de777febef8f25106caea36b94b
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-04-23 12:07:36 +0200

    Added ExternalRedirect component and implemented external authentication basing on new react-router lib.

--- app/components/Routes.js
+++ app/components/Routes.js
@@ -8,16 +8,25 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import Layout from '../containers/layout/Layout';
 import LogoPage from './LogoPage';
 import MaintenanceMode from '../containers/maintenance/MaintenanceModePageMessage';
+import {ExternalRedirect} from './ExternalRedirect';
 
 export default class Routes extends Component {
     render() {
         return (
             <Switch>
-                <Route exact path='/login' component={LogoPage} />
+                <Route exact path='/login' render={() =>
+                    this.props.isSamlEnabled
+                        ? <ExternalRedirect url={this.props.samlSsoUrl} />
+                        : <LogoPage/>
+                } />
+                <Route exact path='/logout' render={() =>
+                    this.props.isSamlEnabled
+                        ? <ExternalRedirect url={this.props.samlPortalUrl} />
+                        : <Redirect to='/login'/>
+                } />
                 <Route exact path='/error' component={LogoPage} />
                 <Route exact path='/noTenants' component={LogoPage} />
                 <Route exact path='/maintenance' component={MaintenanceMode} />
-                <Redirect from='/logout' to='/login' />
                 <Route render={(props) => (
                     this.props.isLoggedIn
                         ? this.props.isInMaintenanceMode
