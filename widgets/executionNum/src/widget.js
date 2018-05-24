ea61c5abb1154e755e1217c632b526c72f5acfcd
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2018-05-24 11:50:51 +0200

    CY-364 - Create a "Number of running executions" widget (#577)
    * Changed executions widget header color to teal
    * Created "Number of running executions" widget (CY-364)
    * Updated admin and non-admin initial templates with "Number of Running Executions" widget.
    * Added space between icon and number in KeyIndicator component
    * Updated "Number of *" widget's thumbnails

---
+++ widgets/executionNum/src/widget.js
@@ -0,0 +1,35 @@
+/**
+ * Created by jakubniezgoda on 23/05/2018.
+ */
+
+Stage.defineWidget({
+    id: 'executionNum',
+    name: 'Number of running executions',
+    description: 'Number of running executions',
+    initialWidth: 2,
+    initialHeight: 8,
+    color : 'teal',
+    showHeader: false,
+    isReact: true,
+    permission: Stage.GenericConfig.WIDGET_PERMISSION('executionNum'),
+    categories: [Stage.GenericConfig.CATEGORY.BLUEPRINTS, Stage.GenericConfig.CATEGORY.CHARTS_AND_STATISTICS],
+    
+    initialConfiguration: [
+        Stage.GenericConfig.POLLING_TIME_CONFIG(10)
+    ],
+    fetchUrl: '[manager]/executions?_include=id&_size=1&status=pending&status=started&status=cancelling&status=force_cancelling',
+
+    render: function(widget,data,error,toolbox) {
+        if (_.isEmpty(data)) {
+            return <Stage.Basic.Loading/>;
+        }
+
+        const {KeyIndicator} = Stage.Basic;
+
+        const num = _.get(data, 'metadata.pagination.total', 0);
+
+        return (
+            <KeyIndicator title="Running Executions" icon="cogs" number={num} />
+        );
+    }
+});
