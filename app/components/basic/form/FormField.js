15a3e87c596a6fb3ce9241767f650d25480bbe0a
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-06-08 12:27:58 +0200

    Improved FormField component to present label, required indicator and help popup

--- app/components/basic/form/FormField.js
+++ app/components/basic/form/FormField.js
@@ -7,6 +7,8 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Form } from 'semantic-ui-react'
 
+import { PopupHelp, Icon } from '../index';
+
 /**
  * FormField is a component to present field and is used in {@link Form} component
  *
@@ -23,6 +25,9 @@ import { Form } from 'semantic-ui-react'
  * ### FormField with error
  * ![FormField](manual/asset/form/FormField_1.png)
  *
+ * ### FormField with help information, label and required indication
+ * ![FormField](manual/asset/form/FormField_2.png)
+ *
  * ```
  * <Form onSubmit={this._createTenant.bind(this)} errors={this.state.errors} ref='createForm'>
  *   <Form.Field error={this.state.errors.tenantName}>
@@ -35,17 +40,46 @@ import { Form } from 'semantic-ui-react'
 export default class FormField extends Component {
     /**
      * propTypes
-     * @property {boolean} [error] error indicator (true - field has error, false - field has no errors)
+     * @property {boolean} [error=null] error indicator (true - field has error, false - field has no errors)
+     * @property {string} [help=''] if not empty, then help description is shown in popup on field's hover and focus
+     * @property {string} [label=''] if not empty, then it's content is shown on top of field
+     * @property {boolean} [required] if true and label is set, then red asterisk icon is presented near label
      */
     static propTypes = {
-        error: PropTypes.any
+        error: PropTypes.any,
+        help: PropTypes.string,
+        label: PropTypes.string,
+        required: PropTypes.bool
+    };
+
+    static defaultProps = {
+        error: null,
+        help: '',
+        label: '',
+        required: false
+    };
+
+    shouldComponentUpdate(nextProps) {
+        return !_.isEqual(this.props, nextProps);
+    }
+
+    getFieldWrapper(props) {
+        let {children, error, label, ...fieldProps} = props;
+        error = (_.isBoolean(error) && error) || !_.isEmpty(error);
+
+        return (
+            <Form.Field {...fieldProps} error={error}>
+                {!_.isEmpty(label) &&  <label>{label}</label>}
+                {children}
+            </Form.Field>
+        );
     };
 
     render() {
-        let error = (_.isBoolean(this.props.error) && this.props.error) || !_.isEmpty(this.props.error);
+        let {help, ...restOfProps} = this.props;
 
-        return (
-            <Form.Field {...this.props} error={error}/>
-        );
+        return _.isEmpty(help)
+            ? this.getFieldWrapper(restOfProps)
+            : <PopupHelp trigger={this.getFieldWrapper(restOfProps)} content={help} />
     }
 }
