4c50a278fcf748d8c0bc882f5d5c5da115dbd6ae
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-06-21 11:05:36 +0200

    Paramerized devServer. Using contextPath from configuration file in devServer.

--- devServer.js
+++ devServer.js
@@ -5,91 +5,59 @@
 const webpack = require('webpack');
 const webpackDevServer = require('webpack-dev-server');
 
-const config = require('./webpack.config');
+const webpackConfig = require('./webpack.config');
+const config = require('./backend/config').get();
+
+const contextPath = config.app.contextPath;
+
+const host = 'localhost';
+const devServerPort = 4000;
+
+const proxyPort = 8088;
+const proxyTarget = `http://${host}:${proxyPort}`;
+
+const proxyOptions = {
+    target: proxyTarget,
+    secure: false
+};
+
+const indexHtml = `${contextPath}/index.html`
 const options = {
-    publicPath: config[0].output.publicPath,
+    publicPath: webpackConfig[0].output.publicPath,
     hot: true,
-    host: 'localhost',
+    host: host,
     inline: true,
     historyApiFallback: {
-        index:'/console/index.html'
+        index: indexHtml
     },
     proxy: {
-        '/console/sp': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/auth': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/config': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/ua': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/source': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/ba': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/clientConfig': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/monitor': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/github': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/style': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/widgets': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/templates': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/tours': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/wb': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/file': {
-            target: 'http://localhost:8088',
-            secure: false
-        },
-        '/console/plugins': {
-            target: 'http://localhost:8088',
-            secure: false
-        }
+        [`${contextPath}/sp`]: proxyOptions,
+        [`${contextPath}/auth`]: proxyOptions,
+        [`${contextPath}/config`]: proxyOptions,
+        [`${contextPath}/ua`]: proxyOptions,
+        [`${contextPath}/source`]: proxyOptions,
+        [`${contextPath}/ba`]: proxyOptions,
+        [`${contextPath}/clientConfig`]: proxyOptions,
+        [`${contextPath}/monitor`]: proxyOptions,
+        [`${contextPath}/github`]: proxyOptions,
+        [`${contextPath}/style`]: proxyOptions,
+        [`${contextPath}/widgets`]: proxyOptions,
+        [`${contextPath}/templates`]: proxyOptions,
+        [`${contextPath}/tours`]: proxyOptions,
+        [`${contextPath}/wb`]: proxyOptions,
+        [`${contextPath}/file`]: proxyOptions,
+        [`${contextPath}/plugins`]: proxyOptions
     }
 };
 
-webpackDevServer.addDevServerEntrypoints(config[0], options);
-const compiler = webpack(config);
+webpackDevServer.addDevServerEntrypoints(webpackConfig[0], options);
+const compiler = webpack(webpackConfig);
 const server = new webpackDevServer(compiler, options);
 
-server.listen(4000, 'localhost', (err) => {
+server.listen(devServerPort, host, (err) => {
     if (err) {
         return console.log(err);
     }
 
-    console.log('Listening at http://localhost:4000/');
+    console.log(`Listening at http://${host}:${devServerPort}/`);
 });
