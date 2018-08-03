541e46ff52450117ede2d654774b6c2142d161c3
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-08-03 13:13:06 +0200

    Added styling to Wizard.Modal component

--- app/components/basic/wizard/WizardModal.js
+++ app/components/basic/wizard/WizardModal.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 import {ErrorMessage, Form, Modal, Step} from './../index';
+import '../../styles/Wizard.css';
 
 export default class WizardModal extends Component {
 
