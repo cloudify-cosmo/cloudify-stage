0814f63704c9b398cc50e355fc8e41859b13e341
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-08-08 16:31:54 +0200

    Added checks for blueprint ID and deployment ID existence in manager Divided tasks adding operations into separate functions

--- widgets/deploymentWizardButtons/src/steps/ConfirmationStep.js
+++ widgets/deploymentWizardButtons/src/steps/ConfirmationStep.js
@@ -39,60 +39,135 @@ class ConfirmationStepContent extends Component {
 
     static propTypes = Stage.Basic.Wizard.Step.Content.propTypes;
 
+    static defaultVisibility = Stage.Common.Consts.defaultVisibility;
+
+    chooseId(baseId, promise, idName) {
+        const maxSuffixNumber = 1000;
+        const isIdInList = (items, id) => !_.isUndefined(_.find(items, {id}));
+
+        let id = baseId;
+        let idChosen = false;
+        return promise()
+            .then(({items}) => {
+                idChosen = !isIdInList(items, id);
+
+                for (let i = 0; i < maxSuffixNumber && !idChosen; i++) {
+                    id = `${baseId}_${i}`;
+                    idChosen = !isIdInList(items, id);
+                }
+
+                return idChosen
+                    ? Promise.resolve(id)
+                    : Promise.reject(`Not found unused ${idName} ID.`);
+            });
+    }
+
+    addPluginsTasks(plugins, tasks) {
+        const pluginActions = new Stage.Common.PluginActions(this.props.toolbox);
+
+        for (let pluginName of _.keys(plugins)) {
+            const plugin = plugins[pluginName];
+            tasks.push({
+                name: `Upload plugin ${pluginName}`,
+                promise: () =>
+                    pluginActions.doUpload(ConfirmationStepContent.defaultVisibility,
+                                           plugin.wagonUrl, plugin.yamlUrl, plugin.wagonFile, plugin.yamlFile)
+            });
+        }
+
+        return Promise.resolve();
+    }
+
+    addSecretsTasks(secrets, tasks) {
+        const secretActions = new Stage.Common.SecretActions(this.props.toolbox);
+
+        for (let secretKey of _.keys(secrets)) {
+            const secretValue = secrets[secretKey];
+            tasks.push({
+                name: `Create secret ${secretKey}`,
+                promise: () => secretActions.doCreate(secretKey, secretValue, ConfirmationStepContent.defaultVisibility, false)
+            })
+        }
+
+        return Promise.resolve();
+    }
+
+    chooseBlueprintId(initialBlueprintId) {
+        const blueprintActions = new Stage.Common.BlueprintActions(this.props.toolbox);
+        return this.chooseId(initialBlueprintId, () => blueprintActions.doGetBlueprints({_search: initialBlueprintId}), 'blueprint');
+    }
+
+    addBlueprintUploadTask(blueprint, tasks) {
+        const blueprintActions = new Stage.Common.BlueprintActions(this.props.toolbox);
+
+        tasks.push({
+            name: `Upload blueprint ${blueprint.blueprintName}`,
+            promise: () => blueprintActions.doUpload(blueprint.blueprintName, blueprint.blueprintYaml, blueprint.blueprintUrl, null, blueprint.blueprintImageUrl, null, ConfirmationStepContent.defaultVisibility)
+        });
+
+        return Promise.resolve();
+    }
+
+    chooseDeploymentId(initialDeploymentId) {
+        const deploymentActions = new Stage.Common.DeploymentActions(this.props.toolbox);
+        return this.chooseId(initialDeploymentId, () => deploymentActions.doGetDeployments({_search: initialDeploymentId}), 'deployment');
+    }
+
+    addDeployBlueprintTask(deploymentId, blueprintId, inputs, tasks) {
+        const blueprintActions = new Stage.Common.BlueprintActions(this.props.toolbox);
+        const executionActions = new Stage.Common.ExecutionActions(this.props.toolbox);
+
+        const waitForDeploymentIsCreated = async () => {
+            const maxNumberOfRetries = 10;
+            const waitingInterval = 1000; //ms
+
+            let deploymentCreated = false;
+            for (let i = 0; i < maxNumberOfRetries && !deploymentCreated; i++) {
+                await new Promise(resolve => setTimeout(resolve, waitingInterval))
+                    .then(() => executionActions.doGetExecutions(deploymentId))
+                    .then(({items}) => {
+                        deploymentCreated = _.isUndefined(_.find(items, {ended_at: null}));
+                    });
+                console.error(i, deploymentCreated);
+            }
+        };
+
+        tasks.push({
+            name: `Deploy ${blueprintId} blueprint as ${deploymentId}`,
+            promise: () => blueprintActions.doDeploy({id: blueprintId}, deploymentId, inputs, ConfirmationStepContent.defaultVisibility)
+                .then(() => waitForDeploymentIsCreated())
+        });
+
+        return Promise.resolve();
+    }
+
+    addRunInstallWorkflowTask(deploymentId, tasks) {
+        const deploymentActions = new Stage.Common.DeploymentActions(this.props.toolbox);
+
+        tasks.push({
+            name: `Execute install workflow on ${deploymentId} deployment`,
+            promise: () => deploymentActions.doExecute({id: deploymentId}, {name: 'install'}, {}, false)
+        });
+
+        return Promise.resolve();
+    }
 
     componentDidMount() {
         const wizardData = this.props.wizardData;
-        const pluginActions = new Stage.Common.PluginActions(this.props.toolbox);
-        const secretActions = new Stage.Common.SecretActions(this.props.toolbox);
-        const blueprintActions = new Stage.Common.BlueprintActions(this.props.toolbox);
+
+        let tasks = [];
+        let blueprintId = '';
+        let deploymentId = '';
 
         this.props.onLoading(this.props.id)
-            .then(() => {
-                const defaultVisibility = Stage.Common.Consts.defaultVisibility;
-                let tasks = [];
-
-                // Plugins
-                for (let pluginName of _.keys(wizardData.plugins)) {
-                    const plugin = wizardData.plugins[pluginName];
-                    tasks.push({
-                        name: `Upload plugin ${pluginName}`,
-                        promise: () => pluginActions.doUpload(defaultVisibility, plugin.wagonUrl, plugin.yamlUrl, plugin.wagonFile, plugin.yamlFile)
-                    })
-                }
-
-                // Secrets
-                for (let secretKey of _.keys(wizardData.secrets)) {
-                    const secretValue = wizardData.secrets[secretKey];
-                    tasks.push({
-                        name: `Create secret ${secretKey}`,
-                        promise: () => secretActions.doCreate(secretKey, secretValue, defaultVisibility, false)
-                    })
-                }
-
-                // Blueprint
-                const blueprint = wizardData.blueprint;
-                tasks.push({
-                    name: `Upload blueprint ${blueprint.blueprintName}`,
-                    promise: () => blueprintActions.doUpload(blueprint.blueprintName, blueprint.blueprintYaml, blueprint.blueprintUrl, null, blueprint.blueprintImageUrl, null, defaultVisibility)
-                });
-
-                // Deployment
-                const inputs = wizardData.inputs;
-                const deploymentId = `${blueprint.blueprintName}_${Math.floor((Math.random() * 100) + 1)}`;
-                tasks.push({
-                    name: `Deploy blueprint ${blueprint.blueprintName}`,
-                    promise: () => blueprintActions.doDeploy(blueprint.blueprintName, deploymentId, inputs, defaultVisibility)
-                });
-
-                // Execution
-                // TODO
-
-                return {
-                    stepData: {
-                        tasks
-                    }
-                };
-            })
+            .then(() => this.addPluginsTasks(wizardData.plugins, tasks))
+            .then(() => this.addSecretsTasks(wizardData.secrets, tasks))
+            .then(() => this.chooseBlueprintId(wizardData.blueprint.blueprintName))
+            .then((id) => {blueprintId = id; return this.addBlueprintUploadTask({...wizardData.blueprint, blueprintName: id}, tasks)})
+            .then(() => this.chooseDeploymentId(wizardData.blueprint.blueprintName))
+            .then((id) => {deploymentId = id; this.addDeployBlueprintTask(id, blueprintId, wizardData.inputs, tasks)})
+            .then(() => this.addRunInstallWorkflowTask(deploymentId, tasks))
+            .then(() => ({stepData: {tasks}}))
             .then((newState) => new Promise((resolve) => this.setState(newState, resolve)))
             .then(() => this.props.onChange(this.props.id, this.state.stepData))
             .catch((error) => this.props.onError(this.props.id, error))
@@ -100,17 +175,21 @@ class ConfirmationStepContent extends Component {
     }
 
     render() {
-        let {Wizard} = Stage.Basic;
+        let {Header, List, Wizard} = Stage.Basic;
         const tasks = this.state.stepData.tasks;
 
         return (
             <Wizard.Step.Content {...this.props}>
-                <h2>To be done:</h2>
-                <ul>
-                {
-                    _.map(tasks, (task) => <li key={task.name}>{task.name}</li>)
-                }
-                </ul>
+                <Header as='h4'>Action list</Header>
+                <List ordered relaxed>
+                    {
+                        _.map(tasks, (task) =>
+                            <List.Item key={task.name}>
+                                {task.name}
+                            </List.Item>
+                        )
+                    }
+                </List>
             </Wizard.Step.Content>
         );
     }
