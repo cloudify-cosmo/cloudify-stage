721f3696ab8b3223623514c39d50e6a12c0c4f45
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-09-28 16:34:36 +0200

    Added helper static methods to ExecutionUtils

--- widgets/common/src/ExecutionUtils.js
+++ widgets/common/src/ExecutionUtils.js
@@ -7,6 +7,22 @@ class ExecutionUtils {
     static FORCE_CANCEL_ACTION = 'force-cancel';
     static KILL_CANCEL_ACTION = 'kill';
 
+    static isCancelledExecution(execution) {
+        return execution.status === 'cancelled';
+    }
+
+    static isFailedExecution(execution) {
+        return execution.status === 'failed';
+    }
+
+    static isSuccessfulExecution(execution) {
+        return execution.status === 'terminated';
+    }
+
+    static isUpdateExecution(execution) {
+        return execution.workflow_id === 'update';
+    }
+
     static isActiveExecution(execution) {
         const EXECUTION_STATES = [ 'terminated', 'failed', 'cancelled', 'pending', 'started', 'cancelling', 'force_cancelling', 'kill_cancelling' ];
         const END_EXECUTION_STATES = [ 'terminated', 'failed', 'cancelled' ];
