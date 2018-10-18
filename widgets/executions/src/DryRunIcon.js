c360948f61fed4611342075b430927a3e0dcdc54
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-10-18 15:40:58 +0200

    Added dry-run indication in Executions widget (CY-661) Replaced System column with Attributes column in Executions widget

---
+++ widgets/executions/src/DryRunIcon.js
@@ -0,0 +1,37 @@
+/**
+ * Created by jakub.niezgoda on 18/10/2018.
+ */
+
+import PropTypes from 'prop-types';
+
+export default class DryRunIcon extends React.Component {
+
+    constructor(props, context){
+        super(props, context);
+    }
+
+    static propTypes = {
+        execution: PropTypes.object
+    };
+
+    static defaultProps = {
+        execution: {is_dry_run: false}
+    };
+
+    render() {
+        let {Icon, Popup} = Stage.Basic;
+        const execution = this.props.execution;
+
+        return execution.is_dry_run
+            ? <Popup wide on='hover'>
+                  <Popup.Trigger>
+                      <Icon name='clipboard check' color='green' />
+                  </Popup.Trigger>
+                  <Popup.Content>
+                      Dry Run
+                  </Popup.Content>
+              </Popup>
+            : null;
+    }
+}
+
