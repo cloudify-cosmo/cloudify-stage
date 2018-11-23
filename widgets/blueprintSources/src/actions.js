c21725168ce13968efb8daa4632d0418b1b31f6b
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-11-23 15:30:37 +0100

    Added support for listing imported blueprints in Blueprint Sources widget (CY-726)

--- widgets/blueprintSources/src/actions.js
+++ widgets/blueprintSources/src/actions.js
@@ -16,6 +16,21 @@ export default class {
         return this.toolbox.getInternal().doGet(`/source/browse/${blueprintId}/archive`);
     }
 
+    doGetImportedBlueprints(blueprintId) {
+        return this.toolbox.getManager().doGet(`/blueprints?id=${blueprintId}&_include=plan`)
+            .then((data) => {
+                const imported = data.items[0].plan.imported;
+                const importedBlueprintPrefix = 'blueprint:';
+
+                return _.isEmpty(imported)
+                    ? []
+                    : _.chain(imported)
+                        .filter((i) => _.startsWith(i, importedBlueprintPrefix))
+                        .map((i) => _.replace(i, importedBlueprintPrefix, ''))
+                        .value();
+            });
+    }
+
     doGetFileContent(path) {
         return this.toolbox.getInternal().doGet('/source/browse/file',{path});
     }
