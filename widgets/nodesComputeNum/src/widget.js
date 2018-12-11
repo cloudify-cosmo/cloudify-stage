2bcffd263b91ed55327c9c99716b049775e799be
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2018-12-11 15:51:09 +0100

    Improved nodesComputeNum widget with new REST API

--- widgets/nodesComputeNum/src/widget.js
+++ widgets/nodesComputeNum/src/widget.js
@@ -1,5 +1,5 @@
 /**
- * Created by pawelposel on 04/11/2016.
+ * Created by jakub.niezgoda on 11/12/2018.
  */
 
 Stage.defineWidget({
@@ -11,28 +11,25 @@ Stage.defineWidget({
     color : 'red',
     showHeader: false,
     isReact: true,
+    hasReadme: true,
     permission: Stage.GenericConfig.WIDGET_PERMISSION('nodesComputeNum'),
     categories: [Stage.GenericConfig.CATEGORY.CHARTS_AND_STATISTICS],
-    
     initialConfiguration: [
         Stage.GenericConfig.POLLING_TIME_CONFIG(30)
     ],
-    fetchUrl: '[manager]/node-instances?_include=host_id&state=started',
+    fetchUrl: '[manager]/summary/node_instances?_target_field=host_id&state=started',
 
-    _getNumberOfComputeNodes(nodeInstances) {
-        return _.size(_.uniq(_.map(nodeInstances, (nodeInstance) => nodeInstance.host_id)));
-    },
-
-    render: function(widget,data,error,toolbox) {
+    render: function(widget, data, error, toolbox) {
         if (_.isEmpty(data)) {
-            return <Stage.Basic.Loading/>;
+            return <Stage.Basic.Loading />;
         }
 
-        let num = this._getNumberOfComputeNodes(data.items);
         let KeyIndicator = Stage.Basic.KeyIndicator;
+        const numberOfComputeNodes = _.chain(data.items)
+            .filter((item) => !_.isNil(item.host_id))
+            .sumBy('node_instances')
+            .value();
 
-        return (
-            <KeyIndicator title="Compute Nodes" icon="server" number={num}/>
-        );
+        return <KeyIndicator title="Compute Nodes" icon="server" number={numberOfComputeNodes} />;
     }
 });
