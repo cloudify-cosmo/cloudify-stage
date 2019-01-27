86d3be080d694b006f809458cc2e6a044c9efb6f
Inbal Amrani <inbala@gigaspaces.com>
2019-01-27 18:30:29 +0200

    CY-828 Add execution status widget fixes

--- widgets/executionsStatus/src/widget.js
+++ widgets/executionsStatus/src/widget.js
@@ -2,9 +2,9 @@
 Stage.defineWidget({
     id: 'executionsStatus',
     name: 'Executions Statuses Graph',
-    description: 'This widget shows the statuses of running executions',
-    initialWidth: 6,
-    initialHeight: 17,
+    description: 'Shows the number of executions per status',
+    initialWidth: 4,
+    initialHeight: 24,
     color : 'blue',
     isReact: true,
     hasReadme: true,
@@ -13,16 +13,23 @@ Stage.defineWidget({
     initialConfiguration: [
         Stage.GenericConfig.POLLING_TIME_CONFIG(10)
     ],
-    fetchUrl: '[manager]/executions?_include=id,status&_include_system_workflows=True&' +
-              'status=pending&status=started&status=queued&status=scheduled',
+    fetchUrl: '[manager]/summary/executions?_target_field=status_display&[params]',
+
+    fetchParams: function(widget, toolbox) {
+        return {
+            blueprint_id: toolbox.getContext().getValue('blueprintId'),
+            deployment_id: toolbox.getContext().getValue('deploymentId'),
+            id: toolbox.getContext().getValue('executionId')
+        };
+    },
 
     render: function(widget, data, error, toolbox) {
         if (_.isEmpty(data) || _.isEmpty(data.items)) {
             return <Stage.Basic.Loading/>;
         }
-        const status_data = _.groupBy(data.items, 'status');
+        const status_data = _.groupBy(data.items, 'status_display');
         const formatted_data = _.map(status_data,
-            (executions, status) => ({'status': status, 'number_of_executions': executions.length}));
+            (executions, status) => ({'status': _.startCase(status), 'number_of_executions': executions.length}));
         let {Graph} = Stage.Basic.Graphs;
         const charts = [{name:'number_of_executions', label:'Number of executions', axisLabel:'status'}];
         return (<Graph type={Graph.BAR_CHART_TYPE} data={formatted_data} charts={charts} xDataKey='status' />);
