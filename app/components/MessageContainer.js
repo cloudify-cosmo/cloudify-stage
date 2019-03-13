3221954a1b5db3b1f45b32936ed7acab326030bb
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2019-03-13 14:00:32 +0100

    Improved MessageContainer styling

--- app/components/MessageContainer.js
+++ app/components/MessageContainer.js
@@ -5,31 +5,43 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-import { Container, Segment } from './basic';
+import { Grid, Segment } from './basic';
 import SplashLoadingScreen from '../utils/SplashLoadingScreen';
 
 export default class MessageContainer extends Component {
 
     static propTypes = {
         children: PropTypes.any.isRequired,
-        className: PropTypes.string
+        textAlign: PropTypes.string,
+        loading: PropTypes.bool,
+        size: PropTypes.string,
+        wide: PropTypes.bool
     };
 
     static defaultProps = {
-        className: 'messageSegment'
+        textAlign: 'center',
+        loading: false,
+        size: 'big',
+        wide: false
     };
 
     render () {
         SplashLoadingScreen.turnOff();
 
+        const style = {margin: '80px auto', textAlign: this.props.textAlign};
+        const widths = this.props.wide
+            ? {mobile: 14, tablet: 14, computer: 12}
+            : {mobile: 12, tablet: 8, computer: 6};
+
         return (
-            <div className='coloredContainer'>
-                <Container fluid textAlign='center'>
-                    <Segment size='big' padded textAlign='center' raised className={this.props.className}>
+            <Grid centered container columns={1}>
+                <Grid.Column {...widths}>
+                    <Segment size={this.props.size} padded raised style={style}
+                             loading={this.props.loading}>
                         {this.props.children}
                     </Segment>
-                </Container>
-            </div>
+                </Grid.Column>
+            </Grid>
         );
     }
 }
