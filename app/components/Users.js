87445425903ed312c2c511930a81aed5db0886c9
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2019-03-14 11:41:40 +0100

    Restricted access to License Management Fixed unit tests

--- app/components/Users.js
+++ app/components/Users.js
@@ -15,12 +15,13 @@ export default class Users extends Component {
         showAllOptions: PropTypes.bool.isRequired,
         isEditMode: PropTypes.bool.isRequired,
         canEditMode: PropTypes.bool.isRequired,
-        canConfigure: PropTypes.bool.isRequired,
         canTemplateManagement: PropTypes.bool.isRequired,
+        canLicenseManagement: PropTypes.bool.isRequired,
         onEditModeChange: PropTypes.func.isRequired,
         onLogout: PropTypes.func.isRequired,
         onReset: PropTypes.func,
-        onTemplates: PropTypes.func
+        onTemplates: PropTypes.func,
+        onLicense: PropTypes.func
     };
 
     componentDidMount() {
@@ -40,45 +41,45 @@ export default class Users extends Component {
             </span>
         );
 
-        var commonElements = [];
-        commonElements.push(<Dropdown.Item key='reset'id='resetMenuItem'
-                                           icon='undo' text='Reset Templates' value='reset'
-                                           title='Reset application screens'
-                                           onClick={this.props.onReset} />);
-        if (this.props.isLicenseRequired) {
-            commonElements.push(<Dropdown.Item key='license' id='licenseMenuItem'
-                                               icon='key' text='License Management' value='license'
-                                               onClick={this.props.onLicense} />);
-        }
-        commonElements.push(<Dropdown.Divider key='log-out-divider'/>);
-        commonElements.push(<Dropdown.Item key='log-out' id='logoutMenuItem'
-                                           icon='log out' text='Logout' value='logout'
-                                           onClick={this.props.onLogout}/>);
+        let dropdownItems = [];
+        dropdownItems.push();
+        dropdownItems.push();
+        dropdownItems.push();
 
         return (
                 <Dropdown item pointing='top right' trigger={userMenuTrigger} className='usersMenu' >
-                    {
-                        this.props.showAllOptions
-                        ?
-                        <Dropdown.Menu>
-                            {
-                                this.props.canEditMode &&
-                                    <Dropdown.Item icon='configure' selected={this.props.isEditMode} active={this.props.isEditMode}
-                                               text={this.props.isEditMode ? 'Exit Edit Mode' : 'Edit Mode'} id='editModeMenuItem'
-                                               value='editMode' onClick={this.onEditModeClick.bind(this)}/>
-                            }
-                            {
-                                this.props.canTemplateManagement &&
-                                    <Dropdown.Item icon='list layout' text='Template Management' value='templates' title='Template management'
-                                               onClick={this.props.onTemplates} id='templatesMenuItem'/>
-                            }
-                            {commonElements}
-                        </Dropdown.Menu>
-                        :
-                        <Dropdown.Menu>
-                            {commonElements}
-                        </Dropdown.Menu>
-                    }
+                    <Dropdown.Menu>
+                        {
+                            this.props.showAllOptions && this.props.canEditMode &&
+                            <Dropdown.Item icon='configure' selected={this.props.isEditMode} active={this.props.isEditMode}
+                                           text={this.props.isEditMode ? 'Exit Edit Mode' : 'Edit Mode'} id='editModeMenuItem'
+                                           value='editMode' onClick={this.onEditModeClick.bind(this)}/>
+                        }
+
+                        {
+                            this.props.showAllOptions && this.props.canTemplateManagement &&
+                            <Dropdown.Item icon='list layout' text='Template Management' value='templates' title='Template management'
+                                           onClick={this.props.onTemplates} id='templatesMenuItem'/>
+                        }
+
+                        <Dropdown.Item key='reset'id='resetMenuItem'
+                                       icon='undo' text='Reset Templates' value='reset'
+                                       title='Reset application screens'
+                                       onClick={this.props.onReset} />
+
+                        {
+                            this.props.showAllOptions && this.props.canLicenseManagement &&
+                            <Dropdown.Item key='license' id='licenseMenuItem'
+                                           icon='key' text='License Management' value='license'
+                                           onClick={this.props.onLicense} />
+                        }
+
+                        <Dropdown.Divider key='log-out-divider'/>
+
+                        <Dropdown.Item key='log-out' id='logoutMenuItem'
+                                       icon='log out' text='Logout' value='logout'
+                                       onClick={this.props.onLogout}/>
+                    </Dropdown.Menu>
                 </Dropdown>
         );
     }
--- test/components/UsersComponentTest.js
+++ test/components/UsersComponentTest.js
@@ -29,12 +29,14 @@ describe('(Component) Users', () => {
                                 canEditMode={true}
                                 canConfigure={true}
                                 canTemplateManagement={true}
+                                canLicenseManagement={true}
                                 onEditModeChange={()=>{}}
                                 onConfigure={()=>{}}
                                 onResetPages={()=>{}}
                                 onReset={()=>{}}
                                 onTemplates={()=>{}}
-                                onLogout={()=>{}}/>);
+                                onLogout={()=>{}}
+                                onLicense={()=>{}} />);
     });
 
     it('renders...', () => {
@@ -43,17 +45,17 @@ describe('(Component) Users', () => {
 
     it('renders user menu with full options list ',()=>{
         expect(wrapper.find(Dropdown)).to.have.length(1); // Showing the users menu
-        expect(wrapper.find(Dropdown.Item).length).to.equal(4); // 4 menu options
-        // expect(wrapper.find(Dropdown.Item).get(1).props.text).to.equal('Configure'); // Having Configure option
-        // expect(wrapper.find(Dropdown.Item).get(1).props.icon).to.equal('options'); // Having settings icon for Configure option
+        expect(wrapper.find(Dropdown.Item).length).to.equal(5); // 4 menu options
         expect(wrapper.find(Dropdown.Item).get(0).props.text).to.equal('Edit Mode'); // Having Edit Mode option
         expect(wrapper.find(Dropdown.Item).get(0).props.icon).to.equal('configure'); // Having configure icon for Edit Mode option
-        expect(wrapper.find(Dropdown.Item).get(1).props.text).to.equal('Template management'); // Having Template management option
+        expect(wrapper.find(Dropdown.Item).get(1).props.text).to.equal('Template Management'); // Having Template management option
         expect(wrapper.find(Dropdown.Item).get(1).props.icon).to.equal('list layout'); // Having list layout icon for Template management option
         expect(wrapper.find(Dropdown.Item).get(2).props.text).to.equal('Reset Templates'); // Having Reset Templates option
         expect(wrapper.find(Dropdown.Item).get(2).props.icon).to.equal('undo'); // Having configure icon for Edit Mode option
-        expect(wrapper.find(Dropdown.Item).get(3).props.text).to.equal('Logout'); // Having Logout option
-        expect(wrapper.find(Dropdown.Item).get(3).props.icon).to.equal('log out'); // Having log out icon for Logout option
+        expect(wrapper.find(Dropdown.Item).get(3).props.text).to.equal('License Management'); // Having License Management option
+        expect(wrapper.find(Dropdown.Item).get(3).props.icon).to.equal('key'); // Having key icon for License Management option
+        expect(wrapper.find(Dropdown.Item).get(4).props.text).to.equal('Logout'); // Having Logout option
+        expect(wrapper.find(Dropdown.Item).get(4).props.icon).to.equal('log out'); // Having log out icon for Logout option
     });
 
     it('renders user menu with limited options list ',()=>{
@@ -71,19 +73,10 @@ describe('(Component) Users', () => {
         wrapper.setProps({isEditMode: true});
 
         expect(wrapper.find(Dropdown)).to.have.length(1); // Showing the users menu
-        expect(wrapper.find(Dropdown.Item).length).to.equal(4); // 4 menu options
+        expect(wrapper.find(Dropdown.Item).length).to.equal(5); // 4 menu options
         expect(wrapper.find(Dropdown.Item).get(0).props.text).to.equal('Exit Edit Mode'); // Having Exit Edit Mode option
         expect(wrapper.find(Dropdown.Item).get(0).props.selected).to.equal(true); // Having Edit Mode option selected
     });
-
-    // it('onConfigure is called',()=>{
-    //     let onConfigure = sinon.spy();
-    //     wrapper.setProps({onConfigure});
-    //
-    //     wrapper.find(Dropdown.Item).filterWhere(element => element.instance().props.text === 'Configure').simulate('click');
-    //
-    //     expect(onConfigure.calledOnce).to.equal(true);
-    // });
 
     it('onEditModeChange is called',()=>{
         let onEditModeChange = sinon.spy();
