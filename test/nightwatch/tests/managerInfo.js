fc921cc4283d3d6ac9874a161cb497e3b3e49b41
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2019-03-27 13:55:17 +0100

    CY-1160 - Add system tests for licensing (#769)
    * Fixed nightwatch tests after licensing changes
    * Moved nightwatch tests to test folder
    * Added cypress to project
    * Added system tests for licensing (CY-1160)

--- test/nightwatch/tests/managerInfo.js
+++ test/nightwatch/tests/managerInfo.js
@@ -2,18 +2,16 @@
  * Created by kinneretzin on 25/12/2016.
  */
 
-var Config =require('../config');
 
 module.exports = {
-    'Manager IP and status': function (client) {
+    'Manager status': function (client) {
         client.login();
 
         var page = client.page.page();
 
         page.section.managerData
             .waitForElementPresent('@statusIconGreen')
-            .clickElement('@version')
-            .assert.containsText('@ip', Config.managerVersion)
+            .clickElement('@statusIconGreen')
             .assert.cssClassPresent('@statusIcon','green');
 
         page.waitForElementVisible('@statusesTitle')
