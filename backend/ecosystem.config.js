c7d046e445acb5121cc0c0646c43fd002d4ca90e
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2019-04-18 09:28:30 +0200

    Improved pm2 configuration for stage backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,13 +3,12 @@ module.exports = {
     name: 'stage-backend',
     script: 'server.js',
     args: process.env.STAGE_BACKEND_ARGS,
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 0,
     autorestart: true,
     max_memory_restart: '1G',
+
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: 'production'
     },
     env_development: {
       NODE_ENV: 'development'
