dc5aa84a8de305c17de223acd7ce85bb9479e9c6
Inbal Amrani <inbala@gigaspaces.com>
2019-05-21 15:42:18 +0300

    CY-1265 Add sites management widget

---
+++ widgets/sites/src/SiteActions.js
@@ -0,0 +1,23 @@
+
+export default class SiteActions {
+    constructor(toolbox) {
+        this.toolbox = toolbox;
+    }
+
+    doGet(name) {
+        return this.toolbox.getManager().doGet(`/sites/${name}`);
+    }
+
+    doDelete(name) {
+        return this.toolbox.getManager().doDelete(`/sites/${name}`);
+    }
+
+    doCreate(name, visibility, location) {
+        return this.toolbox.getManager().doPut(`/sites/${name}`, null, {location, visibility});
+    }
+
+    doUpdate(name, visibility, location=null, new_name=null) {
+        let site_data = _.pickBy({location, visibility, new_name});
+        return this.toolbox.getManager().doPost(`/sites/${name}`, null, site_data);
+    }
+}
---
+++ widgets/sites/src/widget.js
@@ -0,0 +1,50 @@
+
+import SitesTable from './SitesTable';
+
+Stage.defineWidget({
+    id: 'sites',
+    name: 'Sites',
+    description: 'This widget shows a list of available sites and allow managing them',
+    initialWidth: 5,
+    initialHeight: 16,
+    color: 'blue',
+    fetchUrl: {
+        sites: '[manager]/sites[params]',
+        siteDeploymentCount: '[manager]/summary/deployments?_target_field=site_name'
+    },
+    isReact: true,
+    hasReadme: true,
+    permission: Stage.GenericConfig.WIDGET_PERMISSION('sites'),
+    categories: [Stage.GenericConfig.CATEGORY.SYSTEM_RESOURCES],
+    initialConfiguration: [
+        Stage.GenericConfig.POLLING_TIME_CONFIG(30),
+        Stage.GenericConfig.PAGE_SIZE_CONFIG(),
+        Stage.GenericConfig.SORT_COLUMN_CONFIG('name'),
+        Stage.GenericConfig.SORT_ASCENDING_CONFIG(true)
+    ],
+
+    render: function(widget, data, error, toolbox) {
+        if (_.isEmpty(data)) {
+            return <Stage.Basic.Loading/>;
+        }
+        let {sites, siteDeploymentCount} = data;
+        let deploymentsPerSite = {};
+        _.forEach(siteDeploymentCount.items, (item) => {
+            deploymentsPerSite[item.site_name] = item.deployments;
+        });
+        let formattedData = {
+            items: _.map(sites.items, (site) => {
+                return {
+                    ...site,
+                    created_at: Stage.Utils.Time.formatTimestamp(site.created_at),
+                    deploymentCount: deploymentsPerSite[site.name] || 0
+                }
+            }),
+            total : _.get(sites, 'metadata.pagination.total', 0)
+        };
+
+        return (
+            <SitesTable widget={widget} data={formattedData} toolbox={toolbox}/>
+        );
+    }
+});
