a5e6adb69cd2ee7bf659009dd0a515cfce3d2098
idob-cloudify <idob@cloudify.co>
2019-06-17 11:50:40 +0300

    adjust cluster widget to show cluster nodes

--- widgets/highAvailability/src/ClusterManagement.js
+++ widgets/highAvailability/src/ClusterManagement.js
@@ -32,24 +32,10 @@ export default class ClusterManagement extends React.Component {
     }
 
     render() {
-        var {ErrorMessage,Label,Icon} = Stage.Basic;
 
-        var isClusterInitialized = this.props.data.state.initialized;
         return (
             <div>
-                <ErrorMessage error={this.state.error || this.props.data.error} onDismiss={() => this.setState({error: null})} autoHide={true}/>
-
                 {
-                    isClusterInitialized ?
-                    <h3><Icon name='checkmark' color='green'/> Cluster Is initialized</h3> :
-                    <h3>
-                        Cluster is Not Initialized. Use the Cloudify CLI to create or join a cluster,
-                        see the <a target="_blank" href="https://docs.cloudify.co/latest/working_with/manager/high-availability-clusters/">documentation</a>.
-                    </h3>
-                }
-
-                {
-                    isClusterInitialized &&
                     <ClusterNodesList toolbox={this.props.toolbox} widget={this.props.widget} nodes={this.props.data.nodes}/>
                 }
             </div>
