c685218875271074e3278bbb702952ee2d9ee502
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2019-07-30 11:17:34 +0200

    Updated upload package script to use cloudify-ui-common package

--- scripts/uploadPackage.sh
+++ scripts/uploadPackage.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
-MANAGER_USER=${MANAGER_USER:-centos}
-STAGE_PACKAGE=${STAGE_PACKAGE:-./stage.tar.gz}
-COMMON_OPTIONS="-o StrictHostKeyChecking=no"
+STAGE_PACKAGE=${STAGE_PACKAGE:-stage.tar.gz}
+COMMAND="
+  tar xzf stage.tar.gz;
+  sudo service cloudify-stage stop;
+  sudo rsync -ai cloudify-stage /opt;
+  sudo chown -R stage_user:stage_group /opt/cloudify-stage;
+  cd /opt/cloudify-stage/backend;
+  sudo /opt/nodejs/bin/npm run db-migrate;
+  sudo service cloudify-stage restart;"
+
+# Ref.: https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# shellcheck disable=SC1090
+. "${DIR}"/../node_modules/cloudify-ui-common/scripts/upload-package.sh
 
 # Stage package check and download
-[ ! -z ${STAGE_PACKAGE_URL} ] \
-&& echo "Stage package URL set. Downloading package from '${STAGE_PACKAGE_URL}'..." && curl ${STAGE_PACKAGE_URL} --output ${STAGE_PACKAGE} \
-|| echo "Stage package URL not set."
-[ -f ${STAGE_PACKAGE} ] \
-&& echo "Stage package found." \
-|| exit "Stage package - ${STAGE_PACKAGE} - not found."
+if [ -n "${STAGE_PACKAGE_URL}" ]; then
+    echo "Stage package URL set. Downloading package from '${STAGE_PACKAGE_URL}'..."
+    curl "${STAGE_PACKAGE_URL}" --output "${STAGE_PACKAGE}"
+fi
+if [ ! -f "${STAGE_PACKAGE}" ]; then
+    echo "ERROR: Stage package - ${STAGE_PACKAGE} - not found."
+    exit 1
+fi
 
-# SSH key setting
-[ ! -z "$SSH_KEY_PATH" ] \
-&& echo "SSH_KEY_PATH=$SSH_KEY_PATH" \
-|| exit "SSH_KEY_PATH not defined."
-[ -f ${SSH_KEY_PATH} ] \
-&& echo "SSH key found." \
-|| exit "SSH key to Cloudify Manager - '${SSH_KEY_PATH}' - not found."
-
-# Manager IP setting
-[ ! -z "$MANAGER_IP" ] \
-&& echo "MANAGER_IP=$MANAGER_IP" \
-|| exit "MANAGER_IP not defined."
-
-scp -i ${SSH_KEY_PATH} ${COMMON_OPTIONS} ${STAGE_PACKAGE} ${MANAGER_USER}@${MANAGER_IP}:~
-ssh -i ${SSH_KEY_PATH} ${COMMON_OPTIONS} ${MANAGER_USER}@${MANAGER_IP} "tar xzf stage.tar.gz; sudo service cloudify-stage stop; sudo rsync -ai cloudify-stage /opt; sudo chown -R stage_user:stage_group /opt/cloudify-stage; cd /opt/cloudify-stage/backend; sudo /opt/nodejs/bin/npm run db-migrate; sudo service cloudify-stage restart;"
+uploadPackage "${STAGE_PACKAGE}" "${COMMAND}"
