adcf33bebec584cef0df97c14e585f783e18c0fb
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2019-08-14 07:59:45 +0200

    Improved getting path for upload-package.sh script. (#830)

--- scripts/uploadPackage.sh
+++ scripts/uploadPackage.sh
@@ -10,11 +10,19 @@ COMMAND="
   sudo /opt/nodejs/bin/npm run db-migrate;
   sudo service cloudify-stage restart;"
 
-# Ref.: https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# shellcheck disable=SC1090
-. "${DIR}"/../node_modules/cloudify-ui-common/scripts/upload-package.sh
+NODE_MODULES_PATH="$( npm root )"
+UPLOAD_PACKAGE_SCRIPT_PATH="${NODE_MODULES_PATH}/cloudify-ui-common/scripts/upload-package.sh"
 
+if [ ! -f "${UPLOAD_PACKAGE_SCRIPT_PATH}" ]; then
+    echo "ERROR: Cloudify UI Common upload-package.sh script not found."
+    exit 1
+fi
+. "$UPLOAD_PACKAGE_SCRIPT_PATH"
+
+if [ ! -f "${STAGE_PACKAGE}" ]; then
+    echo "ERROR: Stage package - ${STAGE_PACKAGE} - not found."
+    exit 1
+fi
 # Stage package check and download
 if [ -n "${STAGE_PACKAGE_URL}" ]; then
     echo "Stage package URL set. Downloading package from '${STAGE_PACKAGE_URL}'..."
