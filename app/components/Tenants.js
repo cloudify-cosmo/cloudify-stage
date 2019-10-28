48cfb2e671f94d71240e1671737b52bef79634cc
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2019-10-28 09:59:39 +0100

    Added scroll bar and alphabetically sort Tenant selection list/widget (CY-1867)

--- app/components/Tenants.js
+++ app/components/Tenants.js
@@ -3,9 +3,10 @@
  */
 
 import PropTypes from 'prop-types';
-
 import React, { Component } from 'react';
+
 import EventBus from '../utils/EventBus';
+import { Dropdown, Loader, Icon, Input } from './basic';
 
 export default class Tenants extends Component {
     static propTypes = {
@@ -14,44 +15,79 @@ export default class Tenants extends Component {
         onTenantsRefresh: PropTypes.func.isRequired
     };
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            search: ''
+        };
+    }
+
     componentDidMount() {
-        EventBus.on('menu.tenants:refresh',this.props.onTenantsRefresh,this);
+        EventBus.on('menu.tenants:refresh', this.props.onTenantsRefresh, this);
     }
 
     onTenantSelected(tenant) {
         this.props.onTenantChange(tenant.name);
     }
 
+    onSearchChange(value) {
+        this.setState({ search: value });
+    }
+
     render() {
-        let {Dropdown, Loader, Icon} = Stage.Basic;
-
-        let tenants = this.props.manager.tenants;
+        const { tenants } = this.props.manager;
+        const { search } = this.state;
         if (!tenants || !tenants.items || tenants.isFetching) {
-            return <Loader active inverted inline size="small" />
+            return <Loader active inverted inline size="small" />;
         }
+        const filteredTenants = _(tenants.items)
+            .filter(tenant => _.includes(tenant.name, search))
+            .sortBy('name')
+            .value();
 
-        let selectedTenant = tenants.selected || _.get(this.props.manager,'tenants.items[0].name');
+        const selectedTenant = tenants.selected || _.get(tenants, 'items[0].name');
 
         const tenantMenuTrigger = (
             <span>
-                <Icon name="male" circular size="small"/>
-                <span>{selectedTenant ? selectedTenant : 'No Tenants'}</span>
+                <Icon name="male" />
+                <span>{selectedTenant || 'No Tenants'}</span>
             </span>
-        )
+        );
 
         return (
-            <Dropdown item pointing='top right' className='tenantsMenu' trigger={tenantMenuTrigger}>
+            <Dropdown
+                item
+                pointing="top right"
+                className="tenantsMenu"
+                trigger={tenantMenuTrigger}
+                onClose={() => this.onSearchChange('')}
+            >
                 <Dropdown.Menu>
-                    {
-                        tenants.items.map((tenant) =>
-                            <Dropdown.Item key={tenant.name} text={tenant.name} selected={tenant.name === selectedTenant}
-                                           active={tenant.name === selectedTenant}
-                                           onClick={this.onTenantSelected.bind(this,tenant)} />
-                        )
-                    }
+                    <Input
+                        icon="search"
+                        iconPosition="left"
+                        className="search"
+                        value={search}
+                        onClick={e => e.stopPropagation()}
+                        onChange={(e, { value }) => {
+                            e.stopPropagation();
+                            this.onSearchChange(value);
+                        }}
+                    />
+                    <Dropdown.Menu scrolling>
+                        {filteredTenants.map(tenant => (
+                            <Dropdown.Item
+                                key={tenant.name}
+                                text={tenant.name}
+                                selected={tenant.name === selectedTenant}
+                                active={tenant.name === selectedTenant}
+                                onClick={this.onTenantSelected.bind(this, tenant)}
+                            />
+                        ))}
+                    </Dropdown.Menu>
                 </Dropdown.Menu>
             </Dropdown>
         );
     }
 }
-
