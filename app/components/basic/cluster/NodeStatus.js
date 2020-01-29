a5c0907a6f9f5d03537b3a20a4e71061c8c9fee1
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2020-01-29 08:55:42 +0100

    Fixed that in Cluster Status widget node status details are not shown when node status is Fail (CY-2277)

--- app/components/basic/cluster/NodeStatus.js
+++ app/components/basic/cluster/NodeStatus.js
@@ -9,7 +9,7 @@ import { clusterNodeStatusEnum, clusterNodeStatuses } from './consts';
 export default function NodeStatus({ name, type, status, services }) {
     const icon = {
         [clusterNodeStatusEnum.OK]: <Icon name="checkmark" color="green" link />,
-        [clusterNodeStatusEnum.FAIL]: <Icon name="remove" color="red" link />
+        [clusterNodeStatusEnum.Fail]: <Icon name="remove" color="red" link />
     }[status];
 
     return (
