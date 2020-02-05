e59149cb069e13ff9d91ef6e83fa248948c07a12
Jakub Niezgoda <jakub.niezgoda@cloudify.co>
2020-02-05 09:39:28 +0100

    Improved checkDocs.sh script

--- scripts/checkDocs.sh
+++ scripts/checkDocs.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-STAGE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ -z ${CIRCLE_BRANCH+x} ]; then
+  STAGE_BRANCH=${CIRCLE_BRANCH}
+  CURL_OPTIONS="-u ${GITHUB_USERNAME}:${GITHUB_TOKEN}"
+else
+  STAGE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  CURL_OPTIONS=""
+fi
+
 DOCS_BRANCH="master"
 if [[ $STAGE_BRANCH =~ [0-9].[0-9]{1,2}-build ]];then
   DOCS_BRANCH=STAGE_BRANCH
@@ -12,7 +19,7 @@ DOCS_REPOSITORY="docs.getcloudify.org"
 WIDGETS_COMPONENTS_PATH="content/developer/writing_widgets/widgets-components.md"
 WIDGET_COMPONENTS_URL="https://raw.githubusercontent.com/cloudify-cosmo/${DOCS_REPOSITORY}/${DOCS_BRANCH}/${WIDGETS_COMPONENTS_PATH}"
 
-VERSION_IN_DOCS=$(curl --silent "$WIDGET_COMPONENTS_URL" | grep ui_components_link: | sed 's/.*ui-components\/\(.*\)"/\1/')
+VERSION_IN_DOCS=$(curl -s "$CURL_OPTIONS" "$WIDGET_COMPONENTS_URL" | grep ui_components_link: | sed 's/.*ui-components\/\(.*\)"/\1/')
 VERSION_IN_STAGE=$(npm view ${COMPONENTS_REPOSITORY} version)
 
 echo "Checking version of ${COMPONENTS_REPOSITORY} package in official Cloudify documentation ..."
