4bbca92048b7d107b720af96e06cc79bdf2c4073
Jakub Madej <jakub.madej@amartus.com>
2020-03-20 11:49:50 +0100

    Code review follow up

--- test/cypress/integration/tours_spec.js
+++ test/cypress/integration/tours_spec.js
@@ -1,21 +1,18 @@
 import _ from 'lodash';
 
 describe('Tours', () => {
-    before(() => {
-        cy.activate('valid_spire_license')
-            .deleteAllUsersAndTenants()
-            .login();
+    before(() => cy.activate('valid_spire_license').deleteAllUsersAndTenants());
+
+    beforeEach(() => cy.get('#toursButton').click());
+
+    function resetTemplates() {
         cy.get('.usersMenu')
             .click()
             .contains('Reset Templates')
             .click();
         cy.contains('Yes').click();
         cy.get('#loader');
-    });
-
-    beforeEach(function() {
-        cy.get('#toursButton').click();
-    });
+    }
 
     function checkTourSteps(tourName, numberOfSteps) {
         cy.contains(tourName).click();
@@ -23,23 +20,49 @@ describe('Tours', () => {
         cy.contains('Done');
     }
 
-    it('Cloudify Console Overview', () => {
-        checkTourSteps('Cloudify Console Overview', 8);
+    describe('for admin user provide', () => {
+        before(() => {
+            cy.login();
+            resetTemplates();
+        });
+
+        it('Cloudify Console Overview tour', () => {
+            checkTourSteps('Cloudify Console Overview', 8);
+        });
+
+        it('Cloudify Console Dashboard tour', () => {
+            checkTourSteps('Cloudify Console Dashboard', 6);
+        });
+
+        it('Initial Manager Setup tour', () => {
+            checkTourSteps('Initial Manager Setup', 10);
+        });
+
+        it('From Blueprint to Execution tour', () => {
+            checkTourSteps('From Blueprint to Execution', 8);
+        });
+
+        it('Day-two Processes tour', () => {
+            checkTourSteps('Day-two Processes', 7);
+        });
     });
 
-    it('Cloudify Console Dashboard', () => {
-        checkTourSteps('Cloudify Console Dashboard', 6);
-    });
+    describe('for non-admin users provide', () => {
+        before(() => {
+            const username = 'nonadmin';
+            const password = 'password';
+            cy.addUser(username, password)
+                .addUserToTenant(username, 'default_tenant', 'viewer')
+                .login(username, password);
+            resetTemplates();
+        });
 
-    it('Initial Manager Setup', () => {
-        checkTourSteps('Initial Manager Setup', 10);
-    });
+        it('Cloudify Console Overview tour', () => {
+            checkTourSteps('Cloudify Console Overview', 8);
+        });
 
-    it('From Blueprint to Execution', () => {
-        checkTourSteps('From Blueprint to Execution', 8);
-    });
-
-    it('Day-two Processes', () => {
-        checkTourSteps('Day-two Processes', 7);
+        it('Cloudify Console Dashboard tour', () => {
+            checkTourSteps('Cloudify Console Dashboard', 7);
+        });
     });
 });
