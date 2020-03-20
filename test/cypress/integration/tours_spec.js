ef6c4ac24900cabfdc4a52b066d35e6ebf0e56f3
kubama <jakub.madej@amartus.com>
2020-03-20 14:57:23 +0100

    CY-2291 Tours fixed and covered with cypress tests (#941)

---
+++ test/cypress/integration/tours_spec.js
@@ -0,0 +1,68 @@
+import _ from 'lodash';
+
+describe('Tours', () => {
+    before(() => cy.activate('valid_spire_license').deleteAllUsersAndTenants());
+
+    beforeEach(() => cy.get('#toursButton').click());
+
+    function resetTemplates() {
+        cy.get('.usersMenu')
+            .click()
+            .contains('Reset Templates')
+            .click();
+        cy.contains('Yes').click();
+        cy.get('#loader');
+    }
+
+    function checkTourSteps(tourName, numberOfSteps) {
+        cy.contains(tourName).click();
+        _.times(numberOfSteps - 1, () => cy.contains('button:not(.loading)', 'Next').click());
+        cy.contains('Done');
+    }
+
+    describe('for admin user provide', () => {
+        before(() => {
+            cy.login();
+            resetTemplates();
+        });
+
+        it('Cloudify Console Overview tour', () => {
+            checkTourSteps('Cloudify Console Overview', 8);
+        });
+
+        it('Cloudify Console Dashboard tour', () => {
+            checkTourSteps('Cloudify Console Dashboard', 6);
+        });
+
+        it('Initial Manager Setup tour', () => {
+            checkTourSteps('Initial Manager Setup', 10);
+        });
+
+        it('From Blueprint to Execution tour', () => {
+            checkTourSteps('From Blueprint to Execution', 8);
+        });
+
+        it('Day-two Processes tour', () => {
+            checkTourSteps('Day-two Processes', 7);
+        });
+    });
+
+    describe('for non-admin users provide', () => {
+        before(() => {
+            const username = 'nonadmin';
+            const password = 'password';
+            cy.addUser(username, password)
+                .addUserToTenant(username, 'default_tenant', 'viewer')
+                .login(username, password);
+            resetTemplates();
+        });
+
+        it('Cloudify Console Overview tour', () => {
+            checkTourSteps('Cloudify Console Overview', 8);
+        });
+
+        it('Cloudify Console Dashboard tour', () => {
+            checkTourSteps('Cloudify Console Dashboard', 7);
+        });
+    });
+});
