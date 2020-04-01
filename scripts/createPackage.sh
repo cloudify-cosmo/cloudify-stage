0e29a84365ee0e8cdf594ae94ec5b7d2d4c5e6e5
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2020-04-01 13:14:51 +0200

    Updated cloudify-stage deploy procedure to contain package.json reflecting actual dependencies (#947)
    * Sorted package.json files
    * Improved createPackage.sh script

--- scripts/createPackage.sh
+++ scripts/createPackage.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env bash
 set -x
 
-mkdir .tmp
-# make sure its empty (could exist before)
-rm -R .tmp/**
+TMP_DIR=.tmp
+STAGE_PACKAGE=stage.tar.gz
 
-mkdir .tmp/cloudify-stage
-mkdir .tmp/cloudify-stage/dist
-mkdir .tmp/cloudify-stage/backend
-mkdir .tmp/cloudify-stage/conf
+rm -Rf ${TMP_DIR} ${STAGE_PACKAGE}
+mkdir -p ${TMP_DIR}/cloudify-stage/dist ${TMP_DIR}/cloudify-stage/backend ${TMP_DIR}/cloudify-stage/conf
 
-cp -r dist/** .tmp/cloudify-stage/dist
-cp -r backend/** .tmp/cloudify-stage/backend
-cp conf/** .tmp/cloudify-stage/conf
-rm .tmp/cloudify-stage/conf/me.json*
-cp scripts/package-template.json .tmp/cloudify-stage
-cd .tmp
-mv cloudify-stage/package-template.json cloudify-stage/package.json
-tar -cvzf stage.tar.gz **
-#tar -cvzf stage.tar.gz dist/** backend/** conf/** package.json
-cd ..
-cp .tmp/stage.tar.gz .
-rm -R .tmp
+cp -r dist/** ${TMP_DIR}/cloudify-stage/dist
+cp -r backend/** ${TMP_DIR}/cloudify-stage/backend
+cp conf/** ${TMP_DIR}/cloudify-stage/conf
+rm ${TMP_DIR}/cloudify-stage/conf/me.json*
+cp package.json ${TMP_DIR}/cloudify-stage/package.json
+
+( cd ${TMP_DIR} && tar -czf ${STAGE_PACKAGE} cloudify-stage )
+cp ${TMP_DIR}/${STAGE_PACKAGE} .
+rm -R ${TMP_DIR}
