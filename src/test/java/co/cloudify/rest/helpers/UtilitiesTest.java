d9ced100d45638a372daec90d844616fc21855b3
Isaac Shabtay <isaac-s@users.noreply.github.com>
2020-05-30 18:46:55 -0700

    Use @TempDir

--- src/test/java/co/cloudify/rest/helpers/UtilitiesTest.java
+++ src/test/java/co/cloudify/rest/helpers/UtilitiesTest.java
@@ -5,31 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.io.TempDir;
 
 public class UtilitiesTest {
-    private static final Logger logger = LoggerFactory.getLogger(UtilitiesTest.class);
-
-    private static File scratchDir;
-
-    @BeforeAll
-    public static void initAll() throws Exception {
-        scratchDir = Files.createTempDirectory("test").toFile();
-        logger.info("Scratch directory created: {}", scratchDir.getAbsolutePath());
-    }
-
-    @AfterAll
-    public static void teardownAll() throws Exception {
-        logger.info("Deleting scratch directory: {}", scratchDir.getAbsolutePath());
-        FileUtils.deleteDirectory(scratchDir);
-    }
+    @TempDir
+    public static File scratchDir;
 
     @Test
     public void testCopyFile() throws Exception {
