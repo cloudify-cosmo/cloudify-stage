36a4f7d632f67570b96c52a6179e2f3ff72c28e7
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2020-06-09 14:27:18 +0200

    Added tests for Sites Map widget

---
+++ test/cypress/integration/widgets/sites_map_spec.js
@@ -0,0 +1,50 @@
+describe('Sites Map', () => {
+    const reloadDashboardPage = () => {
+        cy.get('.dashboardPageMenuItem.active').click();
+        cy.get('.sitesMapWidget .ui.text.loader').should('not.be.visible');
+    };
+
+    before(() => {
+        cy.activate('valid_spire_license')
+            .login()
+            .waitUntilLoaded()
+            .deleteSites()
+            .createSite({ name: 'Tel-Aviv', location: '32.079991, 34.767291' });
+    });
+
+    it('is not displayed when there is no connection to map tiles provider', () => {
+        cy.server();
+        cy.route({
+            method: 'GET',
+            url: /maps\/0\/0\/0\//,
+            status: 500,
+            response: {}
+        }).as('mapsRoute');
+
+        reloadDashboardPage();
+        cy.get('div.sites-map div.leaflet-layer > div.leaflet-tile-container').should('not.have.descendants', 'img');
+        cy.get('.sitesMapWidget .ui.message').should('contain.text', 'widget content cannot be displayed');
+    });
+
+    it('is displayed when connection to map tiles provider is available', () => {
+        reloadDashboardPage();
+        cy.get('div.sites-map div.leaflet-layer > div.leaflet-tile-container').should('have.descendants', 'img');
+    });
+
+    it('shows markers for each site', () => {
+        reloadDashboardPage();
+        // Verify first site is present on the map
+        cy.get('.leaflet-marker-icon')
+            .should('have.length', 1)
+            .click();
+        cy.get('.leaflet-popup .leaflet-popup-content').should('be.visible');
+        cy.get('.leaflet-popup .leaflet-popup-content h5.header').should('have.text', 'Tel-Aviv');
+
+        // Add second site
+        cy.createSite({ name: 'Bergen', location: '60.389433, 5.332489', visibility: 'private' });
+        reloadDashboardPage();
+
+        // Verify second site is present on the map
+        cy.get('.leaflet-marker-icon').should('have.length', 2);
+    });
+});
