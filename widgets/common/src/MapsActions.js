d6f259f244df371cb00a32e172e769667a550ded
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2020-06-24 12:44:00 +0200

    CY-2684 - Fix for caching issue (#1028)

--- widgets/common/src/MapsActions.js
+++ widgets/common/src/MapsActions.js
@@ -7,9 +7,10 @@ class MapsActions {
         const { urlTemplate } = Stage.Common.Consts.leaflet;
         const testUrl = _.template(urlTemplate, { interpolate: /{([\s\S]+?)}/g })({ x: 0, y: 0, z: 0, r: '' });
 
+        const noCache = new Date().getTime();
         return this.toolbox
             .getInternal()
-            .doGet(testUrl)
+            .doGet(testUrl, { noCache })
             .then(() => true)
             .catch(() => false);
     }
