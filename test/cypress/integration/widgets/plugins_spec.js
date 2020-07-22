c4dea46e1836755fee4b3e5dfc4de2c646e4ea68
Jakub Madej <jakub.madej@amartus.com>
2020-07-22 08:59:24 +0200

    Code review followup

--- test/cypress/integration/widgets/plugins_spec.js
+++ test/cypress/integration/widgets/plugins_spec.js
@@ -1,10 +1,15 @@
 describe('Plugins widget', () => {
-    before(() => cy.activate('valid_trial_license'));
+    before(() =>
+        cy
+            .activate('valid_trial_license')
+            .login()
+            .contains('System Resources')
+            .click()
+    );
 
     beforeEach(() => {
-        cy.deletePlugins().login();
+        cy.deletePlugins();
 
-        cy.contains('System Resources').click();
         cy.contains('Upload').click();
         cy.get('input[name=wagonUrl]').type(
             'https://github.com/cloudify-cosmo/cloudify-openstack-plugin/releases/download/3.2.15/cloudify_openstack_plugin-3.2.15-py27-none-linux_x86_64-centos-Core.wgn'
@@ -18,12 +23,13 @@ describe('Plugins widget', () => {
     });
 
     it('should allow to install new plugin with custom name', () => {
-        cy.get('input[name=title]').type('-edited');
+        cy.get('input[name=title]').type('{backspace}{backspace}-edited');
         cy.get('.ok').click();
         cy.get('.modal').should('not.exist');
 
         cy.log('Verify plugin was uploaded');
-        cy.contains('cloudify-openstack-plugin-edited');
+        cy.get('.pluginsTable tbody tr').should('have.length', 1);
+        cy.contains('cloudify-openstack-plug-edited');
     });
 
     it('should allow to install new plugin with default name', () => {
@@ -31,6 +37,7 @@ describe('Plugins widget', () => {
         cy.get('.modal').should('not.exist');
 
         cy.log('Verify plugin was uploaded');
+        cy.get('.pluginsTable tbody tr').should('have.length', 1);
         cy.contains('cloudify-openstack-plugin');
     });
 });
