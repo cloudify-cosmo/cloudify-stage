ec3f5f4f97d4d92de3049e2257d5d9eb9dda6143
Jakub Madej <jakub.madej@amartus.com>
2020-09-03 15:57:32 +0200

    Added error handling

--- backend/test/migration.test.js
+++ backend/test/migration.test.js
@@ -2,9 +2,9 @@
  * Created by jakub.niezgoda on 07/05/2019.
  */
 
-// eslint-disable-next-line security/detect-child-process
-const { execSync } = require('child_process');
 const config = require('../config').get();
+
+const { execSync } = require('child_process');
 
 describe('Migration script', () => {
     it('prints latest revision for "current" argument', () => {
@@ -12,9 +12,14 @@ describe('Migration script', () => {
         expect(result).toEqual('20200123095213-5_1-CreateBlueprintUserData.js\n');
     });
 
-    it('migrates from 4.5.5 snapshot', () => {
-        execSync('node migration.js downTo 20171011082922-4_2-UserAppsRoleColumnRemoval.js');
-        execSync(`psql ${config.app.db.url} -v ON_ERROR_STOP=1 --single-transaction -f test/snapshots/4.5.5.sql`);
-        execSync('node migration.js up');
+    it('migrates from 4.5.5 snapshot', async () => {
+        try {
+            execSync('node migration.js downTo 20171011082922-4_2-UserAppsRoleColumnRemoval.js');
+            execSync(`psql ${config.app.db.url} -v ON_ERROR_STOP=1 --single-transaction -f test/snapshots/4.5.5.sql`);
+            execSync('node migration.js up');
+        } catch (e) {
+            console.log(e.stdout.toString());
+            throw e;
+        }
     });
 });
