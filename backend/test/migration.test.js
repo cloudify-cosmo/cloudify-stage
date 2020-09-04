2ea88b852120ba0a679d76a399cff833437d8f4d
Jakub Madej <jakub.madej@amartus.com>
2020-09-04 11:54:07 +0200

    Added migrations tests for 4.6 and 5.0.5

--- backend/test/migration.test.js
+++ backend/test/migration.test.js
@@ -3,30 +3,35 @@
  */
 
 const { execSync } = require('child_process');
-const { mkdirSync } = require('fs');
 
 const config = require('../config').get();
-const { getResourcePath } = require('../utils');
 
 describe('Migration script', () => {
-    beforeAll(() => {
-        execSync('node migration.js clear');
-        execSync('node migration.js up');
-    });
+    beforeAll(() => execSync('node migration.js up'));
+
+    beforeEach(() => execSync('node migration.js clear'));
 
     it('prints latest revision for "current" argument', () => {
         const result = execSync('node migration.js current').toString();
         expect(result).toEqual('20200123095213-5_1-CreateBlueprintUserData.js\n');
     });
 
-    it('migrates from 4.5.5 snapshot', async () => {
-        try {
-            execSync('node migration.js downTo 20171011082922-4_2-UserAppsRoleColumnRemoval.js');
-            execSync(`psql ${config.app.db.url} -v ON_ERROR_STOP=1 --single-transaction -f test/snapshots/4.5.5.sql`);
-            execSync('node migration.js up');
-        } catch (e) {
-            console.log(e.stdout.toString());
-            throw e;
-        }
-    });
+    function testMigrationUp(snapshotVersion, initialMigration) {
+        it(`migrates from ${snapshotVersion} snapshot`, () => {
+            try {
+                execSync(`node migration.js downTo ${initialMigration}`);
+                execSync(
+                    `psql ${config.app.db.url} -v ON_ERROR_STOP=1 --single-transaction -f test/snapshots/${snapshotVersion}.sql`
+                );
+                execSync('node migration.js up');
+            } catch (e) {
+                console.log(e.stdout.toString());
+                throw e;
+            }
+        });
+    }
+
+    testMigrationUp('4.5.5', '20171011082922-4_2-UserAppsRoleColumnRemoval.js');
+    testMigrationUp('4.6', '20171011082922-4_2-UserAppsRoleColumnRemoval.js');
+    testMigrationUp('5.0.5', '20190423064931-5_0-CreateWidgetBackendTable.js');
 });
