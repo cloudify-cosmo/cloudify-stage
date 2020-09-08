dbd78800b0fdeb5f4cf39c5d3071a92ef8ecacd8
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2020-09-08 11:48:53 +0200

    CY-3449 - Remove PM2 from stage backend (#1087)
    * Removed PM2 from stage backend
    * Added internal configuration for logs/errors files destination
    * Removed instance handling from server and logger

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,12 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
 
-// Initialize logger
-const LoggerHandler = require('./handler/LoggerHandler');
-
-const logger = LoggerHandler.getLogger('Server');
-
 const config = require('./config');
 const Consts = require('./consts');
+const LoggerHandler = require('./handler/LoggerHandler');
+
+// Initialize logger
+const logger = LoggerHandler.getLogger('Server');
 
 // Initialize the DB connection
 require('./db/Connection');
@@ -104,16 +103,14 @@ app.use(
 app.use(
     `${contextPath}/userData`,
     passport.authenticate('cookie', { session: false }),
-    expressStaticGzip(path.resolve(__dirname, '../dist/userData'), { enableBrotli: true, indexFromEmptyFile: false })
+    expressStaticGzip(
+        path.resolve(__dirname, process.env.NODE_ENV === 'development' ? '../userData' : '../dist/userData'),
+        {
+            enableBrotli: true,
+            indexFromEmptyFile: false
+        }
+    )
 );
-
-// Serving static content only in development mode. In production mode it is served by Nginx.
-if (process.env.LOCAL_ENV === 'true') {
-    app.use(
-        `${contextPath}/static`,
-        expressStaticGzip(path.resolve(__dirname, '../dist/static'), { enableBrotli: true, indexFromEmptyFile: false })
-    );
-}
 
 // API Routes
 app.use(`${contextPath}/sp`, ServerProxy);
@@ -164,9 +161,9 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 404).send({ message: message || err });
 });
 
-const startServer = instanceNumber => {
+const startServer = () => {
     app.listen(Consts.SERVER_PORT, Consts.SERVER_HOST, function() {
-        logger.info(`Server (${String(instanceNumber || 0)}) started in mode ${ServerSettings.settings.mode}`);
+        logger.info(`Server started in mode ${ServerSettings.settings.mode}`);
         if (process.env.NODE_ENV === 'development') {
             logger.info('Server started for development');
         }
@@ -174,26 +171,12 @@ const startServer = instanceNumber => {
     });
 };
 
-const instanceNumber = parseInt(process.env.NODE_APP_INSTANCE);
-if (_.isNaN(instanceNumber) || instanceNumber === 0) {
-    // Application data (widgets, templates) initialization only in the first instance
-    Promise.all([ToursHandler.init(), WidgetHandler.init(), TemplateHandler.init()])
-        .then(() => {
-            logger.info('Tours, widgets and templates data initialized successfully.');
-            startServer(instanceNumber);
-        })
-        .catch(error => {
-            logger.error(`Error during tours, widgets and templates data initialization: ${error}`);
-            process.exit(1);
-        });
-} else {
-    ToursHandler.init()
-        .then(() => {
-            logger.info('Tours data initialized successfully.');
-            startServer(instanceNumber);
-        })
-        .catch(error => {
-            logger.error(`Error during tours data initialization: ${error}`);
-            process.exit(1);
-        });
-}
+Promise.all([ToursHandler.init(), WidgetHandler.init(), TemplateHandler.init()])
+    .then(() => {
+        logger.info('Tours, widgets and templates data initialized successfully.');
+        startServer();
+    })
+    .catch(error => {
+        logger.error(`Error during tours, widgets and templates data initialization: ${error}`);
+        process.exit(1);
+    });
