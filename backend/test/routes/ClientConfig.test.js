f25558a9a1a604edc8c750571e5cbdb652c95917
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2020-10-27 13:41:35 +0100

    Add basic ClientConfig and GitHub routes tests

---
+++ backend/test/routes/ClientConfig.test.js
@@ -0,0 +1,33 @@
+/* eslint-disable node/no-missing-require */
+const _ = require('lodash');
+const request = require('supertest');
+const app = require('app');
+const appConfig = require('conf/app.json');
+const userConfig = require('conf/userConfig.json');
+
+describe('/config endpoint', () => {
+    it('allows to get client config', () => {
+        return new Promise(done => {
+            request(app)
+                .get('/console/config')
+                .then(response => {
+                    expect(response.type).toContain('json');
+                    expect(response.statusCode).toBe(200);
+                    const expectedResponse = {
+                        app: {
+                            whiteLabel: userConfig.whiteLabel,
+                            maps: userConfig.maps,
+                            maintenancePollingInterval: appConfig.maintenancePollingInterval,
+                            singleManager: appConfig.singleManager,
+                            saml: _.omit(appConfig.saml, 'certPath')
+                        },
+                        manager: {
+                            ip: expect.any(String)
+                        }
+                    };
+                    expect(response.body).toStrictEqual(expectedResponse);
+                    done();
+                });
+        });
+    });
+});
