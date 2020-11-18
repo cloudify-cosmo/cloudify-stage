02606827a981dfa5320f3c5ee4b7467e5e47aa94
Jakub Madej <jakub.madej@amartus.com>
2020-11-18 13:09:34 +0100

    Code review follow up

--- backend/routes/Style.js
+++ backend/routes/Style.js
@@ -16,21 +16,6 @@ const logger = require('../handler/LoggerHandler').getLogger('Style');
 
 const styleTemplateFile = path.resolve(__dirname, '../templates', 'style.ejs');
 
-function shadeColor(color, percent) {
-    const num = parseInt(color.slice(1), 16); // Remove the '#'
-    const t = percent > 0 ? 0 : 255;
-    const p = percent < 0 ? percent * -1 : percent;
-    /* eslint-disable no-bitwise */
-    const R = num >> 16;
-    const G = (num >> 8) & 0x00ff;
-    const B = num & 0x0000ff; // extract the RGB
-    /* eslint-enable no-bitwise */
-    const newR = Math.round((t - R) * p) + R;
-    const newG = Math.round((t - G) * p) + G;
-    const newB = Math.round((t - B) * p) + B;
-    return `#${(0x1000000 + newR * 0x10000 + newG * 0x100 + newB).toString(16).slice(1)}`;
-}
-
 router.get('/', (req, res) => {
     const { whiteLabel } = config.app;
     const stylesheetTemplate = fs.readFileSync(styleTemplateFile, 'utf8');
@@ -38,10 +23,7 @@ router.get('/', (req, res) => {
     let stylesheet = ejs.render(stylesheetTemplate, {
         logoUrl: whiteLabel.logoUrl,
         mainColor: whiteLabel.mainColor,
-        headerTextColor: whiteLabel.headerTextColor,
-        sidebarColor: whiteLabel.sidebarColor,
-        sideBarHoverActiveColor: shadeColor(whiteLabel.sidebarColor, 0.1),
-        sidebarTextColor: whiteLabel.sidebarTextColor
+        headerTextColor: whiteLabel.headerTextColor
     });
 
     if (!_.isEmpty(whiteLabel.customCssPath)) {
