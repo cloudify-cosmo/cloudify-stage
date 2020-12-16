3a11678a07af5710d1858c4bcef611ed9f562567
Jakub Madej <jakub.madej@amartus.com>
2020-12-16 14:39:17 +0100

    Added -q to rsync

--- scripts/createPackage.sh
+++ scripts/createPackage.sh
@@ -8,8 +8,8 @@ rm -Rf ${TMP_DIR} ${STAGE_PACKAGE}
 mkdir -p ${TMP_DIR}/cloudify-stage/dist ${TMP_DIR}/cloudify-stage/backend ${TMP_DIR}/cloudify-stage/conf
 
 cp -r dist/** ${TMP_DIR}/cloudify-stage/dist
-rsync -avr --exclude='test/' --exclude='package-lock.json' backend ${TMP_DIR}/cloudify-stage
-rsync -avr --exclude='me.json*' --exclude='manager.json' conf ${TMP_DIR}/cloudify-stage
+rsync -avrq --exclude='test/' --exclude='package-lock.json' backend ${TMP_DIR}/cloudify-stage
+rsync -avrq --exclude='me.json*' --exclude='manager.json' conf ${TMP_DIR}/cloudify-stage
 cp package.json ${TMP_DIR}/cloudify-stage/package.json
 
 ( cd ${TMP_DIR} && tar -czf ${STAGE_PACKAGE} cloudify-stage )
