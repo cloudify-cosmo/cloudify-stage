615aba52f09502c0f09c35629868e888d6eab7cb
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-01-27 12:01:07 +0100

    Remove Refs and focus changes Created logic division between selected and typed values

--- widgets/common/src/LabelsInput.jsx
+++ widgets/common/src/LabelsInput.jsx
@@ -5,13 +5,16 @@ const {
 
 const LabelPropType = PropTypes.shape({ key: PropTypes.string, value: PropTypes.string });
 const LabelsPropType = PropTypes.arrayOf(LabelPropType);
-const RefPropType = PropTypes.oneOfType([PropTypes.func, PropTypes.shape({ current: PropTypes.instanceOf(Element) })]);
 
-function onSearchChange(setSearchQuery, setShowError, unsetShowError) {
+function onSearchChange(onChange, setSearchQuery, setShowError, unsetShowError, resetSelectedValue, setTypedValue) {
     const allowedCharacters = /^[a-z0-9_-]*$/i;
-    return (event, { searchQuery: newSearchQuery }) => {
-        if (allowedCharacters.test(newSearchQuery)) {
-            setSearchQuery(_.toLower(newSearchQuery));
+
+    return (event, { searchQuery: newTypedValue }) => {
+        const lowercasedNewTypedValue = _.toLower(newTypedValue);
+        if (allowedCharacters.test(lowercasedNewTypedValue)) {
+            resetSelectedValue();
+            setTypedValue(lowercasedNewTypedValue);
+            onChange(lowercasedNewTypedValue);
             unsetShowError();
         } else {
             setShowError();
@@ -19,12 +22,22 @@ function onSearchChange(setSearchQuery, setShowError, unsetShowError) {
     };
 }
 
-function onValueChange(onChange, unsetShowError, resetSearchQuery) {
-    return (event, data) => {
-        onChange(event, data);
+function onValueChange(onChange, unsetShowError, resetTypedValue, setSelectedValue) {
+    return value => {
+        resetTypedValue();
+        setSelectedValue(value);
+        onChange(value);
         unsetShowError();
-        resetSearchQuery();
     };
+}
+
+function useResetValues(value, selectedValue, typedValue, resetSelectedValue, resetTypedValue) {
+    React.useEffect(() => {
+        if (value === '') {
+            resetSelectedValue();
+            resetTypedValue();
+        }
+    }, [value]);
 }
 
 function addSearchToUrl(url, search) {
@@ -54,94 +67,100 @@ ValidationErrorPopup.defaultProps = {
     open: false
 };
 
-function LabelKeyDropdown({ value, onChange, toolbox, innerRef }) {
-    const [searchQuery, setSearchQuery, resetSearchQuery] = useResettableState('');
+function LabelKeyDropdown({ onChange, toolbox, value }) {
+    const [selectedValue, setSelectedValue, resetSelectedValue] = useResettableState('');
+    const [typedValue, setTypedValue, resetTypedValue] = useResettableState('');
     const [showError, setShowError, unsetShowError] = useBoolean();
 
-    const { Ref } = Stage.Basic;
     const { DynamicDropdown } = Stage.Common;
 
-    const fetchUrl = addSearchToUrl('/labels/deployments', searchQuery);
+    const fetchUrl = addSearchToUrl('/labels/deployments', typedValue);
+
+    useResetValues(value, selectedValue, typedValue, resetSelectedValue, resetTypedValue);
 
     return (
         <>
             <ValidationErrorPopup open={showError} />
 
-            <Ref innerRef={innerRef}>
-                <DynamicDropdown
-                    additionLabel={i18n.t('widgets.common.labels.newKey')}
-                    allowAdditions
-                    clearable={false}
-                    fetchAll
-                    fetchUrl={fetchUrl}
-                    name="labelKey"
-                    onChange={onValueChange(onChange, unsetShowError, resetSearchQuery)}
-                    onSearchChange={onSearchChange(setSearchQuery, setShowError, unsetShowError)}
-                    placeholder={i18n.t('widgets.common.labels.keyPlaceholder')}
-                    searchQuery={searchQuery}
-                    selectOnNavigation={false}
-                    toolbox={toolbox}
-                    value={value}
-                    valueProp=""
-                    tabIndex={0}
-                />
-            </Ref>
+            <DynamicDropdown
+                clearable={false}
+                fetchAll
+                fetchUrl={fetchUrl}
+                name="labelKey"
+                noResultsMessage={value ? i18n.t('widgets.common.labels.newKey') : undefined}
+                onChange={onValueChange(onChange, unsetShowError, resetTypedValue, setSelectedValue)}
+                onSearchChange={onSearchChange(
+                    onChange,
+                    setTypedValue,
+                    setShowError,
+                    unsetShowError,
+                    resetSelectedValue,
+                    setTypedValue
+                )}
+                placeholder={i18n.t('widgets.common.labels.keyPlaceholder')}
+                searchQuery={typedValue}
+                toolbox={toolbox}
+                value={selectedValue}
+                valueProp=""
+                tabIndex={0}
+            />
         </>
     );
 }
 
 LabelKeyDropdown.propTypes = {
-    innerRef: RefPropType,
     onChange: PropTypes.func,
     toolbox: Stage.PropTypes.Toolbox.isRequired,
     value: PropTypes.string
 };
 
 LabelKeyDropdown.defaultProps = {
-    innerRef: null,
     onChange: _.noop,
     value: null
 };
 
-function LabelValueDropdown({ innerRef, labelKey, onChange, toolbox, value }) {
-    const [searchQuery, setSearchQuery, resetSearchQuery] = useResettableState('');
+function LabelValueDropdown({ labelKey, onChange, toolbox, value }) {
+    const [selectedValue, setSelectedValue, resetSelectedValue] = useResettableState('');
+    const [typedValue, setTypedValue, resetTypedValue] = useResettableState('');
     const [showError, setShowError, unsetShowError] = useBoolean();
 
-    const { Ref } = Stage.Basic;
     const { DynamicDropdown } = Stage.Common;
+    useResetValues(value, selectedValue, typedValue, resetSelectedValue, resetTypedValue);
 
-    const fetchUrl = addSearchToUrl(`/labels/deployments/${labelKey}`, searchQuery);
+    const fetchUrl = addSearchToUrl(`/labels/deployments/${labelKey}`, typedValue);
 
     return (
         <>
             <ValidationErrorPopup open={showError} />
 
-            <Ref innerRef={innerRef}>
-                <DynamicDropdown
-                    additionLabel={i18n.t('widgets.common.labels.newValue')}
-                    allowAdditions
-                    clearable={false}
-                    disabled={!labelKey}
-                    fetchAll
-                    fetchUrl={fetchUrl}
-                    name="labelValue"
-                    onChange={onValueChange(onChange, unsetShowError, resetSearchQuery)}
-                    onSearchChange={onSearchChange(setSearchQuery, setShowError, unsetShowError)}
-                    placeholder={i18n.t('widgets.common.labels.valuePlaceholder')}
-                    searchQuery={searchQuery}
-                    selectOnNavigation={false}
-                    toolbox={toolbox}
-                    value={value}
-                    valueProp=""
-                    tabIndex={labelKey ? 0 : -1}
-                />
-            </Ref>
+            <DynamicDropdown
+                clearable={false}
+                disabled={!labelKey}
+                fetchAll
+                fetchUrl={fetchUrl}
+                name="labelValue"
+                noResultsMessage={value ? i18n.t('widgets.common.labels.newValue') : undefined}
+                onChange={onValueChange(onChange, unsetShowError, resetTypedValue, setSelectedValue)}
+                onSearchChange={onSearchChange(
+                    onChange,
+                    setTypedValue,
+                    setShowError,
+                    unsetShowError,
+                    resetSelectedValue,
+                    setTypedValue
+                )}
+                placeholder={i18n.t('widgets.common.labels.valuePlaceholder')}
+                searchQuery={typedValue}
+                toolbox={toolbox}
+                value={selectedValue}
+                valueProp=""
+                tabIndex={labelKey ? 0 : -1}
+            />
         </>
     );
 }
 
 LabelValueDropdown.propTypes = {
-    innerRef: RefPropType,
     labelKey: PropTypes.string,
     onChange: PropTypes.func,
     toolbox: Stage.PropTypes.Toolbox.isRequired,
@@ -149,31 +168,24 @@ LabelValueDropdown.propTypes = {
 };
 
 LabelValueDropdown.defaultProps = {
-    innerRef: null,
     labelKey: '',
     onChange: _.noop,
     value: null
 };
 
-function LabelAddButton({ disabled, innerRef, onClick }) {
-    const { Button, Ref } = Stage.Basic;
+function LabelAddButton({ disabled, onClick }) {
+    const { Button } = Stage.Basic;
 
-    return (
-        <Ref innerRef={innerRef}>
-            <Button icon="add" onClick={onClick} disabled={disabled} fluid />
-        </Ref>
-    );
+    return <Button icon="add" onClick={onClick} disabled={disabled} fluid />;
 }
 
 LabelAddButton.propTypes = {
     disabled: PropTypes.bool,
-    innerRef: RefPropType,
     onClick: PropTypes.func
 };
 
 LabelAddButton.defaultProps = {
     disabled: false,
-    innerRef: null,
     onClick: _.noop
 };
 
@@ -190,14 +202,22 @@ function LabelsList({ labels, onChangeLabels }) {
 
     return (
         <div className="ui multiple dropdown" style={{ paddingRight: '4.1em', minHeight: '2em' }}>
-            {_.map(labels, ({ key, value }) => {
+            {_.map(labels, ({ key, value, isUsed }) => {
                 const truncatedKey = _.truncate(key, { length: maxLength });
                 const truncatedValue = _.truncate(value, { length: maxLength });
 
                 return (
-                    <Popup open={truncatedKey === key && truncatedValue === value ? false : undefined} wide>
+                    <Popup
+                        key={`${key}:${value}`}
+                        open={truncatedKey === key && truncatedValue === value ? false : undefined}
+                        wide
+                    >
                         <Popup.Trigger>
-                            <Label as="a">
+                            <Label
+                                as="a"
+                                color={!isUsed ? 'green' : undefined}
+                                onClick={event => event.stopPropagation()}
+                            >
                                 {truncatedKey} <span style={{ fontWeight: 'lighter' }}>{truncatedValue}</span>
                                 <Icon
                                     name="delete"
@@ -240,30 +260,11 @@ export default function LabelsInput({ initialValue, toolbox }) {
     const { Divider, Form, Icon } = Stage.Basic;
     const { RevertToDefaultIcon } = Stage.Common;
 
-    const keyDropdownRef = React.useRef();
-    const valueDropdownRef = React.useRef();
-    const addButtonRef = React.useRef();
-
-    const focusDelay = 200;
     const iconStyle = {
         position: 'absolute',
         top: '.7em',
         zIndex: 1
     };
-
-    function focusOnInput(dropdownRef) {
-        const inputNode = _.get(dropdownRef, 'current.childNodes[0]');
-        if (inputNode && _.isFunction(inputNode.focus)) {
-            setTimeout(() => inputNode.focus(), focusDelay);
-        }
-    }
-
-    function focusOnButton(buttonRef) {
-        const buttonNode = _.get(buttonRef, 'current');
-        if (buttonNode && _.isFunction(buttonNode.focus)) {
-            setTimeout(() => buttonNode.focus(), focusDelay);
-        }
-    }
 
     function isNewLabelPresent() {
         return _.findIndex(labels, { key: newLabelKey, value: newLabelValue }) >= 0;
@@ -273,51 +274,64 @@ export default function LabelsInput({ initialValue, toolbox }) {
         return !newLabelKey || !newLabelValue;
     }
 
+    async function isNewLabelUsed() {
+        const fetchUrl = addSearchToUrl(`/labels/deployments/${newLabelKey}`, newLabelValue);
+        return toolbox
+            .getManager()
+            .doGet(fetchUrl)
+            .then(({ items }) => !_.isEmpty(items))
+            .catch(() => false);
+    }
+
     async function onAddLabel() {
         if (!areNewKeyAndValueEmpty() && !isNewLabelPresent()) {
-            const newLabels = [...labels, { key: newLabelKey, value: newLabelValue }];
-            setLabels(newLabels);
-            resetNewLabelKey();
-            resetNewLabelValue();
-            focusOnInput(keyDropdownRef);
+            isNewLabelUsed().then(isUsed => {
+                const newLabels = [...labels, { key: newLabelKey, value: newLabelValue, isUsed }];
+                setLabels(newLabels);
+                resetNewLabelKey();
+                resetNewLabelValue();
+            });
         }
     }
 
     function onChangeLabelValue(labelValue) {
         setNewLabelValue(labelValue);
-        focusOnButton(addButtonRef);
     }
 
     function onChangeLabelKey(labelKey) {
         setNewLabelKey(labelKey);
-        focusOnInput(valueDropdownRef);
+        resetNewLabelValue();
     }
 
     return (
         <StyledSegment open={open}>
-            <RevertToDefaultIcon
-                value={labels}
-                defaultValue={initialValue}
-                onClick={resetLabels}
-                style={{ ...iconStyle, right: '2em' }}
-            />
-            <Icon name="dropdown" link onClick={open ? unsetOpen : setOpen} style={{ ...iconStyle, right: '0.5em' }} />
-            <LabelsList labels={labels} onChangeLabels={setLabels} />
+            <div role="presentation" onClick={open ? unsetOpen : setOpen} style={{ cursor: 'pointer' }}>
+                <RevertToDefaultIcon
+                    value={labels}
+                    defaultValue={initialValue}
+                    onClick={event => {
+                        event.stopPropagation();
+                        resetLabels();
+                    }}
+                    style={{ ...iconStyle, right: '2em' }}
+                />
+                <Icon
+                    name="dropdown"
+                    link
+                    onClick={open ? unsetOpen : setOpen}
+                    style={{ ...iconStyle, right: '0.5em' }}
+                />
+                <LabelsList labels={labels} onChangeLabels={setLabels} />
+            </div>
             {open && (
                 <div style={{ padding: '0 0.5em' }}>
                     <Divider hidden={_.isEmpty(labels)} />
                     <Form.Group>
                         <Form.Field width={7}>
-                            <LabelKeyDropdown
-                                innerRef={keyDropdownRef}
-                                onChange={onChangeLabelKey}
-                                toolbox={toolbox}
-                                value={newLabelKey}
-                            />
+                            <LabelKeyDropdown onChange={onChangeLabelKey} toolbox={toolbox} value={newLabelKey} />
                         </Form.Field>
                         <Form.Field width={7}>
                             <LabelValueDropdown
-                                innerRef={valueDropdownRef}
                                 labelKey={newLabelKey}
                                 onChange={onChangeLabelValue}
                                 toolbox={toolbox}
@@ -328,7 +342,6 @@ export default function LabelsInput({ initialValue, toolbox }) {
                             <LabelAddButton
                                 onClick={onAddLabel}
                                 disabled={areNewKeyAndValueEmpty() || isNewLabelPresent()}
-                                innerRef={addButtonRef}
                             />
                         </Form.Field>
                     </Form.Group>
@@ -340,7 +353,7 @@ export default function LabelsInput({ initialValue, toolbox }) {
 
 LabelsInput.propTypes = {
     toolbox: Stage.PropTypes.Toolbox.isRequired,
-    initialValue: PropTypes.arrayOf(PropTypes.string)
+    initialValue: LabelsPropType
 };
 
 LabelsInput.defaultProps = {
