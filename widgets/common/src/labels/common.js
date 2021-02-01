18285a8415ffcfe9bc2b447e604fecbc10d911f0
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-02-01 08:11:19 +0100

    Externalize and reuse appendQueryParam function

--- widgets/common/src/labels/common.js
+++ widgets/common/src/labels/common.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export function addSearchToUrl(url, search) {
-    return search ? `${url}?_search=${search}` : url;
+    const { appendQueryParam } = Stage.Utils.Url;
+    return search ? appendQueryParam(url, 'search', search) : url;
 }
