a690977af2f90004db0636b48376da42b354da82
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2021-02-04 10:41:55 +0100

    RD-752 - Add Manage Labels modal (#1186)
    * Fix search addSearchToUrl function
    * Add basic ManageLabelsModal implementation

--- widgets/common/src/labels/common.js
+++ widgets/common/src/labels/common.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export function addSearchToUrl(url, search) {
     const { appendQueryParam } = Stage.Utils.Url;
-    return search ? appendQueryParam(url, 'search', search) : url;
+    return search ? appendQueryParam(url, '_search', search) : url;
 }
