6d35b33b9603e84aaff83d900b5154bb1d395b3c
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-02-05 09:35:44 +0100

    RD-1324 - coding style fix, function argument type in JSDoc

--- app/utils/encodeTextToBase64.js
+++ app/utils/encodeTextToBase64.js
@@ -1,19 +1,19 @@
-const CODE_EXP = /%([0-9A-F]{2})/g;
+const codeExpression = /%([0-9A-F]{2})/g;
 
-const getChar = (_part, hex) => String.fromCharCode(parseInt(hex, 16));
+const getChar = (part, hex) => String.fromCharCode(parseInt(hex, 16));
 
 /**
  * Encodes text to base64 with unicode support.
- * The main idea of the method is to escape, using native method,
+ * The main idea of the function is to escape, using native function,
  * unsafe unicode characters to 2 bytes hex codes (e.g. %A4),
  * then convert the codes back to 2 bytes characters,
- * that can be used by native btoa() method.
- * @param {*} text text that may contain any unicode characters
+ * that can be used by native btoa() function.
+ * @param {string} text text that may contain any unicode characters
  * @returns base64 encoded text
  */
 const encodeTextToBase64 = text => {
     const escapedText = encodeURIComponent(text);
-    const convertedText = escapedText.replace(CODE_EXP, getChar);
+    const convertedText = escapedText.replace(codeExpression, getChar);
     return btoa(convertedText);
 };
 
