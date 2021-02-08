4a7f6d42440f2fa2d3834a1e582ec8a7172a356c
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-02-08 16:19:11 +0100

    RD-1355 Add parent selectors when drilling down
    The text that was found by Cypress was somewhere else in the DOM.
    This makes sure that the matched node is in the component under test.

--- test/cypress/integration/widgets/deployments_spec.js
+++ test/cypress/integration/widgets/deployments_spec.js
@@ -48,7 +48,7 @@ describe('Deployments widget', () => {
 
         it('clickToDrillDown option', () => {
             searchForDeployment(deploymentName);
-            cy.contains(deploymentName).click();
+            cy.get('.deploymentsWidget').contains(deploymentName).click();
             cy.location('pathname').should('contain', '_deployment/deployments_test_hw_dep');
             cy.contains('.breadcrumb', 'deployments_test_hw_dep');
 
@@ -57,7 +57,7 @@ describe('Deployments widget', () => {
                 cy.get('input[name="clickToDrillDown"]').parent().click();
             });
 
-            cy.contains(deploymentName).click();
+            cy.get('.deploymentsWidget').contains(deploymentName).click();
             cy.location('pathname').should('not.contain', '_deployment/deployments_test_hw_dep');
             cy.get('.deploymentsWidget');
         });
