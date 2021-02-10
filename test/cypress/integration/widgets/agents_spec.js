f388f75698a0f25df2c609a4e921f0786330f3b5
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-02-10 16:47:21 +0100

    RD-1323 - cypress tests

--- test/cypress/integration/widgets/agents_spec.js
+++ test/cypress/integration/widgets/agents_spec.js
@@ -3,7 +3,9 @@ describe('Agents widget', () => {
     const deploymentName = 'agents_test_deployment';
 
     before(() => {
-        cy.usePageMock('agents', { fieldsToShow: [] })
+        cy.usePageMock('agents', {
+            fieldsToShow: ['Id', 'Node', 'Deployment', 'IP', 'Install Method', 'System', 'Version', 'Actions']
+        })
             .activate()
             .mockLogin()
             .deleteDeployments(deploymentName, true)
@@ -32,5 +34,33 @@ describe('Agents widget', () => {
         cy.get('div[name=nodeInstanceId] .item').click();
         cy.contains('.modal button', 'Install').click();
         cy.contains('Close').click();
+    });
+
+    it('should display agents for search action', () => {
+        cy.interceptSp('GET', '/agents?&_search=test', {
+            metadata: {
+                pagination: {
+                    total: 3,
+                    size: 100,
+                    offset: 0
+                },
+                filtered: null
+            },
+            items: [
+                {
+                    id: 'test',
+                    ip: '127.0.0.1',
+                    deployment: '9f13b1a1798277648adb544a2dd14fb7',
+                    node: 'test',
+                    system: 'centos core',
+                    version: '1.0.0',
+                    host_id: 'test',
+                    install_method: 'remote'
+                }
+            ]
+        }).as('search');
+        cy.get('input[placeholder="Search..."]').type('test');
+        cy.wait('@search');
+        cy.get('table.agentsTable').contains('9f13b1a1798277648adb544a2dd14fb7');
     });
 });
