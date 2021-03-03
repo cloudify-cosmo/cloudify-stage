eecf58e223cc464c3e51b2991803b7a76b34f51f
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-03-03 14:32:11 +0100

    RD-1223 Migrate configureStore to TS

--- app/configureStore.ts
+++ app/configureStore.ts
@@ -1,25 +1,26 @@
-/**
- * Created by kinneretzin on 29/08/2016.
- */
-
 import thunkMiddleware from 'redux-thunk';
 import { routerMiddleware } from 'connected-react-router';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import type { History } from 'history';
 
 import throttle from 'lodash/throttle';
 import StatePersister from './utils/StatePersister';
+import type { ClientConfig } from './utils/ConfigLoader';
 
-import createRootReducer from './reducers';
+import createRootReducer, { ReduxState } from './reducers';
 import { emptyState } from './reducers/managerReducer';
 
-export default (history, config) => {
-    let initialState = StatePersister.load(config.mode) || { manager: emptyState };
-    initialState = { ...initialState, config };
+export default (history: History, config: ClientConfig) => {
+    const reduxManagerState: Required<Pick<ReduxState, 'manager'>> = StatePersister.load(config.mode) || {
+        manager: emptyState
+    };
 
     // Clear login error if has any
-    initialState.manager.err = null;
-    initialState.manager.isLoggingIn = false;
+    reduxManagerState.manager.err = null;
+    reduxManagerState.manager.isLoggingIn = false;
+
+    const initialState: Partial<ReduxState> = { ...reduxManagerState, config };
 
     const store = createStore(
         createRootReducer(history),
