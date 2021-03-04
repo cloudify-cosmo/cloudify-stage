37d19355d4c386be85f22f23787f4dc4d38c78e6
Jakub Madej <jakub.madej@amartus.com>
2021-03-04 09:57:23 +0100

    Refactored Cypress test

--- test/cypress/integration/widgets/labels_spec.ts
+++ test/cypress/integration/widgets/labels_spec.ts
@@ -10,14 +10,14 @@ describe('Labels widget', () => {
             .deleteBlueprints(blueprintName, true)
             .uploadBlueprint('blueprints/simple.zip', blueprintName)
             .deployBlueprint(blueprintName, deploymentName, { server_ip: 'localhost' })
+            .setLabels(deploymentName, [{ existing: 'existing' }])
             .setDeploymentContext(deploymentName);
     });
 
-    it('should allow to manage labels', () => {
-        cy.contains('There are no Labels defined');
-
+    it('should allow to add labels', () => {
         cy.contains('Add').click();
         cy.get('.modal').within(() => {
+            cy.get('a.label').should('not.exist');
             cy.get('.selection').click();
             cy.get('div[name=labelKey] > input').type('sample_key');
             cy.get('div[name=labelValue] > input').type('sample_value');
@@ -29,35 +29,31 @@ describe('Labels widget', () => {
         cy.log('Verify label was added to the table');
         cy.get('.labelsTable').contains('sample_key');
         cy.get('.labelsTable').contains('sample_value');
+    });
 
-        cy.log('Verify added label is not present in the add modal');
-        cy.contains('Add').click();
-        cy.get('.modal').within(() => {
-            cy.get('a.label').should('not.exist');
-            cy.contains('Cancel').click();
-        });
-
-        cy.log('Verify label value can be edited');
-        cy.get('.labelsTable').within(() => {
+    it('should allow to edit labels', () => {
+        cy.contains('tr', 'existing').within(() => {
             cy.get('.edit').click();
             cy.get('.cancel').click();
             cy.get('.edit').click();
             cy.get('input').type('!');
         });
         cy.contains('Only letters, digits and the characters "-", "." and "_" are allowed');
-        cy.get('.labelsTable').within(() => {
-            cy.get('input').should('be.visible');
+        cy.contains('tr', 'existing').within(() => {
             cy.get('.undo').should('not.exist');
             cy.get('input').type('_changed');
             cy.get('.undo').should('be.visible');
             cy.get('.check').click();
             cy.get('input').should('not.exist');
-            cy.contains('sample_value_changed');
         });
+        cy.contains('existing_changed');
+    });
 
-        cy.log('Verify label can be removed');
-        cy.get('.trash').click();
-        cy.contains('Yes').click();
+    it('should allow to remove labels', () => {
+        cy.get('.trash').each(deleteIcon => {
+            cy.wrap(deleteIcon).click();
+            cy.contains('Yes').click();
+        });
         cy.contains('There are no Labels defined');
     });
 });
