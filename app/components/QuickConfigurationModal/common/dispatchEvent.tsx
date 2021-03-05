34e0143022fd572aa635ea037595322404764c4f
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-05 13:02:11 +0100

    RD-1442 - fix for event dispatching

--- app/components/QuickConfigurationModal/common/dispatchEvent.tsx
+++ app/components/QuickConfigurationModal/common/dispatchEvent.tsx
@@ -1,12 +1,8 @@
 const dispatchEvent = (element: HTMLElement, eventName: string) => {
-    const eventArg = document.createEvent('HTMLEvents');
-    const eventAction = (element as any)[`on${eventName}`];
-    if (eventAction) {
-        eventAction.apply(element, eventArg);
-    }
     if ('createEvent' in document) {
-        eventArg.initEvent(eventName, false, true);
-        element.dispatchEvent(eventArg);
+        const event = document.createEvent('HTMLEvents');
+        event.initEvent(eventName, false, true);
+        element.dispatchEvent(event);
     } else {
         (element as any).fireEvent(eventName); // only for backward capability
     }
