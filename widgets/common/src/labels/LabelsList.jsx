670a8889390d07e6b5cbbad78b8c434137620504
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-03-05 09:37:00 +0100

    Remove label truncation in LabelsList

--- widgets/common/src/labels/LabelsList.jsx
+++ widgets/common/src/labels/LabelsList.jsx
@@ -1,36 +1,26 @@
 export default function LabelsList({ labels, onChange }) {
-    const { Label, Icon, Popup } = Stage.Basic;
-    const maxLength = 20;
+    const { Label, Icon } = Stage.Basic;
     const newLabelColor = 'blue';
 
     return (
         <div
             className="ui multiple dropdown"
             /* maxHeight set to 8.2em guarantees 4 lines of labels to be visible without scroller */
-            style={{ paddingRight: '4.1em', minHeight: '2em', maxHeight: '8.2em', overflow: 'auto' }}
+            style={{ paddingRight: '4.1em', minHeight: '2em', maxHeight: '8.2em', overflow: 'auto', maxWidth: '100%' }}
         >
             {_.map(labels, ({ key, value, isInSystem = true }) => {
-                const truncatedKey = _.truncate(key, { length: maxLength });
-                const truncatedValue = _.truncate(value, { length: maxLength });
-                const allowPopup = truncatedKey !== key || truncatedValue !== value;
-
                 return (
-                    <Popup key={`${key}:${value}`} open={allowPopup ? undefined : false} wide>
-                        <Popup.Trigger>
-                            <Label
-                                as="a"
-                                color={isInSystem ? undefined : newLabelColor}
-                                onClick={event => event.stopPropagation()}
-                            >
-                                {truncatedKey} <span style={{ fontWeight: 'lighter' }}>{truncatedValue}</span>
-                                <Icon
-                                    name="delete"
-                                    onClick={() => onChange(_.differenceBy(labels, [{ key, value }], { key, value }))}
-                                />
-                            </Label>
-                        </Popup.Trigger>
-                        <strong>{key}</strong> {value}
-                    </Popup>
+                    <Label
+                        as="a"
+                        color={isInSystem ? undefined : newLabelColor}
+                        onClick={event => event.stopPropagation()}
+                    >
+                        {key} <span style={{ fontWeight: 'lighter' }}>{value}</span>
+                        <Icon
+                            name="delete"
+                            onClick={() => onChange(_.differenceBy(labels, [{ key, value }], { key, value }))}
+                        />
+                    </Label>
                 );
             })}
         </div>
