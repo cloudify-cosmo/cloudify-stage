134763cffd8d17d0500d36ee316f573b187a4cc0
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-03-05 09:37:30 +0100

    Add label sorting by key first, then by value

--- widgets/common/src/labels/LabelsList.jsx
+++ widgets/common/src/labels/LabelsList.jsx
@@ -1,6 +1,7 @@
 export default function LabelsList({ labels, onChange }) {
     const { Label, Icon } = Stage.Basic;
     const newLabelColor = 'blue';
+    const sortedLabels = _.sortBy(labels, ['key', 'value']);
 
     return (
         <div
@@ -8,9 +9,10 @@ export default function LabelsList({ labels, onChange }) {
             /* maxHeight set to 8.2em guarantees 4 lines of labels to be visible without scroller */
             style={{ paddingRight: '4.1em', minHeight: '2em', maxHeight: '8.2em', overflow: 'auto', maxWidth: '100%' }}
         >
-            {_.map(labels, ({ key, value, isInSystem = true }) => {
+            {sortedLabels.map(({ key, value, isInSystem = true }) => {
                 return (
                     <Label
+                        key={`${key}_${value}`}
                         as="a"
                         color={isInSystem ? undefined : newLabelColor}
                         onClick={event => event.stopPropagation()}
