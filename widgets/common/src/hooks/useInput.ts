a8432ad1f72f9dff3458b722f8dc64f366cb971a
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-03-08 12:54:33 +0100

    RD-1408 Migrate common hooks to TS

--- widgets/common/src/hooks/useInput.ts
+++ widgets/common/src/hooks/useInput.ts
@@ -2,16 +2,18 @@
  * Returns a stateful value and functions to manipulate it:
  * `setInput` which sets the value and can be directly used as SUIR `onChange` handler,
  * `clearValue` which sets the value back to the initial one
- *
- * @param initialValue
- * @returns {(*|(function(*, *): *))[]}
  */
-function useInput(initialValue) {
+function useInput<T>(initialValue: T) {
     const { useResettableState } = Stage.Hooks;
 
     const [input, setInput, clearInput] = useResettableState(initialValue);
 
-    return [input, (value, field) => setInput(field ? field.value : value), clearInput];
+    return [input, (value: any, field?: any) => setInput(field ? field.value : value), clearInput] as const;
 }
 
+declare namespace Stage {
+    interface Hooks {
+        useInput: typeof useInput;
+    }
+}
 Stage.defineHook({ useInput });
