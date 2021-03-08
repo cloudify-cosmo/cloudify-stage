ca396e8cff0484dd2fafcd4389aace9a80dd84fc
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-03-08 12:32:40 +0100

    RD-1408 Migrate app hooks to TS

--- app/utils/hooks/useErrors.ts
+++ app/utils/hooks/useErrors.ts
@@ -3,15 +3,13 @@ import { useState } from 'react';
 /**
  * Returns a stateful `errors` object and functions to manipulate it: `setErrors`, `clearErrors` and `setMessageAsError`,
  * which extracts message from given `Error` instance and sets it as actual error
- *
- * @returns {{clearErrors: (function(): void), setErrors: React.Dispatch<React.SetStateAction<{}>>, errors: {}, setMessageAsError: (function(*): void)}}
  */
 export default function useErrors() {
-    const [errors, setErrors] = useState({});
+    const [errors, setErrors] = useState({} as Record<string, any>);
 
     return {
         errors,
-        setMessageAsError: err => setErrors({ errors: err.message }),
+        setMessageAsError: (err: Error) => setErrors({ errors: err.message }),
         clearErrors: () => setErrors({}),
         setErrors
     };
