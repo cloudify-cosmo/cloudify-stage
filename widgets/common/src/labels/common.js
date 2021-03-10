bea412e79ff32b910429a2c54919cc79d22603d1
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-03-10 09:19:55 +0100

    Remove unnecessary ESLint comment

--- widgets/common/src/labels/common.js
+++ widgets/common/src/labels/common.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/prefer-default-export
 export function addSearchToUrl(url, search) {
     const { appendQueryParam } = Stage.Utils.Url;
     return search ? appendQueryParam(url, '_search', search) : url;
