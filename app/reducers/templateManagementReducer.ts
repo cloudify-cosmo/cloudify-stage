027e9813ab4bf45ae5b775008c60ea181c626ff3
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-03-10 10:38:55 +0100

    RD-1590 Migrate templateManagementReducer to TS

--- app/reducers/templateManagementReducer.ts
+++ app/reducers/templateManagementReducer.ts
@@ -1,11 +1,14 @@
-/**
- * Created by kinneretzin on 30/08/2016.
- */
-
 import _ from 'lodash';
+import type { Reducer } from 'redux';
 import * as types from '../actions/types';
 
-const templates = (state = {}, action) => {
+export interface TemplateManagementState {
+    showDrillDownWarn?: boolean;
+    isActive?: boolean;
+    isPageEditMode?: boolean;
+}
+
+const templates: Reducer<TemplateManagementState> = (state = {}, action) => {
     switch (action.type) {
         case types.PAGE_MANAGEMENT_DRILLDOWN_WARN:
             return { ...state, showDrillDownWarn: action.show };
