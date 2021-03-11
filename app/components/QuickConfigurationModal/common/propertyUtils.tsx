2a6dc70a622d5b86924f1fbd3037595d6fc5c3fe
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-11 09:21:27 +0100

    RD-1442 - loadash get and set property methods used

--- app/components/QuickConfigurationModal/common/propertyUtils.tsx
+++ app/components/QuickConfigurationModal/common/propertyUtils.tsx
@@ -1,19 +1,9 @@
+import _ from 'lodash';
+
 export const getObjectProperty = (object: any, propertyPath?: string | null) => {
-    if (propertyPath === undefined || propertyPath === null) {
-        return object;
-    }
-    const pathParts = propertyPath.split('.');
-    return pathParts.reduce((currentObject, currentKey) => currentObject?.[currentKey], object);
+    return propertyPath == null ? object : _.get(object, propertyPath);
 };
 
 export const setObjectProperty = (object: any, propertyPath: string, propertyValue: any) => {
-    const pathParts = propertyPath.split('.');
-    const lastIndex = pathParts.length - 1;
-    let currentObject = object;
-    for (let i = 0; i < lastIndex; i += 1) {
-        const currentKey = pathParts[i];
-        currentObject = currentObject[currentKey] ?? (currentObject[currentKey] = {});
-    }
-    const currentKey = pathParts[lastIndex];
-    currentObject[currentKey] = propertyValue;
+    _.set(object, propertyPath, propertyValue);
 };
