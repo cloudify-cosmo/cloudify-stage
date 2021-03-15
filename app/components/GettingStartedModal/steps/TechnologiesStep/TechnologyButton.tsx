84a8a75df5592e4b0392667507fb3231278ce730
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-15 16:44:14 +0100

    RD-1442 - BooleanFieldHandler component, unified button component

--- app/components/GettingStartedModal/steps/TechnologiesStep/TechnologyButton.tsx
+++ app/components/GettingStartedModal/steps/TechnologiesStep/TechnologyButton.tsx
@@ -1,28 +1,5 @@
-import { Form } from 'cloudify-ui-components';
-import React, { ChangeEvent, CSSProperties, memo, useEffect, useRef, useState } from 'react';
-import { Ref as SemanticRef } from 'semantic-ui-react';
-
-import createCheckboxRefExtractor from '../../common/createCheckboxRefExtractor';
-
-const style: CSSProperties = {
-    border: '1px solid silver',
-    borderRadius: '5px',
-    height: '70px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    cursor: 'pointer'
-};
-
-const defaultStyle: CSSProperties = {
-    ...style,
-    background: 'white'
-};
-
-const selectedStyle: CSSProperties = {
-    ...style,
-    background: '#e6e6e6'
-};
+import React, { ChangeEvent, memo } from 'react';
+import BooleanFieldHandler from '../../common/BooleanFieldHandler';
 
 type Props = {
     name: string;
@@ -32,54 +9,28 @@ type Props = {
     onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
-const TechnologyButton = memo(({ name, logo, label, value, ...other }: Props) => {
-    const inputRef = useRef<HTMLInputElement | null>(null);
-    const [localValue, setLocalValue] = useState(value);
-    useEffect(() => setLocalValue(value), [value]);
-    useEffect(() => {
-        const input = inputRef.current;
-        if (input) {
-            const handle = setTimeout(() => {
-                // extracting current input checked property value to set TechnologyButton internal state
-                // it is necessary to do it after component is rendered - setTimeout was used to do it
-                setLocalValue(input.checked);
-            });
-            return () => {
-                clearTimeout(handle);
-            };
-        }
-        return undefined;
-    }, [inputRef.current]);
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const target = e.currentTarget.firstChild as HTMLInputElement;
-        setLocalValue(target.checked);
-        if (other.onChange) {
-            other.onChange(e);
-        }
-    };
-    const handleClick = () => {
-        setLocalValue(!localValue);
-    };
+const TechnologyButton = memo(({ name, logo, label, value, onChange }: Props) => {
+    const { Button, Image } = Stage.Basic;
     return (
-        <Form.Field
-            className={localValue ? 'checked' : ''}
-            style={localValue ? selectedStyle : defaultStyle}
-            onClick={handleClick}
-        >
-            <SemanticRef innerRef={createCheckboxRefExtractor(inputRef)}>
-                <Form.Checkbox
-                    name={name}
-                    label=" "
-                    checked={localValue}
-                    style={{ display: 'none' }}
-                    // eslint-disable-next-line react/jsx-props-no-spreading
-                    {...other}
-                    onChange={handleChange}
-                />
-            </SemanticRef>
-            <img style={{ maxHeight: '80%' }} src={logo} alt={label} />
-            <span style={{ marginLeft: '10px' }}>{label}</span>
-        </Form.Field>
+        <BooleanFieldHandler name={name} value={value} onChange={onChange}>
+            {(checked, handleChange) => {
+                const handleClick = () => handleChange(!checked);
+                return (
+                    <Button
+                        fluid
+                        basic
+                        size="huge"
+                        name={name}
+                        active={checked}
+                        style={{ height: '70px' }}
+                        onClick={handleClick}
+                    >
+                        <Image src={logo} inline style={{ maxHeight: '100%', cursor: 'pointer' }} />
+                        <span style={{ marginLeft: '10px' }}>{label}</span>
+                    </Button>
+                );
+            }}
+        </BooleanFieldHandler>
     );
 });
 
