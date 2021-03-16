256f205194a95d8ade967c62212985a69e863d8d
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-16 11:11:44 +0100

    RD-1442 - i18n for validation, loadash used to do validation

--- app/components/GettingStartedModal/formValidation.ts
+++ app/components/GettingStartedModal/formValidation.ts
@@ -1,23 +1,18 @@
+import i18n from 'i18next';
 import { GettingStartedSecretsData, GettingStartedTechnologiesData } from './model';
-
-const countTrueProperties = (data: Record<string, boolean | undefined>) =>
-    Object.values(data).reduce((count, value) => (value ? count + 1 : count), 0);
-
-const countEmptyProperties = (data: Record<string, string | undefined>) =>
-    Object.values(data).reduce((count, value) => (value ? count : count + 1), 0);
 
 export const validateTechnologyFields = (data: GettingStartedTechnologiesData) => {
     const errors: string[] = [];
-    if (!countTrueProperties(data)) {
-        errors.push('Please select some technology.');
+    if (!_.some(data, item => item === true)) {
+        errors.push(i18n.t('gettingStartedModal.modal.someTechnologyRequiredError', 'Please select some technology.'));
     }
     return errors;
 };
 
 export const validateSecretFields = (data: GettingStartedSecretsData) => {
     const errors: string[] = [];
-    if (countEmptyProperties(data)) {
-        errors.push('Please fill all fields.');
+    if (_.some(data, item => !item)) {
+        errors.push(i18n.t('gettingStartedModal.modal.allSecretsRequiredError', 'Please type all secrets.'));
     }
     return errors;
 };
