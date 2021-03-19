6e7ebae8eff9680b84bccaf30339c5080f1ee29c
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-19 15:46:48 +0100

    RD-1598 - working blueprints installation logic

--- app/components/GettingStartedModal/installation/tasks.ts
+++ app/components/GettingStartedModal/installation/tasks.ts
@@ -213,8 +213,8 @@ export const createBlueprintsInstallationTasks = (selectedTechnologies: GettingS
         selectedTechnology.blueprints.forEach(blueprint => {
             scheduledBlueprints.push({
                 blueprintName: `${blueprint.name}-${formatDate()}`,
-                blueprintUrl: blueprint.mainBlueprint ?? '',
-                applicationName: blueprint.zipUrl
+                blueprintUrl: blueprint.zipUrl,
+                applicationName: blueprint.mainBlueprint ?? ''
             });
         });
     });
@@ -262,13 +262,13 @@ export const useSecretsInstallationTasks = (
 };
 
 export const useBlueprintsInstallationTasks = (selectedTechnologies: GettingStartedSchema) => {
-    // TODO: fetch blueprints
+    // TODO: fetch blueprints and select better names for blueprints
     return useMemo(() => {
-        // if (currentSecrets.loading) {
-        //     return { loading: currentSecrets.loading };
+        // if (currentBlueprints.loading) {
+        //     return { loading: true };
         // }
-        // if (currentSecrets.error) {
-        //     return { loading: false, error: currentSecrets.error };
+        // if (currentBlueprints.error) {
+        //     return { loading: false, error: currentBlueprints.error };
         // }
         return {
             loading: false,
