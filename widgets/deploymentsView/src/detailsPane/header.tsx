aab3ca89cbad23a67c4c5af550268a43707d5444
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-03-22 17:05:06 +0100

    RD-1743 Add deployment action buttons in details pane header

--- widgets/deploymentsView/src/detailsPane/header.tsx
+++ widgets/deploymentsView/src/detailsPane/header.tsx
@@ -1,3 +1,4 @@
+import { noop } from 'lodash';
 import type { FunctionComponent } from 'react';
 
 import './header.scss';
@@ -8,10 +9,30 @@ export interface DetailsPaneHeaderProps {
 
 const DetailsPaneHeader: FunctionComponent<DetailsPaneHeaderProps> = ({ deploymentName }) => {
     const { Header } = Stage.Basic;
+    const { Widget } = Stage.Shared.Widgets;
 
     return (
         <div className="detailsPaneHeader">
             <Header>{deploymentName}</Header>
+            <Widget
+                widget={{
+                    id: 'bf9c126b-9bb7-4ec8-827d-f5dbf2c7e938',
+                    name: 'Deployment Action Buttons',
+                    // NOTE: arbitrary position, as it is not used
+                    x: 0,
+                    y: 0,
+                    width: 1,
+                    height: 1,
+                    definition: 'deploymentActionButtons',
+                    configuration: {},
+                    drillDownPages: {},
+                    maximized: false
+                }}
+                isEditMode={false}
+                onWidgetRemoved={noop}
+                onWidgetUpdated={noop}
+                standalone
+            />
         </div>
     );
 };
