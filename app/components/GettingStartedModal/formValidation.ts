edd3530ac2a11cb17bed0ffd0189f208bc289b60
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-25 12:53:25 +0100

    RD-1442 - changes after RV

--- app/components/GettingStartedModal/formValidation.ts
+++ app/components/GettingStartedModal/formValidation.ts
@@ -3,7 +3,7 @@ import i18n from 'i18next';
 import type { GettingStartedSchemaSecret, GettingStartedSecretsData, GettingStartedTechnologiesData } from './model';
 
 export const validateTechnologyFields = (data: GettingStartedTechnologiesData) => {
-    if (!_.some(data, item => item === true)) {
+    if (!_.some(data)) {
         return i18n.t(
             'gettingStartedModal.modal.someTechnologyRequiredError',
             'At least one technology needs to be selected.'
@@ -13,7 +13,7 @@ export const validateTechnologyFields = (data: GettingStartedTechnologiesData) =
 };
 
 export const validateSecretFields = (schema: GettingStartedSchemaSecret[], data: GettingStartedSecretsData) => {
-    if (_.some(schema, item => !data[item.name])) {
+    if (_.some(schema, ({ name }) => !data[name])) {
         return i18n.t('gettingStartedModal.modal.allSecretsRequiredError', 'All secret values need to be specified.');
     }
     return null;
