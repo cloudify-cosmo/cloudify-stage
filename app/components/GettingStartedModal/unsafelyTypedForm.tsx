9334edd6c6995cceb73e02a47524c251a879b960
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-25 12:31:02 +0100

    RD-1442 - changes after RV

--- app/components/GettingStartedModal/unsafelyTypedForm.tsx
+++ app/components/GettingStartedModal/unsafelyTypedForm.tsx
@@ -2,8 +2,8 @@ import type { FC } from 'react';
 
 import { Form } from '../basic';
 
-// TODO(RD-1837): remove it after after forms will be changed to tsx version
+// TODO(RD-1837): remove it after forms will be changed to tsx version
 export const UnsafelyTypedForm = (Form as unknown) as FC<{ [x: string]: any }>;
 
-// TODO(RD-1837): remove it after after forms will be changed to tsx version
+// TODO(RD-1837): remove it after forms will be changed to tsx version
 export const UnsafelyTypedFormField = (Form.Field as unknown) as FC<{ [x: string]: any }>;
