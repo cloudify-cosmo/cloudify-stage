5951ea442c5a5bd2944a8088f3d28d25a03840be
Jakub Madej <jakub.madej@amartus.com>
2021-03-26 12:21:01 +0100

    Added return type

--- widgets/filters/src/FilterActions.ts
+++ widgets/filters/src/FilterActions.ts
@@ -1,8 +1,10 @@
+import { Filter } from './types';
+
 export default class FilterActions {
     // eslint-disable-next-line no-useless-constructor
     constructor(private toolbox: Stage.Types.Toolbox) {}
 
-    doList(params: unknown) {
+    doList(params: unknown): Promise<Stage.Types.PaginatedResponse<Filter>> {
         return this.toolbox.getManager().doGet(`/filters?_include=id,created_at,created_by`, params);
     }
 }
