932534f0b3a3f9fd34638b6c16d36910970842f9
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-30 13:47:24 +0200

    RD-1442 - secrets with useInputs

--- app/components/GettingStartedModal/steps/SecretsStep.tsx
+++ app/components/GettingStartedModal/steps/SecretsStep.tsx
@@ -1,8 +1,7 @@
-import React, { memo, useEffect, useState } from 'react';
-
-import type { ChangeEvent } from 'react';
+import React, { memo, useEffect } from 'react';
 
 import { Form } from '../../basic';
+import { useInputs } from '../../../utils/hooks';
 import { UnsafelyTypedForm, UnsafelyTypedFormField } from '../unsafelyTypedForm';
 
 import type { GettingStartedSecretsData, GettingStartedSchemaItem } from '../model';
@@ -14,26 +13,25 @@ type Props = {
 };
 
 const SecretsStep = ({ selectedTechnology, typedSecrets, onChange }: Props) => {
-    const [localTypedSecrets, setLocalTypedSecrets] = useState(typedSecrets ?? {});
-    useEffect(() => setLocalTypedSecrets(typedSecrets ?? {}), [typedSecrets]);
+    const [secretInputs, setSecretInputs, resetSecretInputs] = useInputs(typedSecrets ?? {});
+    useEffect(() => resetSecretInputs(), [typedSecrets]);
     return (
         // TODO(RD-1837): change to <Form> after forms will be changed to tsx version
         <UnsafelyTypedForm>
             {selectedTechnology.secrets.map(({ name, label, type }) => {
-                const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-                    setLocalTypedSecrets({ ...localTypedSecrets, [name]: e.target.value });
-                };
                 const handleBlur = () => {
-                    onChange?.(localTypedSecrets);
+                    // onChange?.(localTypedSecrets);
+                    onChange?.(secretInputs);
                 };
                 return (
                     // TODO(RD-1837): change to <Form.Field key={name}> after forms will be changed to tsx version
                     <UnsafelyTypedFormField key={name}>
                         <Form.Input
                             type={type}
+                            name={name}
                             label={label}
-                            value={localTypedSecrets[name] ?? ''}
-                            onChange={handleChange}
+                            value={secretInputs[name] ?? ''}
+                            onChange={setSecretInputs}
                             onBlur={handleBlur}
                         />
                         {/* TODO(RD-1837): change to </Form.Field> after forms will be changed to tsx version  */}
