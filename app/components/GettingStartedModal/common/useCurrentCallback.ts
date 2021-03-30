3727b7a3753fb729b83bf01ff2f76ab011e0df0f
gregcfy <78533022+gregcfy@users.noreply.github.com>
2021-03-30 15:16:43 +0200

    Update app/components/GettingStartedModal/common/useCurrentCallback.ts
    Co-authored-by: Jakub Niezgoda <jakub.niezgoda@nitarch.pl>

--- app/components/GettingStartedModal/common/useCurrentCallback.ts
+++ app/components/GettingStartedModal/common/useCurrentCallback.ts
@@ -1,16 +1,16 @@
 import { useRef } from 'react';
 
 /**
- * Provides hook that lets to call last provided callback.
- * That hook is useful when we want to send some function into hook that was called in previous rendering.
+ * Provides a hook that lets you to call the last provided callback.
+ * That hook is useful when you want to send into hook a function that was called in the previous rendering.
  * e.g.
  * // let's suppose Component is re-rendered many times
  * const Component = ({ onProgress }) => {
- *     const handleProgress = useCurrentCallback(onProgress); // always updates callback inside when re-rendering occurs
+ *     const handleProgress = useCurrentCallback(onProgress); // always updates the callback inside when re-rendering occurs
  *     // useEffect is called only once when Component was mounted
  *     useEffect(() => {
  *         // some logic here ...
- *         handleProgress(); // calling it we have access to last provided onProgress function
+ *         handleProgress(); // calling it we have an access to the last provided onProgress function
  *     }, []);
  * };
  *
