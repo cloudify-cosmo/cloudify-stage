54d158442d56de19b034dde52edeb9ff3c5230f3
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-03-30 13:09:02 +0200

    RD-1442 - useInputs moved to app/utils/hooks

--- app/utils/hooks/index.ts
+++ app/utils/hooks/index.ts
@@ -3,5 +3,6 @@ import useErrors from './useErrors';
 import useResettableState from './useResettableState';
 import useToggle from './useToggle';
 import useInput from './useInput';
+import useInputs from './useInputs';
 
-export { useBoolean, useErrors, useResettableState, useToggle, useInput };
+export { useBoolean, useErrors, useResettableState, useToggle, useInput, useInputs };
