4fc93a1037fb833b2fafafec206921a52eed4032
Jakub Madej <jakub.madej@amartus.com>
2021-04-06 11:34:56 +0200

    Code review follow up

--- backend/test/routes/Filters.test.js
+++ backend/test/routes/Filters.test.js
@@ -3,56 +3,56 @@ const mockDb = require('../mocks/mockDb');
 require('../mocks/passport');
 
 describe('/filters endpoint', () => {
-    it('allows to get filter usage data', () => {
-        const filterId = 'filterId';
-        const pageName = 'Page1';
-        const widgetName = 'Deployments View 1';
-        const tabWidgetName = 'Deployments View 2';
-        const username = 'test_user';
+    const filterId = 'filterId';
+    const pageName = 'Page1';
+    const widgetName = 'Deployments View 1';
+    const tabWidgetName = 'Deployments View 2';
+    const username = 'test_user';
 
-        function createWidgets(deploymentsViewWidgetName) {
-            return [
-                {
-                    definition: 'deploymentsView',
-                    name: deploymentsViewWidgetName,
-                    configuration: { filterId }
-                }
-            ];
-        }
-
-        mockDb({
-            UserApp: {
-                findAll: () =>
-                    Promise.resolve([
-                        {
-                            appData: {
-                                pages: [
-                                    {
-                                        name: pageName,
-                                        layout: [
-                                            {
-                                                type: 'widgets',
-                                                content: createWidgets(widgetName)
-                                            },
-                                            {
-                                                type: 'tabs',
-                                                content: [
-                                                    {
-                                                        widgets: createWidgets(tabWidgetName)
-                                                    }
-                                                ]
-                                            }
-                                        ]
-                                    }
-                                ]
-                            },
-                            username
-                        }
-                    ])
+    function createWidgets(deploymentsViewWidgetName) {
+        return [
+            {
+                definition: 'deploymentsView',
+                name: deploymentsViewWidgetName,
+                configuration: { filterId }
             }
-        });
-        const app = require('app');
+        ];
+    }
 
+    mockDb({
+        UserApp: {
+            findAll: () =>
+                Promise.resolve([
+                    {
+                        appData: {
+                            pages: [
+                                {
+                                    name: pageName,
+                                    layout: [
+                                        {
+                                            type: 'widgets',
+                                            content: createWidgets(widgetName)
+                                        },
+                                        {
+                                            type: 'tabs',
+                                            content: [
+                                                {
+                                                    widgets: createWidgets(tabWidgetName)
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        username
+                    }
+                ])
+        }
+    });
+    const app = require('app');
+
+    it('returns filter usage data', () => {
         return request(app)
             .get(`/console/filters/usage/${filterId}`)
             .then(response => {
@@ -61,6 +61,15 @@ describe('/filters endpoint', () => {
                     { pageName, widgetName, username },
                     { pageName, widgetName: tabWidgetName, username }
                 ]);
+            });
+    });
+
+    it('returns empty array for unused filter', () => {
+        return request(app)
+            .get(`/console/filters/usage/unused_filter`)
+            .then(response => {
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toEqual([]);
             });
     });
 });
