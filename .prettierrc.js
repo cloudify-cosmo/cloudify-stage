ba274a2ab314cb0d05cd05c78ffbb584ba1dfdef
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-04-08 13:19:36 +0200

    Revert "RD-2001 Add prettier override for JSON files to use 2 spaces"
    This reverts commit a7772a98ac9d855881711e576929273e5ed344e6.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,1 @@
-module.exports = {
-    ...require('./node_modules/cloudify-ui-common/configs/prettier-common.json'),
-    overrides: [
-        {
-            files: '*.json',
-            options: {
-                tabWidth: 2
-            }
-        }
-    ]
-};
+module.exports = require('./node_modules/cloudify-ui-common/configs/prettier-common.json');
