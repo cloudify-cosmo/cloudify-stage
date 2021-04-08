5b2aae97bd59d25885f05871fdc8b87da1f42b7c
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-04-08 14:57:54 +0200

    Add common types

--- widgets/common/src/filters/types.ts
+++ widgets/common/src/filters/types.ts
@@ -2,6 +2,43 @@
 export interface FilterRule {
     key: string;
     values: string[];
-    operator: string;
+    operator: RuleOperator;
     type: 'label' | 'attribute';
 }
+
+export interface FilterRuleRow extends FilterRule {
+    id: string;
+}
+
+export enum RuleType {
+    Label = 'label',
+    Blueprint = 'blueprint_id',
+    SiteName = 'site_name',
+    Creator = 'created_by'
+}
+
+export enum CommonRuleOperator {
+    AnyOf = 'any_of',
+    NotAnyOf = 'not_any_of'
+}
+
+export enum LabelsOnlyRuleOperator {
+    IsNull = 'is_null',
+    IsNotNull = 'is_not_null'
+}
+
+enum AttributesOnlyRuleOperator {
+    Contain = 'contain',
+    NotContain = 'not_contain',
+    StartsWith = 'starts_with',
+    EndWith = 'end_with'
+}
+
+export const RuleOperator = { ...CommonRuleOperator, ...LabelsOnlyRuleOperator, ...AttributesOnlyRuleOperator };
+export type RuleOperator = CommonRuleOperator | LabelsOnlyRuleOperator | AttributesOnlyRuleOperator;
+
+export const LabelsRuleOperator = { ...CommonRuleOperator, ...LabelsOnlyRuleOperator };
+export type LabelsRuleOperator = CommonRuleOperator | LabelsOnlyRuleOperator;
+
+export const AttributesRuleOperator = { ...CommonRuleOperator, ...AttributesOnlyRuleOperator };
+export type AttributesRuleOperator = CommonRuleOperator | AttributesOnlyRuleOperator;
