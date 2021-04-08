2be9b1c76f778f81567e064efee4938afe81a4ae
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-04-08 13:00:29 +0200

    RD-2001 Extract translation strings for the drill-down functionality

--- widgets/deploymentsView/src/detailsPane/DrilldownButtons.tsx
+++ widgets/deploymentsView/src/detailsPane/DrilldownButtons.tsx
@@ -1,7 +1,7 @@
 import type { FunctionComponent } from 'react';
 import styled from 'styled-components';
 
-import { subenvironmentsIcon, subservicesIcon } from '../common';
+import { i18nPrefix, subenvironmentsIcon, subservicesIcon } from '../common';
 import type { Deployment } from '../types';
 
 export interface DrilldownButtonsProps {
@@ -15,8 +15,11 @@ const ButtonsContainer = styled.div`
     margin: 0 1em;
 `;
 
+const i18nDrillDownPrefix = `${i18nPrefix}.drillDown`;
+
 const DrilldownButtons: FunctionComponent<DrilldownButtonsProps> = ({ drillDown, deployment }) => {
     const { Button, Icon } = Stage.Basic;
+    const { i18n } = Stage;
     const {
         id: deploymentName,
         // TODO(RD-2003): fetch the number of only the immediate children
@@ -24,15 +27,21 @@ const DrilldownButtons: FunctionComponent<DrilldownButtonsProps> = ({ drillDown,
         sub_environments_count: subenvironmentsCount
     } = deployment;
 
-    // TODO: use i18n for all texts
-
     const drilldownToSubenvironments = () => {
-        // TODO(RD-2004): add filter rules in context to only show environments
-        drillDown(subdeploymentsDrilldownTemplateName, {}, `${deploymentName} [Environments]`);
+        drillDown(
+            subdeploymentsDrilldownTemplateName,
+            // TODO(RD-2004): add filter rules in context to only show environments
+            {},
+            `${deploymentName} [${i18n.t(`${i18nDrillDownPrefix}.breadcrumbs.environments`)}]`
+        );
     };
     const drilldownToSubservices = () => {
-        // TODO(RD-2004): add filter rules in context to only show services
-        drillDown(subdeploymentsDrilldownTemplateName, {}, `${deploymentName} [Services]`);
+        drillDown(
+            subdeploymentsDrilldownTemplateName,
+            // TODO(RD-2004): add filter rules in context to only show services
+            {},
+            `${deploymentName} [${i18n.t(`${i18nDrillDownPrefix}.breadcrumbs.services`)}]`
+        );
     };
 
     return (
@@ -42,20 +51,22 @@ const DrilldownButtons: FunctionComponent<DrilldownButtonsProps> = ({ drillDown,
                 color="blue"
                 onClick={drilldownToSubenvironments}
                 disabled={subenvironmentsCount === 0}
-                title="Drill down to subenvironments"
+                title={i18n.t(`${i18nDrillDownPrefix}.buttons.subenvironments.title`)}
             >
                 <Icon name={subenvironmentsIcon} />
-                Subenvironments ({subenvironmentsCount}){/* TODO(RD-2005): add icons depending on children state */}
+                {i18n.t(`${i18nDrillDownPrefix}.buttons.subenvironments.label`)} ({subenvironmentsCount})
+                {/* TODO(RD-2005): add icons depending on children state */}
             </Button>
             <Button
                 basic
                 color="blue"
                 onClick={drilldownToSubservices}
                 disabled={subservicesCount === 0}
-                title="Drill down to subservices"
+                title={i18n.t(`${i18nDrillDownPrefix}.buttons.subservices.title`)}
             >
                 <Icon name={subservicesIcon} />
-                Services ({subservicesCount}){/* TODO(RD-2005): add icons depending on children state */}
+                {i18n.t(`${i18nDrillDownPrefix}.buttons.subservices.label`)} ({subservicesCount})
+                {/* TODO(RD-2005): add icons depending on children state */}
             </Button>
         </ButtonsContainer>
     );
