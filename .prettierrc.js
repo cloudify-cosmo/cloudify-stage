4b001409d224f69743e85a2400ce164677bd6dd2
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-04-08 10:41:47 +0200

    RD-2001 Add prettier override for JSON files to use 2 spaces

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,11 @@
-module.exports = require('./node_modules/cloudify-ui-common/configs/prettier-common.json');
+module.exports = {
+    ...require('./node_modules/cloudify-ui-common/configs/prettier-common.json'),
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+};
