3d8016fb9a3daa1c24ac3fb1f0d192cde9ddaddb
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-04-12 13:01:29 +0200

    Fix TS errors coming from DynamicDropdown

--- widgets/common/src/filters/inputs/AttributeValueInput.tsx
+++ widgets/common/src/filters/inputs/AttributeValueInput.tsx
@@ -26,20 +26,20 @@ const AttributeValueInput: FunctionComponent<AttributeValueInputProps> = ({
     if (operator === LabelsFilterRuleOperators.AnyOf || operator === LabelsFilterRuleOperators.NotAnyOf) {
         return (
             <DynamicDropdown
-                clearable={false}
-                multiple
                 fetchUrl={fetchUrl}
-                placeholder={placeholder}
-                valueProp={valueProp}
                 onChange={onChange}
                 toolbox={toolbox}
+                clearable={false}
+                multiple
+                placeholder={placeholder}
                 value={value}
+                valueProp={valueProp}
             />
         );
     }
 
     // TODO(RD-1762): Add support for type 'attribute' and operators different from 'any_of' and 'not_any_of'
-    const textValue = _.castArray(value)[0] || ''; // TODO
+    const textValue = _.castArray(value)[0] || '';
     return <Input type="text" onChange={(_event, { value: newValue }) => onChange([newValue])} value={textValue} />;
 };
 
