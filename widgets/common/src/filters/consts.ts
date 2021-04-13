ac8a8d450dde01cd2d1c23c4105a2d293d12848a
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-04-13 09:49:52 +0200

    Reused i18nPrefix const

--- widgets/common/src/filters/consts.ts
+++ widgets/common/src/filters/consts.ts
@@ -1,3 +1,3 @@
 export const { i18n } = Stage;
 export const i18nPrefix = 'widgets.common.filters.form';
-export const i18nPlaceholdersPrefix = 'widgets.common.filters.form.inputsPlaceholders';
+export const i18nPlaceholdersPrefix = `${i18nPrefix}.inputsPlaceholders`;
