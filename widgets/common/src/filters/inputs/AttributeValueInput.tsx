05f0d1d12086c78ab576676f28f4b47d63f8785e
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-04-14 11:22:57 +0200

    Use MultipleStringValuesInput component in AttributeValueInput

--- widgets/common/src/filters/inputs/AttributeValueInput.tsx
+++ widgets/common/src/filters/inputs/AttributeValueInput.tsx
@@ -1,9 +1,9 @@
-import { castArray } from 'lodash';
 import type { FunctionComponent } from 'react';
 
 import DynamicDropdown from '../../DynamicDropdown';
 import type { CommonAttributeValueInputProps } from './types';
 import { isAnyOfOrNotAnyOfOperator } from './common';
+import MultipleStringValuesInput from './MultipleStringValuesInput';
 
 interface AttributeValueInputProps extends CommonAttributeValueInputProps {
     fetchUrl: string;
@@ -35,9 +35,7 @@ const AttributeValueInput: FunctionComponent<AttributeValueInputProps> = ({
         );
     }
 
-    // TODO(RD-1762): Add support for type 'attribute' and operators different from 'any_of' and 'not_any_of'
-    const textValue = castArray(value)[0] || '';
-    return <Input type="text" onChange={(_event, { value: newValue }) => onChange([newValue])} value={textValue} />;
+    return <MultipleStringValuesInput value={value} onChange={onChange} />;
 };
 
 export default AttributeValueInput;
