e4e3d9c29185183dd375b824f0fe10948dc49067
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-04-16 09:34:06 +0200

    RD-1824 Import castArray from lodash using ES modules

--- widgets/deploymentActionButtons/src/widget.tsx
+++ widgets/deploymentActionButtons/src/widget.tsx
@@ -1,3 +1,4 @@
+import { castArray } from 'lodash';
 import type { ComponentProps } from 'react';
 import DeploymentActionButtons from './DeploymentActionButtons';
 
@@ -46,7 +47,7 @@ Stage.defineWidget<WidgetParams, WidgetData, WidgetConfiguration>({
     fetchParams(_widget, toolbox): WidgetParams {
         const deploymentId = toolbox.getContext().getValue('deploymentId');
         // Deployment Actions Buttons widget does not support multiple actions, thus picking only one deploymentId
-        const firstDeploymentId = _.castArray(deploymentId)[0] as WidgetParams['id'];
+        const firstDeploymentId = castArray(deploymentId)[0] as WidgetParams['id'];
 
         return { id: firstDeploymentId };
     },
