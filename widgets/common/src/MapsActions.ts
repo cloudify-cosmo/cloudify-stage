ae488a4684cf12f138b12f002978ba598e717020
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-04-21 12:57:18 +0200

    RD-2090 Migrate MapsActions to TypeScript

---
+++ widgets/common/src/MapsActions.ts
@@ -0,0 +1,29 @@
+import { template } from 'lodash';
+
+class MapsActions {
+    constructor(private toolbox: Stage.Types.Toolbox) {}
+
+    public isAvailable() {
+        const { urlTemplate } = Stage.Common.Consts.leaflet;
+        const testUrl = template(urlTemplate, { interpolate: /{([\s\S]+?)}/g })({ x: 0, y: 0, z: 0, r: '' });
+
+        const noCache = new Date().getTime();
+        return this.toolbox
+            .getInternal()
+            .doGet(testUrl, { noCache })
+            .then(() => true)
+            .catch(() => false);
+    }
+}
+
+declare global {
+    namespace Stage.Common {
+        /* eslint-disable-next-line import/prefer-default-export */
+        export { MapsActions };
+    }
+}
+
+Stage.defineCommon({
+    name: 'MapsActions',
+    common: MapsActions
+});
