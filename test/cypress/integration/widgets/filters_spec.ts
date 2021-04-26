439fb30cc1fe68694eb380550179d7ac69b18b46
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-04-26 14:06:25 +0200

    Add tests for non-label attributes

--- test/cypress/integration/widgets/filters_spec.ts
+++ test/cypress/integration/widgets/filters_spec.ts
@@ -5,7 +5,21 @@ import {
     FilterRuleRowType,
     FilterRuleType
 } from '../../../../widgets/common/src/filters/types';
-import { isAnyOperator } from '../../../../widgets/common/src/filters/inputs/common';
+
+// TODO: Temporary solution. We should import isAnyOperator from '../../../../widgets/common/src/filters/common'
+function isAnyOperator(operator: FilterRuleOperator) {
+    const anyOperators: FilterRuleOperator[] = [FilterRuleOperators.AnyOf, FilterRuleOperators.NotAnyOf];
+    return anyOperators.includes(operator);
+}
+function isFreeTextOperator(operator: FilterRuleOperator) {
+    const freeTextOperator: FilterRuleOperator[] = [
+        FilterRuleOperators.Contains,
+        FilterRuleOperators.NotContains,
+        FilterRuleOperators.StartsWith,
+        FilterRuleOperators.EndsWith
+    ];
+    return freeTextOperator.includes(operator);
+}
 
 describe('Filters widget', () => {
     before(() => {
@@ -251,12 +265,13 @@ describe('Filters widget', () => {
     });
 
     describe('should allow to define filter rules', () => {
-        before(() => {
-            const blueprintId = 'filters_test_form_blueprint';
-            const deploymentId = 'filters_test_form_deployment';
+        const testPrefix = 'filters_test_form';
+        const blueprintId = `${testPrefix}_blueprint`;
+        const deploymentId = `${testPrefix}_deployment`;
 
-            cy.deleteDeployments(deploymentId)
-                .deleteBlueprints(blueprintId)
+        before(() => {
+            cy.deleteDeployments(testPrefix)
+                .deleteBlueprints(testPrefix)
                 .uploadBlueprint('blueprints/empty.zip', blueprintId)
                 .deployBlueprint(blueprintId, deploymentId)
                 .setLabels(deploymentId, [
@@ -265,6 +280,12 @@ describe('Filters widget', () => {
                     { infra: 'aws' },
                     { infra: 'gcp' },
                     { infra: 'azure' }
+                ])
+                .deleteSites(testPrefix)
+                .createSites([
+                    { name: `${testPrefix}_Cracow` },
+                    { name: `${testPrefix}_Warsaw` },
+                    { name: `${testPrefix}_TelAviv` }
                 ]);
         });
 
@@ -278,37 +299,76 @@ describe('Filters widget', () => {
                 cy.get(`div[option-value="${ruleRowType}"]`).click();
             });
         }
+
         function selectRuleOperator(operator: FilterRuleOperator) {
             withinTheLastRuleRow(() => {
                 cy.get('div[name="ruleOperator"]').click();
                 cy.get(`div[option-value="${operator}"]`).click();
             });
         }
-        function selectRuleAttributeValues(values: string[]) {
+
+        function selectRuleAttributeValues(
+            ruleRowType: FilterRuleRowType,
+            values: string[],
+            newValues = [] as string[],
+            withAutocomplete = false
+        ) {
             withinTheLastRuleRow(() => {
+                const endpoint = ((rowType: FilterRuleRowType) => {
+                    switch (rowType) {
+                        case 'blueprint_id':
+                            return 'blueprints';
+                        case 'site_name':
+                            return 'sites';
+                        case 'created_by':
+                            return 'users';
+                        default:
+                            throw new Error('Unknown rule row type.');
+                    }
+                })(ruleRowType);
                 cy.get('div[name="ruleValue"]').click();
                 values.forEach(value => {
-                    cy.get('div[name="ruleValue"] input').type(`${value}{enter}`);
-                });
-            });
-        }
-        function selectRuleLabelKey(value: string) {
-            withinTheLastRuleRow(() => {
-                cy.get('div[name="labelKey"] input').type(value);
-                cy.get(`div[name="labelKey"] div[option-value="${value}"]`).click();
-            });
-        }
-        function selectRuleLabelValues(values: string[]) {
-            withinTheLastRuleRow(() => {
-                cy.get('div[name="labelValue"]').click();
-                values.forEach(value => {
-                    cy.get('div[name="labelValue"] input').type(`${value}`);
-                    cy.get(`div[option-value="${value}"]`).click();
+                    if (withAutocomplete)
+                        cy.interceptSp('GET', RegExp(`${endpoint}.*_search=${value}`)).as(`value_${value}_Search`);
+                    cy.get('div[name="ruleValue"] input').type(`${value}`);
+                    if (withAutocomplete) cy.wait(`@value_${value}_Search`);
+                    if (newValues.includes(value)) cy.get('[data-additional="true"]').click();
+                    else cy.get(`div[name="ruleValue"] div[option-value="${value}"]`).click();
+                    cy.get(`.label[value="${value}"]`).should('exist');
                 });
             });
         }
 
-        function populateFilterRuleRow(rule: FilterRule) {
+        function selectRuleLabelKey(key: string, newKey = false) {
+            withinTheLastRuleRow(() => {
+                cy.interceptSp('GET', `/labels/deployments?_search=${key}`).as('keySearch');
+                cy.get('div[name="labelKey"] input').type(key);
+                cy.wait('@keySearch');
+                if (newKey) cy.get('[data-additional="true"]').click();
+                else cy.get(`div[name="labelKey"] div[option-value="${key}"]`).click();
+                cy.get(`input.search`).should('not.have.value');
+            });
+        }
+
+        function selectRuleLabelValues(values: string[], newValues = [] as string[]) {
+            if (values.length > 0) {
+                withinTheLastRuleRow(() => {
+                    cy.get('div[name="labelValue"]').click();
+                    values.forEach(value => {
+                        cy.interceptSp('GET', RegExp(`/labels/deployments/.*?_search=${value}`)).as(
+                            `value_${value}_Search`
+                        );
+                        cy.get('div[name="labelValue"] input').type(`${value}`);
+                        cy.wait(`@value_${value}_Search`);
+                        if (newValues.includes(value)) cy.get('[data-additional="true"]').click();
+                        else cy.get(`div[name="labelValue"] div[option-value="${value}"]`).click();
+                        cy.get(`.label[value="${value}"]`).should('exist');
+                    });
+                });
+            }
+        }
+
+        function populateFilterRuleRow(rule: ExtendedFilterRule) {
             const ruleRowType =
                 rule.type === FilterRuleType.Label ? FilterRuleRowType.Label : (rule.key as FilterRuleRowType);
 
@@ -316,17 +376,17 @@ describe('Filters widget', () => {
             selectRuleOperator(rule.operator);
             if (ruleRowType === FilterRuleRowType.Label) {
                 if (isAnyOperator(rule.operator)) {
-                    selectRuleLabelKey(rule.key);
-                    selectRuleLabelValues(rule.values);
+                    selectRuleLabelKey(rule.key, rule.newKey);
+                    selectRuleLabelValues(rule.values, rule.newValues);
                 } else {
-                    selectRuleLabelKey(rule.key);
+                    selectRuleLabelKey(rule.key, rule.newKey);
                 }
             } else {
-                selectRuleAttributeValues(rule.values);
+                selectRuleAttributeValues(ruleRowType, rule.values, rule.newValues, !isFreeTextOperator(rule.operator));
             }
         }
 
-        function populateFilterRuleRows(filterId: string, rules: FilterRule[]) {
+        function populateFilterRuleRows(filterId: string, rules: ExtendedFilterRule[]) {
             cy.contains('Add').click();
             cy.get('.modal').within(() => {
                 cy.contains('.field', 'Filter ID').find('input').type(filterId);
@@ -337,22 +397,26 @@ describe('Filters widget', () => {
             });
         }
 
-        function saveAndVerifyFilter(filterId: string, testRules: FilterRule[]) {
+        function saveAndVerifyFilter(filterId: string, testRules: ExtendedFilterRule[]) {
             cy.interceptSp('PUT', `/filters/deployments/${filterId}`).as('createRequest');
             cy.contains('Save').click();
             cy.wait('@createRequest').then(({ request }) => {
                 const requestRules = request.body.filter_rules;
                 expect(requestRules).to.have.length(testRules.length);
                 testRules.forEach((_rule, index: number) => {
-                    expect(requestRules[index]).to.deep.equal(testRules[index]);
+                    expect(requestRules[index]).to.deep.equal(_.omit(testRules[index], ['newKey', 'newValues']));
                 });
             });
         }
 
+        interface ExtendedFilterRule extends FilterRule {
+            newKey?: boolean;
+            newValues?: string[];
+        }
         type RuleRowTest = {
             name: string;
             testFilterName: string;
-            testFilterRules: FilterRule[];
+            testFilterRules: ExtendedFilterRule[];
         };
 
         const ruleRowTests: RuleRowTest[] = [
@@ -363,15 +427,18 @@ describe('Filters widget', () => {
                     {
                         type: FilterRuleType.Label,
                         key: 'os',
-                        values: ['windows', 'linux'],
-                        operator: FilterRuleOperators.AnyOf
+                        values: ['windows', 'linux', 'mac'],
+                        operator: FilterRuleOperators.AnyOf,
+                        newValues: ['mac']
                     },
 
                     {
                         type: FilterRuleType.Label,
-                        key: 'infra',
-                        values: ['aws'],
-                        operator: FilterRuleOperators.NotAnyOf
+                        key: 'edition',
+                        values: ['silver'],
+                        operator: FilterRuleOperators.NotAnyOf,
+                        newKey: true,
+                        newValues: ['silver']
                     }
                 ]
             },
@@ -397,12 +464,81 @@ describe('Filters widget', () => {
             {
                 name: 'of type "attribute" with operators "any_of" and "not_any_of"',
                 testFilterName: `${filterName}_attribute_1`,
-                testFilterRules: [] // TODO: Add rules
+                testFilterRules: [
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'blueprint_id',
+                        values: [blueprintId, `${testPrefix}_new`],
+                        operator: FilterRuleOperators.AnyOf,
+                        newValues: [`${testPrefix}_new`]
+                    },
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'blueprint_id',
+                        values: [blueprintId],
+                        operator: FilterRuleOperators.NotAnyOf
+                    },
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'site_name',
+                        values: [`${testPrefix}_Warsaw`, `${testPrefix}_Cracow`],
+                        operator: FilterRuleOperators.AnyOf
+                    },
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'site_name',
+                        values: [`${testPrefix}_TelAviv`, `${testPrefix}_Paris`],
+                        operator: FilterRuleOperators.NotAnyOf,
+                        newValues: [`${testPrefix}_Paris`]
+                    },
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'created_by',
+                        values: [`${testPrefix}_user`],
+                        operator: FilterRuleOperators.AnyOf,
+                        newValues: [`${testPrefix}_user`]
+                    },
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'created_by',
+                        values: ['admin'],
+                        operator: FilterRuleOperators.NotAnyOf
+                    }
+                ]
             },
             {
                 name: 'of type "attribute" with operators "contains", "not_contains", "starts_with" and "ends_with"',
                 testFilterName: `${filterName}_attribute_2`,
-                testFilterRules: [] // TODO: Add rules
+                testFilterRules: [
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'blueprint_id',
+                        values: ['blu', 'test'],
+                        operator: FilterRuleOperators.Contains,
+                        newValues: ['blu', 'test']
+                    },
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'site_name',
+                        values: ['site', 'test'],
+                        operator: FilterRuleOperators.NotContains,
+                        newValues: ['site', 'test']
+                    },
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'created_by',
+                        values: ['user', 'test'],
+                        operator: FilterRuleOperators.StartsWith,
+                        newValues: ['user', 'test']
+                    },
+                    {
+                        type: FilterRuleType.Attribute,
+                        key: 'created_by',
+                        values: ['operator'],
+                        operator: FilterRuleOperators.EndsWith,
+                        newValues: ['operator']
+                    }
+                ]
             }
         ];
 
