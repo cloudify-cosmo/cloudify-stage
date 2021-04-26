4f40ac1a991409a1d2bb8dfd2869f57d63efdc16
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-04-26 11:33:23 +0200

    RD-2090 Extract NoDataMessage strings to en.json

--- widgets/common/src/NoDataMessage.tsx
+++ widgets/common/src/NoDataMessage.tsx
@@ -8,16 +8,17 @@ type NoDataMessageProps =
       }
     | { children?: never; error?: { name: string }; repositoryName: string };
 
+const translate = (suffix: string, params?: Record<string, any>) =>
+    Stage.i18n.t(`widgets.common.noDataMessage.${suffix}`, params);
+
 const NoDataMessage: FunctionComponent<NoDataMessageProps> = ({ children, error, repositoryName }) => {
-    const { Message } = Stage.Basic;
-    const { MessageContainer } = Stage.Basic;
+    const { Message, MessageContainer } = Stage.Basic;
+
     return (
         <MessageContainer wide margin="30px auto">
             <Message>
                 {children ||
-                    (((error as Record<string, any>) || {}).name === 'SyntaxError'
-                        ? `The widget content cannot be displayed because configured URL does not point to a valid ${repositoryName} repository JSON data. Please check widget's configuration.`
-                        : `The widget content cannot be displayed because there is no connection to ${repositoryName} repository. Please check network connection and widget's configuration.`)}
+                    translate(error?.name === 'SyntaxError' ? 'invalidUrl' : 'noConnection', { repositoryName })}
             </Message>
         </MessageContainer>
     );
