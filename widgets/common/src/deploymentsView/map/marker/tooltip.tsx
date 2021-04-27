5411739bbdd75455ce4cf50d7d43e50ff275a8b2
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-04-27 12:09:25 +0200

    RD-1526 Show subdeployment counts and statuses in tooltip

--- widgets/common/src/deploymentsView/map/marker/tooltip.tsx
+++ widgets/common/src/deploymentsView/map/marker/tooltip.tsx
@@ -1,13 +1,34 @@
 import { camelCase } from 'lodash';
 import type { FunctionComponent } from 'react';
+import styled from 'styled-components';
 
 import type { Deployment } from '../../types';
 import { mapT } from '../common';
-import { DeploymentStatusIcon } from '../../StatusIcon';
+import { DeploymentStatusIcon, SubdeploymentStatusIcon } from '../../StatusIcon';
+import { subenvironmentsIcon, subservicesIcon } from '../../common';
 
 const tooltipStatusT = (suffix: string) => mapT(`tooltip.status.${suffix}`);
 const markerIconHeight = 41;
 const tooltipOffset = L.point(0, -markerIconHeight);
+
+const TooltipContentContainer = styled.div`
+    display: flex;
+`;
+
+const StatusesContainer = styled.div`
+    margin-left: 1em;
+    // NOTE: 7px = leaflet's tooltip padding&border
+    padding: calc(1em - 7px);
+    padding-left: 1em;
+    border-left: 1px solid;
+    display: flex;
+    flex-direction: column;
+    align-items: space-around;
+    justify-content: center;
+`;
+const StatusContainer = styled.div`
+    margin: 0.5em 0;
+`;
 
 const DeploymentSiteTooltip: FunctionComponent<{ deployment: Deployment; environmentTypeVisible: boolean }> = ({
     deployment,
@@ -15,19 +36,34 @@ const DeploymentSiteTooltip: FunctionComponent<{ deployment: Deployment; environ
 }) => {
     const {
         Leaflet: { Tooltip },
-        Header
+        Header,
+        Icon
     } = Stage.Basic;
 
     return (
         <Tooltip direction="top" offset={tooltipOffset}>
-            <Header as="h4">{deployment.id}</Header>
-            <div>{deployment.blueprint_id}</div>
-            <div>{deployment.site_name}</div>
-            {environmentTypeVisible && <div>{deployment.environment_type}</div>}
-            <div>
-                <DeploymentStatusIcon status={deployment.deployment_status} />
-                {tooltipStatusT(camelCase(deployment.deployment_status))}
-            </div>
+            <TooltipContentContainer>
+                <div>
+                    <Header as="h4">{deployment.id}</Header>
+                    <div>{deployment.blueprint_id}</div>
+                    <div>{deployment.site_name}</div>
+                    {environmentTypeVisible && <div>{deployment.environment_type}</div>}
+                    <div>
+                        <DeploymentStatusIcon status={deployment.deployment_status} />
+                        {tooltipStatusT(camelCase(deployment.deployment_status))}
+                    </div>
+                </div>
+                <StatusesContainer>
+                    <StatusContainer>
+                        {deployment.sub_environments_count} <Icon name={subenvironmentsIcon} />
+                        <SubdeploymentStatusIcon status={deployment.sub_environments_status} />
+                    </StatusContainer>
+                    <StatusContainer>
+                        {deployment.sub_services_count} <Icon name={subservicesIcon} />
+                        <SubdeploymentStatusIcon status={deployment.sub_services_status} />
+                    </StatusContainer>
+                </StatusesContainer>
+            </TooltipContentContainer>
         </Tooltip>
     );
 };
