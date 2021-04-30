15e6953ded1fe16f586c730dc2589c5da2fbfc9d
Jakub Madej <jakub.madej@amartus.com>
2021-04-30 10:47:18 +0200

    Fix for introduced bug

--- widgets/common/src/ExecuteDeploymentModal.jsx
+++ widgets/common/src/ExecuteDeploymentModal.jsx
@@ -149,17 +149,23 @@ export default function ExecuteDeploymentModal({
         const actions = new DeploymentActions(toolbox);
 
         const executePromises = _.map(deploymentsList, id => {
-            const scheduled = schedule ? moment(scheduledTime).format('YYYYMMDDHHmmZ') : undefined;
-            return actions.doExecute(id, name, workflowParameters, { force, dryRun, queue, scheduled }).then(() => {
-                // State updates should be done before calling `onHide` to avoid React errors:
-                // "Warning: Can't perform a React state update on an unmounted component"
-                unsetLoading();
-                clearErrors();
-                onHide();
-                toolbox.getEventBus().trigger('executions:refresh');
-                // NOTE: pass id to keep the current deployment selected
-                toolbox.getEventBus().trigger('deployments:refresh', id);
-            });
+            return actions
+                .doExecute(id, name, workflowParameters, {
+                    force,
+                    dryRun,
+                    queue,
+                    scheduledTime: schedule ? moment(scheduledTime).format('YYYYMMDDHHmmZ') : undefined
+                })
+                .then(() => {
+                    // State updates should be done before calling `onHide` to avoid React errors:
+                    // "Warning: Can't perform a React state update on an unmounted component"
+                    unsetLoading();
+                    clearErrors();
+                    onHide();
+                    toolbox.getEventBus().trigger('executions:refresh');
+                    // NOTE: pass id to keep the current deployment selected
+                    toolbox.getEventBus().trigger('deployments:refresh', id);
+                });
         });
 
         return Promise.all(executePromises).catch(err => {
