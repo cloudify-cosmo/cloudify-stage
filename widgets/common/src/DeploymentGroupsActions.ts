62877b3a36f9f1ee579138791744dc942a882931
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2021-05-10 11:17:02 +0200

    RD-2080 - Enhance support for Bulk Action - Run Workflow (#1344)
    * Added support for all type of filter rules in Bulk Action - Run Workflow
    * Started using new create deployment group API by passing filter_rules instead of deployment_ids
    * Extracted GoToExecutionsPageButton component
    * Added basic error handling and extract common function for group ID construction
    * Added search functionality to Workflow dropdown
    * Aligned RunWorkflowModal and DeployOnModal common code
    * Added Run Workflow test in deployments_view_spec

--- widgets/common/src/DeploymentGroupsActions.ts
+++ widgets/common/src/DeploymentGroupsActions.ts
@@ -1,8 +1,33 @@
+import { FilterRule } from './filters/types';
+
+type NewDeploymentsData = {
+    id: string;
+    labels: Record<string, string>[];
+};
+
+/* eslint-disable camelcase */
+interface CreateDeploymentGroupData {
+    filter_id?: string;
+    filter_rules?: FilterRule[];
+    deployment_ids?: string[];
+    deployments_from_group?: string;
+}
+
+interface CreateNewDeploymentsData {
+    new_deployments: NewDeploymentsData[];
+
+    blueprint_id?: string;
+    default_inputs?: Record<string, any>;
+    visibility?: string;
+    labels?: Record<string, string>[];
+}
+/* eslint-enable camelcase */
+
 export default class DeploymentGroupsActions {
     constructor(private toolbox: Stage.Types.Toolbox) {}
 
-    doCreate(id: string, filterId: string) {
-        return this.toolbox.getManager().doPut(`/deployment-groups/${id}`, null, { filter_id: filterId });
+    doCreate(id: string, data: CreateDeploymentGroupData | CreateNewDeploymentsData) {
+        return this.toolbox.getManager().doPut(`/deployment-groups/${id}`, null, data);
     }
 }
 
