4bc4ec4e8d5fede030cb5eb755ea159fb6f26003
Grzegorz Górecki <grzegorz.gorecki@amartus.com>
2021-05-12 12:29:17 +0200

    RD-2148 - used better urls matching for mocks

--- test/cypress/integration/gettingStarted/blueprintMocks.ts
+++ test/cypress/integration/gettingStarted/blueprintMocks.ts
@@ -1,60 +1,53 @@
-export const mockAwsBlueprintsManager = () =>
-    cy.interceptSp(
-        'GET',
-        '/blueprints?_include=id%2Cdescription%2Cmain_file_name%2Ctenant_name%2Ccreated_at%2Cupdated_at%2Ccreated_by%2Cprivate_resource%2Cvisibility',
-        {
-            body: {
-                metadata: { pagination: { total: 3, size: 50, offset: 0 }, filtered: null },
-                items: [
-                    {
-                        id: 'AWS-VM-Setup-using-CloudFormation',
-                        visibility: 'tenant',
-                        created_at: '2021-05-07T10:06:31.799Z',
-                        main_file_name: 'aws-cloudformation.yaml',
-                        updated_at: '2021-05-07T10:07:56.697Z',
-                        description: null,
-                        tenant_name: 'default_tenant',
-                        created_by: 'admin',
-                        private_resource: false
-                    },
-                    {
-                        id: 'AWS-Basics-VM-Setup',
-                        visibility: 'tenant',
-                        created_at: '2021-05-07T10:06:31.792Z',
-                        main_file_name: 'aws.yaml',
-                        updated_at: '2021-05-07T10:07:54.715Z',
-                        description: 'This blueprint creates an AWS infrastructure environment.\n',
-                        tenant_name: 'default_tenant',
-                        created_by: 'admin',
-                        private_resource: false
-                    },
-                    {
-                        id: 'Kubernetes-AWS-EKS',
-                        visibility: 'tenant',
-                        created_at: '2021-05-07T10:06:31.785Z',
-                        main_file_name: 'blueprint.yaml',
-                        updated_at: '2021-05-07T10:08:04.632Z',
-                        description: null,
-                        tenant_name: 'default_tenant',
-                        created_by: 'admin',
-                        private_resource: false
-                    }
-                ]
-            }
-        }
-    );
-
-export const mockEmptyBlueprintsManager = () =>
+export const mockBlueprintsManager = (items: any[]) =>
     cy.interceptSp(
         'GET',
         /^\/blueprints\?.*\b_include=(\bid\b|\bdescription\b|\bmain_file_name\b|\btenant_name\b|\bcreated_at\b|\bupdated_at\b|\bcreated_by\b|\bprivate_resource\b|\bvisibility\b|,)+/,
         {
             body: {
-                metadata: { pagination: { total: 0, size: 1000, offset: 0 }, filtered: null },
-                items: []
+                metadata: { pagination: { total: items.length, size: 1000, offset: 0 }, filtered: null },
+                items
             }
         }
     );
+
+export const mockAwsBlueprintsManager = () =>
+    mockBlueprintsManager([
+        {
+            id: 'AWS-VM-Setup-using-CloudFormation',
+            visibility: 'tenant',
+            created_at: '2021-05-07T10:06:31.799Z',
+            main_file_name: 'aws-cloudformation.yaml',
+            updated_at: '2021-05-07T10:07:56.697Z',
+            description: null,
+            tenant_name: 'default_tenant',
+            created_by: 'admin',
+            private_resource: false
+        },
+        {
+            id: 'AWS-Basics-VM-Setup',
+            visibility: 'tenant',
+            created_at: '2021-05-07T10:06:31.792Z',
+            main_file_name: 'aws.yaml',
+            updated_at: '2021-05-07T10:07:54.715Z',
+            description: 'This blueprint creates an AWS infrastructure environment.\n',
+            tenant_name: 'default_tenant',
+            created_by: 'admin',
+            private_resource: false
+        },
+        {
+            id: 'Kubernetes-AWS-EKS',
+            visibility: 'tenant',
+            created_at: '2021-05-07T10:06:31.785Z',
+            main_file_name: 'blueprint.yaml',
+            updated_at: '2021-05-07T10:08:04.632Z',
+            description: null,
+            tenant_name: 'default_tenant',
+            created_by: 'admin',
+            private_resource: false
+        }
+    ]);
+
+export const mockEmptyBlueprintsManager = () => mockBlueprintsManager([]);
 
 export const mockBlueprintUpload = (
     blueprintName: string,
