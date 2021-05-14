09d5f929944a9831399e175a79581f9a9afbab85
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-05-14 12:15:44 +0200

    Add missing file

---
+++ test/cypress/support/app_commons.ts
@@ -0,0 +1,6 @@
+import Consts from '../../../app/utils/consts';
+
+// eslint-disable-next-line import/prefer-default-export
+export function getCurrentAppVersion() {
+    return Consts.CURRENT_APP_DATA_VERSION;
+}
