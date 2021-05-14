fe45988f390b67bae0fbbaf660dc7a6068df6bb2
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-05-14 11:58:47 +0200

    Move CURRENT_APP_DATA_VERSION to common app consts and make it dynamic - base on the version from package.json file

--- app/actions/userAppCommon.js
+++ app/actions/userAppCommon.js
@@ -1,11 +1,10 @@
 import _ from 'lodash';
+import Consts from '../utils/consts';
 import Internal from '../utils/Internal';
-
-export const CURRENT_APP_DATA_VERSION = 6;
 
 export function saveUserAppData() {
     return (dispatch, getState) => {
-        const data = { appData: _.pick(getState(), 'pages'), version: CURRENT_APP_DATA_VERSION };
+        const data = { appData: _.pick(getState(), 'pages'), version: Consts.CURRENT_APP_DATA_VERSION };
 
         const internal = new Internal(getState().manager);
         return internal.doPost('/ua', null, data);
