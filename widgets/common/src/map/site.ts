5245405892d784ce38bf93a9bd823f7107a83e6b
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-05-14 12:16:03 +0200

    RD-1527 Use memoizeWithWeakMap for siteToLatLng

--- widgets/common/src/map/site.ts
+++ widgets/common/src/map/site.ts
@@ -14,21 +14,13 @@ export function isSiteWithPosition(site: Site): site is SiteWithPosition {
     return typeof site.latitude === 'number' && typeof site.longitude === 'number';
 }
 
-const siteLatLngMap = new WeakMap<SiteWithPosition, [number, number]>();
+function pureSiteToLatLng(site: SiteWithPosition): [number, number] {
+    return [site.latitude, site.longitude];
+}
 
 /**
  * Gets the latitude and longitude of the site.
  * Maintains stable references for the results.
  * Assumes immutability of the `site` argument (its position will not be mutated)
  */
-export function siteToLatLng(site: SiteWithPosition): [number, number] {
-    const latLng = siteLatLngMap.get(site);
-    if (latLng) {
-        return latLng;
-    }
-
-    const newLatLng: [number, number] = [site.latitude, site.longitude];
-    siteLatLngMap.set(site, newLatLng);
-
-    return newLatLng;
-}
+export const siteToLatLng = Stage.Utils.memoizeWithWeakMap(pureSiteToLatLng);
