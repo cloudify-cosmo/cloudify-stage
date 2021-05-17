f3254100c1366181c48f2602e1136d39a79e910d
Jakub Madej <jakub.madej@amartus.com>
2021-05-17 13:29:12 +0200

    Added backend calls verification

--- test/cypress/integration/widgets/user_management_spec.js
+++ test/cypress/integration/widgets/user_management_spec.js
@@ -15,34 +15,47 @@ describe('User management widget', () => {
     it('should allow to manage users', () => {
         cy.log('Creating new user');
         cy.get('.userManagementWidget .add').click();
-        cy.get('input[name=username]').type(username);
-        const password = 'admin';
-        cy.get('input[name=password]').type(password);
-        cy.get('input[name=confirmPassword]').type(password);
-        cy.get('button.green').click();
-
-        cy.contains('tr', username).within(() => {
-            cy.log('Verifying Admin checkbox is working');
-            cy.get('td:eq(2)').within(() => {
-                cy.get('.checkbox').click();
-                cy.get('.checkbox.checked').click();
-                cy.get('.checkbox:not(.checked)');
-            });
-
-            cy.log('Verifying Active checkbox is working');
-            cy.get('td:eq(3)').within(() => {
-                cy.get('.checkbox').click();
-                cy.get('.checkbox:not(.checked)').click();
-                cy.get('.checkbox.checked');
-            });
-
-            cy.log('Verifying Getting started checkbox is working');
-            cy.get('td:eq(4)').within(() => {
-                cy.get('.checkbox').click();
-                cy.get('.checkbox:not(.checked)').click();
-                cy.get('.checkbox.checked');
-            });
+        cy.get('.modal').within(() => {
+            cy.get('input[name=username]').type(username);
+            const password = 'admin';
+            cy.get('input[name=password]').type(password);
+            cy.get('input[name=confirmPassword]').type(password);
+            cy.get('.checkbox').click();
+            cy.get('button.green').click();
         });
+
+        function verifyCheckbox(name, url, checkedPayload, uncheckedPayload) {
+            cy.log(`Verify ${name} checkbox is working`);
+            const requestAlias = `${name}ChangeRequest`;
+            cy.interceptSp('POST', url).as(requestAlias);
+            cy.contains('th', name)
+                .invoke('index')
+                .then(columnIdx =>
+                    cy
+                        .contains('tr', username)
+                        .find(`td:eq(${columnIdx})`)
+                        .within(() => {
+                            cy.get('.checkbox').click();
+                            cy.wait(`@${requestAlias}`).then(({ request }) =>
+                                expect(request.body).to.deep.equal(uncheckedPayload)
+                            );
+                            cy.get('.checkbox:not(.checked)').click();
+                            cy.wait(`@${requestAlias}`).then(({ request }) =>
+                                expect(request.body).to.deep.equal(checkedPayload)
+                            );
+                            cy.get('.checkbox.checked');
+                        })
+                );
+        }
+
+        verifyCheckbox('Admin', `/users/${username}`, { role: 'sys_admin' }, { role: 'default' });
+        verifyCheckbox('Active', `/users/active/${username}`, { action: 'activate' }, { action: 'deactivate' });
+        verifyCheckbox(
+            'Getting started',
+            `/users/${username}`,
+            { show_getting_started: true },
+            { show_getting_started: false }
+        );
 
         cy.log('Verifying password can be changed');
         cy.contains('tr', username).find('.content').click();
@@ -73,7 +86,7 @@ describe('User management widget', () => {
         cy.contains('tr', username).contains('.label.blue', '1');
 
         cy.log('Verifying user groups and tenants can be removed');
-        cy.contains('tr', username).click();
+        cy.contains(username).click();
         cy.get('.remove').click({ multiple: true });
         cy.contains('No groups available');
         cy.contains('No tenants available');
