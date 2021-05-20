74909759bdfed8bc9e7b4a6d593518ed3b653872
gregcfy <78533022+gregcfy@users.noreply.github.com>
2021-05-20 12:00:05 +0200

    RD-2148 Add detailed E2E tests for Getting Started modal (#1353)
    * RD-2077 - hiding modal after login
    
    * RD-2077 - disabled getting started modal for all tests, fix for admin spec crusing when select covers save button
    
    * RD-2077 - multiple option blur() call after selection in admin spec
    
    * RD-2077 - show_getting_started property used in intercept
    
    * RD-2077 - moved modal disabling inside common logic
    
    * RD-2077 - removed unnecessary call
    
    * RD-2077 - removed unnecessary call
    
    * RD-2077 - removed unnecessary call
    
    * RD-2077 - reload and refresh with disabling modal
    
    * RD-2077 - removed unnecessary call
    
    * RD-2077 - removed unnecessary call
    
    * RD-2148 - getting started checkbox test in user management widget
    
    * RD-2148 - simple getting started modal test
    
    * RD-2148 - removed unused parameters
    
    * RD-2148 - more detailed tests for getting started modal
    
    * RD-2148 - checking getting started modal behaving when plugin is not available
    
    * RD-2148 - getting started modal test for state keeping for switching beetween steps
    
    * RD-2148 - common mocks
    
    * RD-2148 - changed response function to object in cypress tests
    
    * RD-2148 - used query option for urls for mocks
    
    * RD-2148 - used better urls matching for mocks
    
    * RD-2148 - used better urls matching for mocks
    
    * RD-2148 - used better urls matching for mocks
    
    * RD-2148 - used better urls matching for mocks
    
    * RD-2148 - common methods for mocks
    
    * RD-2148 - common methods for mocks
    
    * RD-2148 - common method for enable and disable getting started
    
    * RD-2148 - better name to disable modal flag
    
    * RD-2148 - better way to disable getting started
    
    * RD-2148 - cy.get changed to cy.contains
    
    * RD-2148 - common tests preparation inside befores
    
    * RD-2148 - json files for responses
    
    * RD-2148 - added ticket to use object instead of multiple optional parameters
    
    * RD-2148 - used login mock
    
    * RD-2148 - used login disable / enable modal REST API mock
    
    * RD-2253 - better names for fixtures
    
    * RD-2253 - better names formocking functions
    
    * RD-2148 - fix for message checking in getting started test
    
    * RD-2148 - fix for r method name
    
    * RD-2148 - fix for r method name
    
    * RD-2148 - /users/admin path used in mock
    
    * RD-2148 - .modal with within function in tests
    
    * RD-2148 - clouse button disabled selector removed
    
    * RD-2148 - better way to check if modal is not displayed after cloudify is loaded
    
    * RD-2148 - removed unnecessary description
    
    * RD-2148 - RouteMatcher used for queries
    
    * RD-2148 - nested descriptions, waiting for intercept
    
    * RD-2148 - common method that verifies if installation succeed
    
    * Added backend calls verification
    
    * Getting started modal test refactored
    
    * Removed unused fixtures
    
    * Increased installation timeout
    
    * Removed commented line
    
    Co-authored-by: Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
    
    * Removed unused disableGettingStarted command
    
    * Updated function name
    
    * Removed unnecessary comment
    
    Co-authored-by: Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
    
    * Added comment
    
    Co-authored-by: Grzegorz Górecki <grzegorz.gorecki@amartus.com>
    Co-authored-by: Jakub Madej <jakub.madej@amartus.com>
    Co-authored-by: Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>

---
+++ test/cypress/support/secrets.ts
@@ -0,0 +1,21 @@
+import { addCommands, GetCypressChainableFromCommands } from 'cloudify-ui-common/cypress/support';
+
+declare global {
+    namespace Cypress {
+        // NOTE: necessary for extending the Cypress API
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        export interface Chainable extends GetCypressChainableFromCommands<typeof commands> {}
+    }
+}
+
+const commands = {
+    deleteSecrets: (search: string) => {
+        cy.cfyRequest(`/secrets?_search=${search}`, 'GET').then(response =>
+            response.body.items.forEach(({ key }: { key: string }) => cy.cfyRequest(`/secrets/${key}`, 'DELETE'))
+        );
+    },
+    createSecret: (key: string, value: string) =>
+        cy.cfyRequest(`/secrets/${key}`, 'PUT', null, { value }, { failOnStatusCode: false })
+};
+
+addCommands(commands);
