41780a88a49f8d0648a514ec160bc3c71b1cefc9
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-05-27 13:15:28 +0200

    CY-943 Add tests for existing possible proxy errors
    Use nock to mock out requests to the Manager.

--- backend/test/routes/ServerProxy.test.js
+++ backend/test/routes/ServerProxy.test.js
@@ -1,16 +1,72 @@
 const request = require('supertest');
+const nock = require('nock');
+
+const mockApiUrl = 'https://raw.githubusercontent.com';
+const mockTimeout = 1000;
 
 jest.mock('handler/ManagerHandler', () => ({
-    getApiUrl: () => 'https://raw.githubusercontent.com',
-    updateOptions: jest.fn()
+    getApiUrl: () => mockApiUrl,
+    updateOptions: jest.fn(options => {
+        options.timeout = mockTimeout;
+    })
 }));
 
 const app = require('app');
 const { updateOptions } = require('handler/ManagerHandler');
 
 describe('/sp endpoint', () => {
-    it('sets manager request options', () =>
-        request(app)
-            .put('/console/sp?su=/blueprints')
-            .then(() => expect(updateOptions).toHaveBeenCalledWith({}, 'PUT')));
+    const blueprintsUrl = '/blueprints';
+    const proxyBlueprintsUrl = `/console/sp?su=${blueprintsUrl}`;
+
+    // NOTE: prevents memory leaks
+    // See https://github.com/nock/nock/issues/2057#issuecomment-702401375
+    beforeEach(() => {
+        if (!nock.isActive()) {
+            nock.activate();
+        }
+    });
+    afterEach(() => {
+        nock.restore();
+    });
+
+    it('sets manager request options', () => {
+        nock(mockApiUrl).put(blueprintsUrl).reply(200);
+
+        return request(app)
+            .put(proxyBlueprintsUrl)
+            .then(response => {
+                expect(updateOptions).toHaveBeenCalledWith(expect.any(Object), 'PUT');
+                expect(response.statusCode).toBe(200);
+            });
+    });
+
+    it('returns an error when the Manager is not available', () => {
+        nock(mockApiUrl).put(blueprintsUrl).replyWithError({ connect: true });
+
+        return request(app)
+            .put(proxyBlueprintsUrl)
+            .then(response => {
+                expect(response.statusCode).toBe(500);
+                expect(response.body).toEqual({
+                    message: expect.stringContaining('Manager is not available')
+                });
+            });
+    });
+
+    it('returns an error when the connection times out', () => {
+        // NOTE: using nock's `delayConnection` only yielded `ESOCKETTIMEDOUT`s
+        // despite it being tested in https://github.com/nock/nock/blob/7f7c7b7d34177091b40660a755389dca80ea2fc5/tests/test_delay.js#L207
+        // Thus, manually causing `ETIMEDOUT`
+        // See https://stackoverflow.com/a/41469139/4874344
+        nock(mockApiUrl).put(blueprintsUrl).replyWithError({ code: 'ETIMEDOUT' });
+
+        return request(app)
+            .put(proxyBlueprintsUrl)
+            .then(response => {
+                expect(response.statusCode).toBe(500);
+                expect(response.body).toEqual({
+                    message: expect.stringContaining('Request timed out')
+                });
+            });
+    });
 });
