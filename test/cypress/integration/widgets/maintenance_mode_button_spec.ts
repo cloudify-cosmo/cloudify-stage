d7ce26eaf83d26555a5a8de46bc6b0742ad02629
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-05-28 12:57:52 +0200

    RD-2501 Wait for executions to finish before activating maintenance mode
    Modify the system tests for maintenance mode to wait for executions to
    finish before activating maintenance mode. Otherwise, the test fails
    because activating the maintenance mode takes time (the Manager waits
    for executions to finish first) which causes cascading failures in other
    tests since the maintenance mode is activated.

--- test/cypress/integration/widgets/maintenance_mode_button_spec.ts
+++ test/cypress/integration/widgets/maintenance_mode_button_spec.ts
@@ -1,7 +1,12 @@
+import { waitUntilEmpty } from '../../support/resource_commons';
+
 describe('Maintenance mode button widget', () => {
     before(() => cy.activate('valid_trial_license').usePageMock('maintenanceModeButton').mockLogin());
 
     it('should enter maintenance mode on click', () => {
+        waitUntilEmpty(
+            'executions?status=scheduled&status=queued&status=pending&status=started&status=cancelling&status=force_cancelling&status=kill_cancelling'
+        );
         cy.contains('Activate Maintenance Mode').click();
         cy.contains('Yes').click();
         cy.contains('Deactivate Maintenance Mode').click();
