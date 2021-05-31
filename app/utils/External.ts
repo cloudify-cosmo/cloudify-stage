e63a9aa2bddef9366587b944a4e5438c5d7498b7
Jakub Madej <jakub.madej@amartus.com>
2021-05-31 14:03:22 +0200

    Added jquery typings

--- app/utils/External.ts
+++ app/utils/External.ts
@@ -280,8 +280,6 @@ export default class External {
 
     // eslint-disable-next-line class-methods-use-this
     buildActualUrl(url: string, data?: Record<string, any>) {
-        // TODO: RD-258
-        // @ts-ignore Cannot find $
         const queryString = data ? (url.indexOf('?') > 0 ? '&' : '?') + $.param(data, true) : '';
         return `${url}${queryString}`;
     }
