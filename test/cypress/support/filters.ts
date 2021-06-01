2fc7751acbcb0793aa41c8d18780706b2076150a
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-06-01 11:45:23 +0200

    RD-1295 Fix widget type imports in Cypress tests
    Those imports should be marked as `import type` so webpack does not try
    to compile the imported project if all we need from it is global types
    to be exported.

--- test/cypress/support/filters.ts
+++ test/cypress/support/filters.ts
@@ -1,6 +1,6 @@
 import { addCommands, GetCypressChainableFromCommands } from 'cloudify-ui-common/cypress/support';
 import { waitUntilEmpty } from './resource_commons';
-import '../../../widgets/common/src/filters';
+import type {} from '../../../widgets/common/src/filters';
 
 declare global {
     namespace Cypress {
