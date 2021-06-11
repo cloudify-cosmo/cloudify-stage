49541d5016546aefba44a99f234f5703a48fe8f4
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2021-06-11 09:23:28 +0200

    RD-2453 Fix multiple issues in DynamicDropdown (#1420)
    * Enhanced filter_spec.js
    * Removed overrideOptionsAfterFetch state
    * Added searchQuery dependency to VisibilitySensor to properly discover change when user is typing
    * Fixed DynamicDropdown to handle implicit options properly (required for value to be set)
    
    Co-authored-by: Jakub Madej <jakub.madej@amartus.com>

--- test/cypress/integration/widgets/filter_spec.ts
+++ test/cypress/integration/widgets/filter_spec.ts
@@ -49,8 +49,9 @@ describe('Filter', () => {
                 .deleteBlueprints(blueprintName)
                 .uploadBlueprint('blueprints/empty.zip', blueprintName)
                 .refreshTemplate()
-                .setBlueprintContext(blueprintName)
         );
+
+        beforeEach(() => cy.setBlueprintContext(blueprintName));
 
         it('deployment creation and removal', () => {
             cy.get('.blueprintsWidget').within(() => {
@@ -78,7 +79,7 @@ describe('Filter', () => {
 
             cy.get('.deploymentFilterField > .text.default');
             cy.get('.deploymentFilterField input').type(deploymentName, { force: true });
-            cy.contains('.deploymentFilterField', 'No results found.');
+            cy.contains('.deploymentFilterField .message', 'No results found.');
         });
 
         it('blueprint upload and removal', () => {
@@ -90,7 +91,7 @@ describe('Filter', () => {
 
             cy.get('.blueprintFilterField > .label').should('not.exist');
             cy.get('.blueprintFilterField input').type(blueprintName, { force: true });
-            cy.contains('.blueprintFilterField', 'No results found.');
+            cy.contains('.blueprintFilterField .message', 'No results found.');
         });
     });
 });
