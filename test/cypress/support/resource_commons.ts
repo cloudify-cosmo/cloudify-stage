8c660ae7020f4ecc5847f11f185ac969d64ef96f
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-06-15 13:34:26 +0200

    Improve waitUntil functionality

--- test/cypress/support/resource_commons.ts
+++ test/cypress/support/resource_commons.ts
@@ -1,25 +1,42 @@
-// @ts-nocheck File not migrated fully to TS
 import _ from 'lodash';
 
-function appendQueryParam(url, param, value) {
+function appendQueryParam(url: string, param: string, value: string) {
     return `${url}${url.indexOf('?') > 0 ? '&' : '?'}${param}=${value}`;
 }
 
-export function waitUntilEmpty(resource, search, numberOfRetriesLeft = 60, waitingInterval = 1000) {
+function waitUntil(
+    resource: string,
+    predicate: (response: Cypress.Response) => boolean,
+    { search, numberOfRetriesLeft, waitingInterval } = {
+        search: '',
+        numberOfRetriesLeft: 60,
+        waitingInterval: 1000
+    }
+) {
     if (numberOfRetriesLeft <= 0) {
         throw new Error(`Number of retries exceeded for resource=${resource}, search=${search}.`);
     }
 
     let url = `/${resource}`;
-    if (search) url = appendQueryParam(url, `_search`, search);
+    if (search) {
+        url = appendQueryParam(url, `_search`, search);
+    }
     cy.cfyRequest(url, 'GET').then(response => {
-        if (_.isEmpty(response.body.items)) {
+        if (predicate(response)) {
             return;
         }
         // eslint-disable-next-line cypress/no-unnecessary-waiting
         cy.wait(waitingInterval);
         waitUntilEmpty(resource, search, numberOfRetriesLeft - 1, waitingInterval);
     });
+}
+
+export function waitUntilNotEmpty(resource: string, search: string, numberOfRetriesLeft = 60, waitingInterval = 1000) {
+    waitUntil(resource, response => !_.isEmpty(response.body.items), { search, numberOfRetriesLeft, waitingInterval });
+}
+
+export function waitUntilEmpty(resource: string, search: string, numberOfRetriesLeft = 60, waitingInterval = 1000) {
+    waitUntil(resource, response => _.isEmpty(response.body.items), { search, numberOfRetriesLeft, waitingInterval });
 }
 
 export function minutesToMs(minutes: number) {
