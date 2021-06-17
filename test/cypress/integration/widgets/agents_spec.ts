f7be37e33aa25e8ae9a9753dc020ad5abaf2dd65
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-06-17 10:57:42 +0200

    Improve Install and Validate agents tests

--- test/cypress/integration/widgets/agents_spec.ts
+++ test/cypress/integration/widgets/agents_spec.ts
@@ -1,41 +1,136 @@
-// TODO(RD-2589): fix the flakiness and enable the tests
-describe.skip('Agents widget', () => {
-    const blueprintName = 'agents_test_blueprint';
+import { camelCase, snakeCase, upperFirst } from 'lodash';
+import { secondsToMs } from '../../support/resource_commons';
+
+describe('Agents widget', () => {
+    const pollingTimeSeconds = 5;
     const deploymentName = 'agents_test_deployment';
+    const nodeName = 'host';
+    const nodeInstanceName = 'host_qz2p8t';
 
     before(() => {
         cy.usePageMock('agents', {
             fieldsToShow: ['Id', 'Node', 'Deployment', 'IP', 'Install Method', 'System', 'Version', 'Actions'],
-            pageSize: 15
+            pageSize: 15,
+            pollingTime: pollingTimeSeconds
         })
             .activate()
-            .mockLogin()
-            .deleteDeployments(deploymentName, true)
-            .deleteBlueprints(blueprintName, true)
-            .uploadBlueprint('blueprints/simple.zip', blueprintName)
-            .deployBlueprint(blueprintName, deploymentName, { server_ip: 'localhost' });
+            .mockLogin();
     });
 
-    it('should allow to validate agent', () => {
-        cy.contains('Validate').click();
-        cy.get('div[name=deploymentId] input').type(deploymentName);
-        cy.get('div[name=nodeId]').click();
-        cy.get('div[name=nodeId] .item').click();
-        cy.get('div[name=nodeInstanceId]').click();
-        cy.get('div[name=nodeInstanceId] .item').click();
-        cy.contains('.modal button', 'Validate').click();
-        cy.contains('Close').click();
-    });
+    describe('should allow to', () => {
+        beforeEach(() => {
+            function interceptResourceFetching(resourceName: string, resourceId: string) {
+                cy.interceptSp('GET', `/${resourceName}?_include=id`, {
+                    metadata: {
+                        pagination: { total: 1, size: 1000, offset: 0 },
+                        filtered: null
+                    },
+                    items: [{ id: resourceId }]
+                }).as(`fetch${upperFirst(camelCase(resourceName))}`);
+            }
 
-    it('should allow to install new agent', () => {
-        cy.contains('Install').click();
-        cy.get('div[name=deploymentId] input').type(deploymentName);
-        cy.get('div[name=nodeId]').click();
-        cy.get('div[name=nodeId] .item').click();
-        cy.get('div[name=nodeInstanceId]').click();
-        cy.get('div[name=nodeInstanceId] .item').click();
-        cy.contains('.modal button', 'Install').click();
-        cy.contains('Close').click();
+            cy.interceptSp('GET', '/agents', {
+                metadata: { pagination: { total: 1, size: 15, offset: 0 }, filtered: null },
+                items: [
+                    {
+                        id: 'host_qz2p8t_1cc7dac9-e9c4-492e-ba36-6f5146ed1bd5',
+                        host_id: nodeInstanceName,
+                        ip: '10.239.2.130',
+                        install_method: 'remote',
+                        system: 'centos core',
+                        version: '6.1.0-.dev1',
+                        node: nodeName,
+                        deployment: deploymentName,
+                        tenant_name: 'default_tenant'
+                    }
+                ]
+            }).as('fetchAgents');
+
+            cy.interceptSp('POST', '/executions').as('postExecution');
+
+            interceptResourceFetching('deployments', deploymentName);
+            interceptResourceFetching('nodes', nodeName);
+            interceptResourceFetching('node-instances', nodeInstanceName);
+
+            cy.wait('@fetchAgents', { requestTimeout: secondsToMs(pollingTimeSeconds + 1) });
+            cy.contains(deploymentName);
+            cy.killExecutions(deploymentName);
+        });
+
+        function selectResourceFromDropdown(dropdownName: string, value: string) {
+            cy.get(`div[name=${dropdownName}]`).click();
+            cy.get(`div[name=${dropdownName}] input`).type(value);
+            cy.get(`div[name=${dropdownName}] .item`).click();
+        }
+
+        function fillNodesFilter() {
+            cy.wait('@fetchDeployments');
+            cy.wait('@fetchNodes');
+            cy.wait('@fetchNodeInstances');
+
+            selectResourceFromDropdown('deploymentId', deploymentName);
+            selectResourceFromDropdown('nodeId', nodeName);
+            selectResourceFromDropdown('nodeInstanceId', nodeInstanceName);
+        }
+
+        type InstallMethods = 'Remote' | 'Plugin' | 'Init Script' | 'Provided';
+        function selectInstallMethod(installMethod: InstallMethods) {
+            cy.get('div[name="installMethods"]').click();
+            cy.contains(installMethod).click();
+            cy.get('div[name="installMethods"]').click();
+        }
+
+        function verifyExecution(
+            executionPayload: Record<string, any>,
+            expectedWorkflowId: string,
+            expectedInstallMethod: 'Remote' | 'Plugin' | 'Init Script' | 'Provided',
+            expectedAdditionalParameters: Record<string, any> = {}
+        ) {
+            expect(executionPayload).to.deep.equal({
+                deployment_id: deploymentName,
+                workflow_id: expectedWorkflowId,
+                dry_run: false,
+                force: false,
+                queue: false,
+                parameters: {
+                    node_ids: [nodeName],
+                    node_instance_ids: [nodeInstanceName],
+                    install_methods: [snakeCase(expectedInstallMethod)],
+                    ...expectedAdditionalParameters
+                }
+            });
+        }
+
+        it('validate agent', () => {
+            cy.contains('Validate').click();
+            cy.get('.modal').within(() => {
+                fillNodesFilter();
+                selectInstallMethod('Plugin');
+                cy.contains('button', 'Validate').click();
+            });
+
+            cy.wait('@postExecution').then(({ request }) => {
+                verifyExecution(request.body, 'validate_agents', 'Plugin');
+            });
+            cy.contains('Execution started').should('be.visible');
+            cy.contains('Close').click();
+        });
+
+        it('install new agent', () => {
+            cy.contains('Install').click();
+            cy.get('.modal').within(() => {
+                fillNodesFilter();
+                selectInstallMethod('Remote');
+                cy.contains('button', 'Install').click();
+            });
+
+            cy.wait('@postExecution').then(({ request }) => {
+                verifyExecution(request.body, 'install_new_agents', 'Remote', { stop_old_agent: false });
+            });
+
+            cy.contains('Execution started').should('be.visible');
+            cy.contains('Close').click();
+        });
     });
 
     it('should display agents that match the search phrase', () => {
