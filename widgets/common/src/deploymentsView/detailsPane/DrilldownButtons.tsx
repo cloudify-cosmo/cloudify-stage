d301c9050168a9f4351fe23cec28623074338c1a
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-06-18 10:00:53 +0200

    RD-2005 Use margin instead of adding an extra space in HTML

--- widgets/common/src/deploymentsView/detailsPane/DrilldownButtons.tsx
+++ widgets/common/src/deploymentsView/detailsPane/DrilldownButtons.tsx
@@ -154,8 +154,8 @@ const DrilldownButton: FunctionComponent<DrilldownButtonProps> = ({
             {!result.loading && (
                 <>
                     {' '}
-                    ({result.count}){' '}
-                    <SubdeploymentStatusIcon status={result.status} style={{ marginRight: 0, marginLeft: 0 }} />
+                    ({result.count})
+                    <SubdeploymentStatusIcon status={result.status} style={{ marginRight: 0, marginLeft: '0.2em' }} />
                 </>
             )}
         </Button>
