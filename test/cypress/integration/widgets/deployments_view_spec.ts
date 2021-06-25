ecd45193cd03dbc7b548aaed0f30e45756c0d219
Jakub Madej <jakub.madej@cloudify.co>
2021-06-25 11:25:02 +0200

    Extended test scope to cover newly saved filter application

--- test/cypress/integration/widgets/deployments_view_spec.ts
+++ test/cypress/integration/widgets/deployments_view_spec.ts
@@ -265,9 +265,15 @@ describe('Deployments View widget', () => {
             });
         });
 
-        it('should allow to save new filter', () => {
+        it('should allow to save new filter and apply it', () => {
             const newFilterId = `${filterId}-new`;
             cy.deleteDeploymentsFilter(newFilterId, { ignoreFailure: true });
+            const newFilterRule = {
+                type: FilterRuleType.Attribute,
+                key: FilterRuleAttribute.Blueprint,
+                values: [blueprintName],
+                operator: FilterRuleOperators.Contains
+            };
 
             useDeploymentsViewWidget();
 
@@ -283,18 +289,26 @@ describe('Deployments View widget', () => {
                 cy.contains('Save as').click();
                 cy.get('input[placeholder="Enter new filter ID..."]').type(newFilterId);
                 cy.contains('Save new filter').click().should('not.exist');
+
+                cy.wait('@filterCreate').then(({ request }) => {
+                    const requestRules = request.body.filter_rules;
+                    expect(requestRules).to.have.length(1);
+                    expect(requestRules[0]).to.deep.equal(newFilterRule);
+                });
+
                 cy.contains('.field', 'Filter ID').contains(newFilterId);
+
+                cy.interceptSp('POST', '/searches/deployments').as('deploymentsSearchRequest');
+                cy.contains('Apply').click();
             });
 
-            cy.wait('@filterCreate').then(({ request }) => {
+            cy.get('.modal').should('not.exist');
+            cy.contains('button', 'Filter').should('not.exist');
+            cy.contains('button', newFilterId);
+
+            cy.wait('@deploymentsSearchRequest').then(({ request }) => {
                 const requestRules = request.body.filter_rules;
-                expect(requestRules).to.have.length(1);
-                expect(requestRules[0]).to.deep.equal({
-                    type: FilterRuleType.Attribute,
-                    key: FilterRuleAttribute.Blueprint,
-                    values: [blueprintName],
-                    operator: FilterRuleOperators.Contains
-                });
+                expect(requestRules).to.deep.equal([newFilterRule]);
             });
         });
 
