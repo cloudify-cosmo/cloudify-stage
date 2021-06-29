1d98e399bf50cb7db6f86e9a45c414fb512b420b
Jakub Madej <jakub.madej@cloudify.co>
2021-06-29 11:09:12 +0200

    Disabled flaky test

--- test/cypress/integration/widgets/filters_spec.ts
+++ test/cypress/integration/widgets/filters_spec.ts
@@ -370,7 +370,7 @@ describe('Filters widget', () => {
         });
     });
 
-    describe('should allow to define a filter rule', () => {
+    describe.skip('should allow to define a filter rule', () => {
         const testPrefix = 'filters_test_form';
         const blueprintId = `${testPrefix}_blueprint`;
         const deploymentId = `${testPrefix}_deployment`;
