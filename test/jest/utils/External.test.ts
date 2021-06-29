d0f8e0c8fcf7e354220d5f8979a04dc01031f0d9
Kamil Stepczuk <5058678+stepek@users.noreply.github.com>
2021-06-29 08:29:01 +0200

    RD-2500 Remove jQuery dependency from External.ts. (#1463)
    * RD-2500 Remove jQuery dependency from External.ts.

---
+++ test/jest/utils/External.test.ts
@@ -0,0 +1,26 @@
+import External from 'utils/External';
+
+describe('(Utils) External', () => {
+    describe('buildActualUrl (protected)', () => {
+        const external = new External({});
+
+        it(`should return correct url with params`, () => {
+            expect(
+                // @ts-expect-error testing protected function
+                external.buildActualUrl('https://localhost:8081', {
+                    foo: 'FOO',
+                    bar: 612,
+                    baz: [13, 987, 3]
+                })
+            ).toBe('https://localhost:8081?bar=612&baz=13&baz=987&baz=3&foo=FOO');
+            expect(
+                // @ts-expect-error testing protected function
+                external.buildActualUrl('https://localhost:8081?qux=QUX', {
+                    foo: 'FOO',
+                    bar: 612,
+                    baz: [13, 987, 3]
+                })
+            ).toBe('https://localhost:8081?qux=QUX&bar=612&baz=13&baz=987&baz=3&foo=FOO');
+        });
+    });
+});
