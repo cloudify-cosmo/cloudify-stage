3991ab7bb098b6ecdb3606d209e0f4ced9c2a79b
Grzegorz Rozdzialik <grzegorz.rozdzialik@cloudify.co>
2021-07-06 14:39:57 +0200

    RD-2483 Display uploaded plugin version in Plugins Catalog widget (#1496)
    * Show a "Uploaded version" column in Plugins Catalog
    
    * Add tooltips to plugin upload button and disable it when needed
    
    When the currently uploaded plugin version is the same as the latest
    one, the plugin upload button will be disabled.
    
    The button also now has a tooltip (`title`) to inform the user why it is
    disabled, or what the button will do when it's enabled.
    
    * Add a system test for verifying the uploaded plugin version
    
    * Verifies the displayed uploaded version.
    * Verifies that the upload button is disabled when the uploaded version
      matches the current version.
    
    * Add automatically refreshing Plugins Catalog when plugins change
    
    Verify that the uploaded plugin version is updated when a plugin is
    changed or removed.
    
    * Add force removal of plugin in Plugins Catalog test
    
    There is a dependency between test suites that prevents removing the
    Utilities plugin without "force" when running all the tests.
    Thus, the plugin should be force-removed.

--- widgets/pluginsCatalog/src/Actions.ts
+++ widgets/pluginsCatalog/src/Actions.ts
@@ -1,7 +1,44 @@
+import type { PluginDescription, PluginDescriptionWithVersion } from './types';
+
+interface UploadedPlugin {
+    // NOTE: property names match from the backend ones
+    /* eslint-disable camelcase */
+    package_name: string;
+    package_version: string;
+    /* eslint-disable camelcase */
+}
+
 export default class Actions {
     constructor(private readonly toolbox: Stage.Types.Toolbox, private readonly jsonPath: string) {}
 
-    doGetPluginsList() {
+    async doGetPluginsList(): Promise<PluginDescriptionWithVersion[]> {
+        const pluginDescriptions = await this.doGetPluginsCatalogList();
+
+        const uploadedPlugins: Stage.Types.PaginatedResponse<UploadedPlugin> = await this.toolbox
+            .getManager()
+            .doGet('/plugins?_include=package_name,package_version', {
+                params: {
+                    package_name: pluginDescriptions.map(({ name }) => name),
+                    ...(this.toolbox.getContext().getValue('onlyMyResources')
+                        ? { created_by: this.toolbox.getManager().getCurrentUsername() }
+                        : {})
+                }
+            });
+
+        const uploadedPluginsVersions = new Map(
+            // eslint-disable-next-line camelcase
+            uploadedPlugins.items.map(({ package_name, package_version }) => [package_name, package_version])
+        );
+
+        return pluginDescriptions.map(
+            (pluginDescription): PluginDescriptionWithVersion => ({
+                pluginDescription,
+                uploadedVersion: uploadedPluginsVersions.get(pluginDescription.name)
+            })
+        );
+    }
+
+    private doGetPluginsCatalogList(): Promise<PluginDescription[]> {
         return this.toolbox
             .getInternal()
             .doGet('/external/content', { params: { url: this.jsonPath }, parseResponse: false })
