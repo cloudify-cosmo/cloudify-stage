191a05a09a5a1cfb1b8bbf3b71907e6bed99d64e
Kamil Stepczuk <kamil.stepczuk@gmail.com>
2021-08-03 09:59:35 +0200

    Move BlueprintMarketplaceModal to common dir.

--- widgets/common/src/BlueprintMarketplaceModal.tsx
+++ widgets/common/src/BlueprintMarketplaceModal.tsx
@@ -1,7 +1,5 @@
 import type { FunctionComponent } from 'react';
 
-import { noop } from 'lodash';
-
 interface Tab {
     name: string;
     url: string;
@@ -74,3 +72,15 @@ BlueprintMarketplaceModal.propTypes = {
 };
 
 export default BlueprintMarketplaceModal;
+
+declare global {
+    namespace Stage.Common {
+        // eslint-disable-next-line import/prefer-default-export
+        export { BlueprintMarketplaceModal };
+    }
+}
+
+Stage.defineCommon({
+    name: 'BlueprintMarketplaceModal',
+    common: React.memo(BlueprintMarketplaceModal, _.isEqual)
+});
