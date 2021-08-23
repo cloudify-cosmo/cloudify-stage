826bbc605ab41b2169f3e42a773d7f112b642e4e
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-08-23 14:41:29 +0200

    Use `SemanticIconDropdown` and `SemanticColorDropdown` in Button Link widget configuration

--- widgets/buttonLink/src/widget.tsx
+++ widgets/buttonLink/src/widget.tsx
@@ -1,16 +1,21 @@
-import type { ComponentType, CSSProperties } from 'react';
+import type { SemanticCOLORS, SemanticICONS } from 'semantic-ui-react';
+import type { CSSProperties } from 'react';
 
 interface ButtonLinkWidgetConfiguration {
-    color: string;
+    color: SemanticCOLORS;
+    icon: SemanticICONS;
     label: string;
     url: string;
     fullHeight: boolean;
 }
 
+const widgetId = 'buttonLink';
+const t = Stage.Utils.getT(`widgets.${widgetId}`);
+
 Stage.defineWidget<unknown, unknown, ButtonLinkWidgetConfiguration>({
-    id: 'buttonLink',
-    name: 'Button link',
-    description: 'Opens provided URL in a different tab',
+    id: widgetId,
+    name: t('name'),
+    description: t('description'),
     initialWidth: 2,
     initialHeight: 4,
     showHeader: false,
@@ -18,47 +23,66 @@ Stage.defineWidget<unknown, unknown, ButtonLinkWidgetConfiguration>({
     categories: [Stage.GenericConfig.CATEGORY.BUTTONS_AND_FILTERS],
 
     initialConfiguration: [
-        { id: 'label', name: 'Button label', default: 'Button Link', type: Stage.Basic.GenericField.STRING_TYPE },
-        { id: 'url', name: 'URL address', default: '', type: Stage.Basic.GenericField.STRING_TYPE },
-        { id: 'fullHeight', name: 'Full height', default: false, type: Stage.Basic.GenericField.BOOLEAN_TYPE },
+        {
+            id: 'label',
+            name: t('configuration.label.name'),
+            description: t('configuration.label.description'),
+            default: t('configuration.label.default'),
+            type: Stage.Basic.GenericField.STRING_TYPE
+        },
         {
             id: 'color',
-            name: 'Color',
-            default: '#21ba45',
-            type: Stage.Basic.GenericField.CUSTOM_TYPE,
-            component: Stage.Basic.Form.ColorPicker as ComponentType<
-                Stage.Types.CustomConfigurationComponentProps<string>
-            >
+            name: t('configuration.color.name'),
+            description: t('configuration.color.description'),
+            default: t('configuration.color.default'),
+            component: Stage.Common.SemanticColorDropdown,
+            type: Stage.Basic.GenericField.CUSTOM_TYPE
+        },
+        {
+            id: 'icon',
+            name: t('configuration.icon.name'),
+            description: t('configuration.icon.description'),
+            default: t('configuration.icon.default'),
+            component: Stage.Common.SemanticIconDropdown,
+            type: Stage.Basic.GenericField.CUSTOM_TYPE
+        },
+        {
+            id: 'url',
+            name: t('configuration.url.name'),
+            description: t('configuration.url.description'),
+            default: t('configuration.url.default'),
+            type: Stage.Basic.GenericField.STRING_TYPE
+        },
+        {
+            id: 'fullHeight',
+            name: t('configuration.fullHeight.name'),
+            description: t('configuration.fullHeight.description'),
+            default: false,
+            type: Stage.Basic.GenericField.BOOLEAN_TYPE
         }
     ],
     isReact: true,
     hasReadme: true,
-    permission: Stage.GenericConfig.WIDGET_PERMISSION('buttonLink'),
+    permission: Stage.GenericConfig.WIDGET_PERMISSION(widgetId),
 
     render(widget) {
         const { Button } = Stage.Basic;
-        const { color, fullHeight, label, url } = widget.configuration;
+        const { color, fullHeight, icon, label, url } = widget.configuration;
 
-        const style: CSSProperties = { color: 'white' };
-
+        const style: CSSProperties = {};
         if (fullHeight) {
             style.height = 'calc(100% + 14px)';
         }
 
-        if (color) {
-            style.backgroundColor = color;
-        }
-
         return (
             <Button
-                className="labeled icon"
-                fluid
-                icon="external"
                 disabled={!url}
-                onClick={() => {
-                    window.open(url, '_blank');
-                }}
+                color={color}
                 content={label}
+                icon={icon}
+                fluid
+                labelPosition={icon ? 'left' : undefined}
+                onClick={() => window.open(url, '_blank')}
                 style={style}
             />
         );
