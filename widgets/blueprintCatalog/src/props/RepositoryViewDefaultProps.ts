7ebb893e4ce8160d2d98e0d939e98c58574b058b
Kamil Stepczuk <5058678+stepek@users.noreply.github.com>
2021-08-31 14:30:29 +0200

    RD-2932 Remove confirmation button from blueprint catalog. (#1568)
    * RD-2932 Remove confirmation button from blueprint catalog.
    
    * Refactor to blueprintCatalog TS

--- widgets/blueprintCatalog/src/props/RepositoryViewDefaultProps.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    fetchData: () => {},
-    onSelect: () => {},
-    onUpload: () => {},
-    readmeLoading: null
-};
---
+++ widgets/blueprintCatalog/src/types.ts
@@ -0,0 +1,53 @@
+export interface BlueprintCatalogWidgetConfiguration {
+    jsonPath: string;
+    username: string;
+    password: string;
+    filter: string;
+    displayStyle: 'table' | 'catalog';
+    sortByName: boolean;
+    pageSize: number;
+    sortColumn: string;
+    sortAscending: boolean;
+}
+
+export interface Blueprint {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    // eslint-disable-next-line camelcase
+    created_at: string;
+    // eslint-disable-next-line camelcase
+    updated_at: string;
+    // eslint-disable-next-line camelcase
+    image_url: string;
+    // eslint-disable-next-line camelcase
+    html_url: string;
+    // eslint-disable-next-line camelcase
+    readme_url: string;
+    // eslint-disable-next-line camelcase
+    zip_url: string;
+    // eslint-disable-next-line camelcase
+    main_blueprint: string;
+    isSelected: boolean;
+}
+
+export interface BlueprintCatalogPayload {
+    items: Blueprint[];
+    total: number;
+    source: string;
+    uploadedBlueprints: string[];
+    isAuthenticated?: boolean;
+}
+
+export interface RepositoryViewProps {
+    data: BlueprintCatalogPayload;
+    fetchData: (fetchParams: any) => void;
+    uploadingInProgress?: string[];
+    noDataMessage: string;
+    onReadme: (name: string, url: string) => void;
+    onSelect: (item: Blueprint) => void;
+    onUpload: (name: string, zipUrl: string, imageUrl: string, mainBlueprint: string) => void;
+    readmeLoading: string | null;
+    widget: Stage.Types.Widget<BlueprintCatalogWidgetConfiguration>;
+}
