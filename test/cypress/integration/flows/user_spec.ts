498beb63f667e9d0ebfef716544f1096354675a4
Kamil Stepczuk <kamil.stepczuk@gmail.com>
2021-09-07 12:42:13 +0200

    Move test back.

--- test/cypress/integration/flows/user_spec.ts
+++ test/cypress/integration/flows/user_spec.ts
@@ -23,4 +23,51 @@ describe('User flow', () => {
             cy.get('button.green').click();
         });
     }
+    it('installs deployment from scratch', () => {
+        cy.visitPage('Resources').openTab('Plugins');
+
+        cy.contains('Upload').click();
+        cy.contains('Upload from Marketplace').click();
+
+        cy.contains('.pluginsCatalogWidget tr', 'Utilities').within(() => {
+            cy.get('button').click();
+            cy.get('button', { timeout: minutesToMs(2) }).should('to.be.disabled');
+        });
+
+        cy.visitPage('Resources').openTab('Secrets');
+        createSecret('some_key_1');
+        createSecret('some_key_4');
+        createSecret('some_key_7');
+        createSecret('some_key_10');
+        createSecret('openstack_config__lab1_tenantA');
+
+        cy.visitPage('Blueprints');
+        cy.contains('Upload').click();
+        cy.contains('Upload a blueprint package').click();
+        cy.get('.modal').within(() => {
+            cy.get('input[name=blueprintUrl]')
+                .type(
+                    'https://github.com/cloudify-community/blueprint-examples/releases/download/5.0.5-74/utilities-examples-cloudify_secrets.zip'
+                )
+                .blur();
+            cy.get('input[name=blueprintName]').clear().type(resourceName);
+            cy.get('.button.ok').click();
+        });
+        cy.get('.modal', { timeout: minutesToMs(1) }).should('not.exist');
+
+        cy.getSearchInput().clear().type(resourceName);
+        cy.get('.blueprintsTable > tbody > tr').should('have.length', 1);
+        cy.get('.rocket').click();
+        cy.get('input[name=deploymentName]').type(resourceName);
+        cy.get('button.green').click();
+
+        cy.get('.modal').find('button.ok').click();
+        cy.get('.modal', { timeout: minutesToMs(1) }).should('not.exist');
+
+        cy.contains('.executionsWidget', 'install completed');
+        cy.contains('.eventsTable', "'install' workflow execution succeeded");
+
+        cy.contains('Deployment Info').click();
+        cy.get('#gridContent > .nodeContainer').should('have.length', 3);
+    });
 });
