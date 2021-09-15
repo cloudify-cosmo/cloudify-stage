203424ccf5e37665e23ef5901b55b530b01a1045
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-09-15 14:29:10 +0200

    Migrated snapshots and users custom commands to use Cypress namespace

--- test/cypress/support/snapshots.ts
+++ test/cypress/support/snapshots.ts
@@ -1,3 +1,16 @@
-Cypress.Commands.add('deleteSnapshot', snapshotName =>
-    cy.cfyRequest(`/snapshots/${snapshotName}`, 'DELETE', null, null, { failOnStatusCode: false })
-);
+import { addCommands, GetCypressChainableFromCommands } from 'cloudify-ui-common/cypress/support';
+
+declare global {
+    namespace Cypress {
+        // NOTE: necessary for extending the Cypress API
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        export interface Chainable extends GetCypressChainableFromCommands<typeof commands> {}
+    }
+}
+
+const commands = {
+    deleteSnapshot: (snapshotName: string) =>
+        cy.cfyRequest(`/snapshots/${snapshotName}`, 'DELETE', null, null, { failOnStatusCode: false })
+};
+
+addCommands(commands);
