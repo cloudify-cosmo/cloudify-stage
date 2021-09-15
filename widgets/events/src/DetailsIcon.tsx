fa3e024beaf69ae283cc193e873bdcca788b7a79
kubama <jakub.madej@amartus.com>
2021-09-15 13:44:35 +0200

    RD-3100 Replaced error message popup with a modal (#1604)

---
+++ widgets/events/src/DetailsIcon.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent } from 'react';
+
+const t = Stage.Utils.getT('widgets.events');
+
+interface DetailsIconProps {
+    onClick: () => void;
+}
+
+const DetailsIcon: FunctionComponent<DetailsIconProps> = ({ onClick }) => {
+    const { Icon, Popup } = Stage.Basic;
+
+    return (
+        <Popup on="hover">
+            <Popup.Trigger>
+                <Icon.Group
+                    size="big"
+                    onClick={(e: Event) => {
+                        e.stopPropagation();
+                        onClick();
+                    }}
+                    color="black"
+                >
+                    <Icon name="file text" />
+                    <Icon corner name="zoom" />
+                </Icon.Group>
+            </Popup.Trigger>
+            <Popup.Content>{t('detailsIcon')}</Popup.Content>
+        </Popup>
+    );
+};
+
+export default DetailsIcon;
