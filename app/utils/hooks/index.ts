e1b48ac1bafa7e998acc09d8b71b07d6c4d9a036
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2021-09-16 08:07:54 +0200

    Move useOpenProp hook to app framework

--- app/utils/hooks/index.ts
+++ app/utils/hooks/index.ts
@@ -2,9 +2,20 @@ import useBoolean from './useBoolean';
 import useErrors from './useErrors';
 import useInput from './useInput';
 import useInputs from './useInputs';
+import useOpenProp from './useOpenProp';
 import useResettableState from './useResettableState';
 import useSearchParam from './useSearchParam';
 import useToggle from './useToggle';
 import useWidthObserver from './useWidthObserver';
 
-export { useBoolean, useErrors, useInput, useInputs, useResettableState, useSearchParam, useToggle, useWidthObserver };
+export {
+    useBoolean,
+    useErrors,
+    useInput,
+    useInputs,
+    useOpenProp,
+    useResettableState,
+    useSearchParam,
+    useToggle,
+    useWidthObserver
+};
