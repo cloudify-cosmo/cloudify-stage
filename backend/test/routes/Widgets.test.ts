37f34b2d1a43e4ee75652de484cec324988f6414
Jakub Madej <jakub.madej@cloudify.co>
2021-10-04 14:32:45 +0200

    Updated test to cover one execution branch

--- backend/test/routes/Widgets.test.ts
+++ backend/test/routes/Widgets.test.ts
@@ -1,17 +1,14 @@
 import request from 'supertest';
 import app from 'app';
-import { listWidgets } from 'handler/WidgetsHandler';
+import { getRBAC } from 'handler/AuthHandler';
 
-jest.mock('handler/WidgetsHandler');
+jest.mock('handler/AuthHandler');
+
+(<jest.Mock>getRBAC).mockResolvedValue({ permissions: {} });
 
 describe('/widgets endpoint', () => {
-    it('allows to list widgets', async () => {
-        const expectedResponse = ['expectedWidget'];
-        (<jest.Mock>listWidgets).mockResolvedValue(expectedResponse);
-
-        const response = await request(app).get('/console/widgets/list');
-
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toStrictEqual(expectedResponse);
+    it('should validate install widget permission', async () => {
+        const response = await request(app).delete('/console/widgets/widgetId');
+        expect(response.statusCode).toBe(403);
     });
 });
