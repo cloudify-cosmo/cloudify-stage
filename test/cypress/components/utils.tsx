f86f07b7a4ef661156a11983079de3f193301736
Jakub Madej <jakub.madej@cloudify.co>
2021-10-11 15:48:49 +0200

    Fixed import order

--- test/cypress/components/utils.tsx
+++ test/cypress/components/utils.tsx
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
 import { applyMiddleware, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { routerMiddleware } from 'connected-react-router';
 
 import createRootReducer from 'app/reducers';
-import { routerMiddleware } from 'connected-react-router';
 
 // eslint-disable-next-line import/prefer-default-export
 export function mountWithProvider(component: ReactNode, initialState?: Record<string, any>) {
