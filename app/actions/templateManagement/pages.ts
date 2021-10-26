25b135d6ab7de25cc4062d51dd9aaff6ce67de18
Jakub Madej <jakub.madej@cloudify.co>
2021-10-26 18:32:43 +0200

    Updated unit test

--- app/actions/templateManagement/pages.ts
+++ app/actions/templateManagement/pages.ts
@@ -10,6 +10,27 @@ import { ReduxState } from '../../reducers';
 
 type Page = ReduxState['templates']['pagesDef'][string] & { id: string; oldId?: string };
 
+export function createPageId(name: string, pageDefs: ReduxState['templates']['pagesDef']) {
+    const ids = _.keysIn(pageDefs);
+
+    // Add suffix to make URL unique if same page name already exists
+    let newPageId = _.snakeCase(name.trim());
+
+    let suffix = 1;
+    _.each(ids, id => {
+        if (id.startsWith(newPageId)) {
+            const index = parseInt(id.substring(newPageId.length), 10) || suffix;
+            suffix = Math.max(index + 1, suffix + 1);
+        }
+    });
+
+    if (suffix > 1) {
+        newPageId += suffix;
+    }
+
+    return newPageId;
+}
+
 export function addPage(page: Page) {
     return {
         type: types.ADD_TEMPLATE_PAGE,
--- app/components/templateManagement/pages/useCreatePageId.ts
+++ app/components/templateManagement/pages/useCreatePageId.ts
@@ -1,28 +1,10 @@
 import _ from 'lodash';
 import { useSelector } from 'react-redux';
 import { ReduxState } from '../../../reducers';
+import { createPageId } from '../../../actions/templateManagement/pages';
 
 export default function useCreatePageId() {
     const pageDefs = useSelector((state: ReduxState) => state.templates.pagesDef);
 
-    return (name: string) => {
-        const ids = _.keysIn(pageDefs);
-
-        // Add suffix to make URL unique if same page name already exists
-        let newPageId = _.snakeCase(name.trim());
-
-        let suffix = 1;
-        _.each(ids, id => {
-            if (id.startsWith(newPageId)) {
-                const index = parseInt(id.substring(newPageId.length), 10) || suffix;
-                suffix = Math.max(index + 1, suffix + 1);
-            }
-        });
-
-        if (suffix > 1) {
-            newPageId += suffix;
-        }
-
-        return newPageId;
-    };
+    return (name: string) => createPageId(name, pageDefs);
 }
