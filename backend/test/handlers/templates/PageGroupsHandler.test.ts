87e0b86f39d17d6fd6c34cc37da2172d52a7a728
Jakub Madej <jakub.madej@cloudify.co>
2021-10-26 18:14:13 +0200

    Updated unit test

--- backend/test/handlers/templates/PageGroupsHandler.test.ts
+++ backend/test/handlers/templates/PageGroupsHandler.test.ts
@@ -3,16 +3,22 @@ import { readdirSync, readJsonSync } from 'fs-extra';
 import * as PageGroupsHandler from 'handler/templates/PageGroupsHandler';
 
 jest.mock('fs-extra');
-(<jest.Mock>readdirSync).mockReturnValue(['group1.json', 'group2.json']);
+(<jest.Mock>readdirSync).mockReturnValueOnce(['group1.json', 'group2.json']);
+(<jest.Mock>readdirSync).mockReturnValueOnce(['group3.json']);
 const file1Content = { name: 'Group 1' };
 (<jest.Mock>readJsonSync).mockReturnValueOnce(file1Content);
 (<jest.Mock>readJsonSync).mockImplementationOnce(() => {
     throw Error();
 });
+const file3Content = { name: 'Group 3', updatedBy: 'admin', updatedAt: 'recently' };
+(<jest.Mock>readJsonSync).mockReturnValueOnce(file3Content);
 
 describe('PageGroupsHandler', () => {
     it('should list page groups', () => {
         const result = PageGroupsHandler.listPageGroups();
-        expect(result).toEqual([{ id: 'group1', name: 'Group 1', updatedAt: '', updatedBy: 'Manager', custom: false }]);
+        expect(result).toEqual([
+            { id: 'group1', name: 'Group 1', updatedAt: '', updatedBy: 'Manager', custom: false },
+            { id: 'group3', name: 'Group 3', updatedAt: 'recently', updatedBy: 'admin', custom: true }
+        ]);
     });
 });
