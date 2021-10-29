fb33c4050d098e18780bd60185707f3ec85532b4
Leon Gilyadov <leon@Leons-MacBook-Pro.local>
2021-10-29 14:05:02 +0300

    RD-2807 Add test use cases to implement for number of blueprints widget

---
+++ test/cypress/integration/widgets/number_of_blueprints_spec.ts
@@ -0,0 +1,51 @@
+describe('Number of Blueprints widget', () => {
+    describe('should display the correct number of resources', () => {
+        it('when there are no resources', () => {
+            // TODO
+        });
+
+        it('when there is 1 resource', () => {
+            // TODO
+        });
+
+        it('when there are 2 resources', () => {
+            // TODO
+        });
+    });
+
+    describe('should open default page on click', () => {
+        it('opens default page on click', () => {
+            // TODO
+        });
+    });
+
+    describe('should open chosen page after configuration change on click', () => {
+        it('when "Page to open on click" is "Blueprints"', () => {
+            // TODO
+        });
+
+        it('when "Page to open on click" is "Dashboard"', () => {
+            // TODO
+        });
+
+        it('when "Page to open on click" is "Environments"', () => {
+            // TODO
+        });
+
+        it('when "Page to open on click" is "Executions"', () => {
+            // TODO            
+        });
+
+        it('when "Page to open on click" is "Resources"', () => {
+            // TODO            
+        });
+
+        it('when "Page to open on click" is "Services"', () => {
+            // TODO            
+        });
+
+        it('when "Page to open on click" is "System setup"', () => {
+            // TODO            
+        });
+    });
+});
