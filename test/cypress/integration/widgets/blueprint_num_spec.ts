1321570a5af0280e38db9f13786381ed639c7a18
Leon Gilyadov <leon@Leons-MacBook-Pro.local>
2021-11-03 10:12:23 +0200

    Fix test open default page on click in blueprintNumWidget

--- test/cypress/integration/widgets/blueprint_num_spec.ts
+++ test/cypress/integration/widgets/blueprint_num_spec.ts
@@ -1,44 +1,45 @@
 describe('Number of Blueprints widget', () => {
     const widgetId = 'blueprintNum';
-    const defaultPageName = 'Blueprints';
-    const defaultPageId = 'blueprints';
 
-    before(() => cy.activate().useWidgetWithDefaultConfiguration(widgetId, { pollingTime: 3 }));
+    function clickOnWidget() {
+        cy.get('.statistic').click();
+    }
 
-    beforeEach(() => cy.visitTestPage());
+    function verifyUrl(expectedPageId: string) {
+        cy.location('pathname').should('be.equal', `/console/page/${expectedPageId}`);
+    }
 
-    it('displays the correct number of resources', () => {
-        cy.stageRequest('/console/sp/blueprints', 'GET').then(data => {
-            const num = _.get(data.body, 'metadata.pagination.total', 0);
-            cy.get('.blueprintNumWidget .value').should($el => expect($el.text().trim()).to.equal(num.toString()));
+    function setWidgetConfiguration(pageToOpenOnClick: string) {
+        cy.editWidgetConfiguration(widgetId, () => {
+            cy.setSingleDropdownValue('Page to open on click', pageToOpenOnClick);
+        });
+    }
+
+    describe('Tests without mockLogin', () => {
+        before(() => cy.activate().login().addWidget(widgetId));
+        
+        it('Opens the default page on click', () => {
+            clickOnWidget();
+            verifyUrl('blueprints');
         });
     });
 
-    describe('opens the correct page on click', () => {
+    describe('Tests with mockLogin', () => {
         const pageName = 'Environments';
-        const pageId = 'page_1';
+        const pageId = 'page_0';
+    
         before(() => {
-            cy.addPage(defaultPageName);
+            cy.activate().useWidgetWithDefaultConfiguration(widgetId, { pollingTime: 3 });
             cy.addPage(pageName);
         });
-
-        function setWidgetConfiguration(pageToOpenOnClick: string) {
-            cy.editWidgetConfiguration(widgetId, () => {
-                cy.setSingleDropdownValue('Page to open on click', pageToOpenOnClick);
+    
+        beforeEach(() => cy.visitTestPage());
+    
+        it('displays the correct number of resources', () => {
+            cy.stageRequest('/console/sp/blueprints', 'GET').then(data => {
+                const num = _.get(data.body, 'metadata.pagination.total', 0);
+                cy.get('.blueprintNumWidget .value').should($el => expect($el.text().trim()).to.equal(num.toString()));
             });
-        }
-
-        function clickOnWidget() {
-            cy.get('.statistic').click();
-        }
-
-        function verifyUrl(expectedPageId: string) {
-            cy.location('pathname').should('be.equal', `/console/page/${expectedPageId}`);
-        }
-
-        it('opens page according to the default configuration', () => {
-            clickOnWidget();
-            verifyUrl(defaultPageId);
         });
 
         it('opens chosen page after configuration change on click', () => {
