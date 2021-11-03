0205abaa5e7fbdd50075abdd6cf09b690833fcba
Jakub Madej <jakub.madej@cloudify.co>
2021-11-03 13:25:30 +0100

    Extended component test

--- test/cypress/components/app/PagesList_spec.tsx
+++ test/cypress/components/app/PagesList_spec.tsx
@@ -13,24 +13,41 @@ describe('PagesList', () => {
 
         cy.contains('Top Level Page')
             .should('be.visible')
-            .and('have.attr', 'href', `${Consts.CONTEXT_PATH}/page/topLevelPage`);
+            .and('have.attr', 'href', `${Consts.CONTEXT_PATH}/page/topLevelPage`)
+            .find('.rocket');
 
-        cy.contains('Group 1').should('be.visible');
+        cy.contains('Group 1').should('be.visible').find('.group');
         cy.contains('Subpage 1').should('not.exist');
         cy.contains('Subpage 2').should('not.exist');
-        cy.contains('Group 2').should('be.visible');
+        cy.contains('Group 2').should('be.visible').find('i').should('have.length', 1);
         cy.contains('Subpage 3').should('not.exist');
         cy.contains('Subpage 4').should('not.exist');
 
         cy.contains('Group 1').click();
-        cy.contains('Subpage 1').should('be.visible').and('have.attr', 'href', `/console/page/subPage1`);
-        cy.contains('Subpage 2').should('be.visible').and('have.attr', 'href', `/console/page/subPage2`);
+        cy.contains('Subpage 1').should('be.visible').and('have.attr', 'href', `/console/page/subPage1`).find('.user');
+        cy.contains('Subpage 2')
+            .should('be.visible')
+            .and('have.attr', 'href', `/console/page/subPage2`)
+            .find('i')
+            .should('not.exist');
 
         cy.contains('Group 2').click();
-        cy.contains('Subpage 1').should('be.visible').and('have.attr', 'href', `/console/page/subPage1`);
-        cy.contains('Subpage 2').should('be.visible').and('have.attr', 'href', `/console/page/subPage2`);
-        cy.contains('Subpage 3').should('be.visible').and('have.attr', 'href', `/console/page/subPage3`);
-        cy.contains('Subpage 4').should('be.visible').and('have.attr', 'href', `/console/page/subPage4`);
+        cy.contains('Subpage 1').should('be.visible').and('have.attr', 'href', `/console/page/subPage1`).find('.user');
+        cy.contains('Subpage 2')
+            .should('be.visible')
+            .and('have.attr', 'href', `/console/page/subPage2`)
+            .find('i')
+            .should('not.exist');
+        cy.contains('Subpage 3')
+            .should('be.visible')
+            .and('have.attr', 'href', `/console/page/subPage3`)
+            .find('i')
+            .should('not.exist');
+        cy.contains('Subpage 4')
+            .should('be.visible')
+            .and('have.attr', 'href', `/console/page/subPage4`)
+            .find('i')
+            .should('not.exist');
 
         cy.contains('Group 1').click();
         cy.contains('Subpage 1').should('not.exist');
@@ -42,7 +59,12 @@ describe('PagesList', () => {
 
     it('handles edit mode', () => {
         cy.fixture('pages/pages_with_groups').then(pages => {
-            mountWithProvider(<PagesList isEditMode pageId={pages[0].id} />, { pages });
+            mountWithProvider(
+                <div style={{ marginLeft: 30 }}>
+                    <PagesList isEditMode pageId={pages[0].id} />
+                </div>,
+                { pages }
+            );
 
             cy.contains('Add Page').should('be.visible');
             cy.get('.icon.edit').should('have.length', pages.length);
@@ -51,13 +73,29 @@ describe('PagesList', () => {
             cy.contains('Add Page Group').click();
             cy.contains('.item', 'Page_Group_0').find('.edit').click({ force: true });
             cy.get('input').type('2{enter}');
-            cy.contains('.item', 'Page_Group_02').find('.remove').click({ force: true });
+            cy.contains('.item', 'Page_Group_0').find('.expand').click();
+            cy.get('.popup').within(() => {
+                cy.get('input').type('blind{enter}');
+                cy.contains('Save').click();
+            });
+            cy.contains('.item', 'Page_Group_02').within(() => {
+                cy.get('.blind').should('be.visible');
+                cy.get('.remove').click({ force: true });
+            });
             cy.contains('Page_Group_02').should('not.exist');
 
             cy.contains('Group 1').click();
             cy.contains('.item', 'Subpage 1').find('.edit').click({ force: true });
             cy.get('input').type('2{enter}');
-            cy.contains('.item', 'Subpage 12').find('.remove').click({ force: true });
+            cy.contains('.item', 'Subpage 1').find('.user').click();
+            cy.get('.popup').within(() => {
+                cy.get('input').type('blind{enter}');
+                cy.contains('Save').click();
+            });
+            cy.contains('.item', 'Subpage 12').within(() => {
+                cy.get('.blind').should('be.visible');
+                cy.get('.remove').click({ force: true });
+            });
             cy.contains('Subpage 12').should('not.exist');
         });
     });
