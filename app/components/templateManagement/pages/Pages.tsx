307345845ff3c72b9284ada582bb50788de5f049
Jakub Madej <jakub.madej@cloudify.co>
2021-11-04 17:22:46 +0100

    Migrated component to TS

--- app/components/templateManagement/pages/Pages.tsx
+++ app/components/templateManagement/pages/Pages.tsx
@@ -1,15 +1,9 @@
-// @ts-nocheck File not migrated fully to TS
-/**
- * Created by pposel on 11/08/2017.
- */
-
-import _ from 'lodash';
-import PropTypes from 'prop-types';
-import React from 'react';
+import { noop } from 'lodash';
+import React, { FunctionComponent } from 'react';
 import i18n from 'i18next';
 import CreatePageModal from './CreatePageModal';
 import TemplateList from '../common/TemplateList';
-import { DataTable, Header, Icon, Label, PopupConfirm, Segment } from '../../basic';
+import { DataTable, Header, Icon, PopupConfirm, Segment } from '../../basic';
 import StageUtils from '../../../utils/stageUtils';
 import ItemsList from '../common/ItemsList';
 import ItemsCount from '../common/ItemsCount';
@@ -17,15 +11,36 @@ import ItemsCount from '../common/ItemsCount';
 const tTemplates = StageUtils.getT('templates');
 const tPageManagement = StageUtils.composeT(tTemplates, 'pageManagement');
 
-export default function Pages({
-    onCanDeletePage,
-    onCreatePage,
-    onDeletePage,
-    onEditPage,
-    onPreviewPage,
-    onSelectPage,
-    pages
-}) {
+interface Page {
+    custom: boolean;
+    id: string;
+    name: string;
+    selected: boolean;
+    templates: string[];
+    pageGroups: string[];
+    updatedAt: string;
+    updatedBy: string;
+}
+
+interface PagesProps {
+    onCanDeletePage: (page: Page) => void;
+    onCreatePage: (pageName: string) => Promise<void>;
+    onDeletePage: (page: Page) => void;
+    onEditPage: (page: Page) => void;
+    onPreviewPage: (page: Page) => void;
+    onSelectPage: (page: Page) => void;
+    pages: Page[];
+}
+
+const Pages: FunctionComponent<PagesProps> = ({
+    onCanDeletePage = noop,
+    onCreatePage = noop,
+    onDeletePage = noop,
+    onEditPage = noop,
+    onPreviewPage = noop,
+    onSelectPage = noop,
+    pages = []
+}) => {
     return (
         <Segment color="red">
             <Header dividing as="h5">
@@ -43,6 +58,7 @@ export default function Pages({
 
                 {pages.map(item => {
                     return (
+                        // @ts-ignore DataTable.RowExpandable returns void
                         <DataTable.RowExpandable key={item.id} expanded={item.selected}>
                             <DataTable.Row key={item.id} selected={item.selected} onClick={() => onSelectPage(item)}>
                                 <DataTable.Data>
@@ -65,19 +81,27 @@ export default function Pages({
                                     {item.custom ? (
                                         <div>
                                             <PopupConfirm
-                                                trigger={<Icon name="remove" link onClick={e => e.stopPropagation()} />}
+                                                trigger={
+                                                    <Icon
+                                                        name="remove"
+                                                        link
+                                                        onClick={(e: Event) => e.stopPropagation()}
+                                                    />
+                                                }
                                                 content={i18n.t(
                                                     'templates.pageManagement.removeConfirm',
                                                     'Are you sure to remove this page?'
                                                 )}
                                                 onConfirm={() => onDeletePage(item)}
                                                 onCanConfirm={() => onCanDeletePage(item)}
+                                                onCancel={undefined}
+                                                defaultOpen={false}
                                             />
                                             <Icon
                                                 name="edit"
                                                 link
                                                 className="updatePageIcon"
-                                                onClick={e => {
+                                                onClick={(e: Event) => {
                                                     e.stopPropagation();
                                                     onEditPage(item);
                                                 }}
@@ -89,7 +113,7 @@ export default function Pages({
                                                 name="search"
                                                 link
                                                 className="updatePageIcon"
-                                                onClick={e => {
+                                                onClick={(e: Event) => {
                                                     e.stopPropagation();
                                                     onPreviewPage(item);
                                                 }}
@@ -99,7 +123,7 @@ export default function Pages({
                                 </DataTable.Data>
                             </DataTable.Row>
 
-                            <DataTable.DataExpandable key={item.id}>
+                            <DataTable.DataExpandable key={item.id} numberOfColumns={1}>
                                 <Segment.Group horizontal>
                                     <TemplateList
                                         width="50%"
@@ -125,34 +149,6 @@ export default function Pages({
             </DataTable>
         </Segment>
     );
-}
-
-Pages.propTypes = {
-    onCanDeletePage: PropTypes.func,
-    onCreatePage: PropTypes.func,
-    onDeletePage: PropTypes.func,
-    onEditPage: PropTypes.func,
-    onPreviewPage: PropTypes.func,
-    onSelectPage: PropTypes.func,
-    pages: PropTypes.arrayOf(
-        PropTypes.shape({
-            custom: PropTypes.bool,
-            id: PropTypes.string,
-            name: PropTypes.string,
-            selected: PropTypes.bool,
-            templates: PropTypes.arrayOf(PropTypes.string),
-            updatedAt: PropTypes.string,
-            updatedBy: PropTypes.string
-        })
-    )
 };
 
-Pages.defaultProps = {
-    onCanDeletePage: _.noop,
-    onCreatePage: _.noop,
-    onDeletePage: _.noop,
-    onEditPage: _.noop,
-    onPreviewPage: _.noop,
-    onSelectPage: _.noop,
-    pages: []
-};
+export default Pages;
