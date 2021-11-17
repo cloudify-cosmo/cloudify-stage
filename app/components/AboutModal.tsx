e3e8a8de1dd8260f1b9726bf331c68d8d5632b61
Jakub Madej <jakub.madej@cloudify.co>
2021-11-17 12:39:33 +0100

    Migrated AboutModal to TS

--- app/components/AboutModal.tsx
+++ app/components/AboutModal.tsx
@@ -1,17 +1,10 @@
-// @ts-nocheck File not migrated fully to TS
-/**
- * Created by jakub.niezgoda on 15/03/2019.
- */
-
-import _ from 'lodash';
-import PropTypes from 'prop-types';
-import React, { useContext } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import { HeaderBar } from 'cloudify-ui-components';
 import i18n from 'i18next';
 
 import { push } from 'connected-react-router';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { Button, CancelButton, Divider, Header, Modal } from './basic';
 import Banner from './banner/Banner';
 import CurrentLicense from './license/CurrentLicense';
@@ -19,9 +12,24 @@ import CurrentVersion from './license/CurrentVersion';
 import EulaLink from './license/EulaLink';
 import stageUtils from '../utils/stageUtils';
 import Consts from '../utils/consts';
+import { ReduxState } from '../reducers';
 
-function AboutModal({ canLicenseManagement, license, onHide, onLicenseManagement, open, version }) {
+interface AboutModalProps {
+    open: boolean;
+    onHide: () => void;
+}
+
+const AboutModal: FunctionComponent<AboutModalProps> = ({ onHide, open }) => {
     const theme = useContext(ThemeContext);
+    const dispatch = useDispatch();
+
+    const canLicenseManagement = useSelector(
+        (state: ReduxState) =>
+            state.manager.license.isRequired && stageUtils.isUserAuthorized(Consts.permissions.LICENSE_UPLOAD, manager)
+    );
+    const version = useSelector((state: ReduxState) => state.manager.version);
+    const license = useSelector((state: ReduxState) => state.manager.license.data);
+
     return (
         <Modal open={open} onClose={onHide}>
             <Modal.Header style={{ padding: 0, backgroundColor: theme.mainColor }}>
@@ -48,38 +56,13 @@ function AboutModal({ canLicenseManagement, license, onHide, onLicenseManagement
                         content={i18n.t('help.aboutModal.licenseManagement', 'License Management')}
                         icon="key"
                         color="yellow"
-                        onClick={onLicenseManagement}
+                        onClick={() => dispatch(push(Consts.LICENSE_PAGE_PATH))}
                     />
                 )}
                 <CancelButton content={i18n.t('help.aboutModal.close', 'Close')} onClick={onHide} />
             </Modal.Actions>
         </Modal>
     );
-}
-
-const mapStateToProps = state => {
-    const manager = _.get(state, 'manager', {});
-
-    return {
-        canLicenseManagement:
-            _.get(manager, 'license.isRequired', false) &&
-            stageUtils.isUserAuthorized(Consts.permissions.LICENSE_UPLOAD, manager),
-        version: _.get(manager, 'version', {}),
-        license: _.get(manager, 'license.data', {})
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        onLicenseManagement: () => dispatch(push(Consts.LICENSE_PAGE_PATH))
-    };
-};
-
-const ConnectedModal = connect(mapStateToProps, mapDispatchToProps)(AboutModal);
-
-ConnectedModal.propTypes = {
-    open: PropTypes.bool.isRequired,
-    onHide: PropTypes.func.isRequired
 };
 
 export default ConnectedModal;
