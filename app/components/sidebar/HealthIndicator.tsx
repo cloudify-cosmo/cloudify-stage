8178780ff49c0b2aa17983855298a316f3cc1065
Jakub Madej <jakub.madej@cloudify.co>
2021-11-19 16:53:56 +0100

    Introduced common component for dropdown based sidebar items

--- app/components/sidebar/HealthIndicator.tsx
+++ app/components/sidebar/HealthIndicator.tsx
@@ -1,13 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Dropdown, Icon } from '../basic';
-import SideBarItem from './SideBarItem';
+import { Icon } from '../basic';
 import SideBarItemIcon from './SideBarItemIcon';
 import StageUtils from '../../utils/stageUtils';
 import { getClusterStatus } from '../../actions/clusterStatus';
 import SystemServicesStatus from '../../containers/status/SystemServicesStatus';
 import { clusterStatusEnum } from '../shared/cluster/consts';
 import { ReduxState } from '../../reducers';
+import SideBarDropdownItem from './SideBarDropdownItem';
 
 const t = StageUtils.getT('users');
 
@@ -21,22 +21,20 @@ const HealthIndicator: FunctionComponent = () => {
     const systemStatus = useSelector((state: ReduxState) => state.manager.clusterStatus.status);
     const dotColor = dotColors[systemStatus];
 
-    const trigger = (
-        <SideBarItem>
-            <Icon.Group>
-                <SideBarItemIcon name="heartbeat" />
-                {dotColor && <Icon corner="top right" name="circle" color={dotColor} />}
-            </Icon.Group>
-            {t('health')}
-        </SideBarItem>
-    );
-
     return (
-        <Dropdown trigger={trigger} pointing="left" icon={null} fluid onOpen={() => dispatch(getClusterStatus())}>
-            <Dropdown.Menu style={{ padding: 5, margin: 0, height: 267 }}>
-                <SystemServicesStatus />
-            </Dropdown.Menu>
-        </Dropdown>
+        <SideBarDropdownItem
+            icon={
+                <Icon.Group>
+                    <SideBarItemIcon name="heartbeat" />
+                    {dotColor && <Icon corner="top right" name="circle" color={dotColor} />}
+                </Icon.Group>
+            }
+            label={t('health')}
+            onOpen={() => dispatch(getClusterStatus())}
+            style={{ padding: 5, height: 267 }}
+        >
+            <SystemServicesStatus />
+        </SideBarDropdownItem>
     );
 };
 
