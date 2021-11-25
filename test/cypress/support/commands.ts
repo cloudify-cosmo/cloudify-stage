e7e98c24b3d6368fc2b5db86b4119e431f9a38ff
Jakub Madej <jakub.madej@cloudify.co>
2021-11-25 15:18:06 +0100

    Moved user menu from top to side bar

--- test/cypress/support/commands.ts
+++ test/cypress/support/commands.ts
@@ -211,8 +211,10 @@ const commands = {
     },
     clickSidebarItem: (name: string, expectedPageId: string | null = null) => {
         cy.log(`Clicking '${name}' sidebar item`);
-        cy.get('.sidebar.menu').should('be.visible').trigger('mouseover').contains(name).should('be.visible');
-        cy.get('.sidebar.menu').contains(name).click();
+        cy.get('.sidebar.menu').should('be.visible');
+        cy.get('.sidebar.menu').trigger('mouseover');
+        cy.get('.sidebar.menu').contains('.item', name).should('be.visible');
+        cy.get('.sidebar.menu').contains('.item', name).click();
         if (expectedPageId) {
             cy.location('pathname').should('be.equal', `/console/page/${expectedPageId}`);
         }
