dee5933098d841fde470dca349fec008505709c2
Leon Gilyadov <leon.good.life@gmail.com>
2021-12-15 10:14:51 +0200

    RD-1607 Change missing secrets error in Deploy Blueprints (#1693)
    * RD-1607 Add missing secrets in Deploy Blueprints
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Hide error after adding secrets
    
    * Fix PR
    
    * Fix lint
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Fix PR
    
    * Add system test
    
    * Fix PR
    
    * Extend system test
    
    * Change 'define' with 'it' to 'it' in the system test
    
    * Use Stage.Utils.getT function
    
    * Move code to a new function: comaSeparatedValuesInBrackets
    
    * Fix failed system test
    
    * refactor function name to parseCommaSeparatedValuesInBrackets
    
    * Fix exceeding maximum bundle size
    
    * Move the system test to deployment button and address the PR comments
    
    * Moved Visibility type from Stage to the component
    
    * Use .within in the test
    
    * Use interceptSp for missing secret creation
    
    * Reuse type Visibility in SecretActions and SecretsModal files
    
    * Add .as() and .wait() to cy.interceptSp()
    
    * Revert the function clickIfExist that I wrote
    
    * Revert package-lock.json to be as in the master branch
    
    * Revert package-lock.json to be as in the master branch

--- test/cypress/integration/widgets/deployment_button_spec.ts
+++ test/cypress/integration/widgets/deployment_button_spec.ts
@@ -2,13 +2,15 @@
 describe('Create Deployment Button widget', () => {
     const resourcePrefix = 'deploy_test_';
     const testBlueprintId = `${resourcePrefix}bp`;
+    const requiredSecretsBlueprint = `${resourcePrefix}required_secrets_type`;
 
     before(() => {
         cy.activate('valid_trial_license').usePageMock('deploymentButton').mockLogin();
 
         cy.deleteDeployments(resourcePrefix, true)
             .deleteBlueprints(resourcePrefix, true)
-            .uploadBlueprint('blueprints/simple.zip', testBlueprintId);
+            .uploadBlueprint('blueprints/simple.zip', testBlueprintId)
+            .uploadBlueprint('blueprints/required_secrets.zip', requiredSecretsBlueprint);
 
         const types = ['boolean', 'dict', 'float', 'integer', 'list', 'regex', 'string'];
         types.forEach(type =>
@@ -145,7 +147,7 @@ describe('Create Deployment Button widget', () => {
 
     describe('handles errors during deploy & install process', () => {
         afterEach(() => {
-            cy.get(`.actions > .ui:nth-child(1)`).click();
+            cy.get('.actions > .ui:nth-child(1)').click();
         });
 
         it('handles data validation errors', () => {
@@ -225,6 +227,32 @@ describe('Create Deployment Button widget', () => {
                     'constraint pattern(Ubuntu \\d{2}\\.\\d{2}) operator.'
             );
             cy.get('@string_constraint_pattern').should('have.class', 'error');
+        });
+
+        it('should open Missing Secrets Error message when deploying blueprint with missing secrets', () => {
+            const secretName = 'test';
+
+            selectBlueprintInModal('required_secrets');
+            cy.stageRequest(`/console/sp/secrets/${secretName}`, 'DELETE', { failOnStatusCode: false });
+
+            cy.get('input[name=deploymentName]').type('blahBlahBlah');
+            cy.contains('.modal button', 'Deploy').click();
+            cy.get('form.error .error').within(() => {
+                cy.get('.header').should('have.text', 'Missing Secrets Error');
+                cy.get('p').should('have.text', 'The following required secrets are missing in this tenant:');
+                cy.get('.item').should('have.text', secretName);
+            });
+
+            cy.contains('form.error button', 'Add Missing Secrets').click();
+            cy.contains('.secretsModal button', 'Add').click();
+            cy.get('.secretsModal .error .header').should('have.text', 'Errors in the form');
+            cy.get('.secretsModal .error li').should('have.text', 'Please provide values for secrets');
+
+            cy.get('.secretsModal input').type('aaa');
+            cy.interceptSp('PUT', `/secrets/${secretName}`, { statusCode: 200 }).as('addSecrets');
+            cy.contains('.secretsModal button', 'Add').click();
+            cy.get('form.error .error').should('not.exist');
+            cy.wait('@addSecrets');
         });
     });
 
