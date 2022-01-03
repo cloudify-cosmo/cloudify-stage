3f99df2ad8ac2f6d1c6bcece587b1b3f0aca559f
kubama <jakub.madej@amartus.com>
2022-01-03 15:37:28 +0100

    RD-3661 Added support for Terraform template URL authentication (#1729)

--- widgets/blueprints/src/TerraformActions.ts
+++ widgets/blueprints/src/TerraformActions.ts
@@ -7,7 +7,12 @@ export default class TerraformActions {
         return this.toolbox.getInternal().doPost('/terraform/blueprint', { body });
     }
 
-    doGetTemplateModules(templateZipUrl: string) {
-        return this.toolbox.getInternal().doPost('/terraform/resources', { params: { zipUrl: templateZipUrl } });
+    doGetTemplateModules(templateZipUrl: string, username?: string, password?: string) {
+        const headers = username ? { Authorization: `Basic ${btoa(`${username}:${password}`)}` } : undefined;
+        return this.toolbox.getInternal().doPost('/terraform/resources', {
+            params: { zipUrl: templateZipUrl },
+            headers,
+            validateAuthentication: false
+        });
     }
 }
