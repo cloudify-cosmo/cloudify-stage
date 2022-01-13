b7b0487a95f91764f246bb3518c06ae4da1cace1
Norbert Kruk <norbert.kruk@amartus.com>
2022-01-13 21:33:19 +0100

    RD-3974: Added delay to the response of the request

--- test/cypress/integration/user_menu_spec.ts
+++ test/cypress/integration/user_menu_spec.ts
@@ -18,6 +18,14 @@ describe('User Menu', () => {
         cy.contains(optionName).should('not.exist');
     };
 
+    const delayTenantsRefresh = (delayTime: number) => {
+        cy.intercept('/console/sp/tenants?_include=name&_get_all_results=true', request => {
+            request.on('response', response => {
+                response.setDelay(delayTime);
+            });
+        });
+    };
+
     before(() => {
         cy.activate()
             .deleteAllUsersAndTenants()
@@ -56,6 +64,7 @@ describe('User Menu', () => {
 
         cy.log('Adding new tenant');
         cy.addTenant(newTenantName);
+        delayTenantsRefresh(250);
         cy.contains(Consts.DEFAULT_TENANT).click();
 
         cy.contains('Tenant selection')
