fc7a2d5f246988853e0edc5b7672c7efc4a5024b
Norbert Kruk <norbert.kruk@amartus.com>
2022-01-18 13:43:06 +0100

    RD-3978: Modified validation rules inside the terraform modal

--- widgets/blueprints/src/TerraformModal.tsx
+++ widgets/blueprints/src/TerraformModal.tsx
@@ -50,6 +50,7 @@ function getDynamicTableDropdown(options: DropdownProps['options']) {
 }
 
 const cloudifyResourceRegexp = /^[a-zA-Z][a-zA-Z0-9._-]*$/;
+const cloudifyStaticValueRegexp = /^[a-zA-Z0-9._-]*$/;
 
 const dynamicTableFieldStyle = { height: 38 };
 
@@ -200,7 +201,21 @@ export default function TerraformModal({
             if (
                 find(
                     variablesList,
-                    variable => !isEmpty(variable.value) && !variable.value.match(cloudifyResourceRegexp)
+                    variable =>
+                        !isEmpty(variable.value) &&
+                        variable.source === 'static' &&
+                        !variable.value.match(cloudifyStaticValueRegexp)
+                )
+            ) {
+                formErrors[`${errorPrefix}ValueInvalid`] = tVariableError('staticValueInvalid');
+            }
+            if (
+                find(
+                    variablesList,
+                    variable =>
+                        !isEmpty(variable.value) &&
+                        variable.source !== 'static' &&
+                        !variable.value.match(cloudifyResourceRegexp)
                 )
             ) {
                 formErrors[`${errorPrefix}ValueInvalid`] = tVariableError('valueInvalid');
