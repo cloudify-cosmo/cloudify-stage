95b908d279b3237db1d9816654e81fd7d388607c
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-01-20 14:38:51 +0100

    Remove unused import

--- backend/routes/Auth.ts
+++ backend/routes/Auth.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import passport from 'passport';
 import _ from 'lodash';
 import type { CookieOptions, Request } from 'express';
 
