505dec4670d1ed1ef3a0c096cead7e97cd3dc1e9
Jakub Wasek <jakub.wasek@amartus.com>
2022-01-21 12:26:59 +0100

    RD-2809: Integration tests provided for widget plugins num

--- test/cypress/integration/widgets/plugins_num_spec.ts
+++ test/cypress/integration/widgets/plugins_num_spec.ts
@@ -1,40 +1,44 @@
 describe('Number of Plugins widget', () => {
     const widgetId = 'pluginsNum';
+    const page = 'page_0_test';
+    const pageId = 'page_0';
 
     function clickOnWidget() {
-        cy.get('.pluginsNumWidget .statistic').click();
+        cy.getWidget(widgetId).find('.statistic').click();
     }
 
     function verifyUrl(expectedPageId: string) {
         cy.location('pathname').should('be.equal', `/console/page/${expectedPageId}`);
     }
 
-    before(() => cy.activate('valid_trial_license').mockLogin().addWidget(widgetId));
+    before(() => cy.activate('valid_trial_license').mockLogin().addPage(page).addWidget(widgetId));
 
-    it('Opens the default page on click', () => {
+    it('opens the default page on click', () => {
         const defaultPage = 'plugins';
 
         clickOnWidget();
         verifyUrl(defaultPage);
     });
 
-    it('Opens the "testpage" page on click at the widget with customized configuration', () => {
-        const page = 'page_0_test';
-        const pageId = 'page_0';
+    describe('', () => {
+        beforeEach(() => {
+            cy.visitPage(pageId);
+        });
 
-        cy.addPage(page).addWidget(widgetId);
-        cy.setSearchableDropdownConfigurationField(widgetId, 'Page to open on click', pageId);
-        clickOnWidget();
+        it('displays the correct number of plugins', () => {
+            cy.stageRequest('/console/sp/plugins', 'GET').then(data => {
+                const num = _.get(data.body, 'items', []).length;
+                cy.getWidget(widgetId)
+                    .find('.value')
+                    .should($el => expect($el.text().trim()).to.equal(num.toString()));
+            });
+        });
 
-        verifyUrl(pageId);
-    });
+        it('opens the "testpage" page on click at the widget with customized configuration', () => {
+            cy.setSearchableDropdownConfigurationField(widgetId, 'Page to open on click', pageId);
+            clickOnWidget();
 
-    it('displays the correct number of plugins', () => {
-        cy.stageRequest('/console/sp/plugins', 'GET').then(data => {
-            const num = _.get(data.body, 'items', []).length;
-            cy.addWidget(widgetId)
-                .get('.pluginsNumWidget .value')
-                .should($el => expect($el.text().trim()).to.equal(num.toString()));
+            verifyUrl(pageId);
         });
     });
 });
