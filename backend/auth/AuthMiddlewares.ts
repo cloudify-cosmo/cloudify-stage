d3f853a944b3471c2c728848c6dd7498affc3883
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-01-25 10:38:16 +0100

    Fix AuthenticateMiddleware type

--- backend/auth/AuthMiddlewares.ts
+++ backend/auth/AuthMiddlewares.ts
@@ -1,7 +1,11 @@
 import passport from 'passport';
 import type { Request, Response, NextFunction } from 'express';
 
-type AuthenticateMiddleware = (req: Request, res: Response, next: NextFunction) => ReturnType<typeof authenticateWith>;
+type AuthenticateMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => ReturnType<ReturnType<typeof authenticateWith>>;
 
 function authenticateWith(strategy: 'cookie' | 'token' | 'saml') {
     return passport.authenticate(strategy, { session: false });
