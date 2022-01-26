291f6bbc13f940f555d516c7df1308b2d310a22f
Jakub Wasek <jakub.wasek@amartus.com>
2022-01-26 15:08:58 +0100

    RD-2570: Make sure that videos are removed for passed tests

--- test/cypress/plugins/index.ts
+++ test/cypress/plugins/index.ts
@@ -12,11 +12,25 @@
 // the project's config changing)
 
 import performCommonSetup from 'cloudify-ui-common/cypress/plugins';
+import del from 'del';
 // @ts-ignore Webpack config not in TS
 import getWebpackConfig from '../../../webpack.config';
 
 const setupPluginsAndConfig: Cypress.PluginConfig = (on, config) => {
     config.baseUrl = 'http://localhost:4000';
+
+    // Delete the recorded video for specs that had no retry attempts
+    // https://github.com/cypress-io/cypress/issues/16377
+    on('after:spec', async (spec, results) => {
+        if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests?.some(test => test.attempts.some(attempt => attempt?.state === 'failed'));
+            if (!failures) {
+                // delete the video if the spec passed and no tests retried
+                await del(results.video);
+            }
+        }
+    });
 
     return performCommonSetup(on, config, getWebpackConfig({}, { mode: 'test' })[0]);
 };
