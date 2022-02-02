5c82edc9ce33322b2776c1d3d99d3917244f7511
Leon Gilyadov <leon.good.life@gmail.com>
2022-02-03 01:15:22 +0200

    Update types

--- widgets/common/src/types.ts
+++ widgets/common/src/types.ts
@@ -1,6 +1,37 @@
+import { ComponentProps } from 'react';
+
+export type DropdownValue = string | string[] | null;
+
+export type OnChange = (event: Event, field: typeof Stage.Basic.UnsafelyTypedFormField) => void;
+export type OnDropdownChange = ComponentProps<typeof Stage.Basic.Dropdown>['onChange'];
+export type OnCheckboxChange = ComponentProps<typeof Stage.Basic.Checkbox>['onChange'];
+
 export type Workflow = {
     name: string;
     parameters: Record<string, { description?: string; default?: any; type?: any }>;
     plugin: string;
-    operation?: string;
 };
+
+export type BaseWorkflowInputs = Record<
+    string,
+    {
+        type?: string;
+        default?: string;
+        constraints?: {
+            pattern: string;
+        }[];
+    }
+>;
+
+export type UserWorkflowInputsState = Record<
+    string,
+    | string
+    | number
+    | boolean
+    | string[]
+    | number[]
+    | boolean[]
+    | Record<string, string | number | boolean>
+    | null
+    | undefined
+>;
