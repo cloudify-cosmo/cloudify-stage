e5025489ab2996d71e7009f57ad839b44aeb3bf2
Norbert Kruk <norbert.kruk@amartus.com>
2022-02-11 08:44:42 +0100

    RD-3949: Implemented retry mechanism

--- backend/routes/ContactDetails.ts
+++ backend/routes/ContactDetails.ts
@@ -10,18 +10,23 @@ const logger = getLogger('ContactDetails');
 const router = express.Router();
 const contactDetailsFilePath = getResourcePath('submittedContactDetails.json', true);
 
-const hubspotRequest = (_data: any, createError?: boolean): Promise<string> =>
+router.use(bodyParser.json());
+// router.use(validateEdition);
+
+const hubspotRequest = (_data: ContactDetailsDto, createError?: boolean): Promise<HubspotResponseDto> =>
     new Promise((resolve, reject) => {
         setTimeout(() => {
             if (createError) {
                 reject('error');
             } else {
-                resolve('custom-costumer-id');
+                resolve({
+                    customer_id: '3fdsafdsafads-fads-fdsafsa'
+                });
             }
         }, 500);
     });
 
-const managerRequest = (_data: any, createError?: boolean): Promise<void> =>
+const userLicenseRequest = (_data: any, createError?: boolean): Promise<void> =>
     new Promise((resolve, reject) => {
         setTimeout(() => {
             if (createError) {
@@ -33,7 +38,6 @@ const managerRequest = (_data: any, createError?: boolean): Promise<void> =>
     });
 
 /* eslint-disable camelcase */
-
 interface ContactDetailsDto {
     first_name: string;
     last_name: string;
@@ -44,22 +48,23 @@ interface ContactDetailsDto {
 }
 
 interface HubspotResponseDto {
-    clientId: string;
+    customer_id: string;
 }
+/* eslint-enable camelcase */
 
 enum ContactDetailsStatus {
     NOT_RECEIVED,
-    RECEIVED,
-    SUBMITTED_TO_HUBSPOT,
-    SUBMITTED_TO_MANAGER
+    RECEIVED_FROM_USER,
+    SUBMITED_TO_HUBSPOT,
+    FULLY_SUBMITTED
 }
 
 interface StoredContactDetails {
     status: ContactDetailsStatus;
-    data: ContactDetailsDto | HubspotResponseDto;
+    data: ContactDetailsDto | HubspotResponseDto | Record<string, never>;
 }
 
-const getContactDetailsFile = (): StoredContactDetails | null => {
+const getStoredContactDetails = (): StoredContactDetails | null => {
     try {
         const fileContent = fs.readFileSync(contactDetailsFilePath, 'utf8');
         const jsonContent = JSON.parse(fileContent);
@@ -69,12 +74,16 @@ const getContactDetailsFile = (): StoredContactDetails | null => {
     }
 };
 
-const getContactDetailsStatus = (): ContactDetailsStatus => {
-    const contactDetails = getContactDetailsFile();
+const getContactDetailsStatus = (contactDetails: StoredContactDetails | null): ContactDetailsStatus => {
     return contactDetails?.status || ContactDetailsStatus.NOT_RECEIVED;
 };
 
-const saveContactDetailsData = (contactDetails: StoredContactDetails) => {
+const saveContactDetailsData = (status: ContactDetailsStatus, detailsData: StoredContactDetails['data']) => {
+    const contactDetails: StoredContactDetails = {
+        status,
+        data: detailsData
+    };
+
     try {
         fs.writeFileSync(contactDetailsFilePath, JSON.stringify(contactDetails), 'utf8');
     } catch (error) {
@@ -82,23 +91,71 @@ const saveContactDetailsData = (contactDetails: StoredContactDetails) => {
     }
 };
 
-router.use(bodyParser.json());
-// router.use(validateEdition);
+const makeHubspotRequest = async (contactDetails: ContactDetailsDto): Promise<HubspotResponseDto> => {
+    try {
+        return await hubspotRequest(contactDetails);
+    } catch (error) {
+        saveContactDetailsData(ContactDetailsStatus.RECEIVED_FROM_USER, contactDetails);
+        throw error;
+    }
+};
 
-router.get('/', (req, res) => {
-    const contactDetailsStatus = getContactDetailsStatus();
+const makeUserLicenseRequest = async (hubspotData: HubspotResponseDto): Promise<void> => {
+    try {
+        return await userLicenseRequest(hubspotData);
+    } catch (error) {
+        saveContactDetailsData(ContactDetailsStatus.SUBMITED_TO_HUBSPOT, hubspotData);
+        throw error;
+    }
+};
+
+router.get('/', async (_req, res) => {
+    const contactDetails = getStoredContactDetails();
+    const contactDetailsStatus = getContactDetailsStatus(contactDetails);
+    const detailsShouldBeSubmittedAgain =
+        contactDetailsStatus === ContactDetailsStatus.RECEIVED_FROM_USER ||
+        contactDetailsStatus === ContactDetailsStatus.SUBMITED_TO_HUBSPOT;
+
     res.send({
         details_received: contactDetailsStatus !== ContactDetailsStatus.NOT_RECEIVED
     });
+
+    // Further functionality should be transparent for the user
+    // Because of that, the functionality below is implemented after sending a response to the user
+    if (detailsShouldBeSubmittedAgain) {
+        try {
+            const hubspotResponse =
+                contactDetailsStatus === ContactDetailsStatus.SUBMITED_TO_HUBSPOT
+                    ? (contactDetails!.data as HubspotResponseDto)
+                    : await makeHubspotRequest(contactDetails!.data as ContactDetailsDto);
+
+            await makeUserLicenseRequest(hubspotResponse);
+            saveContactDetailsData(ContactDetailsStatus.FULLY_SUBMITTED, {});
+        } catch (error) {
+            logger.error(error);
+        }
+    }
 });
 
-router.post('/', validateFormData, (req, res) => {
-    saveContactDetailsData({
-        status: ContactDetailsStatus.RECEIVED,
-        data: { clientId: '4' }
-    });
-    res.send(req.body);
-});
+router.post(
+    '/',
+    validateFormData,
+    async (req, res): Promise<void> => {
+        const contactDetails: ContactDetailsDto = req.body;
+
+        res.send({});
+
+        // Further submission of the data should be transparent for the user
+        // Because of that, the functionality below is implemented after sending a response to the user
+        try {
+            const hubspotResponse = await makeHubspotRequest(contactDetails);
+            await makeUserLicenseRequest(hubspotResponse);
+            saveContactDetailsData(ContactDetailsStatus.FULLY_SUBMITTED, {});
+        } catch (error) {
+            logger.error(error);
+        }
+    }
+);
 
 export default router;
 
