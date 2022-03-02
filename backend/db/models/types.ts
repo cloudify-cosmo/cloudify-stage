03982891eb2f0d75d9659996098609e364e65934
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-03-02 20:43:05 +0100

    Improve types imports

--- backend/db/models/types.ts
+++ backend/db/models/types.ts
@@ -1,6 +1,4 @@
-export type { ModelFactory } from 'cloudify-ui-common/backend/db';
-// eslint-disable-next-line import/no-extraneous-dependencies,node/no-unpublished-import
-export type { Model, Optional } from 'sequelize';
+export type { ModelFactory, Model, Optional } from 'cloudify-ui-common/backend/db';
 
 export interface CommonAttributes {
     readonly id: number;
