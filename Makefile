6efa83cf2385138f98d31b2721fe47f13b7c98c8
Jakub Wasek <jakub.wasek@amartus.com>
2022-03-02 13:58:19 +0100

    RD-4051: Improving Makefile and docker-compose.yml

--- Makefile
+++ Makefile
@@ -1,17 +1,31 @@
+CREATE_REQUIRED_DIRECTORY=$(shell test ! -d /var/log/cloudify/stage && sudo mkdir -p /var/log/cloudify/stage && sudo chmod 777 /var/log/cloudify/stage)
+
 install:
 	docker-compose up -d postgres-cfy
+	@echo $(CREATE_REQUIRED_DIRECTORY)
 	cp conf/me.json.template conf/me.json
 	sed -i 's/<MANAGER_IP>/127.0.0.1/g' conf/me.json
 	npm run beforebuild
 	cd backend && npm run db-migrate
 	docker-compose stop
 
+install-dev: install
+	bash ./scripts/loadLatestPremiumDockerImage.sh
+
 # Run it with command:
 # $ make -j2 up
 up: docker-up backend-up frontend-up
 
+# Run it with command:
+# $ make -j2 up-dev
+up-dev: docker-up-dev backend-up frontend-up
 docker-up:
-	docker-compose up -d
+	docker-compose down cfy_manager_local
+	docker-compose --profile prod up -d
+
+docker-up-dev:
+	docker-compose down cfy_manager_local_dockerhub
+	docker-compose --profile dev up -d
 
 backend-up:
 	cd backend && npm run devStart
---
+++ scripts/loadLatestPremiumDockerImage.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# The script has been created to provide a tool for downloading and installing cloudify docker image with one command.
+# It is mainly used inside of Makefile but it can be also used separately.
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[", ]//g');
+
+PACKAGE_VERSION_WITH_BACKSLESH="${PACKAGE_VERSION/-/\/}";
+
+FILENAME="cloudify-manager-aio-docker-${PACKAGE_VERSION}-x86_64.tar";
+
+# we are getting premium image version URL
+LINK="https://repository.cloudifysource.org/cloudify/${PACKAGE_VERSION_WITH_BACKSLESH}-release/${FILENAME}";
+# premium images addresses:
+# https://github.com/cloudify-cosmo/cloudify-premium/blob/master/packages-urls/docker-image-release.yaml
+# community image addresses:
+# https://github.com/cloudify-cosmo/cloudify-versions/blob/master/packages-urls/docker-image-release.yaml
+
+echo "Docker image URL:${LINK}";
+echo "Downloading docker image...";
+
+wget $LINK;
+
+echo "Loading image into the docker...";
+docker load < ${FILENAME};
+
+rm $FILENAME;
+echo "Removed downloaded image";
