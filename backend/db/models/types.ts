0fa00b3ab4b06dc05440e4c8e31bb1d97c1ae818
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-03-02 21:37:58 +0100

    Use types directly from sequelize package

--- backend/db/models/types.ts
+++ backend/db/models/types.ts
@@ -1,4 +1,6 @@
-export type { ModelFactory, Model, Optional } from 'cloudify-ui-common/backend/db';
+// eslint-disable-next-line import/no-extraneous-dependencies,node/no-unpublished-import
+export type { Model, Optional } from 'sequelize';
+export type { ModelFactory } from 'cloudify-ui-common/backend/db';
 
 export interface CommonAttributes {
     readonly id: number;
--- backend/migrations/common/types.ts
+++ backend/migrations/common/types.ts
@@ -1,4 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies,node/no-unpublished-import
+import { DataTypes } from 'sequelize';
 import { UpDownFunction } from 'cloudify-ui-common/backend/migration';
 
-export type { DataTypes, QueryInterface, QueryInterfaceIndexOptions } from 'cloudify-ui-common/backend/migration';
+export type DataTypes = typeof DataTypes;
+// eslint-disable-next-line import/no-extraneous-dependencies,node/no-unpublished-import
+export type { QueryInterface, QueryInterfaceIndexOptions } from 'sequelize';
 export type MigrationObject = { up: UpDownFunction; down: UpDownFunction };
