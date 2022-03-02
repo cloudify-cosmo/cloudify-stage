99776828986c748464ec3d1bb34cda57238be933
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-03-02 13:08:02 +0100

    Add sequelize types to dependencies

---
+++ backend/migrations/types.ts
@@ -0,0 +1,7 @@
+/* eslint-disable node/no-unpublished-import, import/no-extraneous-dependencies */
+import { DataTypes } from 'sequelize';
+import type { UpDownFunction } from 'cloudify-ui-common/backend';
+
+export type { QueryInterface, QueryInterfaceIndexOptions } from 'sequelize';
+export type DataTypes = typeof DataTypes;
+export type MigrationObject = { up: UpDownFunction; down: UpDownFunction };
