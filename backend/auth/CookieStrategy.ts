c8dec21aa09c1a709c79ae7c116b0cab7fdbbaba
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-03-24 12:28:42 +0100

    Passport strategies clean-up

--- backend/auth/CookieStrategy.ts
+++ backend/auth/CookieStrategy.ts
@@ -1,10 +1,11 @@
-// @ts-nocheck File not migrated fully to TS
 import { Strategy } from 'passport-cookie';
 import { getUser } from '../handler/AuthHandler';
 import { TOKEN_COOKIE_NAME } from '../consts';
 
+type PassportDoneFunction = (error: any, user: any, info?: any) => any;
+
 export default () => {
-    return new Strategy({ cookieName: TOKEN_COOKIE_NAME }, (token, done) =>
+    return new Strategy({ cookieName: TOKEN_COOKIE_NAME }, (token: string, done: PassportDoneFunction) =>
         getUser(token)
             .then(user => done(null, user))
             .catch(err => done(null, false, err + token))
--- backend/auth/SamlStrategy.ts
+++ backend/auth/SamlStrategy.ts
@@ -1,6 +1,5 @@
-// @ts-nocheck File not migrated fully to TS
-
 import { Strategy } from 'passport-saml';
+import type { VerifyWithoutRequest } from 'passport-saml';
 import fs from 'fs';
 import { getConfig } from '../config';
 
@@ -9,7 +8,7 @@ export default () => {
     try {
         cert = fs.readFileSync(getConfig().app.saml.certPath, 'utf-8');
     } catch (e) {
-        throw new Error('Could not read SAML certificate [saml.certPath]', e);
+        throw new Error('Could not read SAML certificate [saml.certPath]');
     }
 
     return new Strategy(
@@ -18,6 +17,6 @@ export default () => {
             entryPoint: getConfig().app.saml.ssoUrl,
             cert
         },
-        (user, done) => done(null, user)
+        ((user, done) => done(null, user!)) as VerifyWithoutRequest
     );
 };
