50b75edc75349537505c7ed0a8fa774779fdc7c6
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-03-25 08:36:08 +0100

    Improve logging in CookieStrategy.ts

--- backend/auth/CookieStrategy.ts
+++ backend/auth/CookieStrategy.ts
@@ -1,13 +1,19 @@
 import { Strategy } from 'passport-cookie';
 import { getUser } from '../handler/AuthHandler';
 import { TOKEN_COOKIE_NAME } from '../consts';
+import LoggerHandler from '../handler/LoggerHandler';
 
 type PassportDoneFunction = (error: any, user: any, info?: any) => any;
+
+const logger = LoggerHandler.getLogger('Passport');
 
 export default () => {
     return new Strategy({ cookieName: TOKEN_COOKIE_NAME }, (token: string, done: PassportDoneFunction) =>
         getUser(token)
             .then(user => done(null, user))
-            .catch(err => done(null, false, err + token))
+            .catch(err => {
+                logger.error(`User authentication failed with token "${token}". Error:`, err);
+                return done(null, false, err + token);
+            })
     );
 };
