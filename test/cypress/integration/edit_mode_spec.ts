f66f9c0bb47496272ca990a6311e79277c52eaf9
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-04-01 09:05:17 +0200

    RD-3672 Edit Mode spec adjustments (#1903)
    * Remove checking 'POST /console/ua' request body from Edit Mode spec
    * Remove test retries from Edit Mode spec
    * Migrate Edit Mode spec to TypeScript
    
    Co-authored-by: kubama <jakub.madej@amartus.com>

--- test/cypress/integration/edit_mode_spec.ts
+++ test/cypress/integration/edit_mode_spec.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck File not migrated fully to TS
 describe('Edit mode', () => {
     before(() => cy.activate('valid_trial_license').removeCustomWidgets());
 
@@ -9,36 +8,27 @@ describe('Edit mode', () => {
         cy.intercept('POST', '/console/ua').as('updateUserApps');
     });
 
-    it('should allow to edit widget settings', { retries: { runMode: 2 } }, () => {
+    it('should allow to edit widget settings', () => {
         cy.get('.blueprintsWidget .setting').click({ force: true });
 
-        cy.get('.pollingTime input').type(0);
+        cy.get('.pollingTime input').type('0');
 
         cy.contains('Save').click();
-        cy.wait('@updateUserApps')
-            .its('request.body')
-            .should('have.nested.property', 'appData.pages[0].layout[0].content[1].configuration.pollingTime', 100);
+        cy.wait('@updateUserApps');
     });
 
     it('should allow to remove widget', () => {
         cy.get('.blueprintsWidget .remove').click({ force: true });
-        cy.wait('@updateUserApps')
-            .its('request.body')
-            .should('not.have.nested.property', 'appData.pages[0].layout[0].content[1]');
+        cy.wait('@updateUserApps');
         cy.get('.blueprintsWidget').should('not.exist');
     });
 
-    it('should allow to add widget', { retries: { runMode: 2 } }, () => {
+    it('should allow to add widget', () => {
         const widget1Id = 'pluginsCatalog';
         cy.get('.addWidgetBtn').click();
         cy.get(`*[data-id=${widget1Id}]`).click();
         cy.contains('Add selected widgets').click();
-        cy.wait('@updateUserApps').then(({ request }) => {
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[0].content[2].definition', widget1Id);
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[0].content[2].height');
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[0].content[2].x');
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[0].content[2].y');
-        });
+        cy.wait('@updateUserApps');
 
         cy.contains('Add Widgets Container').click();
         cy.wait('@updateUserApps');
@@ -48,12 +38,7 @@ describe('Edit mode', () => {
         cy.get('.addWidgetBtn:last()').click();
         cy.get(`*[data-id=${widget2Id}]`).click();
         cy.contains('Add selected widgets').click();
-        cy.wait('@updateUserApps').then(({ request }) => {
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[1].content[0].definition', widget2Id);
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[1].content[0].height');
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[1].content[0].x');
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[1].content[0].y');
-        });
+        cy.wait('@updateUserApps');
 
         cy.contains('.widgetName', 'Plugins Catalog');
         cy.contains('.react-grid-layout:last() .widgetName', 'Blueprints');
@@ -80,13 +65,11 @@ describe('Edit mode', () => {
         cy.wait('@updateUserApps');
 
         cy.get('.editModeButton .edit:eq(0)').click();
-        cy.get('.modal input[type=text]').type(2);
+        cy.get('.modal input[type=text]').type('2');
         cy.get('.modal .toggle').click();
 
         cy.contains('Save').click();
-        cy.wait('@updateUserApps')
-            .its('request.body')
-            .should('have.nested.property', 'appData.pages[0].layout[1].content[0].name', 'New Tab2');
+        cy.wait('@updateUserApps');
 
         cy.log('Verify default flag was set');
         cy.get('.editModeButton .edit:eq(0)').click();
@@ -98,10 +81,7 @@ describe('Edit mode', () => {
         cy.get('.modal .toggle').click();
 
         cy.contains('Save').click();
-        cy.wait('@updateUserApps').then(({ request }) => {
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[1].content[0].isDefault', false);
-            expect(request.body).to.have.nested.property('appData.pages[0].layout[1].content[1].isDefault', true);
-        });
+        cy.wait('@updateUserApps');
 
         cy.log('Verify previous tab is no longer default');
         cy.get('.editModeButton .edit:eq(0)').click();
@@ -161,7 +141,12 @@ describe('Edit mode', () => {
             }
         }
 
-        function submitInvalidWidget(widgetName, expectedError, expectedStatus, expectedDelete = false) {
+        function submitInvalidWidget(
+            widgetName: string,
+            expectedError: string,
+            expectedStatus: number,
+            expectedDelete = false
+        ) {
             submitWidget(widgetName);
 
             cy.wait('@installWidget').its('response.statusCode').should('equal', expectedStatus);
@@ -267,12 +252,13 @@ describe('Edit mode', () => {
             const widgetName = 'Backend';
             const widgetId = `testWidget${widgetName}`;
             const url = 'https://this-page-intentionally-left-blank.org/';
-            const verifyRequest = service =>
+            const verifyRequest = (service: string) =>
                 cy.wait(service).then(({ request, response }) => {
                     expect(request.headers).contain({ 'widget-id': widgetId });
-                    expect(response.statusCode).equals(200);
+                    expect(response?.statusCode).equals(200);
                 });
-            const verifyResponse = text => cy.get('.widgetContent .ui.segment pre code').should('contain.text', text);
+            const verifyResponse = (text: string) =>
+                cy.get('.widgetContent .ui.segment pre code').should('contain.text', text);
 
             cy.intercept({ method: 'GET', pathname: '/console/wb/manager', query: { endpoint: 'version' } }).as(
                 'managerService'
