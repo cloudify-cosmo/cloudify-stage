6e6e87b42bc27d1ef3eaaf1ec3abb777ed261e35
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-04-04 09:57:05 +0200

    RD-4666 Fix error 500 in Topology widget & make Topology widget spec more robust (#1905)
    * Fix Internal Server Error in Topology widget when plugin icon is fetched
    * Pass 304 error from `/plugins/icons` endpoint to the frontend
    * Fix clearing context to refresh topology widget

--- test/cypress/integration/widgets/topology_spec.ts
+++ test/cypress/integration/widgets/topology_spec.ts
@@ -63,7 +63,7 @@ describe('Topology', () => {
 
             cy.log('Check terraform module details');
             waitForDeploymentToBeInstalled(deploymentId);
-            cy.clearDeploymentContext().setDeploymentContext(deploymentId);
+            cy.clearBlueprintContext().clearDeploymentContext().setDeploymentContext(deploymentId);
             cy.contains('#gridContainer > #gridSvg > #gridContent > .nodeContainer > .title', 'terraform');
             cy.contains('#gridContainer > #gridSvg > #gridContent > .nodeContainer > .title', 'cloud_resources');
             getTerraformDetailsButton().should('not.have.css', 'visibility', 'hidden').click({ force: true });
