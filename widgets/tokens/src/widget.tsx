1387337866831b45d2395d606e3800f3e3f7204f
Norbert Kruk <norbert.kruk@amartus.com>
2022-04-06 13:43:37 +0200

    RD-4423: Provided configuration parameters

--- widgets/tokens/src/widget.tsx
+++ widgets/tokens/src/widget.tsx
@@ -6,9 +6,18 @@ Stage.defineWidget<any, any, any>({
     isReact: true,
     hasReadme: true,
     fetchUrl: '[manager]/tokens[params]',
-    permission: Stage.GenericConfig.WIDGET_PERMISSION('tokens'),
+    // permission: Stage.GenericConfig.WIDGET_PERMISSION('tokens'),
 
-    initialConfiguration: [Stage.GenericConfig.POLLING_TIME_CONFIG(10)],
+    initialConfiguration: [
+        Stage.GenericConfig.POLLING_TIME_CONFIG(10),
+        {
+            id: 'showExpiredTokens',
+            name: 'Show expired tokens',
+            type: Stage.Basic.GenericField.BOOLEAN_TYPE,
+            default: false,
+            description: 'Defines if expired tokens should be listed'
+        }
+    ],
 
     render(_widget, _data, _error, _toolbox) {
         return <div>I'm alive!</div>;
