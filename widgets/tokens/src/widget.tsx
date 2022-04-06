e3e8fb86eba9285d55214855570f543f0af8b6e6
Norbert Kruk <norbert.kruk@amartus.com>
2022-04-06 12:20:12 +0200

    RD-4423: Initialized tokens widget

---
+++ widgets/tokens/src/widget.tsx
@@ -0,0 +1,17 @@
+Stage.defineWidget<any, any, any>({
+    id: 'tokens',
+    name: 'Tokens',
+    initialWidth: 12,
+    initialHeight: 16,
+    isReact: true,
+    hasReadme: true,
+    fetchUrl: '[manager]/tokens[params]',
+
+    initialConfiguration: [Stage.GenericConfig.POLLING_TIME_CONFIG(10)],
+
+    render(_widget, _data, _error, _toolbox) {
+        return <div>I'm alive!</div>;
+    }
+});
+
+export {};
