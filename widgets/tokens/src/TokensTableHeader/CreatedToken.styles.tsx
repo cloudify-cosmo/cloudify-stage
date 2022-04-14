3546d8df2fd5bd7724f088efa630b6edf77e326e
Norbert Kruk <norbert.kruk@amartus.com>
2022-04-14 08:39:57 +0200

    RD-4423: Added warning about closing the modal

---
+++ widgets/tokens/src/TokensTableHeader/CreatedToken.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ButtonsWrapper = styled.span`
+    margin-left: 12px;
+`;
--- widgets/tokens/src/TokensTableHeader/CreatedToken.tsx
+++ widgets/tokens/src/TokensTableHeader/CreatedToken.tsx
@@ -1,4 +1,5 @@
 import type { ReceivedToken } from './CreateTokenModal';
+import { ButtonsWrapper } from './CreatedToken.styles';
 
 const {
     Basic: { Message, Button, CopyToClipboardButton },
@@ -15,18 +16,21 @@ const CreatedToken = ({ token }: CreatedTokenProps) => {
     const displayedToken = isTokenValueVisible ? token.value : maskedTokenValue;
 
     return (
-        <Message success>
-            <div>
+        <>
+            <Message success>
                 {`New token has been created: ${displayedToken}`}
-                <Button
-                    basic
-                    compact
-                    icon={isTokenValueVisible ? 'eye slash' : 'eye'}
-                    onClick={isTokenValueVisible ? hideTokenValue : showTokenValue}
-                />
-                <CopyToClipboardButton text={displayedToken} />
-            </div>
-        </Message>
+                <ButtonsWrapper>
+                    <Button
+                        basic
+                        compact
+                        icon={isTokenValueVisible ? 'eye slash' : 'eye'}
+                        onClick={isTokenValueVisible ? hideTokenValue : showTokenValue}
+                    />
+                    <CopyToClipboardButton text={displayedToken} />
+                </ButtonsWrapper>
+            </Message>
+            <Message warning>You won't be able to show or copy the token value after closing this modal</Message>
+        </>
     );
 };
 
