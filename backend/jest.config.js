748f6420e5857938bb3d4e1f66ef060af2294ac9
Jakub Wasek <jakub.wasek@amartus.com>
2022-04-19 14:25:56 +0200

    RD-4288: Backend coverage check test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,7 +1,36 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line security/detect-child-process
+const { execSync } = require('child_process');
+
+let changedFiles;
+try {
+    // Collect coverage only for files changed after this check was first introduced
+    changedFiles = execSync(
+        'git diff --name-only --relative 1b7bf59d979519d3b465d3f336d5ea69ba7404d7 -- "*.ts" "**/*.js" "**/*.ts"'
+    )
+        .toString()
+        .trim()
+        .split('\n');
+} catch (e) {
+    console.error(e.stderr.toString());
+    throw e;
+}
+
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: [...changedFiles, 'config.ts', '!migrations/**', '!jest.config.js', '!migration.ts'],
+    coverageThreshold: {
+        '**/*.{js,ts}': {
+            branches: 1,
+            functions: 1,
+            lines: 1,
+            statements: 1
+        }
+    },
     moduleNameMapper: {
         elkjs: '<rootDir>/node_modules/elkjs'
     },
+    coverageReporters: ['text'],
     moduleDirectories: ['node_modules', '..'],
     testEnvironment: 'node'
 };
