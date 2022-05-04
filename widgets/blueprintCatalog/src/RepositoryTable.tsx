8e728f985450fd582cde174964e045782acb1c0e
Norbert Kruk <norbert.kruk@amartus.com>
2022-05-04 13:28:58 +0200

    RD-4556: Provided missing typing imports

--- widgets/blueprintCatalog/src/RepositoryTable.tsx
+++ widgets/blueprintCatalog/src/RepositoryTable.tsx
@@ -1,5 +1,5 @@
 import { noop } from 'lodash';
-import { FunctionComponent } from 'react';
+import type { FunctionComponent } from 'react';
 
 import Utils from './utils';
 import Consts from './consts';
