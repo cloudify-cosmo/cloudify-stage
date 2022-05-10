4d743f3f6f9e4d90fc7da98f6f22fb1a09dae555
Jakub Madej <jakub.madej@cloudify.co>
2022-05-10 13:36:40 +0200

    Fixed accidental change

--- app/components/shared/widgets/Widget.tsx
+++ app/components/shared/widgets/Widget.tsx
@@ -343,7 +343,7 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 // NOTE: necessary type asssertion for the generic parameter to work
-const ConnectedWidget = connector(Widget) as unknown as <Configuration>(
+const ConnectedWidget = (connector(Widget) as unknown) as <Configuration>(
     props: WidgetOwnProps<Configuration>
 ) => ReactElement<WidgetProps<Configuration>>;
 
