dc7a2303bdc1ca9cb38a616b1a41791c25349abc
Norbert Kruk <norbert.kruk@amartus.com>
2022-05-12 15:20:16 +0200

    RD-4556: Encapsulated context methods

--- widgets/blueprintCatalog/src/utils.ts
+++ widgets/blueprintCatalog/src/utils.ts
@@ -5,28 +5,25 @@ const widgetTranslation = Stage.Utils.getT(`widgets.${Consts.WIDGET_ID}`);
 
 const getWidgetTranslation = (translationPath = '') => Stage.Utils.composeT(widgetTranslation, translationPath);
 
-const setUploadingBlueprintAcrossCatalogTabs = (toolbox: WidgetlessToolbox, blueprintName: string) => {
-    toolbox.getContext().setValue(Consts.CONTEXT_KEY.UPLOADING_BLUEPRINT, blueprintName);
-};
+class BlueprintCatalogContext {
+    static setUploadingBlueprint(toolbox: WidgetlessToolbox, blueprintName: string) {
+        toolbox.getContext().setValue(Consts.CONTEXT_KEY.UPLOADING_BLUEPRINT, blueprintName);
+    }
 
-const getUploadingBlueprintFromCatalogTabs = (toolbox: WidgetlessToolbox) => {
-    return toolbox.getContext().getValue(Consts.CONTEXT_KEY.UPLOADING_BLUEPRINT);
-};
+    static getUploadingBlueprint(toolbox: WidgetlessToolbox) {
+        return toolbox.getContext().getValue(Consts.CONTEXT_KEY.UPLOADING_BLUEPRINT);
+    }
 
-const isUploadingBlueprintOnAnotherCatalogTab = (toolbox: WidgetlessToolbox): boolean => {
-    return !!getUploadingBlueprintFromCatalogTabs(toolbox);
-};
+    static isUploadingBlueprint(toolbox: WidgetlessToolbox) {
+        return !!BlueprintCatalogContext.getUploadingBlueprint(toolbox);
+    }
 
-const resetUploadingBlueprintAcrossCatalogTabs = (toolbox: WidgetlessToolbox) => {
-    setUploadingBlueprintAcrossCatalogTabs(toolbox, '');
-};
+    static resetUploadingBlueprint(toolbox: WidgetlessToolbox) {
+        BlueprintCatalogContext.setUploadingBlueprint(toolbox, '');
+    }
+}
 
 export default {
     getWidgetTranslation,
-    blueprintCatalogContext: {
-        setUploadingBlueprint: setUploadingBlueprintAcrossCatalogTabs,
-        getUploadingBlueprint: getUploadingBlueprintFromCatalogTabs,
-        isUploadingBlueprint: isUploadingBlueprintOnAnotherCatalogTab,
-        resetUploadingBlueprint: resetUploadingBlueprintAcrossCatalogTabs
-    }
+    blueprintCatalogContext: BlueprintCatalogContext
 };
