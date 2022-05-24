531c93de2195ac86e4cb0f2eab7f6c313dd74988
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-05-24 08:03:02 +0200

    RD-1461 Remove `ClientConfigs` and `Applications` tables from Stage DB (#1979)
    * Remove ClientConfig table from Stage DB
    * Migrate config.ts file to TS
    * Remove Applications table from Stage DB
    * Update architecture-backend.txt

--- app/reducers/configReducer.ts
+++ app/reducers/configReducer.ts
@@ -5,7 +5,6 @@ import type { ClientConfig } from '../utils/ConfigLoader';
 
 export interface ConfigState extends ClientConfig {
     isEditMode?: boolean;
-    clientConfig?: any;
 }
 
 // NOTE: Initial state is always provided via configureStore
@@ -13,10 +12,8 @@ const config: Reducer<ConfigState> = (state = {} as ConfigState, action) => {
     switch (action.type) {
         case types.SET_CONFIG_EDIT_MODE:
             return { ...state, isEditMode: action.isEditMode };
-        case types.SET_CLIENT_CONFIG:
-            return { ...state, clientConfig: action.config };
         case types.LOGOUT:
-            return { ...state, clientConfig: null, isEditMode: false };
+            return { ...state, isEditMode: false };
         default:
             return state;
     }
--- backend/db/Connection.ts
+++ backend/db/Connection.ts
@@ -2,19 +2,15 @@ import { getDbModule } from 'cloudify-ui-common/backend';
 import { getConfig } from '../config';
 import loggerFactory from '../handler/LoggerHandler';
 
-import ApplicationsModel from './models/ApplicationsModel';
 import BlueprintAdditionsModel from './models/BlueprintAdditionsModel';
 import BlueprintUserDataModel from './models/BlueprintUserDataModel';
-import ClientConfigsModel from './models/ClientConfigsModel';
 import ResourcesModel from './models/ResourcesModel';
 import UserAppsModel from './models/UserAppsModel';
 import WidgetBackendsModel from './models/WidgetBackendsModel';
 
 const dbModule = getDbModule(getConfig().app.db, loggerFactory, [
-    ApplicationsModel,
     BlueprintAdditionsModel,
     BlueprintUserDataModel,
-    ClientConfigsModel,
     ResourcesModel,
     UserAppsModel,
     WidgetBackendsModel
