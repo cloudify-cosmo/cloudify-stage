6d4d38db245db715a5ba582e2cefee4fcb021ec6
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-05-27 21:31:52 +0200

    Add migration for BlueprintAdditions table removal

--- backend/db/Connection.ts
+++ backend/db/Connection.ts
@@ -2,14 +2,12 @@ import { getDbModule } from 'cloudify-ui-common/backend';
 import { getConfig } from '../config';
 import loggerFactory from '../handler/LoggerHandler';
 
-import BlueprintAdditionsModel from './models/BlueprintAdditionsModel';
 import BlueprintUserDataModel from './models/BlueprintUserDataModel';
 import ResourcesModel from './models/ResourcesModel';
 import UserAppsModel from './models/UserAppsModel';
 import WidgetBackendsModel from './models/WidgetBackendsModel';
 
 const dbModule = getDbModule(getConfig().app.db, loggerFactory, [
-    BlueprintAdditionsModel,
     BlueprintUserDataModel,
     ResourcesModel,
     UserAppsModel,
---
+++ backend/migrations/20220527182401-6_4-RemoveBlueprintAdditionsTable.ts
@@ -0,0 +1,44 @@
+import sharp from 'sharp';
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+import { createBlueprintAdditionsModel } from './20170425132017-4_0-init';
+import type { MigrationObject } from './common/types';
+import BlueprintAdditions from '../db/models/BlueprintAdditionsModel';
+import { getResourcePath } from '../utils';
+
+const iconsDirectory = 'blueprint-icons';
+const iconFilename = 'icon.png';
+
+export const { up, down }: MigrationObject = {
+    up: async (queryInterface, Sequelize, logger) => {
+        const blueprintAdditions = await BlueprintAdditions(queryInterface.sequelize, Sequelize).findAll();
+
+        /* eslint-disable no-await-in-loop */
+        for (let i = 0; i < blueprintAdditions.length; i += 1) {
+            const { blueprintId, image, imageUrl } = blueprintAdditions[i];
+            const iconPath = getResourcePath(path.join(iconsDirectory, blueprintId), true);
+
+            logger.info(`Migrating icon for '${blueprintId}' blueprint...`);
+            try {
+                let imageBuffer: Buffer = image;
+                if (imageUrl) {
+                    logger.info(`Fetching icon from URL: ${imageUrl}...`);
+                    imageBuffer = (await axios.get(imageUrl, { responseType: 'arraybuffer' })).data;
+                }
+                await fs.promises.mkdir(iconPath, { recursive: true });
+                await sharp(imageBuffer).toFile(path.join(iconPath, iconFilename));
+                logger.info('Icon migration finished successfully.');
+            } catch (error) {
+                logger.error('Icon migration failed.', error);
+            }
+        }
+        /* eslint-enable no-await-in-loop */
+
+        return queryInterface
+            .dropTable('BlueprintAdditions')
+            .then(() => queryInterface.removeIndex('BlueprintAdditions', ['blueprintId'], { type: 'UNIQUE' }));
+    },
+
+    down: (queryInterface, Sequelize) => createBlueprintAdditionsModel(queryInterface, Sequelize)
+};
