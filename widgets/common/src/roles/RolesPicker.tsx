2ea72a447864b85bdfe2d7567db5a20a128da4cd
Norbert Kruk <norbert.kruk@amartus.com>
2022-05-30 09:06:38 +0200

    RD-4808: Migrated class component to functional one

--- widgets/common/src/roles/RolesPicker.tsx
+++ widgets/common/src/roles/RolesPicker.tsx
@@ -1,42 +1,43 @@
 import type { DropdownProps } from 'semantic-ui-react';
 
-export default class RolesPicker extends React.Component<{
+const { Form } = Stage.Basic;
+
+interface RolesPickerProps {
     onUpdate: (name: DropdownProps['name'], value: DropdownProps['value']) => void;
     resources: Record<string, string>;
     resourceName: string;
     toolbox: Stage.Types.WidgetlessToolbox;
-}> {
-    handleInputChange: DropdownProps['onChange'] = (_proxy, field) => {
-        const { onUpdate } = this.props;
+}
+
+const RolesPicker = ({ onUpdate, resources, resourceName, toolbox }: RolesPickerProps) => {
+    const handleInputChange: DropdownProps['onChange'] = (_proxy, field) => {
         onUpdate(field.name, field.value);
     };
 
-    render() {
-        const { resourceName, resources, toolbox } = this.props;
-        const { Form } = Stage.Basic;
-        const roleOptions = _.reverse(
-            _.map(_.filter(toolbox.getManagerState().roles, { type: 'tenant_role' }), role => {
-                return { text: role.name, value: role.name };
-            })
-        );
+    const roleOptions = _.reverse(
+        _.map(_.filter(toolbox.getManagerState().roles, { type: 'tenant_role' }), role => {
+            return { text: role.name, value: role.name };
+        })
+    );
 
-        return (
-            <span>
-                {_.map(resources, (role, resource) => {
-                    return (
-                        <Form.Field key={resource} label={`Choose a role for ${resourceName} ${resource}:`}>
-                            <Form.Dropdown
-                                placeholder="Choose a role"
-                                selection
-                                options={roleOptions}
-                                name={resource}
-                                value={role}
-                                onChange={this.handleInputChange}
-                            />
-                        </Form.Field>
-                    );
-                })}
-            </span>
-        );
-    }
-}
+    return (
+        <span>
+            {_.map(resources, (role, resource) => {
+                return (
+                    <Form.Field key={resource} label={`Choose a role for ${resourceName} ${resource}:`}>
+                        <Form.Dropdown
+                            placeholder="Choose a role"
+                            selection
+                            options={roleOptions}
+                            name={resource}
+                            value={role}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Field>
+                );
+            })}
+        </span>
+    );
+};
+
+export default RolesPicker;
