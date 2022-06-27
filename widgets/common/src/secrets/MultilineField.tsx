18af36fa0b6a0a4e35cc84e883792354ffd3f47f
Norbert Kruk <norbert.kruk@amartus.com>
2022-06-27 15:12:31 +0200

    RD-4625: Adjusted field props

--- widgets/common/src/secrets/MultilineField.tsx
+++ widgets/common/src/secrets/MultilineField.tsx
@@ -3,9 +3,12 @@ import type { FormTextAreaProps } from 'semantic-ui-react';
 type MultilineFieldProps = Pick<FormTextAreaProps, 'name' | 'placeholder' | 'value' | 'onChange'>;
 
 const { Form } = Stage.Basic;
+const minFieldRows = 10;
 
 const MultilineField = ({ name, placeholder, value, onChange }: MultilineFieldProps) => {
-    return <Form.TextArea name={name} placeholder={placeholder} autoHeight value={value} onChange={onChange} />;
+    return (
+        <Form.TextArea name={name} placeholder={placeholder} value={value} onChange={onChange} rows={minFieldRows} />
+    );
 };
 
 export default MultilineField;
