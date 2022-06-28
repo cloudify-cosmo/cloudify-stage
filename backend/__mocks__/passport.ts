d7f7fbc0a6b3ba46f464ec9699a0be004a9efb49
Patrycja Kubiczek <patrycja@cloudify.co>
2022-06-28 12:59:35 +0200

    modify mocks in passport.ts

--- backend/__mocks__/passport.ts
+++ backend/__mocks__/passport.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from 'express';
 import { noop } from 'lodash';
 
 const middlewareMock: RequestHandler = (req, _res, next) => {
-    req.user = { username: 'testuser', role: '', group_system_roles: {}, tenants: {} };
+    req.user = { username: 'testuser', role: '', group_system_roles: {}, tenants: {}, show_getting_started: undefined };
     next();
 };
 
