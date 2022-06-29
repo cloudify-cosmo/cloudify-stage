3e2055d97a4d2f5be1f075e4865ba9eb32f63e43
Norbert Kruk <norbert.kruk@amartus.com>
2022-06-29 09:06:25 +0200

    RD-4625: Adjusted PasswordField props

--- widgets/common/src/secrets/PasswordField.tsx
+++ widgets/common/src/secrets/PasswordField.tsx
@@ -1,17 +1,19 @@
 import type { FormInputProps } from 'semantic-ui-react';
 
+// TODO: Simplify props typing
 export interface PasswordFieldProps extends Pick<FormInputProps, 'name' | 'onChange'> {
     value?: FormInputProps['value'];
     fluid?: FormInputProps['fluid'];
     disabled?: FormInputProps['disabled'];
     style?: FormInputProps['style'];
+    children?: FormInputProps['children'];
 }
 
 const { Form, Icon } = Stage.Basic;
 const { useToggle } = Stage.Hooks;
 
 // TODO: Rething if the name will be appropriate after adding toggling functionality
-const PasswordField = ({ name, value, onChange, disabled, fluid, style }: PasswordFieldProps) => {
+const PasswordField = ({ name, value, onChange, disabled, fluid, style, children }: PasswordFieldProps) => {
     const [isPasswordMasked, togglePasswordVisibility] = useToggle(true);
     return (
         <Form.Input
@@ -23,7 +25,9 @@ const PasswordField = ({ name, value, onChange, disabled, fluid, style }: Passwo
             fluid={fluid}
             style={style}
             icon={<Icon name={isPasswordMasked ? 'eye slash' : 'eye'} onClick={togglePasswordVisibility} link />}
-        />
+        >
+            {children}
+        </Form.Input>
     );
 };
 
