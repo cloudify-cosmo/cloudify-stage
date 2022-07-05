d95ab2e3f11cc716557680748e15a74e9ab08ffe
Patrycja Kubiczek <patrycja@cloudify.co>
2022-07-05 12:23:05 +0200

    restore Button

--- app/components/InstallWidgetModal.tsx
+++ app/components/InstallWidgetModal.tsx
@@ -6,7 +6,7 @@ import React, { Component } from 'react';
 import i18n from 'i18next';
 import EventBus from '../utils/EventBus';
 import StageUtils from '../utils/stageUtils';
-import { ApproveButton, Form, Icon, Message, Modal } from './basic/index';
+import { ApproveButton, Button, Form, Icon, Message, Modal } from './basic/index';
 
 export default class InstallWidgetModal extends Component {
     static initialState = {
