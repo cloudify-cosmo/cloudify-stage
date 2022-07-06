1db5692a2bf9995759c73010b6c117b9a02b00a4
Leon Gilyadov <leon.good.life@gmail.com>
2022-07-06 11:24:09 +0300

    fix ts errors

--- app/components/shared/IdPopup.tsx
+++ app/components/shared/IdPopup.tsx
@@ -7,14 +7,19 @@ import { CopyToClipboardButton, Label, Popup } from '../basic';
 interface IdPopupProps {
     buttonPosition?: 'left' | 'right';
     id: string;
-    selected: boolean;
+    selected?: boolean;
 }
 
 const IdPopup: FunctionComponent<IdPopupProps> = ({ buttonPosition = 'left', id = '', selected = true }) => {
     const button = <CopyToClipboardButton content={i18n.t('shared.idPopup.copyButton')} text={id} />;
 
     return (
-        <Popup wide hoverable position="right center" onClick={e => e.stopPropagation()}>
+        <Popup
+            wide
+            hoverable
+            position="right center"
+            onClick={(e: React.MouseEvent<HTMLElement>) => e.stopPropagation()}
+        >
             <Popup.Trigger>
                 <Label style={{ opacity: selected ? '1' : '0.2' }}>{i18n.t('shared.idPopup.label')}</Label>
             </Popup.Trigger>
