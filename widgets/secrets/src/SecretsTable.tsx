7bce369f72b594db599e8568e7853ac2f7d99c09
Leon Gilyadov <leon.good.life@gmail.com>
2022-07-11 17:29:01 +0300

    Secret type, eslint-disable camelcase multiline

--- widgets/secrets/src/SecretsTable.tsx
+++ widgets/secrets/src/SecretsTable.tsx
@@ -2,7 +2,7 @@
 import type { FunctionComponent } from 'react';
 import CreateModal from './CreateModal';
 import UpdateModal from './UpdateModal';
-import type { Secret } from './props/SecretPropType';
+import type { Secret } from './types';
 import SecretPropType from './props/SecretPropType';
 
 const t = Stage.Utils.getT('widgets.secrets');
--- widgets/secrets/src/props/SecretPropType.ts
+++ widgets/secrets/src/props/SecretPropType.ts
@@ -1,18 +1,3 @@
-export type Secret = {
-    // eslint-disable-next-line camelcase
-    created_at?: string;
-    // eslint-disable-next-line camelcase
-    created_by?: string;
-    // eslint-disable-next-line camelcase
-    is_hidden_value?: boolean;
-    key?: string;
-    // eslint-disable-next-line camelcase
-    tenant_name?: string;
-    // eslint-disable-next-line camelcase
-    updated_at?: string;
-    visibility?: string;
-};
-
 export default PropTypes.shape({
     created_at: PropTypes.string,
     created_by: PropTypes.string,
