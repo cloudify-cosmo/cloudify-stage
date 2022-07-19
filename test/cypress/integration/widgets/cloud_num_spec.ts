cb4b21ce2fcd4f31fd5822dd9716cc0488b61577
Leon Gilyadov <leon.good.life@gmail.com>
2022-07-19 13:32:28 +0300

    Update cloud_num_spec

--- test/cypress/integration/widgets/cloud_num_spec.ts
+++ test/cypress/integration/widgets/cloud_num_spec.ts
@@ -1,5 +1,5 @@
 describe('Number of clouds widget', () => {
-    before(() => cy.activate().usePageMock('cloudNum').mockLogin());
+    before(() => cy.activate().usePageMock('cloudNum', { icon: 'cloud' }).mockLogin());
 
     it('should display cloud icon', () => {
         cy.get('.statistic .value i').should('have.class', 'cloud');
