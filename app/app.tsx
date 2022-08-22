f4c1dcbaa5bf75dfc69c85bc94d5087acf9408ce
Norbert Kruk <norbert.kruk@amartus.com>
2022-08-22 14:39:52 +0200

    RD-3480: Initialized new icon font

--- app/app.tsx
+++ app/app.tsx
@@ -1,5 +1,6 @@
 // @ts-nocheck File not migrated fully to TS
 import './styles/style.scss';
+import './fontello/css/fontello.css';
 
 // Import semantic ui styles and themes
 import 'semantic-ui-less/semantic.less';
