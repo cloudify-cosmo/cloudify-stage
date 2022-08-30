95a874050c0f8092e6c545b38b565f9d946875c6
Leon Gilyadov <leon.good.life@gmail.com>
2022-08-30 14:22:48 +0300

    draft implementation

--- app/components/GettingStartedModal/steps/SecretsStep.tsx
+++ app/components/GettingStartedModal/steps/SecretsStep.tsx
@@ -1,9 +1,17 @@
 import React, { memo, useEffect, useMemo } from 'react';
 
 import { Form } from '../../basic';
-import { useInputs } from '../../../utils/hooks';
+import { useInputs, useResettableState } from '../../../utils/hooks';
 
 import type { GettingStartedSecretsData, GettingStartedSchemaItem } from '../model';
+
+const validateEmail = (email: string) => {
+    return email
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 
 type Props = {
     selectedEnvironment: GettingStartedSchemaItem;
@@ -26,6 +34,11 @@ const SecretsStep = ({ selectedEnvironment, typedSecrets, onChange }: Props) => 
 
     const [secretInputs, setSecretInputs, resetSecretInputs] = useInputs(typedSecrets || defaultSecretInputs);
 
+    let defaultErrors = { ...secretInputs };
+    Object.keys(defaultErrors).forEach(v => (defaultErrors[v] = false));
+
+    const [errors, setErrors, clearErrors] = useResettableState(defaultErrors);
+
     useEffect(() => resetSecretInputs(), [typedSecrets]);
     useEffect(() => {
         if (!typedSecrets) onChange(defaultSecretInputs);
@@ -36,12 +49,25 @@ const SecretsStep = ({ selectedEnvironment, typedSecrets, onChange }: Props) => 
             {selectedEnvironment.secrets.map(({ name, label, type }) => {
                 const handleBlur = () => {
                     onChange(secretInputs);
+                    if (
+                        secretInputs.hasOwnProperty('gcp_client_email') &&
+                        secretInputs.gcp_client_email &&
+                        !validateEmail(secretInputs.gcp_client_email)
+                    ) {
+                        setErrors({
+                            ...errors,
+                            gcp_client_email: {
+                                content: 'Please enter a valid email address'
+                            }
+                        });
+                    }
                 };
                 return (
                     <Form.Field key={name}>
                         <Form.Input
                             type={type}
                             name={name}
+                            error={errors[name]}
                             label={label}
                             value={secretInputs[name]}
                             onChange={setSecretInputs}
