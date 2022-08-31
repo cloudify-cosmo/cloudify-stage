b9a9b66e0b760d4c9412f9401d98bb15a58e1b08
Leon Gilyadov <leon.good.life@gmail.com>
2022-08-31 17:52:10 +0300

    RD-5684 Blank screen on Getting Started Modal

--- app/components/GettingStartedModal/steps/EnvironmentsStep/EnvironmentButton.tsx
+++ app/components/GettingStartedModal/steps/EnvironmentsStep/EnvironmentButton.tsx
@@ -1,28 +1,22 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo } from 'react';
 
-import useResettableState from '../../../../utils/hooks/useResettableState';
 import { Form, Button, Image } from '../../../basic';
 
 type Props = {
     logo: string;
     label: string;
     value?: boolean;
-    onChange?: (value: boolean) => void;
+    onChange?: () => void;
 };
 
-const EnvironmentButton = memo(({ logo, label, value, onChange }: Props) => {
-    const [localValue, setLocalValue, resetLocalValue] = useResettableState(value);
-    useEffect(() => resetLocalValue(), [value]);
-
+const EnvironmentButton = memo(({ logo, label, onChange }: Props) => {
     const handleClick = () => {
-        const newLocalValue = !localValue;
-        setLocalValue(newLocalValue);
-        onChange?.(newLocalValue);
+        onChange?.();
     };
 
     return (
         <Form.Field>
-            <Button fluid basic size="huge" active={localValue} style={{ height: 70 }} onClick={handleClick}>
+            <Button fluid basic size="huge" style={{ height: 70 }} onClick={handleClick}>
                 <Image src={logo} inline style={{ maxHeight: '100%', cursor: 'pointer' }} />
                 <span style={{ marginLeft: 10 }}>{label}</span>
             </Button>
