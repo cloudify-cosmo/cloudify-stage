4edc4115d5bfe79a824ef3eea07ad1e40fa7873d
Leon Gilyadov <leon.good.life@gmail.com>
2022-09-12 17:53:04 +0300

    update

--- backend/handler/SourceHandler.ts
+++ backend/handler/SourceHandler.ts
@@ -121,7 +121,6 @@ export async function browseArchiveFile(req, timestamp, path) {
     const data = fs.readFileSync(absolutePath);
     const size = fs.lstatSync(absolutePath).size;
     const isBinaryFile = isBinaryFileSync(data, size);
-    console.log('LEON DEBUG', isBinaryFile);
     if (!isBinaryFile) {
         return fs.readFile(absolutePath, 'utf-8');
     }
