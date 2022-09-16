2c190861a3690ae3bfa7e881dcfec5a5077bb9e8
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-09-16 10:32:31 +0200

    Add types for widgets routes

--- backend/routes/Widgets.ts
+++ backend/routes/Widgets.ts
@@ -1,43 +1,58 @@
-// @ts-nocheck File not migrated fully to TS
-
 import express from 'express';
+import type { RequestHandler } from 'express';
 import * as WidgetsHandler from '../handler/WidgetsHandler';
 import { getRBAC, isAuthorized } from '../handler/AuthHandler';
 import { getTokenFromCookies } from '../utils';
+import type {
+    GetWidgetsListResponse,
+    GetWidgetsUsedResponse,
+    PutWidgetsInstallQueryParams,
+    PutWidgetsInstallResponse,
+    PutWidgetsUpdateQueryParams,
+    PutWidgetsUpdateResponse
+} from './Widgets.types';
 
 const router = express.Router();
 
 router.use(express.json());
 
-router.get('/list', (req, res, next) => {
+router.get<never, GetWidgetsListResponse>('/list', (_req, res, next) => {
     WidgetsHandler.listWidgets()
         .then(widgets => res.send(widgets))
         .catch(next);
 });
 
-async function validateInstallWidgetsPermission(req, res, next) {
+const validateInstallWidgetsPermission: RequestHandler = async (req, res, next) => {
     const permissionId = 'stage_install_widgets';
     const rbac = await getRBAC(getTokenFromCookies(req));
     const permission = rbac.permissions[permissionId];
-    if (!isAuthorized(req.user, permission)) {
+    if (!isAuthorized(req.user!, permission)) {
         res.sendStatus(403);
     }
     next();
-}
+};
 
-router.put('/install', validateInstallWidgetsPermission, (req, res, next) => {
-    WidgetsHandler.installWidget(req.query.url, req.user.username, req)
-        .then(data => res.send(data))
-        .catch(next);
-});
+router.put<never, PutWidgetsInstallResponse, any, PutWidgetsInstallQueryParams>(
+    '/install',
+    validateInstallWidgetsPermission,
+    (req, res, next) => {
+        WidgetsHandler.installWidget(req.query.url, req.user!.username, req)
+            .then(data => res.send(data))
+            .catch(next);
+    }
+);
 
-router.put('/update', validateInstallWidgetsPermission, (req, res, next) => {
-    WidgetsHandler.updateWidget(req.query.id, req.query.url, req)
-        .then(data => res.send(data))
-        .catch(next);
-});
+router.put<never, PutWidgetsUpdateResponse, any, PutWidgetsUpdateQueryParams>(
+    '/update',
+    validateInstallWidgetsPermission,
+    (req, res, next) => {
+        WidgetsHandler.updateWidget(req.query.id, req.query.url, req)
+            .then(data => res.send(data))
+            .catch(next);
+    }
+);
 
-router.get('/:widgetId/used', (req, res, next) => {
+router.get<{ widgetId: string }, GetWidgetsUsedResponse>('/:widgetId/used', (req, res, next) => {
     WidgetsHandler.isWidgetUsed(req.params.widgetId)
         .then(result => res.send(result))
         .catch(next);
