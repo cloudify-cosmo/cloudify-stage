00a5bea72bef573fea23db463f6670e7cb0a3040
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-09-16 11:44:01 +0200

    Add types for filters route

--- backend/routes/Filters.ts
+++ backend/routes/Filters.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getFilterUsage } from '../handler/FilterHandler';
+import type { GetFiltersUsageResponse } from './Filters.types';
 
 const router = express.Router();
 
-router.get('/usage/:filterId', (req, res, next) => {
+router.get<{ filterId: string }, GetFiltersUsageResponse>('/usage/:filterId', (req, res, next) => {
     getFilterUsage(req.params.filterId)
         .then(result => res.send(result))
         .catch(next);
