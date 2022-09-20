67f37d1a91c43a28d9809ce5827d7e7e878629cf
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-09-20 12:33:29 +0200

    Update test coverage levels

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,30 +1,15 @@
-/* eslint-disable no-console */
-// eslint-disable-next-line security/detect-child-process
-const { execSync } = require('child_process');
-
-let changedFiles;
-try {
-    // Collect coverage only for files changed after this check was first introduced
-    changedFiles = execSync(
-        'git diff --name-only --relative 1b7bf59d979519d3b465d3f336d5ea69ba7404d7 -- "*.ts" "**/*.js" "**/*.ts"'
-    )
-        .toString()
-        .trim()
-        .split('\n');
-} catch (e) {
-    console.error(e.stderr.toString());
-    throw e;
-}
-
 module.exports = {
     collectCoverage: true,
-    collectCoverageFrom: [...changedFiles, 'config.ts', '!db/**', '!migrations/**', '!jest.config.js', '!migration.ts'],
+    collectCoverageFrom: ['!migration.ts', '!migrations/**', '!**/*types.ts', '!**/*.json'],
     coverageThreshold: {
-        '**/*.{js,ts}': {
-            branches: 1,
-            functions: 1,
-            lines: 1,
-            statements: 1
+        '.': {
+            statements: 52,
+            branches: 37,
+            lines: 53,
+            functions: 44
+        },
+        '**/*': {
+            branches: 2
         }
     },
     moduleNameMapper: {
