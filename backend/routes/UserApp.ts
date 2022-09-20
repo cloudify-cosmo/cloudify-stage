ed00e92ff9289f63dab180f72cb8f316c6ac84ba
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-09-20 13:11:58 +0200

    Simplify GET /ua response type

--- backend/routes/UserApp.ts
+++ backend/routes/UserApp.ts
@@ -28,7 +28,7 @@ router.get('/', (req, res: Response<GetUserAppResponse>, next) => {
         }
     })
         .then(userApp => {
-            res.send(userApp || ({} as UserAppsInstance));
+            res.send(userApp);
         })
         .catch(next);
 });
