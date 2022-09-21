c5b110d0e813fa3f06501df2d3b02e36facb20d9
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-09-21 10:22:58 +0200

    Add test for ua route to satisfy backend test coverage threshold

--- backend/test/routes/UserApp.test.ts
+++ backend/test/routes/UserApp.test.ts
@@ -5,18 +5,13 @@ import mockDb from '../mockDb';
 jest.mock('db/Connection');
 jest.mock('handler/ManagerHandler');
 
+const userAppRow = { username: 'test', appDataVersion: 600, mode: 'main', tenant: 'default_tenant', appData: {} };
+
 describe('/ua endpoint', () => {
     it('allows to get user layout', () => {
         mockDb({
             UserApps: {
-                findOne: () =>
-                    Promise.resolve({
-                        username: 'test',
-                        appDataVersion: 600,
-                        mode: 'main',
-                        tenant: 'default_tenant',
-                        appData: {}
-                    })
+                findOne: () => Promise.resolve(userAppRow)
             }
         });
 
@@ -24,13 +19,46 @@ describe('/ua endpoint', () => {
             .get('/console/ua')
             .then(response => {
                 expect(response.statusCode).toBe(200);
+                expect(response.body).toStrictEqual(userAppRow);
+            });
+    });
+
+    it('responds with error when trying to get not existing user layout', () => {
+        mockDb({
+            UserApps: {
+                findOne: () => Promise.reject()
+            }
+        });
+
+        return request(app)
+            .get('/console/ua')
+            .then(response => {
+                expect(response.statusCode).toBe(404);
                 expect(response.body).toStrictEqual({
-                    username: 'test',
-                    appDataVersion: 600,
-                    mode: 'main',
-                    tenant: 'default_tenant',
-                    appData: {}
+                    message: 'Not Found'
                 });
+            });
+    });
+});
+
+describe('/ua/clear-pages endpoint', () => {
+    it('allows to clear pages', () => {
+        const updateMock = jest.fn();
+        mockDb({
+            UserApps: {
+                findOne: () =>
+                    Promise.resolve({
+                        ...userAppRow,
+                        update: updateMock
+                    })
+            }
+        });
+
+        return request(app)
+            .get('/console/ua/clear-pages')
+            .then(response => {
+                expect(response.status).toBe(200);
+                expect(updateMock).toHaveBeenCalledWith({ appData: { pages: [] } });
             });
     });
 });
