1d5b9d6b0287d3680b13d505a42ebe5c17ff9663
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-09-29 08:28:07 +0200

    Add comment

--- app/utils/templatesLoader.ts
+++ app/utils/templatesLoader.ts
@@ -9,6 +9,7 @@ import type {
     GetTemplatesResponse
 } from '../../backend/routes/Templates.types';
 
+// NOTE: Solution based on https://stackoverflow.com/questions/41253310/typescript-retrieve-element-type-information-from-array-type
 type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[]
     ? ElementType
     : never;
