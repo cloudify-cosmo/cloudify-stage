136d6bf71ed0ee9d997e9b6c085008760afd4b5c
bartosz-lewinski <bartosz.lewinski@cloudify.co>
2022-10-28 11:18:55 +0200

    lint fix

--- widgets/common/src/deploymentsView/DeploymentLabeLConfigurationInput.tsx
+++ widgets/common/src/deploymentsView/DeploymentLabeLConfigurationInput.tsx
@@ -2,9 +2,12 @@ import type { FunctionComponent } from 'react';
 import React from 'react';
 import KeyDropdown from '../labels/KeyDropdown';
 
-const DeploymentLabelConfigurationInput: FunctionComponent<
-    Stage.Types.CustomConfigurationComponentProps<string[]>
-> = ({ value, onChange, name, widgetlessToolbox }) => {
+const DeploymentLabelConfigurationInput: FunctionComponent<Stage.Types.CustomConfigurationComponentProps<string[]>> = ({
+    value,
+    onChange,
+    name,
+    widgetlessToolbox
+}) => {
     return (
         <KeyDropdown
             onChange={(newValue: string[]) => {
