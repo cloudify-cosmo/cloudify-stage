945b95d992f3d8791073e8fc8b7efa624a4e23a2
Leon Gilyadov <leon.good.life@gmail.com>
2022-10-30 19:42:31 +0700

    merge, resolve conflict

--- app/actions/manager/clusterStatus.ts
+++ app/actions/manager/clusterStatus.ts
@@ -5,6 +5,7 @@ import { ActionType } from '../types';
 import Manager from '../../utils/Manager';
 import { forEachWidget } from '../page';
 import type { PageMenuItem } from '../pageMenu';
+import { ClusterServiceStatus } from '../../components/shared/cluster/types';
 
 export type FetchClusterStatusRequestAction = Action<ActionType.FETCH_CLUSTER_STATUS_REQUEST>;
 // TODO(RD-5591/RD-5755): Add proper typings once Cluster Status API is typed properly
@@ -71,7 +72,9 @@ export function getClusterStatus(
             .doGet(`/cluster-status?summary=${fetchOnlySummary}`)
             .then(data => {
                 const { services, status } = data;
-                dispatch(fetchClusterStatusSuccess(status, fetchOnlySummary ? undefined : services));
+                dispatch(
+                    fetchClusterStatusSuccess(ClusterServiceStatus[status], fetchOnlySummary ? undefined : services)
+                );
             })
             .catch(err => {
                 dispatch(fetchClusterStatusFailure(err));
