68c6a740431eb58c0325ae449184a2c2432403e2
Norbert Kruk <norbert.kruk@amartus.com>
2022-11-02 14:42:50 +0100

    RD-6023: Removed demo sandbox

--- widgets/blueprints/src/BlueprintsList.tsx
+++ widgets/blueprints/src/BlueprintsList.tsx
@@ -1,6 +1,4 @@
 import type { ComponentProps } from 'react';
-import styled from 'styled-components';
-import { Form } from 'cloudify-ui-components';
 
 import BlueprintsCatalog from './BlueprintsCatalog';
 import BlueprintsTable from './BlueprintsTable';
@@ -149,29 +147,8 @@ export default class BlueprintList extends React.Component<BlueprintListProps, B
 
         const BlueprintsView = shouldShowTable ? BlueprintsTable : BlueprintsCatalog;
 
-        const StyledDemoRow = styled.div`
-            display: flex;
-            gap: 16px;
-            & + & {
-                margin-top: 24px;
-            }
-        `;
-
         return (
             <div>
-                <Form>
-                    <StyledDemoRow>
-                        <Form.Input value="Example value" />
-                        <Form.Input placeholder="Placeholder" />
-                        <Form.Input value="Disabled value" disabled />
-                    </StyledDemoRow>
-                    <StyledDemoRow>
-                        <Form.TextArea value="Example value" />
-                        <Form.TextArea placeholder="Placeholder" />
-                        <Form.TextArea value="Disabled value" disabled />
-                    </StyledDemoRow>
-                </Form>
-                <div style={{ height: '500px' }} />
                 <ErrorMessage error={error} onDismiss={() => this.setState({ error: null })} autoHide />
 
                 <div className="uploadBlueprintButton">
