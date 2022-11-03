2f2be1f943cdba3fb6a7592529cdac66e7d0846b
Jakub Madej <jakub.madej@cloudify.co>
2022-11-03 13:48:40 +0100

    Fixed cy test

--- test/cypress/integration/user_configuration_spec.ts
+++ test/cypress/integration/user_configuration_spec.ts
@@ -20,6 +20,13 @@ describe('User configuration', () => {
         });
     }
 
+    function verifyLogoUrl(selector: string) {
+        cy.log('Verifying logoUrl...');
+        cy.location('protocol').then(protocol =>
+            cy.get(selector).should('have.css', 'background-image', `url("${protocol}//test.com/logo.png")`)
+        );
+    }
+
     before(cy.activate);
 
     describe('allows to customize Login page', () => {
@@ -31,12 +38,7 @@ describe('User configuration', () => {
         });
 
         it('logo', () => {
-            cy.log('Verifying logoUrl...');
-            cy.location('protocol').then(protocol =>
-                cy
-                    .get('.loginContainer .logo')
-                    .should('have.css', 'background-image', `url("${protocol}//test.com/logo.png")`)
-            );
+            verifyLogoUrl('.loginContainer .logo');
         });
 
         it('colors', () => {
@@ -75,8 +77,7 @@ describe('User configuration', () => {
         });
 
         it('logo', () => {
-            cy.log('Verifying logoUrl...');
-            cy.get('.sidebarContainer .logo').should('have.css', 'background-image', `url("http://test.com/logo.png")`);
+            verifyLogoUrl('.sidebarContainer .logo');
         });
 
         it('version details', () => {
