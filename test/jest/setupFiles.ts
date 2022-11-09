8802e06f74763fd03f0c1f706dbca05629f24dd3
Jakub Madej <jakub.madej@cloudify.co>
2022-11-09 12:00:20 +0100

    Post merge fix

--- test/jest/setupFiles.ts
+++ test/jest/setupFiles.ts
@@ -1,9 +1,7 @@
 import { TextDecoder, TextEncoder } from 'util';
 
-export {};
-
 // NOTE: Workaround for https://github.com/jsdom/jsdom/issues/2524
 if (typeof TextEncoder !== 'undefined' && typeof TextDecoder !== 'undefined') {
-    global.TextEncoder = TextEncoder;
+    (global as any).TextEncoder = TextEncoder;
     (global as any).TextDecoder = TextDecoder;
 }
