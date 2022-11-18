ad1d63524ae553408c46d988aecca060d6eb9e40
Patrycja Kubiczek <patrycja@cloudify.co>
2022-11-18 13:48:47 +0100

    RD-6194 PP adjust font size in sidebar

--- app/components/shared/cluster/ClusterService.tsx
+++ app/components/shared/cluster/ClusterService.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import i18n from 'i18next';
 import type { FunctionComponent } from 'react';
-
+import styled from 'styled-components';
 import { clusterServiceIcon } from './consts';
 import { Header, Icon, Label } from '../../basic';
 import type { ClusterService as ClusterServiceName } from './types';
@@ -10,20 +10,44 @@ interface ClusterServiceProps {
     name: ClusterServiceName;
     isExternal: boolean;
 }
+
+const ClusterWrapper = styled.div`
+    vertical-align: middle;
+    padding: 10px;
+    overflow: auto;
+`;
+
+const ClusterHeader = styled(Header)`
+    margin: 0;
+}`;
+
+const ClusterIcon = styled(Icon)`
+    &&&& {
+        display: inline-block;
+        margin-right: 0.75rem;
+    }
+`;
+
+const ClusterText = styled.span`
+    font-size: 16px;
+`;
+
+const ClusterLabel = styled(Label)`
+    margin-left: 10px;
+    float: right;
+`;
+
 const ClusterService: FunctionComponent<ClusterServiceProps> = ({ name, isExternal }) => {
     const icon = clusterServiceIcon(name);
 
     return (
-        <div style={{ verticalAlign: 'middle', padding: 10, overflow: 'auto' }}>
-            <Header floated="left" style={{ margin: 0 }}>
-                <Icon name={icon} size="large" /> {i18n.t(`cluster.${name}`)}
-            </Header>
-            {isExternal && (
-                <Label color="black" style={{ marginLeft: 10, float: 'right' }}>
-                    External
-                </Label>
-            )}
-        </div>
+        <ClusterWrapper>
+            <ClusterHeader floated="left">
+                <ClusterIcon name={icon} size="large" />
+                <ClusterText> {i18n.t(`cluster.${name}`)} </ClusterText>
+            </ClusterHeader>
+            {isExternal && <ClusterLabel color="black">External</ClusterLabel>}
+        </ClusterWrapper>
     );
 };
 export default ClusterService;
