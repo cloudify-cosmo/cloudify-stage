955adc4dd2f7fb9f063d03ada7c47d7c5cb92717
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-11-22 10:49:01 +0100

    Set cache-control header to turn off response caching

--- test/cypress/support/config.ts
+++ test/cypress/support/config.ts
@@ -12,13 +12,17 @@ declare global {
 
 const commands = {
     modifyConfigResponseBody: (responseBodyInterceptor: (responseBody: ClientConfig) => ClientConfig) => {
-        cy.intercept('GET', '/console/config', req => {
+        cy.intercept('GET', '/console/config', request => {
             // NOTE: Deleting `if-none-match` header to avoid getting "304 Not Modified" response
             //       which doesn't have any data in the body. For details check:
             //       https://glebbahmutov.com/blog/cypress-intercept-problems/#cached-response
-            delete req.headers['if-none-match'];
-            req.on('response', response => {
+            delete request.headers['if-none-match'];
+            request.on('response', response => {
                 const { body } = response;
+                // NOTE: Turning off response caching, so the next call to /console/config
+                //       won't use modified response. For details check:
+                //       https://docs.cypress.io/api/commands/intercept#cy-intercept-and-request-caching
+                response.headers['cache-control'] = 'no-cache';
                 response.send(responseBodyInterceptor(body));
             });
         });
