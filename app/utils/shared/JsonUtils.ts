ce07e8924d339dfd74310c815fe9866f554cb5d2
Norbert Kruk <norbert.kruk@amartus.com>
2022-11-29 12:23:44 +0100

    RD-5753: Migrated JsonUtils

--- app/utils/shared/JsonUtils.ts
+++ app/utils/shared/JsonUtils.ts
@@ -1,10 +1,8 @@
-// @ts-nocheck File not migrated fully to TS
-
 import _ from 'lodash';
 import { types } from 'cloudify-ui-common-frontend';
 
 export default class JsonUtils {
-    static stringify(value, indented = false, ignoreEmpty = false) {
+    static stringify(value: any, indented = false, ignoreEmpty = false) {
         if (!ignoreEmpty && value === '') {
             return '';
         }
@@ -17,19 +15,19 @@ export default class JsonUtils {
         return _.trim(stringifiedValue, '"');
     }
 
-    static toType(obj) {
+    static toType: typeof types.toType = obj => {
         return types.toType(obj);
-    }
+    };
 
-    static toCloudifyType(obj) {
+    static toCloudifyType: typeof types.toCloudifyType = obj => {
         return types.toCloudifyType(obj);
-    }
+    };
 
-    static getStringValue(value) {
+    static getStringValue: typeof types.getStringValue = (value: string) => {
         return types.getStringValue(value);
-    }
+    };
 
-    static getTypedValue(value) {
+    static getTypedValue: typeof types.getStringValue = value => {
         return types.getTypedValue(value);
-    }
+    };
 }
