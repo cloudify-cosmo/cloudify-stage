f45ef2bb619ea4c71f5e02ec4e5b60b2bc7e2973
Patrycja Kubiczek <patrycja@cloudify.co>
2022-12-07 10:42:15 +0100

    move mockedData to fixtures

--- test/cypress/integration/widgets/secretProviders_spec.ts
+++ test/cypress/integration/widgets/secretProviders_spec.ts
@@ -5,46 +5,19 @@ describe('Secret Providers widget', () => {
         pageSize: 0 // NOTE: Setting page size to 0 to list all secret providers and be able to find the one created in test
     };
 
-    const secretProviders = [
-        {
-            created_at: '2022-12-01T12:12:57.007Z',
-            id: 'test',
-            visibility: 'tenant',
-            name: 'Secret Provider 1',
-            type: 'test',
-            connection_parameters: null,
-            updated_at: null,
-            tenant_name: 'default_tenant',
-            created_by: 'admin',
-            resource_availability: 'tenant',
-            private_resource: false
-        },
-        {
-            created_at: '2022-12-01T12:12:57.007Z',
-            id: 'test_2',
-            visibility: 'tenant',
-            name: 'Secret Provider 2',
-            type: 'test',
-            connection_parameters: null,
-            updated_at: null,
-            tenant_name: 'default_tenant',
-            created_by: 'admin',
-            resource_availability: 'tenant',
-            private_resource: false
-        }
-    ];
-
     before(() => {
         cy.activate('valid_trial_license').usePageMock(widgetId, widgetConfiguration).mockLogin();
-        cy.interceptSp('GET', '/secrets-providers*', {
-            items: secretProviders,
-            metadata: {
-                pagination: {
-                    total: 1,
-                    size: 1,
-                    offset: 0
+        cy.fixture('secret_providers/secret_providers').then(secretProviders => {
+            cy.interceptSp('GET', '/secrets-providers*', {
+                items: secretProviders,
+                metadata: {
+                    pagination: {
+                        total: 1,
+                        size: 1,
+                        offset: 0
+                    }
                 }
-            }
+            });
         });
     });
 
