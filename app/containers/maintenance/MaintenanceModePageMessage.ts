f25026671710816e35cb95dbf317888535ebae73
Leon Gilyadov <leon.good.life@gmail.com>
2022-12-07 17:12:56 +0200

    Migrate app/containers/maintenance/MaintenanceModePageMessage.ts to TypeScript

--- app/containers/maintenance/MaintenanceModePageMessage.ts
+++ app/containers/maintenance/MaintenanceModePageMessage.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck File not migrated fully to TS
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
@@ -6,8 +5,10 @@ import MaintenanceModePageMessage from '../../components/maintenance/Maintenance
 import stageUtils from '../../utils/stageUtils';
 import Consts from '../../utils/consts';
 import { getClusterStatus } from '../../actions/manager/clusterStatus';
+import type { ReduxState } from '../../reducers';
+import type { ReduxThunkDispatch } from '../../configureStore';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state: ReduxState) => {
     const canMaintenanceMode = stageUtils.isUserAuthorized(Consts.permissions.STAGE_MAINTENANCE_MODE, state.manager);
     const showServicesStatus = stageUtils.isUserAuthorized(Consts.permissions.STAGE_SERVICES_STATUS, state.manager);
     return {
@@ -17,7 +18,7 @@ const mapStateToProps = state => {
         showServicesStatus
     };
 };
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch: ReduxThunkDispatch) => {
     return {
         navigateToHome: () => {
             return dispatch(push(Consts.PAGE_PATH.HOME));
