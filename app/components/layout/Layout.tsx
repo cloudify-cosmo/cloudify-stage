5fb57ae55d71306d849fdf8e60b3e1466136fac8
Leon Gilyadov <leon.good.life@gmail.com>
2022-12-07 16:06:54 +0200

    Change Layout to functional component for useDispatch, useSelector

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import log from 'loglevel';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import type { ReduxState } from '../../reducers';
 import type { ReduxThunkDispatch } from '../../configureStore';
 
@@ -19,37 +19,28 @@ import UserAppDataAutoSaver from '../../utils/UserAppDataAutoSaver';
 import ScrollToTop from './ScrollToTop';
 import TemplateManagement from '../templateManagement/TemplateManagement';
 
-export interface LayoutProps {
-    doLogout: (error: string) => void;
-    initialPageLoad: () => Promise<void>;
-    isLoading: boolean;
-    isUserAuthorizedForTemplateManagement: boolean;
-}
-
-export interface LayoutState {
-    initialized: boolean;
-}
-
-class Layout extends Component<LayoutProps, LayoutState> {
-    static initialState = {
-        initialized: false
+export default function Layout() {
+    const [initialized, setInitialized] = useState(false);
+    const isLoading = useSelector((state: ReduxState) => state.app.loading);
+    const isUserAuthorizedForTemplateManagement = useSelector(
+        (state: ReduxState) =>
+            state.manager &&
+            state.manager.permissions &&
+            stageUtils.isUserAuthorized(Consts.permissions.STAGE_TEMPLATE_MANAGEMENT, state.manager)
+    );
+    const dispatch: ReduxThunkDispatch = useDispatch();
+    const doLogout = (err: string) => {
+        return dispatch(logout(err));
     };
-
-    constructor(props: LayoutProps, context: any) {
-        super(props, context);
-        this.state = Layout.initialState;
-    }
-
-    componentDidMount() {
-        const { doLogout, initialPageLoad } = this.props;
-        initialPageLoad()
+    useEffect(() => {
+        dispatch(intialPageLoad())
             .then(() => {
                 StatusPoller.getPoller()?.start();
                 UserAppDataAutoSaver.getAutoSaver()?.start();
-                this.setState({ initialized: true });
+                setInitialized(true);
             })
-            .catch(e => {
-                switch (e) {
+            .catch((err: string) => {
+                switch (err) {
                     case UNAUTHORIZED_ERR: // Handled by Interceptor
                         break;
                     case NO_PAGES_FOR_TENANT_ERR:
@@ -57,80 +48,52 @@ class Layout extends Component<LayoutProps, LayoutState> {
                         doLogout(i18n.t('noPages'));
                         break;
                     default:
-                        log.error('Initializing user data failed', e);
+                        log.error('Initializing user data failed', err);
                         doLogout(i18n.t('pageLoadError', 'Error initializing user data, cannot load page'));
                 }
             });
+
+        return () => {
+            StatusPoller.getPoller()?.stop();
+            UserAppDataAutoSaver.getAutoSaver()?.stop();
+        };
+    }, []);
+
+    if (isLoading) {
+        SplashLoadingScreen.turnOn();
+        return null;
+    }
+    SplashLoadingScreen.turnOff();
+
+    if (!initialized) {
+        return null;
     }
 
-    componentWillUnmount() {
-        StatusPoller.getPoller()?.stop();
-        UserAppDataAutoSaver.getAutoSaver()?.stop();
-    }
-
-    render() {
-        const { isLoading, isUserAuthorizedForTemplateManagement } = this.props;
-        const { initialized } = this.state;
-
-        if (isLoading) {
-            SplashLoadingScreen.turnOn();
-            return null;
-        }
-        SplashLoadingScreen.turnOff();
-
-        if (!initialized) {
-            return null;
-        }
-
-        return (
-            <ScrollToTop>
-                <Switch>
-                    {isUserAuthorizedForTemplateManagement && (
-                        <Route exact path="/template_management" component={TemplateManagement} />
-                    )}
-                    {isUserAuthorizedForTemplateManagement && (
-                        <Route
-                            exact
-                            path="/page_preview/:pageId"
-                            render={({ match }) => <PageManagement pageId={match.params.pageId} />}
-                        />
-                    )}
-                    {isUserAuthorizedForTemplateManagement && (
-                        <Route
-                            exact
-                            path="/page_edit/:pageId"
-                            render={({ match }) => <PageManagement pageId={match.params.pageId} isEditMode />}
-                        />
-                    )}
-                    <Route exact path="/page/:pageId/:pageName" component={Home} />
-                    <Route exact path="/page/:pageId" component={Home} />
-                    <Route exact path={Consts.PAGE_PATH.HOME} component={Home} />
-                    <Route render={() => <Redirect to={Consts.PAGE_PATH.ERROR_404} />} />
-                </Switch>
-            </ScrollToTop>
-        );
-    }
+    return (
+        <ScrollToTop>
+            <Switch>
+                {isUserAuthorizedForTemplateManagement && (
+                    <Route exact path="/template_management" component={TemplateManagement} />
+                )}
+                {isUserAuthorizedForTemplateManagement && (
+                    <Route
+                        exact
+                        path="/page_preview/:pageId"
+                        render={({ match }) => <PageManagement pageId={match.params.pageId} />}
+                    />
+                )}
+                {isUserAuthorizedForTemplateManagement && (
+                    <Route
+                        exact
+                        path="/page_edit/:pageId"
+                        render={({ match }) => <PageManagement pageId={match.params.pageId} isEditMode />}
+                    />
+                )}
+                <Route exact path="/page/:pageId/:pageName" component={Home} />
+                <Route exact path="/page/:pageId" component={Home} />
+                <Route exact path={Consts.PAGE_PATH.HOME} component={Home} />
+                <Route render={() => <Redirect to={Consts.PAGE_PATH.ERROR_404} />} />
+            </Switch>
+        </ScrollToTop>
+    );
 }
-
-const mapStateToProps = (state: ReduxState) => {
-    return {
-        isLoading: state.app.loading,
-        isUserAuthorizedForTemplateManagement:
-            state.manager &&
-            state.manager.permissions &&
-            stageUtils.isUserAuthorized(Consts.permissions.STAGE_TEMPLATE_MANAGEMENT, state.manager)
-    };
-};
-
-const mapDispatchToProps = (dispatch: ReduxThunkDispatch) => {
-    return {
-        initialPageLoad: () => {
-            return dispatch(intialPageLoad());
-        },
-        doLogout: (err: string) => {
-            return dispatch(logout(err));
-        }
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Layout);
