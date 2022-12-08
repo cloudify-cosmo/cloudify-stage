65ed343634215580ae2c3da62f84797e8e5f97bb
Norbert Kruk <norbert.kruk@amartus.com>
2022-12-08 12:27:12 +0100

    RD-6465: Resolved timeout issue

--- test/cypress/integration/widgets/filter_spec.ts
+++ test/cypress/integration/widgets/filter_spec.ts
@@ -98,6 +98,7 @@ describe('Filter', () => {
         it('deployment creation and removal', () => {
             const deploymentName = `${blueprintName}-deployment`;
             cy.interceptSp('DELETE', `/deployments/${deploymentName}`).as('deleteDeployment');
+            cy.interceptSp('GET', '/deployments').as('deploymentList');
             cy.get('.blueprintsWidget').within(() => {
                 cy.getSearchInput().scrollIntoView().clear().type(blueprintName);
                 getBlueprintSegment(blueprintName).find('.rocket').click();
@@ -120,6 +121,7 @@ describe('Filter', () => {
             cy.contains('Yes').click();
             cy.wait('@deleteDeployment');
 
+            cy.wait('@deploymentList');
             cy.contains('.deploymentsWidget .row', deploymentName).should('not.exist');
 
             cy.get('.deploymentFilterField > .text.default');
