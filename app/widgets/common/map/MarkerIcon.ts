5357322a7df2618324b22eb2bc3583e64fc3a208
Jakub Madej <jakub.madej@cloudify.co>
2022-12-12 14:21:05 +0100

    Removed backend import

--- app/widgets/common/map/MarkerIcon.ts
+++ app/widgets/common/map/MarkerIcon.ts
@@ -1,11 +1,11 @@
-import { CONTEXT_PATH } from '../../../../backend/consts';
+import Consts from '../../../utils/consts';
 
 export type MarkerIconColor = 'blue' | 'green' | 'grey' | 'red' | 'yellow';
 
 export function createMarkerIcon(color: MarkerIconColor) {
     return new window.L.Icon({
-        iconUrl: `${CONTEXT_PATH}/static/images/marker-icon-${color}.png`,
-        shadowUrl: `${CONTEXT_PATH}/static/images/marker-shadow.png`,
+        iconUrl: `${Consts.CONTEXT_PATH}/static/images/marker-icon-${color}.png`,
+        shadowUrl: `${Consts.CONTEXT_PATH}/static/images/marker-shadow.png`,
         iconAnchor: [12, 41],
         popupAnchor: [1, -34]
     });
