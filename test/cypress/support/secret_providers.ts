9f9efdffb2a6dea030ca1a7c8e2bf83a1a2e060b
Patrycja <patrycja@cloudify.co>
2022-12-15 10:25:25 +0100

    RD-6363 Add support for deleting Secret Provider (#2315)
    * RD-6361 Create basic Secret Providers widget
    
    * change data sorting keys
    
    * merge master 2
    
    * WIP
    
    * update secretproviders_spec
    
    * Update test/cypress/support/secret_providers.ts
    
    Co-authored-by: Jakub Niezgoda <jakub.niezgoda@cloudify.co>
    
    * Update test/cypress/integration/widgets/secretProviders_spec.ts
    
    Co-authored-by: Jakub Niezgoda <jakub.niezgoda@cloudify.co>
    
    * adjustments after CR
    
    * replace getT
    
    * improve path import
    
    * improve test
    
    * remove todo
    
    * remove bordered from icon
    
    * Update test/cypress/integration/widgets/secret_providers_spec.ts
    
    Co-authored-by: Norbert Kruk <39031791+Vorbert-Kruk@users.noreply.github.com>
    
    * update test and RemoveSecretProviderButton, remove types.ts
    
    * add catch
    
    * create SecretProvidersTable.utils
    
    * update translation fn
    
    * remove width from table columns
    
    * CR adjustments
    
    * CR adjustments for RemoveSecretProviderButton
    
    * adding validation
    
    Co-authored-by: Jakub Niezgoda <jakub.niezgoda@cloudify.co>
    Co-authored-by: Norbert Kruk <39031791+Vorbert-Kruk@users.noreply.github.com>

---
+++ test/cypress/support/secret_providers.ts
@@ -0,0 +1,24 @@
+import type { GetCypressChainableFromCommands } from 'cloudify-ui-common-cypress/support';
+import { addCommands } from 'cloudify-ui-common-cypress/support';
+import type { SecretProvidersWidget } from 'widgets/secretProviders/src/widget.types';
+
+declare global {
+    namespace Cypress {
+        // NOTE: necessary for extending the Cypress API
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        export interface Chainable extends GetCypressChainableFromCommands<typeof commands> {}
+    }
+}
+
+const commands = {
+    createSecretProvider: (secretProvider: Partial<SecretProvidersWidget.DataItem>) => {
+        const { name, type, visibility } = secretProvider;
+        return cy.cfyRequest('/secrets-providers', 'PUT', null, { name, type, visibility });
+    },
+
+    getSecretProvider: (key: string) => cy.cfyRequest(`/secrets-providers/${key}`, 'GET'),
+
+    deleteSecretProvider: (key: string) => cy.cfyRequest(`/secrets-providers/${key}`, 'DELETE')
+};
+
+addCommands(commands);
