8b579ec03f6ab515a6b2c3eda2950bcb9e6e9b05
Jakub Madej <jakub.madej@cloudify.co>
2022-12-15 15:01:58 +0100

    Further fixes for missing labels

--- app/widgets/common/filters/RuleRemoveButton.tsx
+++ app/widgets/common/filters/RuleRemoveButton.tsx
@@ -7,8 +7,8 @@ interface RuleRemoveButtonProps {
     onClick: ComponentProps<typeof Button>['onClick'];
 }
 
-const removeButtonLabel = getTranslation('buttons.removeRule');
 const RuleRemoveButton: FunctionComponent<RuleRemoveButtonProps> = ({ onClick }) => {
+    const removeButtonLabel = getTranslation('buttons.removeRule');
     return <Button aria-label={removeButtonLabel} title={removeButtonLabel} basic icon="trash" onClick={onClick} />;
 };
 export default RuleRemoveButton;
