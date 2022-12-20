b361bc9842ceee9a933866820f7197709793e543
Norbert Kruk <norbert.kruk@amartus.com>
2022-12-20 16:46:23 +0100

    RD-6630: Removed default onWidgetUpdate value

--- app/components/shared/widgets/PageContent.tsx
+++ app/components/shared/widgets/PageContent.tsx
@@ -31,7 +31,7 @@ export interface PageContentProps {
 }
 
 export default function PageContent({
-    onWidgetUpdated = noop,
+    onWidgetUpdated,
     onWidgetRemoved = noop,
     onWidgetAdded = noop,
     onTabAdded = noop,
