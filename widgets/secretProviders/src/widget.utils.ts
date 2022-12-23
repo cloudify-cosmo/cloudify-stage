7755e73d0ee177dc210fd29adb805d11162a46ba
Patrycja <patrycja@cloudify.co>
2022-12-23 14:59:57 +0100

    Update widgets/secretProviders/src/widget.utils.ts
    Co-authored-by: Norbert Kruk <39031791+Vorbert-Kruk@users.noreply.github.com>

--- widgets/secretProviders/src/widget.utils.ts
+++ widgets/secretProviders/src/widget.utils.ts
@@ -9,19 +9,19 @@ interface Form {
 }
 
 export const validateModalForm = (form: Form, isCreateModal: boolean) => {
-    const newErrors: Record<string, string> = {};
+    const errors: Record<string, string> = {};
     if (isCreateModal) {
         if (!form.providerName) {
-            newErrors.providerName = translateSecretProviders('form.errors.providerName.required');
+            errors.providerName = translateSecretProviders('form.errors.providerName.required');
         } else if (!Consts.ID_REGEX.test(form.providerName)) {
-            newErrors.providerName = translateSecretProviders('form.errors.providerName.invalid');
+            errors.providerName = translateSecretProviders('form.errors.providerName.invalid');
         }
     }
     if (!form.hostname) {
-        newErrors.hostname = translateSecretProviders('form.errors.hostname');
+        errors.hostname = translateSecretProviders('form.errors.hostname');
     }
     if (!form.authorizationToken) {
-        newErrors.authorizationToken = translateSecretProviders('form.errors.authorizationToken');
+        errors.authorizationToken = translateSecretProviders('form.errors.authorizationToken');
     }
-    return newErrors;
+    return errors;
 };
