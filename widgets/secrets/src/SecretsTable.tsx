a1b0c88fea454080d121a7d0ee63b60431445a70
Patrycja Kubiczek <patrycja@cloudify.co>
2023-01-04 14:04:01 +0100

    adjust widths

--- widgets/secrets/src/SecretsTable.tsx
+++ widgets/secrets/src/SecretsTable.tsx
@@ -211,7 +211,7 @@ export default class SecretsTable extends React.Component<SecretsTableProps, Sec
                 >
                     <DataTable.Column label={translateSecrets('columns.key')} name="key" width="20%" />
                     <DataTable.Column label={translateSecrets('columns.value')} width="20%" />
-                    <DataTable.Column label={translateSecrets('columns.hidden')} name="is_hidden_value" width="10%" />
+                    <DataTable.Column label={translateSecrets('columns.hidden')} name="is_hidden_value" width="5%" />
                     <DataTable.Column label={translateSecrets('columns.created')} name="created_at" width="10%" />
                     <DataTable.Column label={translateSecrets('columns.updated')} name="updated_at" width="10%" />
                     <DataTable.Column
@@ -221,7 +221,7 @@ export default class SecretsTable extends React.Component<SecretsTableProps, Sec
                     />
                     <DataTable.Column label={translateSecrets('columns.creator')} name="created_by" width="10%" />
                     <DataTable.Column label={translateSecrets('columns.tenant')} name="tenant_name" width="10%" />
-                    <DataTable.Column width="10%" />
+                    <DataTable.Column width="5" />
 
                     {data.items.map(item => {
                         return (
