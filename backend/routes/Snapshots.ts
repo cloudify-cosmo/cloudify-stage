73d8c77317486f6d35388f3abb2298a28d49b676
kubama <jakub.madej@amartus.com>
2023-01-05 15:34:00 +0100

    RD-4792 RD-4793 Add snapshot support for page groups and widgets (#2355)

--- backend/routes/Snapshots.ts
+++ backend/routes/Snapshots.ts
@@ -4,9 +4,11 @@ import { omit } from 'lodash';
 import { db } from '../db/Connection';
 import type { UserAppsInstance } from '../db/models/UserAppsModel';
 import type { UserAppsAttributes } from '../db/models/UserAppsModel.types';
-import type { Page, Template } from '../handler/templates/types';
+import type { Page, PageGroup, Template } from '../handler/templates/types';
 import { checkTemplateExists, createTemplate, getUserTemplates } from '../handler/templates/TemplatesHandler';
 import { checkPageExists, createPage, getUserPages } from '../handler/templates/PagesHandler';
+import { checkPageGroupExists, createPageGroup, getUserPageGroups } from '../handler/templates/PageGroupsHandler';
+import { createUserWidgetsSnapshot, restoreUserWidgetsSnapshot } from '../handler/widgets/WidgetsHandler';
 
 const router = express.Router();
 
@@ -51,13 +53,14 @@ router.use(express.json());
     });
 })();
 
-(function templates() {
-    const propertiesToOmit = ['name', 'custom'] as const;
-    type PropertiesToOmit = typeof propertiesToOmit[number];
-    type TemplatesSnapshot = Omit<Template, PropertiesToOmit>[];
+const commonPropertiesToOmit = ['custom'] as const;
+type CommonPropertiesToOmit = typeof commonPropertiesToOmit[number];
 
+const templatePropertiesToOmit = [...commonPropertiesToOmit, 'name'] as const;
+export type TemplatesSnapshot = Omit<Template, typeof templatePropertiesToOmit[number]>[];
+(() => {
     router.get('/templates', (_req, res: Response<TemplatesSnapshot>, _next) => {
-        res.send(getUserTemplates().map(template => omit(template, propertiesToOmit)));
+        res.send(getUserTemplates().map(template => omit(template, templatePropertiesToOmit)));
     });
 
     router.post<never, never, TemplatesSnapshot>('/templates', (req, res, next) => {
@@ -79,13 +82,10 @@ router.use(express.json());
     });
 })();
 
-(function pages() {
-    const propertiesToOmit = ['custom'] as const;
-    type PropertiesToOmit = typeof propertiesToOmit[number];
-    type PagesSnapshot = Omit<Page, PropertiesToOmit>[];
-
+export type PagesSnapshot = Omit<Page, CommonPropertiesToOmit>[];
+(() => {
     router.get('/pages', (_req, res: Response<PagesSnapshot>, _next) => {
-        res.send(getUserPages().map(page => omit(page, propertiesToOmit)));
+        res.send(getUserPages().map(page => omit(page, commonPropertiesToOmit)));
     });
 
     router.post<never, never, PagesSnapshot>('/pages', (req, res, next) => {
@@ -98,6 +98,40 @@ router.use(express.json());
                     )
                 )
             )
+            .then(() => res.status(201).end())
+            .catch(next);
+    });
+})();
+
+export type PageGroupsSnapshot = Omit<PageGroup, CommonPropertiesToOmit>[];
+(() => {
+    router.get('/page-groups', (_req, res: Response<PageGroupsSnapshot>, _next) => {
+        res.send(getUserPageGroups().map(pageGroup => omit(pageGroup, commonPropertiesToOmit)));
+    });
+
+    router.post<never, never, PageGroupsSnapshot>('/page-groups', (req, res, next) => {
+        Promise.all(req.body.map(pageGroupData => checkPageGroupExists(pageGroupData)))
+            .catch(message => Promise.reject({ message, status: 400 }))
+            .then(() =>
+                Promise.all(
+                    req.body.map(pageGroupData =>
+                        createPageGroup(pageGroupData, pageGroupData.updatedBy, pageGroupData.updatedAt)
+                    )
+                )
+            )
+            .then(() => res.status(201).end())
+            .catch(next);
+    });
+})();
+
+(() => {
+    router.get('/widgets', (_req, res, next) => {
+        const snapshot = createUserWidgetsSnapshot(next);
+        snapshot.pipe(res);
+    });
+
+    router.post('/widgets', (req, res, next) => {
+        restoreUserWidgetsSnapshot(req)
             .then(() => res.status(201).end())
             .catch(next);
     });
