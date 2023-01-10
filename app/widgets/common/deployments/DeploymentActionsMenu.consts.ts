0f95837e69dba5552a737686bf59245235a69bf7
Norbert Kruk <norbert.kruk@amartus.com>
2023-01-10 16:00:12 +0100

    RD-4899: Adjusted property naming

--- app/widgets/common/deployments/DeploymentActionsMenu.consts.ts
+++ app/widgets/common/deployments/DeploymentActionsMenu.consts.ts
@@ -10,7 +10,7 @@ export const actions = Object.freeze({
 });
 
 export const permissions = {
-    executeWorkflowPermission: 'execution_start',
+    executeWorkflow: 'execution_start',
     deploymentDelete: 'deployment_delete',
     deploymentUpdateCreate: 'deployment_update_create',
     deploymentSetSite: 'deployment_set_site',
@@ -19,12 +19,12 @@ export const permissions = {
 
 export type MenuItem = { name: string; icon: string; permission: string };
 export const menuItems: MenuItem[] = [
-    { name: actions.install, icon: 'play', permission: permissions.executeWorkflowPermission },
+    { name: actions.install, icon: 'play', permission: permissions.executeWorkflow },
     { name: actions.update, icon: 'edit', permission: permissions.deploymentUpdateCreate },
     { name: actions.deployOn, icon: 'rocket', permission: permissions.deploymentCreate },
     { name: actions.setSite, icon: 'building', permission: permissions.deploymentSetSite },
     { name: actions.manageLabels, icon: 'tags', permission: permissions.deploymentCreate },
-    { name: actions.uninstall, icon: 'recycle', permission: permissions.executeWorkflowPermission },
+    { name: actions.uninstall, icon: 'recycle', permission: permissions.executeWorkflow },
     { name: actions.delete, icon: 'trash alternate', permission: permissions.deploymentDelete },
     { name: actions.forceDelete, icon: 'trash', permission: permissions.deploymentDelete }
 ];
