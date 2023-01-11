570e6c3fabc26b7e51407e16937b13ddb318d1a1
Leon Gilyadov <leon.good.life@gmail.com>
2023-01-11 11:32:53 +0200

    Remove unused loading; Remove as GettingStartedSchema

--- app/components/GettingStartedModal/useFetchSchemas.ts
+++ app/components/GettingStartedModal/useFetchSchemas.ts
@@ -5,7 +5,6 @@ import type { GettingStartedSchema } from './model';
 function useFetchSchemas() {
     const [gettingStartedSchema, setGettingStartedSchema] = useState<GettingStartedSchema | null>(null);
     const [cloudSetupSchema, setCloudSetupSchema] = useState<GettingStartedSchema | null>(null);
-    const [loading, setLoading] = useState(true);
     const [error, setError, clearError] = useResettableState<string | null>(null);
 
     useEffect(() => {
@@ -13,21 +12,19 @@ function useFetchSchemas() {
             fetch('https://repository.cloudifysource.org/cloudify/getting-started/6.4/gettingStarted.schema.json')
                 .then(response => response.json())
                 .then(response => {
-                    setGettingStartedSchema(response as GettingStartedSchema);
+                    setGettingStartedSchema(response);
                 }),
             fetch('https://repository.cloudifysource.org/cloudify/getting-started/6.4/cloudSetup.schema.json')
                 .then(response => response.json())
                 .then(response => {
-                    setCloudSetupSchema(response as GettingStartedSchema);
+                    setCloudSetupSchema(response);
                 })
-        ])
-            .catch(err => {
-                setError(err);
-            })
-            .finally(() => setLoading(false));
+        ]).catch(err => {
+            setError(err);
+        });
     }, []);
 
-    return [gettingStartedSchema, cloudSetupSchema, error, clearError, loading] as const;
+    return [gettingStartedSchema, cloudSetupSchema, error, clearError] as const;
 }
 
 export default useFetchSchemas;
