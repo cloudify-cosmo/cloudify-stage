10ae0d418ce77e2138bdcaeca90fb1fa33527934
kubama <jakub.madej@amartus.com>
2023-01-12 13:00:16 +0100

    Update widgets/executions/src/DryRunIcon.tsx
    Co-authored-by: Jakub Niezgoda <jakub.niezgoda@cloudify.co>

--- widgets/executions/src/DryRunIcon.tsx
+++ widgets/executions/src/DryRunIcon.tsx
@@ -1,6 +1,6 @@
 import type { Execution } from 'app/utils/shared/ExecutionUtils';
 
-export default function DryRunIcon({ execution }: { execution?: Execution }) {
+export default function DryRunIcon({ execution }: { execution?: Pick<Execution, 'is_dry_run'> }) {
     const { Icon, Popup } = Stage.Basic;
 
     return execution?.is_dry_run ? (
