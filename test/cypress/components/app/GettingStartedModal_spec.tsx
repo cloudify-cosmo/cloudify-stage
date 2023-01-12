63b1e29d47d8a218ed6f8207e126102b584d2dbb
Leon Gilyadov <leon.good.life@gmail.com>
2023-01-12 10:56:13 +0200

    Undo changes in component tesr

--- test/cypress/components/app/GettingStartedModal_spec.tsx
+++ test/cypress/components/app/GettingStartedModal_spec.tsx
@@ -5,7 +5,6 @@ import '../initAppContext';
 
 import GettingStartedModal from 'app/components/GettingStartedModal';
 import StageUtils from 'app/utils/stageUtils';
-import { cloudSetupSchemaUrl, gettingStartedSchemaUrl } from 'app/components/GettingStartedModal/useFetchSchemas';
 import userConfig from 'conf/userConfig.json';
 import type { ManagerData } from 'app/reducers/managerReducer';
 import { emptyState } from 'app/reducers/managerReducer';
@@ -25,12 +24,6 @@ describe('GettingStartedModal', () => {
                 } as ManagerData
             }
         );
-
-        cy.intercept('GET', gettingStartedSchemaUrl).as('gettingStartedSchema');
-        cy.wait('@gettingStartedSchema');
-
-        cy.intercept('GET', cloudSetupSchemaUrl).as('cloudSetupSchema');
-        cy.wait('@cloudSetupSchema');
 
         cy.contains('Welcome to Cloudify');
     });
