8c50a32bbcce389b77e623b1e00bb647fc34447f
kubama <jakub.madej@amartus.com>
2023-01-16 12:40:52 +0100

    RD-4796 Add user configuration files snapshot endpoints (#2381)

---
+++ backend/test/routes/snapshots/blueprint-layouts.test.ts
@@ -0,0 +1,53 @@
+import request from 'supertest';
+import app from 'app';
+import mockDb from '../../mockDb';
+
+jest.mock('db/Connection');
+
+describe('/snapshots/blueprint-layouts endpoint', () => {
+    const blueprintUserDataRow = {
+        blueprintId: 'new',
+        username: 'admin',
+        layout: {},
+        createdAt: '2022-06-01T18:30:57.450Z',
+        updatedAt: '2022-12-27T15:25:52.460Z'
+    };
+
+    it('allows to get snapshot data', () => {
+        mockDb({
+            BlueprintUserData: {
+                findAll: () => Promise.resolve([blueprintUserDataRow]),
+                getAttributes: () => ({})
+            }
+        });
+
+        return request(app)
+            .get('/console/snapshots/blueprint-layouts')
+            .then(response => {
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toStrictEqual([blueprintUserDataRow]);
+            });
+    });
+
+    it('allows to restore snapshot data', () => {
+        const create = jest.fn(() => Promise.resolve());
+        const transaction = 'transaction';
+
+        mockDb({
+            BlueprintUserData: {
+                create
+            },
+            sequelize: {
+                transaction: (f: any) => f(transaction)
+            }
+        });
+
+        return request(app)
+            .post('/console/snapshots/blueprint-layouts')
+            .send([blueprintUserDataRow])
+            .then(response => {
+                expect(response.statusCode).toBe(201);
+                expect(create).toHaveBeenCalledWith(blueprintUserDataRow, { transaction });
+            });
+    });
+});
---
+++ backend/test/routes/snapshots/templates.test.ts
@@ -0,0 +1,53 @@
+import type { Template } from 'handler/templates/types';
+import type { TemplatesSnapshot } from 'routes/Snapshots';
+import { omit } from 'lodash';
+import { createTemplate, getUserTemplates } from 'handler/templates/TemplatesHandler';
+import request from 'supertest';
+import app from 'app';
+
+jest.mock('handler/templates/TemplatesHandler');
+
+describe('/snapshots/templates endpoint', () => {
+    const template: Template = {
+        id: 'test',
+        name: 'test',
+        custom: true,
+        data: {
+            pages: [],
+            roles: [],
+            tenants: []
+        },
+        updatedBy: 'user',
+        updatedAt: '2022-06-01T18:30:57.450Z'
+    };
+
+    const templatesSnapshot: TemplatesSnapshot = [omit(template, 'name', 'custom')];
+
+    it('allows to get snapshot data', () => {
+        (<jest.Mock>getUserTemplates).mockReturnValue([template]);
+
+        return request(app)
+            .get('/console/snapshots/templates')
+            .then(response => {
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toStrictEqual(templatesSnapshot);
+            });
+    });
+
+    it('allows to restore snapshot data', () => {
+        return request(app)
+            .post('/console/snapshots/templates')
+            .send(templatesSnapshot)
+            .then(response => {
+                expect(response.statusCode).toBe(201);
+                expect(createTemplate).toHaveBeenCalledWith(
+                    expect.objectContaining({
+                        data: expect.objectContaining(omit(template.data, 'pages')),
+                        pages: template.data.pages
+                    }),
+                    template.updatedBy,
+                    template.updatedAt
+                );
+            });
+    });
+});
