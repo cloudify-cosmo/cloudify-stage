f29c708960ddad85b8800399dd243d403490c62e
Leon Gilyadov <leon.good.life@gmail.com>
2023-01-17 14:13:05 +0200

    RD-6409 Turn on the Edit in Composer flag by default

--- widgets/blueprints/src/widget.tsx
+++ widgets/blueprints/src/widget.tsx
@@ -59,7 +59,7 @@ Stage.defineWidget<unknown, unknown, BlueprintsWidgetConfiguration>({
             id: 'showComposerOptions',
             type: Stage.Basic.GenericField.BOOLEAN_TYPE,
             name: t('configuration.showComposerOptions'),
-            default: false
+            default: true
         },
         {
             id: 'filterRules',
