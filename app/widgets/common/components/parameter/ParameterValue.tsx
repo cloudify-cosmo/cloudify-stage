32bc70cbf9635468de3efd44c1fb25eead94593a
Norbert Kruk <norbert.kruk@amartus.com>
2023-01-24 10:42:32 +0100

    RD-5754: Migrated components directory

--- app/widgets/common/components/parameter/ParameterValue.tsx
+++ app/widgets/common/components/parameter/ParameterValue.tsx
@@ -1,79 +1,71 @@
-// @ts-nocheck File not migrated fully to TS
 import React from 'react';
+import type { CSSProperties } from 'react';
+import { isObject } from 'lodash';
 import { types } from 'cloudify-ui-common-frontend';
 import highlighterStyles from 'react-syntax-highlighter/dist/esm/styles/hljs/idea';
-import PropTypes from 'prop-types';
 import { CopyToClipboardButton, HighlightText } from '../../../../components/basic';
 import Url from '../../../../utils/shared/UrlUtils';
 import Json from '../../../../utils/shared/JsonUtils';
-import { AnyData } from '../../../../utils/props';
+
+const getValueElement = (value: ParameterValueProps['value'], stringValue: string) => {
+    const commonStyle = {
+        display: 'inline-block',
+        padding: '0.5em',
+        whiteSpace: 'pre-wrap',
+        wordBreak: 'break-word'
+    } as CSSProperties;
+
+    switch (types.toType(value)) {
+        case 'array':
+        case 'object':
+            return <HighlightText language="json">{stringValue}</HighlightText>;
+        case 'number':
+            return <code style={{ ...commonStyle, ...highlighterStyles['hljs-number'] }}>{stringValue}</code>;
+        case 'boolean':
+        case 'null':
+            return <code style={{ ...commonStyle, ...highlighterStyles['hljs-keyword'] }}>{stringValue}</code>;
+        case 'string':
+            return Url.isUrl(stringValue) ? (
+                <a rel="noopener noreferrer" target="_blank" href={stringValue}>
+                    {stringValue}
+                </a>
+            ) : (
+                <code style={{ ...commonStyle, ...highlighterStyles['hljs-string'] }}>{stringValue}</code>
+            );
+        default:
+            return <code style={{ ...commonStyle, ...highlighterStyles['hljs-literal'] }}>{stringValue}</code>;
+    }
+};
+
+interface ParameterValueProps {
+    /**
+     * @property [value=''] parameter value (original type)
+     * @default ''
+     */
+    value: any;
+
+    /**
+     * @property if set to true, then CopyToClipboardButton will be shown
+     * @default true
+     */
+    showCopyButton?: boolean;
+}
 
 /**
  * ParameterValue is a component which shows parameter (e.g. deployment/blueprint inputs, outputs, runtime properties, ...)
  * in nice user-friendly formatted manner with copy to clipboard button.
  */
-export default class ParameterValue extends React.Component<{ value: any; showCopyButton?: boolean }> {
-    shouldComponentUpdate(nextProps) {
-        return !_.isEqual(this.props, nextProps);
-    }
+const ParameterValue = ({ value = '', showCopyButton = true }: ParameterValueProps) => {
+    const stringValue = isObject(value) ? Json.stringify(value, true) : Json.getStringValue(value);
 
-    getValueElement(stringValue) {
-        const commonStyle = {
-            display: 'inline-block',
-            padding: '0.5em',
-            whiteSpace: 'pre-wrap',
-            wordBreak: 'break-word'
-        };
-        const { value: typedValue } = this.props;
-
-        switch (types.toType(typedValue)) {
-            case 'array':
-            case 'object':
-                return <HighlightText language="json">{stringValue}</HighlightText>;
-            case 'number':
-                return <code style={{ ...commonStyle, ...highlighterStyles['hljs-number'] }}>{stringValue}</code>;
-            case 'boolean':
-            case 'null':
-                return <code style={{ ...commonStyle, ...highlighterStyles['hljs-keyword'] }}>{stringValue}</code>;
-            case 'string':
-                return Url.isUrl(stringValue) ? (
-                    <a rel="noopener noreferrer" target="_blank" href={stringValue}>
-                        {stringValue}
-                    </a>
-                ) : (
-                    <code style={{ ...commonStyle, ...highlighterStyles['hljs-string'] }}>{stringValue}</code>
-                );
-            default:
-                return <code style={{ ...commonStyle, ...highlighterStyles['hljs-literal'] }}>{stringValue}</code>;
-        }
-    }
-
-    render() {
-        const { showCopyButton, value } = this.props;
-
-        const stringValue = _.isObject(value) ? Json.stringify(value, true) : Json.getStringValue(value);
-
-        return showCopyButton ? (
-            <div>
-                <CopyToClipboardButton text={stringValue} className="rightFloated" />
-                {this.getValueElement(stringValue)}
-            </div>
-        ) : (
-            this.getValueElement(stringValue)
-        );
-    }
-}
-
-/**
- * @property {any} [value=''] parameter value (original type)
- * @property {boolean} [showCopyButton=true] if set to true, then CopyToClipboardButton will be shown
- */
-ParameterValue.propTypes = {
-    value: AnyData,
-    showCopyButton: PropTypes.bool
+    return showCopyButton ? (
+        <div>
+            <CopyToClipboardButton text={stringValue} className="rightFloated" />
+            {getValueElement(value, stringValue)}
+        </div>
+    ) : (
+        getValueElement(value, stringValue)
+    );
 };
 
-ParameterValue.defaultProps = {
-    value: '',
-    showCopyButton: true
-};
+export default ParameterValue;
