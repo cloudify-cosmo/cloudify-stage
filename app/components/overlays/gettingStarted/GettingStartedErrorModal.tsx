d1eaf7552e9078bf18effdd47d550f580fa853f6
Leon Gilyadov <leon.good.life@gmail.com>
2023-02-06 10:17:37 +0200

    Remove wrong comment

--- app/components/overlays/gettingStarted/GettingStartedErrorModal.tsx
+++ app/components/overlays/gettingStarted/GettingStartedErrorModal.tsx
@@ -12,9 +12,7 @@ const GettingStartedErrorModal = ({ onClose }: GettingStartedErrorModalProps) =>
     return (
         <Modal open>
             <Modal.Header>{t('errorModal.title')}</Modal.Header>
-            <Modal.Content>
-                {t('errorModal.description')} {/* Todo: add loader icon */}
-            </Modal.Content>
+            <Modal.Content>{t('errorModal.description')}</Modal.Content>
             <Modal.Actions>
                 <Button content={t('errorModal.close')} onClick={onClose} />
             </Modal.Actions>
