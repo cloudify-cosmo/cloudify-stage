25438e27dc28ce621753ea40d3515add187b817d
Norbert Kruk <norbert.kruk@amartus.com>
2023-02-09 12:27:43 +0100

    RD-6452: Migrated away from implicit lodash imports

--- app/widgets/common/inputs/utils/getInputsWithoutValues.ts
+++ app/widgets/common/inputs/utils/getInputsWithoutValues.ts
@@ -1,13 +1,15 @@
+import { forEach, isEmpty } from 'lodash';
+
 export default function getInputsWithoutValues(inputs: Record<string, any>, inputsValues: Record<string, any>) {
     const { Json } = Stage.Utils;
     const inputsWithoutValues: Record<string, true> = {};
 
-    _.forEach(inputs, (inputObj, inputName) => {
+    forEach(inputs, (inputObj, inputName) => {
         if (inputObj.default !== undefined || inputObj.hidden || inputObj.required === false) return;
 
         const stringInputValue = Json.getStringValue(inputsValues[inputName]);
 
-        if (_.isEmpty(stringInputValue)) {
+        if (isEmpty(stringInputValue)) {
             inputsWithoutValues[inputName] = true;
         }
     });
