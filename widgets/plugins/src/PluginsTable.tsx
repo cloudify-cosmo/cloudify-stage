4c728fec60fe4d37247e73120dfd21a459cc62d0
Norbert Kruk <norbert.kruk@amartus.com>
2023-02-16 13:56:30 +0100

    Migrated away from using translation function acronym

--- widgets/plugins/src/PluginsTable.tsx
+++ widgets/plugins/src/PluginsTable.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import MarketplaceModal from './MarketplaceModal';
 import type { FetchedPluginItem, PluginsWidget } from './widget.types';
 
-const t = Stage.Utils.getT('widgets.plugins');
+const translate = Stage.Utils.getT('widgets.plugins');
 
 interface PluginItem extends FetchedPluginItem {
     isSelected: boolean;
@@ -71,7 +71,7 @@ const PluginsTable: FunctionComponent<PluginsTableProps> = ({ data, toolbox, wid
 
     function deletePlugin() {
         if (!selectedPlugin) {
-            setError(t('deleteError'));
+            setError(translate('deleteError'));
             return;
         }
 
@@ -108,18 +108,26 @@ const PluginsTable: FunctionComponent<PluginsTableProps> = ({ data, toolbox, wid
                 selectable
                 searchable
                 className="pluginsTable"
-                noDataMessage={t('noData')}
+                noDataMessage={translate('noData')}
             >
                 <DataTable.Column name="id" />
                 <DataTable.Column />
-                <DataTable.Column label={t('columns.plugin')} name="title" width="20%" />
-                <DataTable.Column label={t('columns.packageName')} name="package_name" width="20%" />
-                <DataTable.Column label={t('columns.packageVersion')} name="package_version" width="10%" />
-                <DataTable.Column label={t('columns.supportedPlatform')} name="supported_platform" width="10%" />
-                <DataTable.Column label={t('columns.distribution')} name="distribution" width="10%" />
-                <DataTable.Column label={t('columns.distributionRelease')} name="distribution_release" width="10%" />
-                <DataTable.Column label={t('columns.uploadedAt')} name="uploaded_at" width="15%" />
-                <DataTable.Column label={t('columns.creator')} name="created_by" width="15%" />
+                <DataTable.Column label={translate('columns.plugin')} name="title" width="20%" />
+                <DataTable.Column label={translate('columns.packageName')} name="package_name" width="20%" />
+                <DataTable.Column label={translate('columns.packageVersion')} name="package_version" width="10%" />
+                <DataTable.Column
+                    label={translate('columns.supportedPlatform')}
+                    name="supported_platform"
+                    width="10%"
+                />
+                <DataTable.Column label={translate('columns.distribution')} name="distribution" width="10%" />
+                <DataTable.Column
+                    label={translate('columns.distributionRelease')}
+                    name="distribution_release"
+                    width="10%"
+                />
+                <DataTable.Column label={translate('columns.uploadedAt')} name="uploaded_at" width="15%" />
+                <DataTable.Column label={translate('columns.creator')} name="created_by" width="15%" />
                 <DataTable.Column width="10%" />
 
                 {data.items.map(item => {
@@ -157,14 +165,14 @@ const PluginsTable: FunctionComponent<PluginsTableProps> = ({ data, toolbox, wid
                                 <Icon
                                     name="download"
                                     link
-                                    title={t('download')}
+                                    title={translate('download')}
                                     onClick={(event: Event) => downloadPlugin(item, event)}
                                     style={{ marginBottom: 4 }}
                                 />
                                 <Icon
                                     name="trash"
                                     link
-                                    title={t('delete')}
+                                    title={translate('delete')}
                                     onClick={(event: Event) => deletePluginConfirm(item, event)}
                                 />
                             </DataTable.Data>
@@ -173,10 +181,10 @@ const PluginsTable: FunctionComponent<PluginsTableProps> = ({ data, toolbox, wid
                 })}
 
                 <DataTable.Action>
-                    <Dropdown button text={t('upload.button')}>
+                    <Dropdown button text={translate('upload.button')}>
                         <Menu direction="left">
-                            <Item text={t('upload.marketplace')} onClick={showMarketplaceUploadModal} />
-                            <Item text={t('upload.package')} onClick={showPackageUploadModal} />
+                            <Item text={translate('upload.marketplace')} onClick={showMarketplaceUploadModal} />
+                            <Item text={translate('upload.package')} onClick={showPackageUploadModal} />
                         </Menu>
                     </Dropdown>
                 </DataTable.Action>
@@ -187,7 +195,7 @@ const PluginsTable: FunctionComponent<PluginsTableProps> = ({ data, toolbox, wid
             <MarketplaceModal open={marketplaceUploadModalShown} onHide={hideMarketplaceUploadModal} />
 
             <DeleteConfirm
-                resourceName={t('deleteLabel', {
+                resourceName={translate('deleteLabel', {
                     name: selectedPlugin?.package_name ?? '',
                     version: selectedPlugin?.package_version ?? ''
                 })}
