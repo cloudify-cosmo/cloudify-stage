f15c5bf582b34a288014b09778af1a1c977de029
Norbert Kruk <39031791+Vorbert-Kruk@users.noreply.github.com>
2023-02-27 14:12:07 +0100

    RD-6890 Extended login error messages mapping mechanism  (#2447)
    * RD-6444: Synchronized widgets documentation
    
    * RD-6890: Extended login error messages mapping mechanism
    
    * RD-6890: Upated system test scenarios
    
    * Simplify login error handling
    
    * RD-6890: Refactored and adjusted code after recent simplification
    
    * RD-6890: Adjusted unit test
    
    ---------
    
    Co-authored-by: Jakub Niezgoda <jakub.niezgoda@amartus.com>

--- app/actions/manager/auth.ts
+++ app/actions/manager/auth.ts
@@ -102,6 +102,7 @@ export function login(
             .catch(err => {
                 log.log(err);
                 dispatch(loginFailure(username, err));
+                throw err;
             });
     };
 }
