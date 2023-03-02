3dc8a41275edc26ccfdd0869578d55fbec47d2a8
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2023-03-02 12:06:44 +0100

    Use accurate type for deployment resource in Deployments View widget

--- app/widgets/common/deploymentsView/types.ts
+++ app/widgets/common/deploymentsView/types.ts
@@ -18,6 +18,24 @@ export enum DeploymentStatus {
     RequiresAttention = 'requires_attention'
 }
 
-export type Deployment = Omit<FullDeploymentData, 'groups'>;
+export const deploymentFields = [
+    'id',
+    'display_name',
+    'site_name',
+    'blueprint_id',
+    'latest_execution_status',
+    'deployment_status',
+    'environment_type',
+    'latest_execution_total_operations',
+    'latest_execution_finished_operations',
+    'sub_services_count',
+    'sub_services_status',
+    'sub_environments_count',
+    'sub_environments_status',
+    'labels',
+    'capabilities'
+] as const;
+
+export type Deployment = Pick<FullDeploymentData, typeof deploymentFields[number]>;
 
 export type DeploymentsResponse = Stage.Types.PaginatedResponse<Deployment>;
