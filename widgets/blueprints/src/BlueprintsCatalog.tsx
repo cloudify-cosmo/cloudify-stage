c8f6d773fc2f46cff9dc806a5443309f8660bb42
Patrycja Kubiczek <patrycja@cloudify.co>
2023-03-06 11:42:37 +0100

    add margin bottom

--- widgets/blueprints/src/BlueprintsCatalog.tsx
+++ widgets/blueprints/src/BlueprintsCatalog.tsx
@@ -11,6 +11,7 @@ const StyledGridWrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 3fr));
     grid-gap: 20px;
+    margin-bottom: 1rem;
 `;
 
 export default function BlueprintsCatalog({
