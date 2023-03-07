2a026e525cd2c8a6dcb30844e4f61c48f5a85e32
Patrycja Kubiczek <patrycja@cloudify.co>
2023-03-07 08:14:14 +0100

    simplify GridWrapper

--- app/widgets/common/components/GridWrapper.tsx
+++ app/widgets/common/components/GridWrapper.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
 import styled from 'styled-components';
 
-type GridWrapperProps = {
-    /**
-     * ReactNode content
-     */
-    children?: React.ReactNode;
-};
-
-const StyledDiv = styled.div<GridWrapperProps>`
+const GridWrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 3fr));
     grid-gap: 20px;
     margin-bottom: 1rem;
 `;
-
-const GridWrapper = ({ children, ...rest }: GridWrapperProps) => <StyledDiv {...rest}>{children}</StyledDiv>;
 
 export default GridWrapper;
