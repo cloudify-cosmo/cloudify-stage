be84fa3e73932778535a6df514b98e9bf3feb092
kubama <jakub.madej@amartus.com>
2023-03-15 13:08:10 +0100

    RND-96 Add validations and error handling (#2474)

--- app/widgets/common/helmModal/HelmModal.tsx
+++ app/widgets/common/helmModal/HelmModal.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { Toolbox } from 'app/utils/StageAPI';
 import { set } from 'lodash';
 import type { PostHelmBlueprintRequestBody, PostHelmBlueprintResponse } from 'backend/routes/blueprints/Helm.types';
-import type { ClusterCredential } from 'backend/handler/HelmHandler.types';
+import type { ClusterCredentialName, ClusterCredentials } from 'backend/handler/HelmHandler.types';
 import { ApproveButton, CancelButton, Form, LoadingOverlay, Modal } from '../../../components/basic';
-import { useBoolean, useInput } from '../../../utils/hooks';
+import { useBoolean, useErrors, useInput } from '../../../utils/hooks';
 import AccordionSection from '../components/accordion/AccordionSection';
 import ClusterCredentialInput from './ClusterCredentialInput';
 import StageUtils from '../../../utils/stageUtils';
@@ -17,27 +17,64 @@ interface HelmModalProps {
 }
 
 const translate = StageUtils.getT('widgets.blueprints.helmModal');
+const translateError = StageUtils.composeT(translate, 'errors');
+
+const initialCredentials: ClusterCredentials = {
+    host: { source: 'input', value: '' },
+    api_key: { source: 'secret', value: '' },
+    ssl_ca_cert: { source: 'secret', value: '' }
+};
 
 export default function HelmModal({ onHide, toolbox }: HelmModalProps) {
     const [repository, setRepository] = useInput('');
     const [chart, setChart] = useInput('');
     const [blueprintId, setBlueprintId] = useInput('');
     const [description, setDescription] = useInput('');
-    const [host, setHost] = useInput<ClusterCredential | undefined>(undefined);
-    const [apiKey, setApiKey] = useInput<ClusterCredential | undefined>(undefined);
-    const [certificate, setCertificate] = useInput<ClusterCredential | undefined>(undefined);
+
+    const [credentials, setCredentials] = useState<ClusterCredentials>(initialCredentials);
+
     const [submitInProgress, startSubmit, stopSubmit] = useBoolean();
+    const { performValidations, getContextError, errors } = useErrors<
+        'repository' | 'blueprintId' | ClusterCredentialName
+    >();
 
     function handleSubmit() {
+        performValidations(validationErrors => {
+            if (!repository) {
+                validationErrors.repository = translateError('noRepositoryUrl');
+            } else if (!Stage.Utils.Url.isUrl(repository)) {
+                validationErrors.repository = translateError('invalidRepositoryUrl');
+            }
+
+            if (!blueprintId) {
+                validationErrors.blueprintId = translateError('noBlueprintName');
+            } else if (!blueprintId.match(Consts.idRegex)) {
+                validationErrors.blueprintId = translateError('invalidBlueprintName');
+            }
+
+            function validateCredential(credentialName: ClusterCredentialName) {
+                const credential = credentials[credentialName];
+                if (!credential?.value) {
+                    validationErrors[credentialName] = translateError(`${credential?.source}Missing`);
+                }
+            }
+
+            validateCredential('host');
+            validateCredential('api_key');
+            validateCredential('ssl_ca_cert');
+        }, performSubmit);
+    }
+
+    function performSubmit() {
         startSubmit();
-        return toolbox
+        toolbox
             .getInternal()
             .doPost<PostHelmBlueprintResponse, PostHelmBlueprintRequestBody>('/helm/blueprint', {
                 body: {
                     repository,
                     chart,
                     description,
-                    clusterCredentials: { host: host!, api_key: apiKey!, ssl_ca_cert: certificate! }
+                    clusterCredentials: credentials
                 }
             })
             .then(blueprintContent => {
@@ -62,21 +99,43 @@ export default function HelmModal({ onHide, toolbox }: HelmModalProps) {
             .catch(stopSubmit);
     }
 
+    function renderClusterCredentialInput(clusterCredential: ClusterCredentialName) {
+        return (
+            <ClusterCredentialInput
+                toolbox={toolbox}
+                clusterCredential={clusterCredential}
+                initialSource={initialCredentials[clusterCredential].source}
+                onChange={changedClusterCredential =>
+                    setCredentials({ ...credentials, [clusterCredential]: changedClusterCredential })
+                }
+                error={getContextError(clusterCredential)}
+            />
+        );
+    }
+
     return (
         <Modal open onClose={onHide}>
             {submitInProgress && <LoadingOverlay />}
             <Modal.Header>{translate('header')}</Modal.Header>
 
             <Modal.Content>
-                <Form>
+                <Form errors={errors ? [] : undefined}>
                     <AccordionSection title={translate('mainSection.header')}>
-                        <Form.Field label={translate('mainSection.helmRepo')}>
+                        <Form.Field
+                            required
+                            label={translate('mainSection.helmRepo')}
+                            error={getContextError('repository')}
+                        >
                             <Form.Input onChange={setRepository} />
                         </Form.Field>
                         <Form.Field label={translate('mainSection.helmChart')}>
                             <Form.Input onChange={setChart} />
                         </Form.Field>
-                        <Form.Field label={translate('mainSection.blueprintName')}>
+                        <Form.Field
+                            required
+                            label={translate('mainSection.blueprintName')}
+                            error={getContextError('blueprintId')}
+                        >
                             <Form.Input onChange={setBlueprintId} />
                         </Form.Field>
                         <Form.Field label={translate('mainSection.blueprintDescription')}>
@@ -84,18 +143,9 @@ export default function HelmModal({ onHide, toolbox }: HelmModalProps) {
                         </Form.Field>
                     </AccordionSection>
                     <AccordionSection title={translate('credentialsSection.header')}>
-                        <ClusterCredentialInput
-                            toolbox={toolbox}
-                            clusterCredential="host"
-                            initialSource="input"
-                            onChange={setHost}
-                        />
-                        <ClusterCredentialInput toolbox={toolbox} clusterCredential="api_key" onChange={setApiKey} />
-                        <ClusterCredentialInput
-                            toolbox={toolbox}
-                            clusterCredential="ssl_ca_cert"
-                            onChange={setCertificate}
-                        />
+                        {renderClusterCredentialInput('host')}
+                        {renderClusterCredentialInput('api_key')}
+                        {renderClusterCredentialInput('ssl_ca_cert')}
                     </AccordionSection>
                 </Form>
             </Modal.Content>
