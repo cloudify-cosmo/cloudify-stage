8fc9ec93f6a00f301be91e210cb859bc8a311e6e
Leon Gilyadov <leon.good.life@gmail.com>
2023-03-20 10:35:52 +0200

    remove duplication

--- backend/handler/SourceHandler.types.ts
+++ backend/handler/SourceHandler.types.ts
@@ -1,13 +1,15 @@
-export interface ScanningDir {
+interface ScanningItemCommon {
     key: string;
     title: string;
+    isDir: boolean;
+}
+
+export interface ScanningDir extends ScanningItemCommon {
     isDir: true;
     children: ScanningItem[];
 }
 
-export interface ScanningFile {
-    key: string;
-    title: string;
+export interface ScanningFile extends ScanningItemCommon {
     isDir: false;
 }
 
