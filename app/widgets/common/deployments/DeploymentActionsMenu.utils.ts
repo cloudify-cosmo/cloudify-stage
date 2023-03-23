10e6a8f4a03503848bd06e18d16ba80568833dbb
Leon Gilyadov <leon.good.life@gmail.com>
2023-03-23 12:20:22 +0200

    RD-7117 Stage system tests failed on deployments_spec

--- app/widgets/common/deployments/DeploymentActionsMenu.utils.ts
+++ app/widgets/common/deployments/DeploymentActionsMenu.utils.ts
@@ -4,7 +4,7 @@ import type { MenuItem } from './DeploymentActionsMenu.consts';
 import { permissions, actions } from './DeploymentActionsMenu.consts';
 
 function hasEnvironmentLabel(deploymentLabels: Label[]) {
-    return deploymentLabels.some(deploymentLabel => {
+    return deploymentLabels?.some(deploymentLabel => {
         return deploymentLabel.key === 'csys-obj-type' && deploymentLabel.value === 'environment';
     });
 }
