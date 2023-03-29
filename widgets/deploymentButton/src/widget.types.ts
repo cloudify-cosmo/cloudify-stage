3821f8f9728dbb8c748186a7dbf7e971692ee330
Norbert Kruk <norbert.kruk@amartus.com>
2023-03-29 12:42:51 +0200

    RND-57: Added ability to disable deployment button when there are no blueprints

---
+++ widgets/deploymentButton/src/widget.types.ts
@@ -0,0 +1,16 @@
+// import type { PollingTimeConfiguration, DataTableConfiguration } from '../../../app/utils/GenericConfig';
+
+import type { FullBlueprintData } from 'app/widgets/common/blueprints/BlueprintActions';
+import type { ButtonConfiguration } from 'app/widgets/common/configuration/buttonConfiguration';
+import type { FilterRule } from 'app/widgets/common/filters/types';
+
+export declare namespace DeploymentButtonWidget {
+    export type DataItem = Pick<FullBlueprintData, 'id'>;
+
+    interface Configuration extends ButtonConfiguration {
+        toolbox: Stage.Types.Toolbox;
+        blueprintFilterRules: FilterRule[];
+    }
+
+    export type Data = Stage.Types.WidgetData<Stage.Types.PaginatedResponse<DataItem>>;
+}
