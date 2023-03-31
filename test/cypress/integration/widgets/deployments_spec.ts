a72229cc1f6ece413ed211239630f2e2f7e56e17
Patrycja Kubiczek <patrycja@cloudify.co>
2023-03-31 15:56:32 +0200

    remove unnecessary 'checkLabelPresence'

--- test/cypress/integration/widgets/deployments_spec.ts
+++ test/cypress/integration/widgets/deployments_spec.ts
@@ -185,7 +185,7 @@ describe('Deployments widget', () => {
             cy.setLabels(deploymentId, [{ a: 'b' }]);
             cy.interceptSp('PATCH', `/deployments/${deploymentId}`).as('updateLabels');
             cy.interceptSp('GET', { path: `/deployments/${deploymentId}?_include=labels` }).as('fetchLabels');
-            cy.interceptSp('GET', `/labels/deployments`).as('checkLabelPresence');
+            cy.interceptSp('GET', `/labels/deployments`);
 
             executeDeploymentAction(deploymentId, deploymentName, 'Manage Labels');
             cy.get('.modal').within(() => {
