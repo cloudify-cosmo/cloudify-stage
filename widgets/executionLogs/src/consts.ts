f11532e8bd8889b2ca273dfef18a5a6e3fd99fc3
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2023-04-14 13:08:57 +0200

    Add "Show more logs" button

--- widgets/executionLogs/src/consts.ts
+++ widgets/executionLogs/src/consts.ts
@@ -1,3 +1,9 @@
+export const widgetId = 'executionLogs';
+
+export const translate = Stage.Utils.getT(`widgets.${widgetId}`);
+
+export const basePageSize = 50;
+
 export const commonIncludeKeys = [
     '_storage_id',
     'execution_id',
