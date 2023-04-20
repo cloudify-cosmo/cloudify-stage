19ccdac8822b775f0e1481196a3f6e31fc44e973
patrycja@cloudify.co <patrycja@cloudify.co>
2023-04-20 10:07:07 +0200

    revert changes

--- app/widgets/common/deploymentsView/common.ts
+++ app/widgets/common/deploymentsView/common.ts
@@ -1,7 +1,4 @@
 import type { SemanticICONS } from 'semantic-ui-react';
-import i18n from 'i18next';
-import { deploymentTypeFilterRule } from './detailsPane/drilldownButtons/SubdeploymentDrilldownButton.consts';
-import type { SubdeploymentDrilldownButtonProps } from './detailsPane/drilldownButtons/SubdeploymentDrilldownButton';
 
 export const i18nPrefix = 'widgets.deploymentsView';
 export const i18nMessagesPrefix = `${i18nPrefix}.messages`;
@@ -14,8 +11,6 @@ export const filterRulesContextKey = 'filterRules';
 export const mapOpenContextKey = 'mapOpen';
 
 export const parentDeploymentLabelKey = 'csys-obj-parent';
-
-const subdeploymentsDrilldownTemplateName = 'drilldownDeployments';
 
 /**
  * NOTE: drilldownContext contains contexts for each page, starting with
@@ -32,22 +27,3 @@ export const getParentPageContext = (drilldownContext: Stage.Types.ReduxState['d
 
 export const selectDeployment = (toolbox: Stage.Types.Toolbox, deploymentId: string) =>
     toolbox.getContext().setValue('deploymentId', deploymentId);
-
-const getDeploymentsDrillDownContext = (type: 'environments' | 'services', mapOpen: boolean) => {
-    return { [filterRulesContextKey]: [deploymentTypeFilterRule[type]], [mapOpenContextKey]: mapOpen };
-};
-
-const getDeploymentsDrillDownPageName = (deploymentId: string, type: 'environments' | 'services') => {
-    return `${deploymentId} [${i18n.t(`${i18nDrillDownPrefix}.breadcrumbs.${type}`)}]`;
-};
-
-export const drilldownToSubdeployments = (
-    deploymentId: string,
-    drillDown: SubdeploymentDrilldownButtonProps['drillDown'],
-    type: 'environments' | 'services',
-    mapOpen: boolean
-) => {
-    const context = getDeploymentsDrillDownContext(type, mapOpen);
-    const pageName = getDeploymentsDrillDownPageName(deploymentId, type);
-    drillDown(subdeploymentsDrilldownTemplateName, context, pageName);
-};
--- app/widgets/common/deploymentsView/detailsPane/drilldownButtons/SubdeploymentDrilldownButton.tsx
+++ app/widgets/common/deploymentsView/detailsPane/drilldownButtons/SubdeploymentDrilldownButton.tsx
@@ -1,12 +1,19 @@
 import type { FunctionComponent } from 'react';
 import React, { useMemo } from 'react';
-
-import { drilldownToSubdeployments, subenvironmentsIcon, subservicesIcon } from '../../common';
+import i18n from 'i18next';
+import {
+    filterRulesContextKey,
+    i18nDrillDownPrefix,
+    mapOpenContextKey,
+    subenvironmentsIcon,
+    subservicesIcon
+} from '../../common';
 import { SubdeploymentStatusIcon } from '../../StatusIcon';
 import { shouldDisplaySubdeploymentButton, tDrillDownButtons } from './common';
 import DrilldownButton from './DrilldownButton';
 import type { LoadedSubdeploymentsResult, SubdeploymentsResult } from './subdeployments-result';
 import { Icon } from '../../../../../components/basic';
+import { deploymentTypeFilterRule } from './SubdeploymentDrilldownButton.consts';
 
 export interface SubdeploymentDrilldownButtonProps {
     type: 'environments' | 'services';
@@ -26,13 +33,20 @@ const SubdeploymentDrilldownButton: FunctionComponent<SubdeploymentDrilldownButt
     const icon = type === 'services' ? subservicesIcon : subenvironmentsIcon;
     const shouldBeDisplayed = useMemo(() => shouldDisplaySubdeploymentButton(result), [result]);
 
+    const subdeploymentsDrilldownTemplateName = 'drilldownDeployments';
+
+    const drilldownToSubdeployments = () => {
+        drillDown(
+            subdeploymentsDrilldownTemplateName,
+            { [filterRulesContextKey]: [deploymentTypeFilterRule[type]], [mapOpenContextKey]: mapOpen },
+            `${deploymentName} [${i18n.t(`${i18nDrillDownPrefix}.breadcrumbs.${type}`)}]`
+        );
+    };
+
     return (
         <>
             {shouldBeDisplayed && (
-                <DrilldownButton
-                    onClick={() => drilldownToSubdeployments(deploymentName, drillDown, type, mapOpen)}
-                    title={tDrillDownButtons(`${type}.title`)}
-                >
+                <DrilldownButton onClick={drilldownToSubdeployments} title={tDrillDownButtons(`${type}.title`)}>
                     <Icon name={icon} />
                     {tDrillDownButtons(`${type}.label`)} ({(result as LoadedSubdeploymentsResult).count})
                     <SubdeploymentStatusIcon
